3a97b3beb6ea66c56823dd319beaa630bf9e5064
Artur Michalek <artur.michalek@swmansion.com>
2023-07-06 17:14:25 +0200

    Fix path to corelib

--- starknet-foundry/crates/forge/tests/integration/common/corelib.rs
+++ starknet-foundry/crates/forge/tests/integration/common/corelib.rs
@@ -4,7 +4,7 @@ use assert_fs::TempDir;
 pub fn corelib() -> TempDir {
     let corelib = TempDir::new().unwrap();
     corelib
-        .copy_from("../cairo/corelib/src", &["**/*"])
+        .copy_from("../../../cairo/corelib/src", &["**/*"])
         .unwrap();
     corelib
 }
