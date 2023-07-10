7237fd34b4c588436dcdfe683298dc1fb99c1eb9
Karol Bisztyga <karol.bisztyga@swmansion.com>
2023-07-10 14:54:12 +0200

    add test with succ and fail

--- starknet-foundry/crates/cast/tests/e2e/multicall.rs
+++ starknet-foundry/crates/cast/tests/e2e/multicall.rs
@@ -79,3 +79,25 @@ async fn test_invoke_fail() {
     assert!(out.stdout.len() == 0);
     assert!(stderr_str.contains("There is no contract at the specified address"));
 }
+
+#[tokio::test]
+async fn test_deploy_success_invoke_fails() {
+    let mut args = default_cli_args();
+
+    let path = project_root::get_project_root().expect("failed to get project root path");
+    let path = Path::new(&path).join("tests/data/multicall_configs/deploy_succ_invoke_fail.toml");
+    let path_str = path.to_str().expect("failed converting path to str");
+
+    args.append(&mut vec!["multicall", "--path", path_str]);
+
+    let snapbox = runner(&args);
+    let bdg = snapbox.assert();
+    let out = bdg.get_output();
+
+    let stdout_str =
+        std::str::from_utf8(&out.stdout).expect("failed to convert command output to string");
+    let stderr_str =
+        std::str::from_utf8(&out.stderr).expect("failed to convert command output to string");
+    assert!(stdout_str.contains("command: Deploy"));
+    assert!(stderr_str.contains("error: There is no contract at the specified address"));
+}
