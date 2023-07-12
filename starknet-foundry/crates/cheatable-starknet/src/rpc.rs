fa34e0cb84b8d3593ad63b75c4600f299df150f4
Maksymilian Demitraszek <maksymilian.demitraszek@protonmail.com>
2023-07-12 16:08:24 +0200

    Some progress

--- starknet-foundry/crates/cheatable-starknet/src/rpc.rs
+++ starknet-foundry/crates/cheatable-starknet/src/rpc.rs
@@ -1,12 +1,20 @@
 use anyhow::Result;
-use cairo_vm::{hint_processor::hint_processor_definition::{HintProcessor, HintReference}, vm::{vm_core::VirtualMachine, runners::cairo_runner::RunResources, errors::{hint_errors::HintError, vm_errors::VirtualMachineError}}, types::exec_scope::ExecutionScopes, serde::deserialize_program::ApTracking};
+use cairo_vm::{hint_processor::hint_processor_definition::{HintProcessor, HintReference}, vm::{vm_core::VirtualMachine, runners::cairo_runner::RunResources, errors::{hint_errors::HintError, vm_errors::VirtualMachineError}}, types::{exec_scope::ExecutionScopes, relocatable::Relocatable}, serde::deserialize_program::ApTracking};
 use std::{sync::Arc, collections::HashMap, any::Any};
 
-use blockifier::{state::{cached_state::CachedState, state_api::State}, execution::{entry_point::{CallEntryPoint, CallType, ExecutionResources, EntryPointExecutionContext, EntryPointExecutionResult, CallInfo, FAULTY_CLASS_HASH}, errors::{EntryPointExecutionError, PreExecutionError}, contract_class::{ContractClassV1, ContractClass}, cairo1_execution::{VmExecutionContext, initialize_execution_context, prepare_call_arguments, finalize_execution, run_entry_point}, syscalls::hint_processor::SyscallHintProcessor, common_hints::HintExecutionResult}};
+use blockifier::{state::{cached_state::CachedState, state_api::State}, execution::{entry_point::{CallEntryPoint, CallType, ExecutionResources, EntryPointExecutionContext, EntryPointExecutionResult, CallInfo, FAULTY_CLASS_HASH}, errors::{EntryPointExecutionError, PreExecutionError}, contract_class::{ContractClassV1, ContractClass}, cairo1_execution::{VmExecutionContext, initialize_execution_context, prepare_call_arguments, finalize_execution, run_entry_point}, syscalls::hint_processor::{SyscallHintProcessor, SyscallExecutionError}, common_hints::HintExecutionResult, deprecated_syscalls::DeprecatedSyscallSelector, execution_utils::stark_felt_from_ptr}};
 use cairo_felt_blockifier::Felt252;
 use starknet_api::{core::{ContractAddress, PatriciaKey, EntryPointSelector, ClassHash}, hash::{StarkFelt, StarkHash}, patricia_key, transaction::{Calldata, TransactionVersion}, deprecated_contract_class::EntryPointType};
-use cairo_lang_casm::{hints::{Hint, StarknetHint}, operand::ResOperand};
+use cairo_lang_casm::{hints::{Hint, StarknetHint}, operand::{ResOperand, BinOpOperand, Operation, DerefOrImmediate, Register}};
 use crate::{state::DictStateReader, constants::{TEST_ACCOUNT_CONTRACT_ADDRESS, build_transaction_context, build_block_context}};
+
+use blockifier::execution::syscalls::{
+    deploy, emit_event, get_block_hash, get_execution_info, keccak, library_call,
+    library_call_l1_handler, replace_class, send_message_to_l1, storage_read, storage_write,
+    StorageReadResponse, StorageWriteResponse, SyscallRequest, SyscallRequestWrapper,
+    SyscallResponse, SyscallResponseWrapper, SyscallResult, 
+};
+type SyscallSelector = DeprecatedSyscallSelector;
 
 
 // This can mutate state, the name of the syscall is not very good
@@ -144,7 +152,6 @@ pub fn execute_if_cairo1(
 }
 
 
-
 pub struct CheatableSyscallHandler<'a> {
     pub syscall_handler: SyscallHintProcessor<'a>,
     pub rolled_contracts: HashMap<Felt252, Felt252>,
@@ -162,11 +169,15 @@ impl HintProcessor for CheatableSyscallHandler<'_> {
         let maybe_extended_hint = hint_data.downcast_ref::<Hint>();
 
         if let Some(Hint::Starknet(StarknetHint::SystemCall { system })) = maybe_extended_hint {
-            return self.execute_syscall(system, vm);
+            if let Some(Hint::Starknet(hint)) = maybe_extended_hint {
+                return self.execute_next_syscall(vm, hint);
+            }
         }
         self.syscall_handler
             .execute_hint(vm, exec_scopes, hint_data, constants, run_resources)
     }
+
+
 
     /// Trait function to store hint in the hint processor by string.
     fn compile_hint(
@@ -180,51 +191,121 @@ impl HintProcessor for CheatableSyscallHandler<'_> {
     }
 }
 
+/// Retrieves a [Relocatable] from the VM given a [ResOperand].
+/// A [ResOperand] represents a CASM result expression, and is deserialized with the hint.
+fn get_ptr_from_res_operand_unchecked(vm: &mut VirtualMachine, res: &ResOperand) -> Relocatable {
+    let (cell, base_offset) = match res {
+        ResOperand::Deref(cell) => (cell, Felt252::from(0)),
+        ResOperand::BinOp(BinOpOperand {
+            op: Operation::Add,
+            a,
+            b: DerefOrImmediate::Immediate(b),
+        }) => (a, Felt252::from(b.clone().value)),
+        _ => panic!("Illegal argument for a buffer."),
+    };
+    let base = match cell.register {
+        Register::AP => vm.get_ap(),
+        Register::FP => vm.get_fp(),
+    };
+    let cell_reloc = (base + (cell.offset as i32)).unwrap();
+    (vm.get_relocatable(cell_reloc).unwrap() + &base_offset).unwrap()
+}
+
 impl CheatableSyscallHandler<'_> {
-    fn execute_syscall(
+    fn peak_into
+
+    pub fn execute_next_syscall(
         &mut self,
-        system: &ResOperand,
         vm: &mut VirtualMachine,
-    ) -> Result<(), HintError> {
-        let (cell, offset) = extract_buffer(system);
-        let mut system_ptr = get_ptr(vm, cell, &offset)?;
-    
-        let selector = felt_from_pointer(vm, &mut system_ptr)
-            .unwrap()
-            .to_bytes_be();
-    
-        let gas_counter = usize_from_pointer(vm, &mut system_ptr).unwrap();
-        let contract_address = felt_from_pointer(vm, &mut system_ptr).unwrap();
-        let entry_point_selector = felt_from_pointer(vm, &mut system_ptr).unwrap();
-    
-        let start = relocatable_from_pointer(vm, &mut system_ptr).unwrap();
-        let end = relocatable_from_pointer(vm, &mut system_ptr).unwrap();
-        let calldata = read_data_from_range(vm, start, end).unwrap();
-    
-        let calldata_blockifier: Vec<blockifier_Felt252> = calldata.into_iter().map(|v| convert_to_blockifier_felt(v)).collect();
-        assert_eq!(std::str::from_utf8(&selector).unwrap(), "CallContract");
-        let result_blockfier: Vec<blockifier_Felt252> = call_contract(
-            &convert_to_blockifier_felt(contract_address),
-            &convert_to_blockifier_felt(entry_point_selector),
-            &calldata_blockifier,
-            blockifier_state,
-        )
-        .unwrap();
-        let result: Vec<Felt252> = result_blockfier.into_iter().map(|v| convert_from_blockifier_felt(v)).collect();
-    
-        insert_at_pointer(vm, &mut system_ptr, gas_counter).unwrap();
-        insert_at_pointer(vm, &mut system_ptr, Felt252::from(0)).unwrap();
-    
-        let mut ptr = vm.add_memory_segment();
-        let start = ptr;
-        for value in result {
-            insert_at_pointer(vm, &mut ptr, value).unwrap();
+        hint: &StarknetHint,
+    ) -> HintExecutionResult {
+        let StarknetHint::SystemCall{ system: syscall } = hint else {
+            return Err(HintError::CustomHint(
+                "Test functions are unsupported on starknet.".into()
+            ));
+        };
+        let initial_syscall_ptr = get_ptr_from_res_operand_unchecked(vm, syscall);
+        self.verify_syscall_ptr(initial_syscall_ptr)?;
+
+        let selector = SyscallSelector::try_from(self.read_next_syscall_selector(vm)?)?;
+        
+
+        // Keccak resource usage depends on the input length, so we increment the syscall count
+        // in the syscall execution callback.
+        if selector != SyscallSelector::Keccak {
+            self.increment_syscall_count(&selector);
         }
-        let end = ptr;
-    
-        insert_at_pointer(vm, &mut system_ptr, start).unwrap();
-        insert_at_pointer(vm, &mut system_ptr, end).unwrap();
-    
+
+        match selector {
+            SyscallSelector::CallContract => {
+                self.execute_syscall(vm, call_contract, constants::CALL_CONTRACT_GAS_COST)
+            }
+            SyscallSelector::Deploy => self.execute_syscall(vm, deploy, constants::DEPLOY_GAS_COST),
+            SyscallSelector::EmitEvent => {
+                self.execute_syscall(vm, emit_event, constants::EMIT_EVENT_GAS_COST)
+            }
+            SyscallSelector::GetBlockHash => {
+                self.execute_syscall(vm, get_block_hash, constants::GET_BLOCK_HASH_GAS_COST)
+            }
+            SyscallSelector::GetExecutionInfo => {
+                self.execute_syscall(vm, get_execution_info, constants::GET_EXECUTION_INFO_GAS_COST)
+            }
+            SyscallSelector::Keccak => self.execute_syscall(vm, keccak, constants::KECCAK_GAS_COST),
+            SyscallSelector::LibraryCall => {
+                self.execute_syscall(vm, library_call, constants::LIBRARY_CALL_GAS_COST)
+            }
+            SyscallSelector::LibraryCallL1Handler => {
+                self.execute_syscall(vm, library_call_l1_handler, constants::LIBRARY_CALL_GAS_COST)
+            }
+            SyscallSelector::ReplaceClass => {
+                self.execute_syscall(vm, replace_class, constants::REPLACE_CLASS_GAS_COST)
+            }
+            SyscallSelector::Secp256k1Add => {
+                self.execute_syscall(vm, secp256k1_add, constants::SECP256K1_ADD_GAS_COST)
+            }
+            SyscallSelector::Secp256k1GetPointFromX => self.execute_syscall(
+                vm,
+                secp256k1_get_point_from_x,
+                constants::SECP256K1_GET_POINT_FROM_X_GAS_COST,
+            ),
+            SyscallSelector::Secp256k1GetXy => {
+                self.execute_syscall(vm, secp256k1_get_xy, constants::SECP256K1_GET_XY_GAS_COST)
+            }
+            SyscallSelector::Secp256k1Mul => {
+                self.execute_syscall(vm, secp256k1_mul, constants::SECP256K1_MUL_GAS_COST)
+            }
+            SyscallSelector::Secp256k1New => {
+                self.execute_syscall(vm, secp256k1_new, constants::SECP256K1_NEW_GAS_COST)
+            }
+            SyscallSelector::SendMessageToL1 => {
+                self.execute_syscall(vm, send_message_to_l1, constants::SEND_MESSAGE_TO_L1_GAS_COST)
+            }
+            SyscallSelector::StorageRead => {
+                self.execute_syscall(vm, storage_read, constants::STORAGE_READ_GAS_COST)
+            }
+            SyscallSelector::StorageWrite => {
+                self.execute_syscall(vm, storage_write, constants::STORAGE_WRITE_GAS_COST)
+            }
+            _ => Err(HintError::UnknownHint(
+                format!("Unsupported syscall selector {selector:?}.").into(),
+            )),
+        }
+    }
+
+    fn read_next_syscall_selector(&mut self, vm: &mut VirtualMachine) -> SyscallResult<StarkFelt> {
+        let selector = stark_felt_from_ptr(vm, &mut self.syscall_ptr)?;
+
+        Ok(selector)
+    }
+
+    fn verify_syscall_ptr(&self, actual_ptr: Relocatable) -> SyscallResult<()> {
+        if actual_ptr != self.syscall_handler.syscall_ptr {
+            return Err(SyscallExecutionError::BadSyscallPointer {
+                expected_ptr: self.syscall_handler.syscall_ptr,
+                actual_ptr,
+            });
+        }
+
         Ok(())
     }
 }
