0d7c842ce82ebfd149ecb4ef0329eac27577cd9e
Artur Michalek <artur.michalek@swmansion.com>
2023-07-14 10:17:04 +0200

    Remove unnecessary e2e tests

--- starknet-foundry/crates/forge/tests/e2e/running.rs
+++ starknet-foundry/crates/forge/tests/e2e/running.rs
@@ -108,66 +108,6 @@ fn with_non_matching_filter() {
 }
 
 #[test]
-fn with_declare() {
-    let temp = assert_fs::TempDir::new().unwrap();
-    temp.copy_from("tests/data/declare_test", &["**/*.cairo", "**/*.toml"])
-        .unwrap();
-
-    let snapbox = runner();
-
-    snapbox
-        .current_dir(&temp)
-        .assert()
-        .success()
-        .stdout_matches(indoc! {r#"Collected 3 test(s) and 3 test file(s)
-            Running 0 test(s) from src/contract1.cairo
-            Running 0 test(s) from src/lib.cairo
-            Running 3 test(s) from tests/test_declare.cairo
-            [PASS] test_declare::test_declare::test_declare_simple
-            [PASS] test_declare::test_declare::multiple_contracts
-            [FAIL] test_declare::test_declare::non_existent_contract
-
-            Failure data:
-                Got an exception while executing a hint:
-                Failed to get contract artifact for name = GoodbyeStarknet. Make sure starknet target is correctly defined in Scarb.toml file.
-
-            Tests: 2 passed, 1 failed, 0 skipped
-        "#});
-}
-
-#[test]
-fn run_with_multiple_contracts() {
-    let temp = assert_fs::TempDir::new().unwrap();
-    temp.copy_from("tests/data/multicontract", &["**/*.cairo", "**/*.toml"])
-        .unwrap();
-
-    let snapbox = runner();
-
-    snapbox
-        .current_dir(&temp)
-        .assert()
-        .success()
-        .stdout_matches(indoc! {r#"Collected 6 test(s) and 5 test file(s)
-            Running 0 test(s) from src/contract1.cairo
-            Running 0 test(s) from src/contract2.cairo
-            Running 1 test(s) from src/lib.cairo
-            [PASS] [..]src::test_fib
-            Running 2 test(s) from tests/ext_function_test.cairo
-            [PASS] ext_function_test::ext_function_test::test_my_test
-            [PASS] ext_function_test::ext_function_test::test_simple
-            Running 3 test(s) from tests/test_simple.cairo
-            [PASS] test_simple::test_simple::test_simple
-            [PASS] test_simple::test_simple::test_simple2
-            [FAIL] test_simple::test_simple::test_failing
-
-            Failure data:
-                original value: [8111420071579136082810415440747], converted to a string: [failing check]
-
-            Tests: 5 passed, 1 failed, 0 skipped
-        "#});
-}
-
-#[test]
 fn with_print() {
     let temp = assert_fs::TempDir::new().unwrap();
     temp.copy_from("tests/data/print_test", &["**/*.cairo", "**/*.toml"])
