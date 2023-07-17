b987c85aefa9077ca6e3e7cfb09d00bc4eb9e425
war-in <marcin.warchol20@gmail.com>
2023-07-17 10:35:29 +0200

    versions -> versions tuple

--- starknet-foundry/crates/cast/tests/utils/build_contracts.sh
+++ starknet-foundry/crates/cast/tests/utils/build_contracts.sh
@@ -5,8 +5,8 @@ set -e
 VERSIONS=("v1 0.4.1" "v2 0.5.2")
 ASDF_DATA_DIR=$(asdf info | grep -e "ASDF_DATA_DIR" | awk -F '=' '{print $2}')
 
-for versions in "${VERSIONS[@]}"; do
-  read -r cairo_version scarb_version <<< "$versions"
+for versions_tuple in "${VERSIONS[@]}"; do
+  read -r cairo_version scarb_version <<< "$versions_tuple"
 
   CONTRACTS_DIRECTORY="$(git rev-parse --show-toplevel)/starknet-foundry/crates/cast/tests/data/contracts/$cairo_version"
   SCARB_BIN="$ASDF_DATA_DIR/installs/scarb/$scarb_version/bin/scarb"
