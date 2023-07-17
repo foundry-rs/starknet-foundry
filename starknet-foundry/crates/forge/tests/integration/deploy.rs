8f2c2140cb62ab9f4864804f5079ebb2f328583f
Artur Michalek <artur.michalek@swmansion.com>
2023-07-17 16:49:35 +0200

    Add test for too many/too few calldata arguments

--- starknet-foundry/crates/forge/tests/integration/deploy.rs
+++ starknet-foundry/crates/forge/tests/integration/deploy.rs
@@ -114,3 +114,112 @@ fn deploy_fails_on_calldata_when_contract_has_no_constructor() {
 
     assert_failed!(result);
 }
+
+#[test]
+fn test_deploy_fails_on_missing_constructor_arguments() {
+    let test = test_case!(
+        indoc!(
+            r#"
+        use result::ResultTrait;
+        use cheatcodes::RevertedTransactionTrait;
+        use cheatcodes::PreparedContract;
+        use array::ArrayTrait; 
+            
+        #[test]
+        fn deploy_invalid_calldata() {
+            let mut calldata = ArrayTrait::new();
+        
+            let class_hash = declare('HelloStarknet').unwrap();
+            let prepared = PreparedContract { class_hash: class_hash, constructor_calldata: @calldata };
+            let contract_address = deploy(prepared).unwrap();
+        
+            assert(2 == 2, '2 == 2');
+        }
+    "#
+        ),
+        Contract::new(
+            "HelloStarknet",
+            indoc!(
+                r#"
+                #[starknet::contract]
+                mod HelloStarknet {
+                    #[storage]
+                    struct Storage {}
+
+                    #[constructor]
+                    fn constructor(ref self: ContractState, arg1: felt252, arg2: felt252) {}
+                }
+        "#
+            )
+        )
+    );
+
+    let result = run(
+        &test.path().unwrap(),
+        Some(test.linked_libraries()),
+        &Default::default(),
+        Some(&Utf8PathBuf::from_path_buf(corelib().to_path_buf()).unwrap()),
+        &test.contracts(corelib().path()).unwrap(),
+        &Utf8PathBuf::from_path_buf(predeployed_contracts().to_path_buf()).unwrap(),
+    )
+    .unwrap();
+
+    assert_failed!(result);
+}
+
+#[test]
+fn test_deploy_fails_on_too_many_constructor_arguments() {
+    let test = test_case!(
+        indoc!(
+            r#"
+        use result::ResultTrait;
+        use cheatcodes::RevertedTransactionTrait;
+        use cheatcodes::PreparedContract;
+        use array::ArrayTrait;
+
+        #[test]
+        fn deploy_invalid_calldata() {
+            let mut calldata = ArrayTrait::new();
+            calldata.append(1);
+            calldata.append(2);
+            calldata.append(3);
+            calldata.append(4);
+            calldata.append(5);
+
+            let class_hash = declare('HelloStarknet').unwrap();
+            let prepared = PreparedContract { class_hash: class_hash, constructor_calldata: @calldata };
+            let contract_address = deploy(prepared).unwrap();
+
+            assert(2 == 2, '2 == 2');
+        }
+    "#
+        ),
+        Contract::new(
+            "HelloStarknet",
+            indoc!(
+                r#"
+                #[starknet::contract]
+                mod HelloStarknet {
+                    #[storage]
+                    struct Storage {}
+
+                    #[constructor]
+                    fn constructor(ref self: ContractState, arg1: felt252, arg2: felt252) {}
+                }
+        "#
+            )
+        )
+    );
+
+    let result = run(
+        &test.path().unwrap(),
+        Some(test.linked_libraries()),
+        &Default::default(),
+        Some(&Utf8PathBuf::from_path_buf(corelib().to_path_buf()).unwrap()),
+        &test.contracts(corelib().path()).unwrap(),
+        &Utf8PathBuf::from_path_buf(predeployed_contracts().to_path_buf()).unwrap(),
+    )
+    .unwrap();
+
+    assert_failed!(result);
+}
