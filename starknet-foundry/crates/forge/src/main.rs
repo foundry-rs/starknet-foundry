3ddfb1f82da3de83a3f49b31b481ad55cdac3b3a
Piotr Magiera <piotrmagiera150@gmail.com>
2023-07-19 17:16:54 +0200

    added temporary corelib

--- starknet-foundry/crates/forge/src/main.rs
+++ starknet-foundry/crates/forge/src/main.rs
@@ -12,6 +12,7 @@ use forge::{pretty_printing, RunnerConfig};
 use forge::scarb::{get_contracts_map, try_get_starknet_artifacts_path};
 use std::process::Command;
 
+static CORELIB_PATH: Dir = include_dir!("../corelib/src");
 static PREDEPLOYED_CONTRACTS: Dir = include_dir!("crates/cheatable-starknet/predeployed-contracts");
 
 #[derive(Parser, Debug)]
@@ -28,6 +29,14 @@ struct Args {
     exit_first: bool,
 }
 
+fn load_corelib() -> Result<TempDir> {
+    let tmp_dir = tempdir()?;
+    CORELIB_PATH
+        .extract(&tmp_dir)
+        .context("Failed to copy corelib to temporary directory")?;
+    Ok(tmp_dir)
+}
+
 fn load_predeployed_contracts() -> Result<TempDir> {
     let tmp_dir = tempdir()?;
     PREDEPLOYED_CONTRACTS
@@ -38,6 +47,12 @@ fn load_predeployed_contracts() -> Result<TempDir> {
 
 fn main_execution() -> Result<()> {
     let args = Args::parse();
+
+    // TODO
+    let corelib_dir = load_corelib()?;
+    let corelib_path: PathBuf = corelib_dir.path().into();
+    let corelib = Utf8PathBuf::try_from(corelib_path.clone())
+        .context("Failed to convert corelib path to Utf8PathBuf")?;
 
     let predeployed_contracts_dir = load_predeployed_contracts()?;
     let predeployed_contracts_path: PathBuf = predeployed_contracts_dir.path().into();
@@ -57,7 +72,7 @@ fn main_execution() -> Result<()> {
     for package in &scarb_metadata.workspace.members {
         let forge_config = forge::scarb::config_from_scarb_for_package(&scarb_metadata, package)?;
 
-        let (base_path, corelib_path, dependencies, target_name) =
+        let (base_path, _corelib_path, dependencies, target_name) =
             forge::scarb::dependencies_for_package(&scarb_metadata, package)?;
         let runner_config = RunnerConfig::new(
             args.test_name.clone(),
@@ -76,7 +91,7 @@ fn main_execution() -> Result<()> {
             &base_path,
             &Some(dependencies.clone()),
             &runner_config,
-            Some(&corelib_path),
+            Some(&corelib),
             &contracts,
             &predeployed_contracts,
         )?;
