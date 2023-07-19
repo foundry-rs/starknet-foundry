7f1c5957ba6d4ff527299811395049da1224ccac
Arcticae <tomekgsd@gmail.com>
2023-07-19 16:28:04 +0200

    Add cases for constructor invoking and incorrect class hash

--- starknet-foundry/crates/forge/tests/integration/deploy.rs
+++ starknet-foundry/crates/forge/tests/integration/deploy.rs
@@ -1,6 +1,6 @@
 use crate::common::corelib::{corelib, predeployed_contracts};
 use crate::common::runner::Contract;
-use crate::{assert_failed, assert_passed, test_case};
+use crate::{assert_case_output_contains, assert_failed, assert_passed, test_case};
 use camino::Utf8PathBuf;
 use forge::run;
 use indoc::indoc;
@@ -222,4 +222,133 @@ fn test_deploy_fails_on_too_many_constructor_arguments() {
     .unwrap();
 
     assert_failed!(result);
+}
+
+#[test]
+fn test_deploy_fails_with_incorrect_class_hash() {
+    let test = test_case!(
+        indoc!(
+            r#"
+        use result::ResultTrait;
+        use cheatcodes::RevertedTransactionTrait;
+        use cheatcodes::PreparedContract;
+        use array::ArrayTrait;
+
+        #[test]
+        fn deploy_not_existing_class_hash() {
+            let mut calldata = ArrayTrait::new();
+
+            let prepared = PreparedContract { class_hash: 'made-up-class-hash', constructor_calldata: @calldata };
+            let contract_address = deploy(prepared).unwrap();
+        }
+    "#
+        ),
+        Contract::new(
+            "HelloStarknet",
+            indoc!(
+                r#"
+                #[starknet::contract]
+                mod HelloStarknet {
+                    #[storage]
+                    struct Storage {}
+                }
+        "#
+            )
+        )
+    );
+
+    let result = run(
+        &test.path().unwrap(),
+        &Some(test.linked_libraries()),
+        &Default::default(),
+        Some(&Utf8PathBuf::from_path_buf(corelib().to_path_buf()).unwrap()),
+        &test.contracts(corelib().path()).unwrap(),
+        &Utf8PathBuf::from_path_buf(predeployed_contracts().to_path_buf()).unwrap(),
+    )
+    .unwrap();
+
+    assert_case_output_contains!(result, "deploy_not_existing_class_hash", "not declared");
+    assert_failed!(result);
+}
+
+#[test]
+fn test_deploy_invokes_the_constructor() {
+    let test = test_case!(
+        indoc!(
+            r#"
+        use option::OptionTrait;
+        use result::ResultTrait;
+        use cheatcodes::RevertedTransactionTrait;
+        use cheatcodes::PreparedContract;
+        use array::ArrayTrait;
+        use traits::TryInto;
+        use starknet::ContractAddress;
+        use starknet::Felt252TryIntoContractAddress;
+        
+        #[starknet::interface]
+        trait ThingGetter<TContractState> {
+            fn get_thing(self: @TContractState) -> felt252;
+        }
+        
+        #[test]
+        fn deploy_not_existing_class_hash() {
+            let mut calldata = ArrayTrait::new();
+            calldata.append(420);
+
+            let class_hash = declare('HelloStarknet').unwrap();
+            let prepared = PreparedContract { class_hash: class_hash, constructor_calldata: @calldata };
+            let contract_address = deploy(prepared).unwrap();
+            let contract_address: ContractAddress = contract_address.try_into().unwrap();
+            
+            let thing_getter = ThingGetterDispatcher { contract_address };
+            
+            let thing = thing_getter.get_thing();
+            
+            assert(thing == 420, 'Incorrect thing');
+        }
+    "#
+        ),
+        Contract::new(
+            "HelloStarknet",
+            indoc!(
+                r#"
+                #[starknet::interface]
+                trait ThingGetter<TContractState> {
+                    fn get_thing(self: @TContractState) -> felt252;
+                }
+                
+                #[starknet::contract]
+                mod HelloStarknet {
+                    #[storage]
+                    struct Storage {
+                        stored_thing: felt252
+                    }
+                    #[constructor]
+                     fn constructor(ref self: ContractState, arg1: felt252) {
+                        self.stored_thing.write(arg1)
+                     }
+                     
+                     #[external(v0)]
+                     impl ThingGetterImpl of super::ThingGetter<ContractState> {
+                        fn get_thing(self: @ContractState) -> felt252 {
+                            self.stored_thing.read()
+                        }
+                     }
+                }
+        "#
+            )
+        )
+    );
+
+    let result = run(
+        &test.path().unwrap(),
+        &Some(test.linked_libraries()),
+        &Default::default(),
+        Some(&Utf8PathBuf::from_path_buf(corelib().to_path_buf()).unwrap()),
+        &test.contracts(corelib().path()).unwrap(),
+        &Utf8PathBuf::from_path_buf(predeployed_contracts().to_path_buf()).unwrap(),
+    )
+    .unwrap();
+
+    assert_passed!(result);
 }
