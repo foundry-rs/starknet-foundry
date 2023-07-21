ed52ec953eda889d341a45fd3dfc2b69f911772e
war-in <marcin.warchol20@gmail.com>
2023-07-21 15:59:22 +0200

    add some tests

--- starknet-foundry/crates/cast/tests/e2e/account/create.rs
+++ starknet-foundry/crates/cast/tests/e2e/account/create.rs
@@ -1,5 +1,5 @@
 use crate::helpers::constants::{CONTRACTS_DIR, NETWORK, URL};
-use crate::helpers::fixtures::default_cli_args;
+use crate::helpers::fixtures::{default_cli_args, duplicate_directory_with_salt};
 use crate::helpers::runner::runner;
 use camino::Utf8PathBuf;
 use indoc::indoc;
@@ -9,7 +9,6 @@ use std::fs;
 #[tokio::test]
 pub async fn test_happy_case() {
     let accounts_file = "./tmp/accounts.json";
-    let current_dir = Utf8PathBuf::from(CONTRACTS_DIR.to_string() + "/v1/balance");
     let args = vec![
         "--url",
         URL,
@@ -25,9 +24,7 @@ pub async fn test_happy_case() {
         "0x1",
     ];
 
-    let snapbox = Command::new(cargo_bin!("sncast"))
-        .current_dir(&current_dir)
-        .args(args);
+    let snapbox = runner(&args);
     let bdg = snapbox.assert();
     let out = bdg.get_output();
 
@@ -37,8 +34,7 @@ pub async fn test_happy_case() {
     assert!(stdout_str.contains("message: Account successfully created. Prefund generated address with at least 432300000000 tokens. It is good to send more in the case of higher demand, max_fee * 2 = 864600000000"));
     assert!(stdout_str.contains("address: "));
 
-    let contents =
-        fs::read_to_string(current_dir.join(accounts_file)).expect("Unable to read created file");
+    let contents = fs::read_to_string(accounts_file).expect("Unable to read created file");
     assert!(contents.contains("my_account"));
     assert!(contents.contains("alpha-goerli"));
     assert!(contents.contains("private_key"));
@@ -46,7 +42,125 @@ pub async fn test_happy_case() {
     assert!(contents.contains("address"));
     assert!(contents.contains("salt"));
 
-    fs::remove_dir_all(current_dir.join(accounts_file).parent().unwrap()).unwrap();
+    fs::remove_dir_all(Utf8PathBuf::from(accounts_file).parent().unwrap()).unwrap();
+}
+
+#[tokio::test]
+pub async fn test_happy_case_generate_salt() {
+    let accounts_file = "./tmp2/accounts.json";
+    let args = vec![
+        "--url",
+        URL,
+        "--network",
+        NETWORK,
+        "--accounts-file",
+        accounts_file,
+        "account",
+        "create",
+        "--name",
+        "my_account",
+    ];
+
+    let snapbox = runner(&args);
+    let bdg = snapbox.assert();
+    let out = bdg.get_output();
+
+    let stdout_str =
+        std::str::from_utf8(&out.stdout).expect("failed to convert command output to string");
+    assert!(stdout_str.contains("command: Create account"));
+    assert!(stdout_str.contains("message: Account successfully created. Prefund generated address with at least 432300000000 tokens. It is good to send more in the case of higher demand, max_fee * 2 = 864600000000"));
+    assert!(stdout_str.contains("address: "));
+
+    let contents = fs::read_to_string(accounts_file).expect("Unable to read created file");
+    assert!(contents.contains("my_account"));
+    assert!(contents.contains("alpha-goerli"));
+    assert!(contents.contains("private_key"));
+    assert!(contents.contains("public_key"));
+    assert!(contents.contains("address"));
+    assert!(contents.contains("salt"));
+
+    fs::remove_dir_all(Utf8PathBuf::from(accounts_file).parent().unwrap()).unwrap();
+}
+
+#[tokio::test]
+pub async fn test_happy_case_add_profile() {
+    let current_dir = Utf8PathBuf::from(duplicate_directory_with_salt(
+        CONTRACTS_DIR.to_string() + "/v1/balance",
+        "put",
+        "1",
+    ));
+    let accounts_file = "./accounts.json";
+
+    let args = vec![
+        "--url",
+        URL,
+        "--network",
+        NETWORK,
+        "--accounts-file",
+        accounts_file,
+        "account",
+        "create",
+        "--name",
+        "my_account",
+        "--add-profile",
+    ];
+
+    let snapbox = Command::new(cargo_bin!("sncast"))
+        .current_dir(&current_dir)
+        .args(args);
+    let bdg = snapbox.assert();
+    let out = bdg.get_output();
+
+    let stdout_str =
+        std::str::from_utf8(&out.stdout).expect("failed to convert command output to string");
+    assert!(stdout_str.contains("add_profile: Profile successfully added to Scarb.toml"));
+
+    let contents =
+        fs::read_to_string(current_dir.join("Scarb.toml")).expect("Unable to read Scarb.toml");
+    assert!(contents.contains("[tool.sncast.my_account]"));
+    assert!(contents.contains("account = \"my_account\""));
+
+    fs::remove_dir_all(current_dir).unwrap();
+}
+
+#[tokio::test]
+pub async fn test_profile_already_exists() {
+    let current_dir = Utf8PathBuf::from(duplicate_directory_with_salt(
+        CONTRACTS_DIR.to_string() + "/v1/balance",
+        "put",
+        "2",
+    ));
+    let accounts_file = "./accounts.json";
+
+    let args = vec![
+        "--url",
+        URL,
+        "--network",
+        NETWORK,
+        "--accounts-file",
+        accounts_file,
+        "account",
+        "create",
+        "--name",
+        "myprofile",
+        "--add-profile",
+    ];
+
+    let snapbox = Command::new(cargo_bin!("sncast"))
+        .current_dir(&current_dir)
+        .args(args);
+    let bdg = snapbox.assert();
+    let out = bdg.get_output();
+
+    let stdout_str =
+        std::str::from_utf8(&out.stdout).expect("failed to convert command stderr to string");
+    assert!(stdout_str.contains(
+        "add_profile: Failed to add myprofile profile to the Scarb.toml. Profile already exists"
+    ));
+    assert!(stdout_str.contains("command: Create account"));
+    assert!(stdout_str.contains("message: Account successfully created. Prefund generated address with at least 432300000000 tokens. It is good to send more in the case of higher demand, max_fee * 2 = 864600000000"));
+
+    fs::remove_dir_all(current_dir).unwrap();
 }
 
 #[tokio::test]
