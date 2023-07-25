26866d978928b80a11135b06c4b672e028872773
Arcticae <tomekgsd@gmail.com>
2023-07-25 11:47:50 +0200

    Review

--- starknet-foundry/crates/forge/tests/integration/start_roll.rs
+++ starknet-foundry/crates/forge/tests/integration/start_roll.rs
@@ -83,13 +83,11 @@ fn start_roll_in_constructor_test() {
             
             #[test]
             fn test_roll_constructor_simple() {
-                assert(1 == 1, 'simple check');
                 let class_hash = declare('ConstructorRollChecker').unwrap();
                 let prepared = PreparedContract { class_hash: class_hash, constructor_calldata: @ArrayTrait::new() };
                 let contract_address: ContractAddress = 2598896470772924212281968896271340780432065735045468431712403008297614014532.try_into().unwrap();
                 start_roll(contract_address, 234);
                 let contract_address: ContractAddress = deploy(prepared).unwrap().try_into().unwrap();
-                contract_address.print();
             
                 let dispatcher = IConstructorRollCheckerDispatcher { contract_address };
                 assert(dispatcher.get_stored_block_number() == 234, 'Wrong stored blk_nb');
