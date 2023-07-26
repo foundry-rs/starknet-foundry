6c94db2d8b2429d52108cd407f1cf1334ee47de3
Artur Michalek <artur.michalek@swmansion.com>
2023-07-26 10:21:39 +0200

    Add more nonce tests

--- starknet-foundry/crates/forge/tests/integration/starknet.rs
+++ starknet-foundry/crates/forge/tests/integration/starknet.rs
@@ -81,6 +81,7 @@ fn timestamp_doesnt_decrease_between_transactions() {
 
     let result = run(
         &test.path().unwrap(),
+        &test.path().unwrap().join("src/lib.cairo"),
         &Some(test.linked_libraries()),
         &Default::default(),
         Some(&Utf8PathBuf::from_path_buf(corelib().to_path_buf()).unwrap()),
@@ -192,6 +193,7 @@ fn block_doesnt_decrease_between_transactions() {
 
     let result = run(
         &test.path().unwrap(),
+        &test.path().unwrap().join("src/lib.cairo"),
         &Some(test.linked_libraries()),
         &Default::default(),
         Some(&Utf8PathBuf::from_path_buf(corelib().to_path_buf()).unwrap()),
@@ -283,6 +285,141 @@ fn nonce_increases_between_transactions() {
 
     let result = run(
         &test.path().unwrap(),
+        &test.path().unwrap().join("src/lib.cairo"),
+        &Some(test.linked_libraries()),
+        &Default::default(),
+        Some(&Utf8PathBuf::from_path_buf(corelib().to_path_buf()).unwrap()),
+        &test.contracts(corelib().path()).unwrap(),
+        &Utf8PathBuf::from_path_buf(predeployed_contracts().to_path_buf()).unwrap(),
+    )
+    .unwrap();
+
+    assert_passed!(result);
+}
+
+// TODO Make nonce behavior consistent with Starknet
+#[ignore]
+#[allow(clippy::too_many_lines)]
+#[test]
+fn nonce_increases_between_deploys_and_declares() {
+    let test = test_case!(
+        indoc!(
+            r#"
+            use array::ArrayTrait;
+            use result::ResultTrait;
+            use option::OptionTrait;
+            use traits::TryInto;
+            use starknet::ContractAddress;
+            use starknet::Felt252TryIntoContractAddress;
+            use cheatcodes::PreparedContract;
+
+            #[starknet::interface]
+            trait INoncer<TContractState> {
+                fn write_nonce(ref self: TContractState);
+                fn read_nonce(self: @TContractState) -> felt252;
+            }
+
+            #[test]
+            fn nonce_increases_between_transactions() {
+                let class_hash = declare('Noncer').unwrap();
+                let prepared = PreparedContract { class_hash: class_hash, constructor_calldata: @ArrayTrait::new() };
+                let contract_address = deploy(prepared).unwrap();
+                let contract_address: ContractAddress = contract_address.try_into().unwrap();
+                let dispatcher = INoncerDispatcher { contract_address };
+
+                dispatcher.write_nonce();
+                let nonce = dispatcher.read_nonce();
+
+                let class_hash1 = declare('Contract1').unwrap();
+                let class_hash2 = declare('Contract2').unwrap();
+
+                dispatcher.write_nonce();
+                let new_nonce = dispatcher.read_nonce();
+
+                assert(new_nonce == nonce + 3, 'nonce doesnt increase');
+
+                let prepared1 = PreparedContract { class_hash: class_hash1, constructor_calldata: @ArrayTrait::new() };
+                let prepared2 = PreparedContract { class_hash: class_hash2, constructor_calldata: @ArrayTrait::new() };
+                deploy(prepared1).unwrap();
+                deploy(prepared2).unwrap();
+
+                dispatcher.write_nonce();
+                let new_new_nonce = dispatcher.read_nonce();
+
+                assert(new_new_nonce == new_nonce + 3, 'nonce doesnt increase');
+            }
+    "#
+        ),
+        Contract::new(
+            "Noncer",
+            indoc!(
+                r#"
+                #[starknet::interface]
+                trait INoncer<TContractState> {
+                    fn write_nonce(ref self: TContractState);
+                    fn read_nonce(self: @TContractState) -> felt252;
+                }
+
+                #[starknet::contract]
+                mod Noncer {
+                    use array::ArrayTrait;
+                    use starknet::get_tx_info;
+                    use box::BoxTrait;
+
+                    #[storage]
+                    struct Storage {
+                        nonce: felt252,
+                    }
+
+                    #[external(v0)]
+                    impl INoncerImpl of super::INoncer<ContractState> {
+                        fn write_nonce(ref self: ContractState) {
+                            let tx_info = get_tx_info().unbox();
+                            let nonce = tx_info.nonce;
+                            self.nonce.write(nonce);
+                        }
+
+                        fn read_nonce(self: @ContractState) -> felt252 {
+                            self.nonce.read()
+                        }
+                    }
+                }
+    "#
+            )
+        ),
+        Contract::new(
+            "Contract1",
+            indoc!(
+                r#"
+                #[starknet::contract]
+                mod Contract1 {
+                    #[storage]
+                    struct Storage {}
+                }
+        "#
+            )
+        ),
+        Contract::new(
+            "Contract2",
+            indoc!(
+                r#"
+                #[starknet::contract]
+                mod Contract2 {
+                    #[storage]
+                    struct Storage {}
+
+                    fn get_two(self: @ContractState) -> felt252 {
+                        2
+                    }
+                }
+        "#
+            )
+        )
+    );
+
+    let result = run(
+        &test.path().unwrap(),
+        &test.path().unwrap().join("src/lib.cairo"),
         &Some(test.linked_libraries()),
         &Default::default(),
         Some(&Utf8PathBuf::from_path_buf(corelib().to_path_buf()).unwrap()),
