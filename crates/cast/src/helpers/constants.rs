5f854c084e3c7b86f04f0e4747d8d49c36e55acd
war-in <marcin.warchol20@gmail.com>
2023-07-31 16:18:34 +0200

    bring back default_retires

--- crates/cast/src/helpers/constants.rs
+++ crates/cast/src/helpers/constants.rs
@@ -16,4 +16,4 @@ pub const UDC_ADDRESS: &str = "0x041a78e741e5af2fec34b695679bc6891742439f7afb848
 pub const OZ_CLASS_HASH: &str =
     "0x058d97f7d76e78f44905cc30cb65b91ea49a4b908a76703c54197bca90f81773";
 // used in wait_for_tx. Txs are fetched every 5 seconds
-pub const DEFAULT_RETRIES: u8 = 100;
+pub const DEFAULT_RETRIES: u8 = 12;
