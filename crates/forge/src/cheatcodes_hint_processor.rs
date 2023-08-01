3c9ac0641a427b25fe323d6d806a9554bcfc0bd2
Radinyn <igorurbanik23@gmail.com>
2023-08-01 12:44:05 +0200

    Removed blockifier felt

--- crates/forge/src/cheatcodes_hint_processor.rs
+++ crates/forge/src/cheatcodes_hint_processor.rs
@@ -272,7 +272,7 @@ impl CairoHintProcessor<'_> {
                     inputs[0].clone().to_be_bytes(),
                 )?)?);
                 let value = inputs[1].clone();
-                self.start_roll(contract_address, convert_to_blockifier_felt(&value))
+                self.start_roll(contract_address, value)
             }
             "stop_roll" => {
                 let contract_address = ContractAddress(PatriciaKey::try_from(StarkFelt::new(
@@ -286,7 +286,7 @@ impl CairoHintProcessor<'_> {
                     inputs[0].clone().to_be_bytes(),
                 )?)?);
                 let value = inputs[1].clone();
-                self.start_warp(contract_address, convert_to_blockifier_felt(&value))
+                self.start_warp(contract_address, value)
             }
             "stop_warp" => {
                 let contract_address = ContractAddress(PatriciaKey::try_from(StarkFelt::new(
@@ -372,34 +372,20 @@ fn execute_syscall(
 
     let calldata = buffer.next_arr().unwrap();
 
-    let calldata_blockifier: Vec<Felt252> =
-        calldata.iter().map(convert_to_blockifier_felt).collect();
     assert_eq!(std::str::from_utf8(&selector).unwrap(), "CallContract");
 
     let call_result = call_contract(
-        &convert_to_blockifier_felt(&contract_address),
-        &convert_to_blockifier_felt(&entry_point_selector),
-        &calldata_blockifier,
+        &contract_address,
+        &entry_point_selector,
+        &calldata,
         blockifier_state,
         cheated_state,
     )
     .unwrap_or_else(|err| panic!("Transaction execution error: {err}"));
 
     let (result, exit_code) = match call_result {
-        CallContractOutput::Success { ret_data } => (
-            ret_data
-                .iter()
-                .map(convert_from_blockifier_felt)
-                .collect::<Vec<Felt252>>(),
-            0,
-        ),
-        CallContractOutput::Panic { panic_data } => (
-            panic_data
-                .iter()
-                .map(convert_from_blockifier_felt)
-                .collect::<Vec<Felt252>>(),
-            1,
-        ),
+        CallContractOutput::Success { ret_data } => (ret_data, 0),
+        CallContractOutput::Panic { panic_data } => (panic_data, 1),
     };
 
     buffer.write(gas_counter).unwrap();
@@ -820,68 +806,6 @@ mod test {
             assert_eq!(
                 actual_class_hash,
                 ClassHash(stark_felt!(expected_class_hash))
-            );
-        }
-    }
-}
-
-fn convert_to_blockifier_felt(val: &Felt252) -> Felt252 {
-    let v = val.to_bigint();
-    Felt252::from(v)
-}
-
-fn convert_from_blockifier_felt(val: &Felt252) -> Felt252 {
-    let v = val.to_bigint();
-    Felt252::from(v)
-}
-
-#[cfg(test)]
-mod test_felt_conversions {
-    use super::convert_from_blockifier_felt;
-    use super::convert_to_blockifier_felt;
-    use cairo_felt::{
-        Felt252 as cairo_Felt252, Felt252, FIELD_HIGH as cairo_FIELD_HIGH,
-        FIELD_LOW as cairo_FIELD_LOW,
-    };
-
-    use num_traits::ToPrimitive;
-
-    // Does not re-export the consts
-    const BLOCKIFIER_FIELD_HIGH: u128 = (1 << 123) + (17 << 64);
-    const BLOCKIFIER_FIELD_LOW: u128 = 1;
-
-    #[test]
-    fn test_conversion_to_blockifier_felt() {
-        for value in [
-            BLOCKIFIER_FIELD_LOW,
-            200_u128,
-            400_000_000_u128,
-            9_999_999_999_999_u128,
-            BLOCKIFIER_FIELD_HIGH,
-        ] {
-            let from = cairo_Felt252::from(value);
-            let to = convert_to_blockifier_felt(&from);
-            assert_eq!(
-                to.to_u128().expect("Downcast failure"),
-                from.to_u128().expect("Downcast failure")
-            );
-        }
-    }
-
-    #[test]
-    fn test_conversion_from_blockifier_felt() {
-        for value in [
-            cairo_FIELD_LOW,
-            200_u128,
-            400_000_000_u128,
-            9_999_999_999_999_u128,
-            cairo_FIELD_HIGH,
-        ] {
-            let from = Felt252::from(value);
-            let to = convert_from_blockifier_felt(&from);
-            assert_eq!(
-                to.to_u128().expect("Downcast failure"),
-                from.to_u128().expect("Downcast failure")
             );
         }
     }
