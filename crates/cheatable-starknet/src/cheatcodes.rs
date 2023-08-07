daff79a563752a22ba866bc1477aa6625a6b998c
Radinyn <igorurbanik23@gmail.com>
2023-08-07 13:37:20 +0200

    Removed unused API

--- crates/cheatable-starknet/src/cheatcodes.rs
+++ crates/cheatable-starknet/src/cheatcodes.rs
@@ -56,16 +56,6 @@ impl From<EnhancedHintError> for CheatcodeError {
     }
 }
 
-pub fn write_cheatcode_panic(buffer: &mut MemBuffer, panic_data: &[Felt252]) {
-    buffer.write(1).expect("Failed to insert err code");
-    buffer
-        .write(panic_data.len())
-        .expect("Failed to insert panic_data len");
-    buffer
-        .write_data(panic_data.iter())
-        .expect("Failed to insert error in memory");
-}
-
 #[derive(Debug, PartialEq, Clone)]
 pub struct ContractArtifacts {
     pub sierra: String,
