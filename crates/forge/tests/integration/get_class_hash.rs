bbdcd8860f5b1878050b2b4fb0c38a0c792f41ec
glihm <dev@glihm.net>
2023-08-10 12:56:19 -0600

    add a call to HelloStarknet after upgrade

--- crates/forge/tests/integration/get_class_hash.rs
+++ crates/forge/tests/integration/get_class_hash.rs
@@ -61,6 +61,14 @@ fn get_class_hash_replace_class() {
                 fn upgrade(ref self: T, class_hash: ClassHash);
             }
 
+            #[starknet::interface]
+            trait IHelloStarknet<TContractState> {
+                fn increase_balance(ref self: TContractState, amount: felt252);
+                fn get_balance(self: @TContractState) -> felt252;
+                fn do_a_panic(self: @TContractState);
+                fn do_a_panic_with(self: @TContractState, panic_data: Array<felt252>);
+            }
+
             #[test]
             fn test_get_class_hash_replace_class() {
                 let class_hash = declare('GetClassHashCheckerUpg');
@@ -77,6 +85,10 @@ fn get_class_hash_replace_class() {
                 let hsn_class_hash = declare('HelloStarknet');
                 IUpgradeableDispatcher { contract_address }.upgrade(hsn_class_hash);
                 assert(get_class_hash(contract_address) == hsn_class_hash, 'Bad class hash upg');
+
+                let hello_dispatcher = IHelloStarknetDispatcher { contract_address };
+                hello_dispatcher.increase_balance(42);
+                assert(hello_dispatcher.get_balance() == 42, 'Fail to get balance');
             }
         "#
         ),
