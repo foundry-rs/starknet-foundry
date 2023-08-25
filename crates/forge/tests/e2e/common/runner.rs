0a4bdeff6ad3c3867d254ff2932c4ce8012dc50b
Tomasz Kumor <tomasz.kumor@swmansion.com>
2023-08-25 12:10:45 +0200

    update to new fmt version

--- crates/forge/tests/e2e/common/runner.rs
+++ crates/forge/tests/e2e/common/runner.rs
@@ -51,10 +51,8 @@ pub(crate) fn setup_package(package_name: &str) -> TempDir {
     temp
 }
 
-/**
- * In the context of GITHUB actions, get the source branch that triggered the workflow run.
- * Locally returns current branch.
- */
+/// In the context of GITHUB actions, get the source branch that triggered the workflow run.
+/// Locally returns current branch.
 pub(crate) fn gen_current_branch() -> String {
     let name: &str = "BRANCH_NAME";
     match env::var(name) {
