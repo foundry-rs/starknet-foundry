d4503fbb7abfdb3e2ee88387e2958b926f28c65c
war-in <marcin.warchol20@gmail.com>
2023-08-30 11:24:02 +0200

    hide rpc url

--- crates/cheatnet/tests/common/state.rs
+++ crates/cheatnet/tests/common/state.rs
@@ -1,5 +1,6 @@
 use camino::Utf8PathBuf;
 use cheatnet::CheatnetState;
+use dotenv::dotenv;
 use starknet::core::types::BlockId;
 use starknet::core::types::BlockTag::Latest;
 
@@ -12,8 +13,12 @@ pub fn create_cheatnet_state() -> CheatnetState {
 #[allow(clippy::module_name_repetitions)]
 pub fn create_cheatnet_fork_state() -> CheatnetState {
     let predeployed_contracts = Utf8PathBuf::from("predeployed-contracts");
+    dotenv().ok();
+    let node_url = std::env::var("CHEATNET_RPC_URL")
+        .expect("CHEATNET_RPC_URL must be set in the .env file");
+
     CheatnetState::new(
         &predeployed_contracts,
-        Some(("http://188.34.188.184:9545/rpc/v0.4", BlockId::Tag(Latest))),
+        Some((&node_url, BlockId::Tag(Latest))),
     )
 }
