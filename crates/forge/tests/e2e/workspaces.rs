cd41b08e42161e0c253a195ed6913352e01de581
Bartosz Rybarski <bartosz.rybarski@swmansion.com>
2023-09-07 15:45:50 +0200

    Added workspace test

--- crates/forge/tests/e2e/workspaces.rs
+++ crates/forge/tests/e2e/workspaces.rs
@@ -190,6 +190,54 @@ fn run_for_entire_workspace() {
 }
 
 #[test]
+fn run_for_entire_workspace_inside_package() {
+    let temp = assert_fs::TempDir::new().unwrap();
+    temp.copy_from("tests/data/hello_workspaces", &["**/*.cairo", "**/*.toml"])
+        .unwrap();
+
+    let package_dir = temp.join(PathBuf::from("crates/addition"));
+
+    let snapbox = runner().arg("--workspace");
+
+    snapbox
+        .current_dir(&package_dir)
+        .assert()
+        .success()
+        .stdout_matches(indoc! {r#"
+        [..]Compiling[..]
+        [..]Finished[..]
+        Collected 4 test(s) and 3 test file(s) from addition package
+        Running 1 inline test(s)
+        [PASS] addition::tests::it_works
+        Running 2 test(s) from tests/nested/test_nested.cairo
+        [PASS] test_nested::test_two
+        [PASS] test_nested::test_two_and_two
+        Running 1 test(s) from tests/test_simple.cairo
+        [PASS] test_simple::simple_case
+        Tests: 4 passed, 0 failed, 0 skipped
+
+
+        Collected 1 test(s) and 1 test file(s) from fibonacci package
+        Running 1 inline test(s)
+        [PASS] fibonacci::tests::it_works
+        Tests: 1 passed, 0 failed, 0 skipped
+
+
+        Collected 3 test(s) and 2 test file(s) from hello_workspaces package
+        Running 1 inline test(s)
+        [PASS] hello_workspaces::test_simple
+        Running 2 test(s) from tests/test_failing.cairo
+        [FAIL] test_failing::test_failing
+        
+        Failure data:
+            original value: [8111420071579136082810415440747], converted to a string: [failing check]
+        
+        [SKIP] test_failing::test_another_failing
+        Tests: 1 passed, 1 failed, 1 skipped
+        "#});
+}
+
+#[test]
 fn run_for_entire_workspace_and_specific_package() {
     let temp = assert_fs::TempDir::new().unwrap();
     temp.copy_from("tests/data/hello_workspaces", &["**/*.cairo", "**/*.toml"])
