86a069bc82374e1e10b1e50d781f368e247b7167
Tomasz Kumor <tomasz.kumor@swmansion.com>
2023-09-08 16:01:41 +0200

    adds missing files

---
+++ crates/cheatnet/src/cheatcodes/setup_fork.rs
@@ -0,0 +1,14 @@
+use crate::forking::state::ForkStateReader;
+use crate::CheatnetState;
+
+use starknet::core::types::BlockId;
+
+impl CheatnetState {
+    #[must_use]
+    pub fn setup_fork(&mut self, url: &str, block_id: BlockId) -> &mut CheatnetState {
+        let fork_state_reader = ForkStateReader::new(url, block_id);
+        self.blockifier_state.state.fork_state_reader = Some(fork_state_reader);
+
+        self
+    }
+}
