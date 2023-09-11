a7dd36af43e7e34d1be86603ec0206da19699717
Maksymilian Demitraszek <maksymilian.demitraszek@protonmail.com>
2023-09-11 11:00:43 +0200

    lint

--- crates/conversions/src/felt252.rs
+++ crates/conversions/src/felt252.rs
@@ -3,11 +3,11 @@ use blockifier::execution::execution_utils::felt_to_stark_felt;
 use cairo_felt::Felt252;
 use cairo_lang_runner::short_string::as_cairo_short_string;
 use starknet::core::types::FieldElement;
+use starknet_api::core::Nonce;
 use starknet_api::{
     core::{ClassHash, ContractAddress, PatriciaKey},
     hash::{StarkFelt, StarkHash},
 };
-use starknet_api::core::Nonce;
 
 impl StarknetConversions for Felt252 {
     fn to_felt252(&self) -> Felt252 {
