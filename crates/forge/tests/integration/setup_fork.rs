a700b2dfe65308b5389266245b0a293fb2b10a54
war-in <marcin.warchol20@gmail.com>
2023-09-13 10:05:28 +0200

    add simple test

---
+++ crates/forge/tests/integration/setup_fork.rs
@@ -0,0 +1,50 @@
+use crate::integration::common::running_tests::run_test_case;
+use crate::{assert_passed, test_case};
+use indoc::indoc;
+
+#[test]
+fn fork_simple() {
+    let test = test_case!(indoc!(
+        r#"
+            use result::ResultTrait;
+            use array::ArrayTrait;
+            use option::OptionTrait;
+            use traits::TryInto;
+            use starknet::ContractAddress;
+            use starknet::Felt252TryIntoContractAddress;
+            use starknet::contract_address_const;
+            use snforge_std::{ ForkConfig, ForkTrait, BlockTag, BlockId };
+
+            #[starknet::interface]
+            trait IHelloStarknet<TContractState> {
+                fn increase_balance(ref self: TContractState, amount: felt252);
+                fn get_balance(self: @TContractState) -> felt252;
+            }
+
+            #[test]
+            fn test_fork_simple() {
+                let fork_config = ForkConfig {
+                    url: array!['http://188.34.188.184:9545/rpc/', 'v0.4'],
+                    block: BlockId::Tag(BlockTag::Latest(()))
+                };
+                fork_config.set_up();
+
+                let dispatcher = IHelloStarknetDispatcher {
+                    contract_address: contract_address_const::<3216637956526895219277698311134811322769343974163380838558193911733621219342>()
+                };
+
+                let balance = dispatcher.get_balance();
+                assert(balance == 2, 'Balance should be 2');
+
+                dispatcher.increase_balance(100);
+
+                let balance = dispatcher.get_balance();
+                assert(balance == 102, 'Balance should be 102');
+            }
+        "#
+    ));
+
+    let result = run_test_case(&test);
+
+    assert_passed!(result);
+}
