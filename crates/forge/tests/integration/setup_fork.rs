bcd502c628a84915b943d333eb42c00291682dc8
war-in <marcin.warchol20@gmail.com>
2023-09-13 11:02:26 +0200

    do not reveal node address

--- crates/forge/tests/integration/setup_fork.rs
+++ crates/forge/tests/integration/setup_fork.rs
@@ -1,10 +1,15 @@
 use crate::integration::common::running_tests::run_test_case;
 use crate::{assert_passed, test_case};
-use indoc::indoc;
+use dotenv::dotenv;
+use indoc::formatdoc;
 
 #[test]
 fn fork_simple() {
-    let test = test_case!(indoc!(
+    dotenv().ok();
+    let node_url =
+        std::env::var("CHEATNET_RPC_URL").expect("CHEATNET_RPC_URL must be set in the .env file");
+
+    let test = test_case!(formatdoc!(
         r#"
             use result::ResultTrait;
             use array::ArrayTrait;
@@ -13,25 +18,25 @@ fn fork_simple() {
             use starknet::ContractAddress;
             use starknet::Felt252TryIntoContractAddress;
             use starknet::contract_address_const;
-            use snforge_std::{ ForkConfig, ForkTrait, BlockTag, BlockId };
+            use snforge_std::{{ ForkConfig, ForkTrait, BlockTag, BlockId }};
 
             #[starknet::interface]
-            trait IHelloStarknet<TContractState> {
+            trait IHelloStarknet<TContractState> {{
                 fn increase_balance(ref self: TContractState, amount: felt252);
                 fn get_balance(self: @TContractState) -> felt252;
-            }
+            }}
 
             #[test]
-            fn test_fork_simple() {
-                let fork_config = ForkConfig {
-                    url: array!['http://188.34.188.184:9545/rpc/', 'v0.4'],
+            fn test_fork_simple() {{
+                let fork_config = ForkConfig {{
+                    url: array!['{}', '{}'],
                     block: BlockId::Tag(BlockTag::Latest(()))
-                };
+                }};
                 fork_config.set_up();
 
-                let dispatcher = IHelloStarknetDispatcher {
+                let dispatcher = IHelloStarknetDispatcher {{
                     contract_address: contract_address_const::<3216637956526895219277698311134811322769343974163380838558193911733621219342>()
-                };
+                }};
 
                 let balance = dispatcher.get_balance();
                 assert(balance == 2, 'Balance should be 2');
@@ -40,9 +45,11 @@ fn fork_simple() {
 
                 let balance = dispatcher.get_balance();
                 assert(balance == 102, 'Balance should be 102');
-            }
-        "#
-    ));
+            }}
+        "#,
+        node_url[..31].to_string(),
+        node_url[31..].to_string()
+    ).as_str());
 
     let result = run_test_case(&test);
 
