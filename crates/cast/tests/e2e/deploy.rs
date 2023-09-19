7acde3be4acf65253837c9ec7792b1a580f3e819
Wojciech Szymczyk <wojciech.szymczyk@swmansion.com>
2023-09-19 10:24:13 +0200

    [tests] Use dynamically calculated hashes and addresses instead of hard-coded ones (#650)
    <!-- Reference any GitHub issues resolved by this PR -->
    
    This PR does not close any issue, but is a part of
    https://github.com/foundry-rs/starknet-foundry/issues/429
    Hashes and addresses changed every time we updated devnet/compilers.
    This is a bit tiresome to update them each time, plus there probably is
    no need for that. This PR makes the tests to use hashes and addresses
    taken directly from our code.
    
    ## Introduced changes
    
    <!-- A brief description of the changes -->
    
    - do not use hard coded hashes and addresses
    
    ## Breaking changes
    
    <!-- List of all breaking changes, if applicable -->
    None
    
    ## Checklist
    
    <!-- Make sure all of these are complete -->
    
    - [X] Linked relevant issue
    - [X] Updated relevant documentation
    - [X] Added relevant tests
    - [X] Performed self-review of the code
    - [X] Added changes to `CHANGELOG.md`

--- crates/cast/tests/e2e/deploy.rs
+++ crates/cast/tests/e2e/deploy.rs
@@ -1,15 +1,14 @@
-use crate::helpers::constants::{
-    ACCOUNT, CONTRACT_WITH_CONSTRUCTOR_CLASS_HASH_V1, CONTRACT_WITH_CONSTRUCTOR_CLASS_HASH_V2,
-    MAP_CLASS_HASH_V1, MAP_CLASS_HASH_V2,
+use crate::helpers::constants::ACCOUNT;
+use crate::helpers::fixtures::{
+    default_cli_args, from_env, get_transaction_hash, get_transaction_receipt,
 };
-use crate::helpers::fixtures::{default_cli_args, get_transaction_hash, get_transaction_receipt};
 use crate::helpers::runner::runner;
 use indoc::indoc;
 use starknet::core::types::TransactionReceipt::Invoke;
 use test_case::test_case;
 
-#[test_case(MAP_CLASS_HASH_V1, "user1" ; "when cairo1 contract")]
-#[test_case(MAP_CLASS_HASH_V2, "user2" ; "when cairo2 contract")]
+#[test_case(from_env("CAST_MAP_V1_CLASS_HASH").unwrap().as_str(), "user1" ; "when cairo1 contract")]
+#[test_case(from_env("CAST_MAP_V2_CLASS_HASH").unwrap().as_str(), "user2" ; "when cairo2 contract")]
 #[tokio::test]
 async fn test_happy_case(class_hash: &str, account: &str) {
     let mut args = default_cli_args();
@@ -37,8 +36,8 @@ async fn test_happy_case(class_hash: &str, account: &str) {
     assert!(matches!(receipt, Invoke(_)));
 }
 
-#[test_case(CONTRACT_WITH_CONSTRUCTOR_CLASS_HASH_V1, "user3" ; "when cairo1 contract")]
-#[test_case(CONTRACT_WITH_CONSTRUCTOR_CLASS_HASH_V2, "user4" ; "when cairo2 contract")]
+#[test_case(from_env("CAST_WITH_CONSTRUCTOR_V1_CLASS_HASH").unwrap().as_str(), "user3" ; "when cairo1 contract")]
+#[test_case(from_env("CAST_WITH_CONSTRUCTOR_V2_CLASS_HASH").unwrap().as_str(), "user4" ; "when cairo2 contract")]
 #[tokio::test]
 async fn test_happy_case_with_constructor(class_hash: &str, account: &str) {
     let mut args = default_cli_args();
@@ -63,8 +62,8 @@ async fn test_happy_case_with_constructor(class_hash: &str, account: &str) {
     assert!(matches!(receipt, Invoke(_)));
 }
 
-#[test_case(CONTRACT_WITH_CONSTRUCTOR_CLASS_HASH_V1, "user3" ; "when cairo1 contract")]
-#[test_case(CONTRACT_WITH_CONSTRUCTOR_CLASS_HASH_V2, "user4" ; "when cairo2 contract")]
+#[test_case(from_env("CAST_WITH_CONSTRUCTOR_V1_CLASS_HASH").unwrap().as_str(), "user3" ; "when cairo1 contract")]
+#[test_case(from_env("CAST_WITH_CONSTRUCTOR_V2_CLASS_HASH").unwrap().as_str(), "user4" ; "when cairo2 contract")]
 fn test_wrong_calldata(class_hash: &str, account: &str) {
     let mut args = default_cli_args();
     args.append(&mut vec![
@@ -101,8 +100,8 @@ async fn test_contract_not_declared() {
     assert!(output.contains("Class with hash 0x1 is not declared."));
 }
 
-#[test_case(MAP_CLASS_HASH_V1, "user1" ; "when cairo1 contract")]
-#[test_case(MAP_CLASS_HASH_V2, "user2" ; "when cairo2 contract")]
+#[test_case(from_env("CAST_MAP_V1_CLASS_HASH").unwrap().as_str(), "user1" ; "when cairo1 contract")]
+#[test_case(from_env("CAST_MAP_V2_CLASS_HASH").unwrap().as_str(), "user2" ; "when cairo2 contract")]
 fn test_contract_already_deployed(class_hash: &str, account: &str) {
     let mut args = default_cli_args();
     args.append(&mut vec![
@@ -122,8 +121,8 @@ fn test_contract_already_deployed(class_hash: &str, account: &str) {
     assert!(output.contains("StarknetErrorCode.CONTRACT_ADDRESS_UNAVAILABLE"));
 }
 
-#[test_case(MAP_CLASS_HASH_V1, "user1" ; "when cairo1 contract")]
-#[test_case(MAP_CLASS_HASH_V2, "user2" ; "when cairo2 contract")]
+#[test_case(from_env("CAST_MAP_V1_CLASS_HASH").unwrap().as_str(), "user1" ; "when cairo1 contract")]
+#[test_case(from_env("CAST_MAP_V2_CLASS_HASH").unwrap().as_str(), "user2" ; "when cairo2 contract")]
 fn test_too_low_max_fee(class_hash: &str, account: &str) {
     let mut args = default_cli_args();
     args.append(&mut vec![
