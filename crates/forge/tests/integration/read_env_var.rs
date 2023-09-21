ad9c97f7ac7301f88e3501f257c838dd5a46dbf8
Artur Michalek <artur.michalek@swmansion.com>
2023-09-21 12:39:25 +0200

    Add more env var integration tests

--- crates/forge/tests/integration/read_env_var.rs
+++ crates/forge/tests/integration/read_env_var.rs
@@ -1,6 +1,8 @@
 use crate::integration::common::running_tests::run_test_case;
-use crate::{assert_passed, test_case};
+use crate::{assert_case_output_contains, assert_failed, assert_passed, test_case};
+use cairo_felt::Felt252;
 use indoc::indoc;
+use num_bigint::BigUint;
 use std::env;
 
 #[test]
@@ -33,7 +35,7 @@ fn read_felt252() {
         use snforge_std::read_env_var;
 
         #[test]
-        fn test_read_short_string() {
+        fn test_read_felt252() {
             let result = read_env_var('MY_ENV_VAR');
             assert(result == 1234567, 'failed reading env var');
         }
@@ -43,4 +45,57 @@ fn read_felt252() {
     let result = run_test_case(&test);
 
     assert_passed!(result);
+}
+
+#[test]
+fn read_invalid_felt252() {
+    let value = (Felt252::prime() + BigUint::from(1_u32)).to_string();
+    env::set_var("MY_ENV_VAR", &value);
+
+    let test = test_case!(indoc!(
+        r#"
+        use snforge_std::read_env_var;
+
+        #[test]
+        fn test_read_invalid_felt252() {
+            let result = read_env_var('MY_ENV_VAR');
+        }
+    "#
+    ));
+
+    let result = run_test_case(&test);
+
+    assert_failed!(result);
+    assert_case_output_contains!(
+        result,
+        "test_read_invalid_felt252",
+        &format!("Failed to parse value = {value} to felt")
+    );
+}
+
+#[test]
+fn read_invalid_short_string() {
+    let value =
+        "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+    env::set_var("MY_ENV_VAR", &value);
+
+    let test = test_case!(indoc!(
+        r#"
+        use snforge_std::read_env_var;
+
+        #[test]
+        fn test_read_invalid_short_string() {
+            let result = read_env_var('MY_ENV_VAR');
+        }
+    "#
+    ));
+
+    let result = run_test_case(&test);
+
+    assert_failed!(result);
+    assert_case_output_contains!(
+        result,
+        "test_read_invalid_short_string",
+        &format!("Failed to parse value = {value} to felt")
+    );
 }
