76734fc0d241e262803a7b5e9bac84ddd53a289e
Artur Michalek <artur.michalek@swmansion.com>
2023-09-28 17:36:35 +0200

    Expand benchmark code, configure parameters

--- crates/forge/benches/my_benchmark.rs
+++ crates/forge/benches/my_benchmark.rs
@@ -1,5 +1,6 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, Criterion};
 use indoc::indoc;
+use std::time::Duration;
 use utils::runner::Contract;
 use utils::running_tests::run_test_case;
 use utils::{assert_passed, test_case};
@@ -11,13 +12,27 @@ fn simple_declare() {
         use result::ResultTrait;
         use traits::Into;
         use starknet::ClassHashIntoFelt252;
-        use snforge_std::declare;
+        use snforge_std::{ declare, ContractClassTrait };
+
+        #[starknet::interface]
+        trait IHelloStarknet<TContractState> {
+            fn increase_balance(ref self: TContractState, amount: felt252);
+            fn decrease_balance(ref self: TContractState, amount: felt252);
+            fn get_balance(self: @TContractState) -> felt252;
+        }
 
         #[test]
         fn test_declare_simple() {
             assert(1 == 1, 'simple check');
             let contract = declare('HelloStarknet');
-            assert(contract.class_hash.into() != 0, 'proper class hash');
+            let contract_address = contract.deploy(@ArrayTrait::new()).unwrap();
+            let dispatcher = IHelloStarknetDispatcher { contract_address };
+
+            let balance = dispatcher.get_balance();
+            dispatcher.increase_balance(100);
+            let balance = dispatcher.get_balance();
+            dispatcher.decrease_balance(100);
+            let balance = dispatcher.get_balance();
         }
         "#
         ),
@@ -43,19 +58,28 @@ fn simple_declare() {
                     fn decrease_balance(ref self: ContractState, amount: felt252) {
                         self.balance.write(self.balance.read() - amount);
                     }
+
+                    //Get the balance.
+                    #[external(v0)]
+                    fn get_balance(self: @ContractState) -> felt252 {
+                        self.balance.read()
+                    }
                 }
                 "#
             )
         )
     );
-
     let result = run_test_case(&test);
 
     assert_passed!(result);
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("declare_simple", |b| b.iter(simple_declare));
+    let mut group = c.benchmark_group("declare-group");
+    group.sample_size(10);
+    group.measurement_time(Duration::from_secs(30));
+    group.bench_function("declare_simple", |b| b.iter(simple_declare));
+    group.finish();
 }
 
 criterion_group!(benches, criterion_benchmark);
