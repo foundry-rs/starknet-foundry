1caf93c5a2bfce7119eadfbd6357d1d6d76b5dc2
0xicosahedron <btc_icosahedron@protonmail.com>
2023-10-06 15:48:07 +0530

    add more tests

--- crates/cast/tests/e2e/show_config.rs
+++ crates/cast/tests/e2e/show_config.rs
@@ -2,10 +2,12 @@ use crate::helpers::runner::runner;
 use indoc::indoc;
 
 #[tokio::test]
-async fn test_all_config_in_cli() {
+async fn test_show_config_from_scarb_toml() {
     let args = vec![
         "--path-to-scarb-toml",
         "tests/data/show_config/all_Scarb.toml",
+        "--profile",
+        "profile1",
         "show-config",
     ];
 
@@ -14,9 +16,62 @@ async fn test_all_config_in_cli() {
     snapbox.assert().success().stdout_eq(indoc! {r#"
         command: show-config
         account: user1
-        account_file_path: ../test
+        account_file_path: ../account-file
         chain_id: alpha-goerli
+        keystore: ../keystore
+        profile: profile1
         rpc_url: http://127.0.0.1:5055/rpc
-        scarb_path: tests/data/show_config/all_Scarb.toml
+        scarb_path: crates/cast/tests/data/show_config/all_Scarb.toml
+    "#});
+}
+
+#[tokio::test]
+async fn test_show_config_from_cli() {
+    let args = vec![
+        "--account",
+        "user2",
+        "--url",
+        "http://127.0.0.1:5055/rpc",
+        "--accounts-file",
+        "../account-file",
+        "--keystore",
+        "../keystore",
+        "show-config",
+    ];
+
+    let snapbox = runner(&args);
+
+    snapbox.assert().success().stdout_eq(indoc! {r#"
+        command: show-config
+        account: user2
+        account_file_path: ../account-file
+        chain_id: alpha-goerli
+        keystore: ../keystore
+        rpc_url: http://127.0.0.1:5050/rpc
+    "#});
+}
+
+#[tokio::test]
+async fn test_show_config_from_cli_and_scarb() {
+    let args = vec![
+        "--account",
+        "user2",
+        "--path-to-scarb-toml",
+        "tests/data/show_config/all_Scarb.toml",
+        "--profile",
+        "profile1",
+        "show-config",
+    ];
+
+    let snapbox = runner(&args);
+
+    snapbox.assert().success().stdout_eq(indoc! {r#"
+        command: show-config
+        account: user2
+        account_file_path: ../account-file
+        chain_id: alpha-goerli
+        keystore: ../keystore
+        profile: profile1
+        rpc_url: http://127.0.0.1:5050/rpc
     "#});
 }
