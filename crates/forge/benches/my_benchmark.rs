90a1288b4474bc4682666da77771637574787453
Artur Michalek <artur.michalek@swmansion.com>
2023-10-09 11:48:35 +0200

    Add benchmark for compiling

--- crates/forge/benches/my_benchmark.rs
+++ crates/forge/benches/my_benchmark.rs
@@ -1,9 +1,73 @@
+use assert_fs::fixture::PathCopy;
+use assert_fs::TempDir;
+use camino::Utf8PathBuf;
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
+use forge::collecting::{collect_test_crates, TestCrate};
+use forge::TestCrateType;
 use indoc::indoc;
+use std::path::PathBuf;
+use std::str::FromStr;
 use std::time::Duration;
+use test_collector::LinkedLibrary;
+use utils::corelib::corelib_path;
 use utils::runner::{Contract, TestCase};
 use utils::running_tests::run_test_case;
 use utils::{assert_passed, test_case};
+
+fn setup_collect_tests() -> TempDir {
+    let temp = TempDir::new().unwrap();
+    temp.copy_from("tests/data/simple_package", &["**/*.cairo", "**/*.toml"])
+        .unwrap();
+
+    temp
+}
+
+fn collect_tests(package: &TempDir) {
+    let temp_dir = TempDir::new().unwrap();
+    let path = Utf8PathBuf::from_path_buf(package.to_path_buf()).unwrap();
+
+    collect_test_crates(&path, "simple_package", &path, &temp_dir).unwrap();
+}
+
+fn setup_compile_tests() -> (TestCrate, Vec<LinkedLibrary>, Utf8PathBuf, TempDir) {
+    let package = setup_collect_tests();
+    let path = Utf8PathBuf::from_path_buf(package.to_path_buf())
+        .unwrap()
+        .join("src");
+
+    let snforge_std_path = PathBuf::from_str("../../snforge_std")
+        .unwrap()
+        .canonicalize()
+        .unwrap();
+    let linked_libraries = vec![
+        LinkedLibrary {
+            name: "simple_package".to_string(),
+            path: PathBuf::from(path.clone()),
+        },
+        LinkedLibrary {
+            name: "snforge_std".to_string(),
+            path: snforge_std_path.join("src"),
+        },
+    ];
+
+    let test_crate = TestCrate {
+        crate_root: path,
+        crate_name: "simple_package".to_string(),
+        crate_type: TestCrateType::Lib,
+    };
+
+    (test_crate, linked_libraries, corelib_path(), package)
+}
+
+fn compile_tests(
+    test_crate: &TestCrate,
+    linked_libraries: &[LinkedLibrary],
+    corelib_path: &Utf8PathBuf,
+) {
+    test_crate
+        .compile_tests(linked_libraries, corelib_path)
+        .unwrap();
+}
 
 fn setup_declare_and_interact() -> TestCase {
     test_case!(
@@ -104,6 +168,8 @@ fn declare_and_interact(test: &TestCase) {
 
 fn criterion_benchmark(c: &mut Criterion) {
     let test = setup_declare_and_interact();
+    let collect_tests_input = setup_collect_tests();
+    let compile_tests_input = setup_compile_tests();
 
     let mut group = c.benchmark_group("forge-benchmark-group");
     group.sample_size(10);
@@ -113,10 +179,18 @@ fn criterion_benchmark(c: &mut Criterion) {
         &test,
         |b, s| b.iter(|| declare_and_interact(s)),
     );
+    group.bench_with_input(
+        BenchmarkId::new("collect_tests", format!("{test:?}")),
+        &collect_tests_input,
+        |b, s| b.iter(|| collect_tests(s)),
+    );
+    group.bench_with_input(
+        BenchmarkId::new("compile_tests", format!("{test:?}")),
+        &compile_tests_input,
+        |b, (s1, s2, s3, _)| b.iter(|| compile_tests(s1, s2, s3)),
+    );
     group.finish();
 }
-
-// TODO benchmark test compilation
 
 criterion_group!(benches, criterion_benchmark);
 criterion_main!(benches);
