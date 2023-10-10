7684105ff66b4daa4c6ea999f2de04b16a4992aa
Arcticae <tomekgsd@gmail.com>
2023-10-10 13:26:46 +0200

    lint

--- crates/cheatnet/src/constants.rs
+++ crates/cheatnet/src/constants.rs
@@ -209,10 +209,7 @@ pub fn build_testing_state(predeployed_contracts: &Utf8PathBuf) -> DictStateRead
     let test_contract_class_hash = ClassHash(stark_felt!(TEST_CONTRACT_CLASS_HASH));
 
     let class_hash_to_class = HashMap::from([
-        (
-            test_account_class_hash,
-            ContractClass::V1(account_class.clone()),
-        ),
+        (test_account_class_hash, ContractClass::V1(account_class)),
         // This is dummy put here only to satisfy blockifier
         // this class is not used and the test contract cannot be called
         (test_contract_class_hash, contract_class_no_entrypoints()),
