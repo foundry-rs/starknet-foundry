bd93405c9731854107ea61a398938e7786fcce37
Tomasz Rejowski <34059895+Arcticae@users.noreply.github.com>
2023-10-10 14:24:55 +0200

    Implement deploy in execution layer (#828)
    <!-- Reference any GitHub issues resolved by this PR -->
    
    Closes #805 
    
    ## Introduced changes
    
    ## Breaking changes
    
    <!-- List of all breaking changes, if applicable -->
    
    ## Checklist
    
    <!-- Make sure all of these are complete -->
    
    - [ ] Linked relevant issue
    - [ ] Updated relevant documentation
    - [ ] Added relevant tests
    - [ ] Performed self-review of the code
    - [ ] Added changes to `CHANGELOG.md`
    
    ---------
    
    Co-authored-by: Piotr Magiera <56825108+piotmag769@users.noreply.github.com>

--- crates/forge/tests/integration/dispatchers.rs
+++ crates/forge/tests/integration/dispatchers.rs
@@ -577,7 +577,7 @@ fn nonexistent_method_call() {
     assert_case_output_contains!(
         result,
         "test_nonexistent_method_call",
-        "Entry point selector 0x01fdb214e1495025fa4baf660d34f03c0d8b5037cf10311d2a3202a806aa9485 not found in contract 0x00de051f30e9666abb4facfd77c48e4a64c2e30738292a6f5e217ff9cda5fee5"
+        "Entry point selector 0x01fdb214e1495025fa4baf660d34f03c0d8b5037cf10311d2a3202a806aa9485 not found in contract 0x0351160f49d8a48506d9ac3a3983f57d5bb2257ef0e583836bdfa0ce5fd45690"
     );
 }
 
