c1fb2405eac4e28fc4a829b603f0d85cf5bc8f4b
war-in <marcin.warchol20@gmail.com>
2023-10-13 13:40:39 +0200

    rename

--- crates/forge/src/running.rs
+++ crates/forge/src/running.rs
@@ -236,8 +236,10 @@ fn get_fork_state_reader(
     fork_config: &Option<ForkConfig>,
 ) -> Result<Option<ForkStateReader>> {
     match &fork_config {
-        Some(ForkConfig::Params(url, block_id)) => {
-            let block_id = get_block_id(url, *block_id)?;
+        Some(ForkConfig::Params(url, mut block_id)) => {
+            if let BlockId::Tag(Latest) = block_id {
+                block_id = get_latest_block_number(url)?;
+            }
             Ok(Some(ForkStateReader::new(
                 url,
                 block_id,
@@ -251,20 +253,13 @@ fn get_fork_state_reader(
     }
 }
 
-fn get_block_id(url: &str, block_id: BlockId) -> Result<BlockId> {
-    match block_id {
-        BlockId::Tag(Latest) => {
-            let client = JsonRpcClient::new(HttpTransport::new(Url::parse(url).unwrap()));
-            let runtime = Runtime::new().expect("Could not instantiate Runtime");
+fn get_latest_block_number(url: &str) -> Result<BlockId> {
+    let client = JsonRpcClient::new(HttpTransport::new(Url::parse(url).unwrap()));
+    let runtime = Runtime::new().expect("Could not instantiate Runtime");
 
-            match runtime.block_on(client.get_block_with_tx_hashes(BlockId::Tag(Latest))) {
-                Ok(MaybePendingBlockWithTxHashes::Block(block)) => {
-                    Ok(BlockId::Number(block.block_number))
-                }
-                _ => Err(anyhow!("Could not get the latest block number".to_string())),
-            }
-        }
-        _ => Ok(block_id),
+    match runtime.block_on(client.get_block_with_tx_hashes(BlockId::Tag(Latest))) {
+        Ok(MaybePendingBlockWithTxHashes::Block(block)) => Ok(BlockId::Number(block.block_number)),
+        _ => Err(anyhow!("Could not get the latest block number".to_string())),
     }
 }
 
@@ -288,11 +283,13 @@ fn find_params_and_build_fork_state_reader(
                 _ => unreachable!(),
             })
             .collect::<Vec<_>>();
-        let [Some(block_id)] = block_id[..] else {
+        let [Some(mut block_id)] = block_id[..] else {
             return Ok(None);
         };
 
-        let block_id = get_block_id(&fork.url, block_id)?;
+        if let BlockId::Tag(Latest) = block_id {
+            block_id = get_latest_block_number(&fork.url)?;
+        }
         return Ok(Some(ForkStateReader::new(
             &fork.url,
             block_id,
