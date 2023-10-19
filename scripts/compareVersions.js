0fd752dd790b98beb2f073a679b2aad90dd8b11f
Artur Michalek <artur.michalek@swmansion.com>
2023-10-19 10:12:28 +0200

    Trigger release on push, compare versions from toml and latest release

---
+++ scripts/compareVersions.js
@@ -0,0 +1,14 @@
+const semver = require('semver')
+
+if (process.argv.length !== 4) {
+    console.error('Two arguments requred');
+    process.exit(1);
+}
+
+const old_version = process.argv[2];
+const new_version = process.argv[3];
+
+if (!semver.gt(new_version, old_version)) {
+    console.error('Version is not greater than previous release');
+    process.exit(2);
+}
