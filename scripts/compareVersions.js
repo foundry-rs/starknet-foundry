26b59bb44591db7c532b447c020ce3f927bc87ee
Artur Michalek <artur.michalek@swmansion.com>
2023-10-23 12:28:32 +0200

    Remove commented out code

--- scripts/compareVersions.js
+++ scripts/compareVersions.js
@@ -8,8 +8,4 @@ if (process.argv.length !== 4) {
 const old_version = process.argv[2];
 const new_version = process.argv[3];
 
-// if (!semver.gt(new_version, old_version)) {
-//     console.error('Version is not greater than previous release');
-//     process.exit(2);
-// }
 console.log((semver.gt(new_version, old_version)).toString())
