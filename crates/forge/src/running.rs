e2431563097fb8abacf2b139101f5b43c7120275
tkumor3 <tomasz.kumor@swmansion.com>
2023-10-24 09:48:31 +0200

    return skip on channel close (#936)
    <!-- Reference any GitHub issues resolved by this PR -->
    
    Closes #
    
    ## Introduced changes
    
    <!-- A brief description of the changes -->
    
    -
    -
    
    ## Breaking changes
    
    <!-- List of all breaking changes, if applicable -->
    
    ## Checklist
    
    <!-- Make sure all of these are complete -->
    
    - [x] Linked relevant issue
    - [x] Updated relevant documentation
    - [x] Added relevant tests
    - [x] Performed self-review of the code
    - [x] Added changes to `CHANGELOG.md`
    
    ---------
    
    Co-authored-by: Artur Michałek <52135326+cptartur@users.noreply.github.com>

--- crates/forge/src/running.rs
+++ crates/forge/src/running.rs
@@ -90,7 +90,7 @@ pub(crate) fn blocking_run_from_test(
         // a channel is used to receive information indicating
         // that the execution of the task is no longer necessary.
         if send.is_closed() {
-            return Err(anyhow::anyhow!("stop spawn_blocking"));
+            return Ok(TestCaseSummary::Interrupted {});
         }
         run_test_case(
             args,
