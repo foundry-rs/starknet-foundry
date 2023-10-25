e19c59b2570dd15cb7517359be53362c7e31b967
Maksymilian Demitraszek <maksymilian.demitraszek@protonmail.com>
2023-10-25 14:30:10 +0200

    fixed script

--- scripts/release.sh
+++ scripts/release.sh
@@ -8,5 +8,5 @@ rm CHANGELOG.md.bak 2> /dev/null
 sed -i.bak "/\[workspace.package\]/,/version =/ s/version = \".*/version = \"${VERSION}\"/" Cargo.toml
 rm Cargo.toml.bak 2> /dev/null
 
-cargo update forge
-cargo update cast
+cargo update -p forge
+cargo update -p cast
