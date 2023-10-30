71128d5758a32c16f18fea8e1fcb7f5bb257f011
devnet0x <devnet0x@gmail.com>
2023-10-30 10:34:52 -0300

    Fix short string test and optimize helper code

--- crates/conversions/tests/helpers/hex.rs
+++ crates/conversions/tests/helpers/hex.rs
@@ -4,36 +4,28 @@ use starknet_api::hash::StarkFelt;
 
 #[must_use]
 pub fn str_hex_to_stark_felt(hex_string: &str) -> StarkFelt {
-    let hex_string = hex_string.strip_prefix("0x").unwrap().to_string();
-    let mut byte_array = [0u8; 32];
-    for i in 0..32 {
-        let byte_str = &hex_string[i * 2..i * 2 + 2];
-        byte_array[i] = u8::from_str_radix(byte_str, 16).unwrap();
-    }
-
-    StarkFelt::new(byte_array).unwrap()
+    let bytes = hex_string_to_bytes(hex_string);
+    StarkFelt::new(bytes).unwrap()
 }
 
 #[must_use]
 pub fn str_hex_to_felt252(hex_string: &str) -> Felt252 {
-    let hex_string = hex_string.strip_prefix("0x").unwrap().to_string();
-    let mut bytes = [0u8; 32];
-    for i in 0..32 {
-        let byte_str = &hex_string[i * 2..i * 2 + 2];
-        bytes[i] = u8::from_str_radix(byte_str, 16).unwrap();
-    }
-
+    let bytes = hex_string_to_bytes(hex_string);
     Felt252::from_bytes_be(&bytes)
 }
 
 #[must_use]
 pub fn str_hex_to_field_element(hex_string: &str) -> FieldElement {
+    let bytes = hex_string_to_bytes(hex_string);
+    FieldElement::from_bytes_be(&bytes).unwrap()
+}
+
+fn hex_string_to_bytes(hex_string: &str) -> [u8; 32] {
     let hex_string = hex_string.strip_prefix("0x").unwrap().to_string();
     let mut bytes = [0u8; 32];
     for i in 0..32 {
         let byte_str = &hex_string[i * 2..i * 2 + 2];
         bytes[i] = u8::from_str_radix(byte_str, 16).unwrap();
     }
-
-    FieldElement::from_bytes_be(&bytes).unwrap()
+    bytes
 }
