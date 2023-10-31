d2158c3fb5dfbe1f41f015f0d77fc72e63b818fa
Artur Michalek <artur.michalek@swmansion.com>
2023-10-31 17:27:46 +0100

    Extract declare_deploy_and_interact cairo code to separate files

--- crates/forge/benches/my_bench/declare_deploy_and_interact.rs
+++ crates/forge/benches/my_bench/declare_deploy_and_interact.rs
@@ -1,4 +1,5 @@
-use indoc::indoc;
+use std::fs;
+use std::path::Path;
 use test_utils::runner::Contract;
 use test_utils::runner::TestCase;
 use test_utils::running_tests::run_test_case;
@@ -6,92 +7,13 @@ use test_utils::{assert_passed, test_case};
 
 pub fn setup() -> TestCase {
     test_case!(
-        indoc!(
-            r#"
-        use result::ResultTrait;
-        use traits::Into;
-        use starknet::ClassHashIntoFelt252;
-        use starknet::ContractAddress;
-        use starknet::Felt252TryIntoContractAddress;
-        use snforge_std::{ declare, ContractClassTrait, start_prank, start_roll, start_warp };
-
-        #[starknet::interface]
-        trait IHelloStarknet<TContractState> {
-            fn increase_balance(ref self: TContractState, amount: felt252);
-            fn decrease_balance(ref self: TContractState, amount: felt252);
-            fn get_balance(self: @TContractState) -> felt252;
-            fn interact_with_state(self: @TContractState) -> (felt252, felt252, felt252);
-        }
-
-        #[test]
-        fn declare_and_interact() {
-            assert(1 == 1, 'simple check');
-            let contract = declare('HelloStarknet');
-            let contract_address = contract.deploy(@ArrayTrait::new()).unwrap();
-            let dispatcher = IHelloStarknetDispatcher { contract_address };
-
-            let balance = dispatcher.get_balance();
-            dispatcher.increase_balance(100);
-            let balance = dispatcher.get_balance();
-            dispatcher.decrease_balance(100);
-            let balance = dispatcher.get_balance();
-
-            start_prank(contract_address, 1234.try_into().unwrap());
-            start_roll(contract_address, 234);
-            start_warp(contract_address, 123);
-
-            let (x, y, z) = dispatcher.interact_with_state();
-        }
-        "#
-        ),
-        Contract::new(
-            "HelloStarknet",
-            indoc!(
-                r#"
-                #[starknet::contract]
-                mod HelloStarknet {
-                    use box::BoxTrait;
-                    use starknet::ContractAddressIntoFelt252;
-                    use starknet::ContractAddress;
-                    use integer::U64IntoFelt252;
-                    use option::Option;
-                    use traits::Into;
-
-                    #[storage]
-                    struct Storage {
-                        balance: felt252,
-                    }
-
-                    // Increases the balance by the given amount.
-                    #[external(v0)]
-                    fn increase_balance(ref self: ContractState, amount: felt252) {
-                        self.balance.write(self.balance.read() + amount);
-                    }
-
-                    // Decreases the balance by the given amount.
-                    #[external(v0)]
-                    fn decrease_balance(ref self: ContractState, amount: felt252) {
-                        self.balance.write(self.balance.read() - amount);
-                    }
-
-                    //Get the balance.
-                    #[external(v0)]
-                    fn get_balance(self: @ContractState) -> felt252 {
-                        self.balance.read()
-                    }
-
-                    #[external(v0)]
-                    fn interact_with_state(self: @ContractState) -> (felt252, felt252, felt252) {
-                        let caller_address: felt252 = starknet::get_caller_address().into();
-                        let block_number = starknet::get_block_info().unbox().block_number;
-                        let block_timestamp = starknet::get_block_info().unbox().block_timestamp;
-
-                        (caller_address, block_number.into(), block_timestamp.into())
-                    }
-                }
-                "#
-            )
+        &fs::read_to_string("tests/data/contracts/benchmarks/test_declare_deploy_interact.cairo")
+            .unwrap(),
+        Contract::from_code_path(
+            "HelloStarknet".to_string(),
+            Path::new("tests/data/contracts/benchmarks/declare_deploy_interact.cairo")
         )
+        .unwrap()
     )
 }
 
