812c2e40b8870b3f73eb3d123561ceec60b1f0ab
Wojciech Szymczyk <wojciech.szymczyk@swmansion.com>
2023-11-06 13:57:06 +0100

    Build devnet instead of installing it directly from git

--- scripts/install_devnet.sh
+++ scripts/install_devnet.sh
@@ -5,7 +5,13 @@ DEVNET_INSTALL_DIR="$(git rev-parse --show-toplevel)/crates/cast/tests/utils/dev
 DEVNET_REPO="https://github.com/0xSpaceShard/starknet-devnet-rs.git"
 DEVNET_REV="72c11f6"
 
-cargo install --git "$DEVNET_REPO" --rev "$DEVNET_REV" --root "$DEVNET_INSTALL_DIR"
+git clone "$DEVNET_REPO" "$DEVNET_INSTALL_DIR/$DEVNET_REV" || echo "Repo already checked out!"
+pushd "$DEVNET_INSTALL_DIR/$DEVNET_REV"
+cargo build --release
+popd
+
+mkdir -p "$DEVNET_INSTALL_DIR/bin/"
+cp "$DEVNET_INSTALL_DIR/$DEVNET_REV/target/release/starknet-devnet" "$DEVNET_INSTALL_DIR/bin/."
 
 echo "All done!"
 exit 0
