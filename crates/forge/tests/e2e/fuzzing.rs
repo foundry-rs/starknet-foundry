d74fa31d90a08a488d07d3ce88d74cf2ef424ed7
Tomasz Kumor <tomasz.kumor@swmansion.com>
2023-11-06 09:03:46 +0100

    fix tests

--- crates/forge/tests/e2e/fuzzing.rs
+++ crates/forge/tests/e2e/fuzzing.rs
@@ -200,7 +200,6 @@ fn fuzzing_exit_first() {
 
         Failures:
             tests::exit_first_fuzz::exit_first_fails_test
-
         "#}
     );
 }
@@ -230,8 +229,6 @@ fn fuzzing_exit_first_single_fail() {
 
         Failures:
             tests::exit_first_single_fail::exit_first_fails_test
-
-        Tests: 0 passed, 1 failed, 1 skipped
         "#}
     );
 }
