48416221c5e07ac4f18b4bcc4dcc72e436968fb0
Maksim Zdobnikau <maksim.zdobnikau@swmansion.com>
2023-11-13 23:51:31 +0100

    Add current_dir arg to runner

--- crates/cast/tests/helpers/runner.rs
+++ crates/cast/tests/helpers/runner.rs
@@ -1,7 +1,12 @@
 use snapbox::cmd::{cargo_bin, Command};
+use std::path::Path;
 
 #[must_use]
-pub fn runner(args: &[&str]) -> Command {
+pub fn runner(args: &[&str], current_dir: Option<&Path>) -> Command {
     let command = Command::new(cargo_bin!("sncast")).args(args);
+    let command = match current_dir {
+        Some(dir) => command.current_dir(dir),
+        None => command,
+    };
     command
 }
