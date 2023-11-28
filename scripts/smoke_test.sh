9f68ee0ed459fa0db8dd120592a8cf5097a4629a
Artur Michalek <artur.michalek@swmansion.com>
2023-11-28 10:05:31 +0100

    Store RPC url in secret

--- scripts/smoke_test.sh
+++ scripts/smoke_test.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-SNFORGE_PATH="$1"
-SNCAST_PATH="$2"
-REPO_URL="$3"
-REVISION="$4"
+RPC_URL="$1"
+SNFORGE_PATH="$2"
+SNCAST_PATH="$3"
+REPO_URL="$4"
+REVISION="$5"
 
 # Check forge
 
@@ -16,7 +17,7 @@ popd || exit
 
 # Check cast
 
-OUTPUT=$($SNCAST_PATH --url http://188.34.188.184:9545/rpc/v0.4 call --contract-address 0x071c8d74edc89330f314f3b1109059d68ebfa68874aa91e9c425a6378ffde00e --function "get_balance")
+OUTPUT=$($SNCAST_PATH --url "$RPC_URL":9545/rpc/v0.4 call --contract-address 0x071c8d74edc89330f314f3b1109059d68ebfa68874aa91e9c425a6378ffde00e --function "get_balance")
 
 EXPECTED=$'command: call\nresponse: [0x2]'
 
