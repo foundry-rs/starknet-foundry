205048fcbb75bba7b2f67c3d8a855c06e32155d5
drknzz <kamil.jankowski.x@gmail.com>
2023-12-04 07:33:49 +0100

    read_felts -> read_vec

--- crates/cheatnet/src/runtime_extensions/forge_runtime_extension/mod.rs
+++ crates/cheatnet/src/runtime_extensions/forge_runtime_extension/mod.rs
@@ -627,20 +627,16 @@ fn read_felt(buffer: &[Felt252], idx: &mut usize) -> Felt252 {
     buffer[*idx - 1].clone()
 }
 
-fn read_felts(buffer: &[Felt252], idx: &mut usize, count: usize) -> Vec<Felt252> {
+fn read_vec(buffer: &[Felt252], idx: &mut usize, count: usize) -> Vec<Felt252> {
     *idx += count;
     buffer[*idx - count..*idx].to_vec()
 }
 
-fn read_option(buffer: &[Felt252], idx: &mut usize) -> bool {
-    *idx += 1;
-    !buffer[*idx - 1].is_one()
-}
-
 fn read_option_felt(buffer: &[Felt252], idx: &mut usize) -> Option<Felt252> {
-    read_option(buffer, idx).then(|| read_felt(buffer, idx))
+    *idx += 1;
+    (!buffer[*idx - 1].is_one()).then(|| read_felt(buffer, idx))
 }
 
 fn read_option_vec(buffer: &[Felt252], idx: &mut usize) -> Option<Vec<Felt252>> {
-    read_option_felt(buffer, idx).map(|count| read_felts(buffer, idx, count.to_usize().unwrap()))
+    read_option_felt(buffer, idx).map(|count| read_vec(buffer, idx, count.to_usize().unwrap()))
 }
