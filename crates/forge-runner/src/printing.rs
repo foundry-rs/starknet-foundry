41f0cdbcd3dc3a079f7f64a13f249f3647761b22
Maksymilian Demitraszek <maksymilian.demitraszek@protonmail.com>
2023-12-11 10:22:44 +0100

    Cleanup printing

--- crates/forge-runner/src/printing.rs
+++ crates/forge-runner/src/printing.rs
@@ -1,4 +1,4 @@
-use crate::{test_case_summary::TestCaseSummary, test_crate_summary::AnyTestCaseSummary};
+use crate::{test_case_summary::TestCaseSummary, test_crate_summary::AnyTestCaseSummary, fuzzer};
 use console::style;
 
 fn result_header(any_test_result: &AnyTestCaseSummary) -> String {
@@ -17,36 +17,37 @@ fn result_header(any_test_result: &AnyTestCaseSummary) -> String {
 pub(crate) fn print_test_result(any_test_result: &AnyTestCaseSummary) {
     let result_header = result_header(any_test_result);
     let result_name = any_test_result.name().unwrap();
-    let result_msg = any_test_result.msg().ok_or(Some(String::new())).unwrap();
-    let mut fuzzer_report = String::new();
+    let result_msg = any_test_result.msg().unwrap_or(Some(String::new())).unwrap();
+    let mut fuzzer_report = None;
     if let AnyTestCaseSummary::Fuzzing(test_result) = any_test_result {
         if let Some(runs) = test_result.runs() {
             fuzzer_report = {
                 if matches!(test_result, TestCaseSummary::Failed { .. }) {
                     let arguments = test_result.arguments();
-                    format!(" (fuzzer runs = {runs}, arguments = {arguments:?})")
+                    Some(format!(" (fuzzer runs = {runs}, arguments = {arguments:?})"))
                 } else {
-                    format!(" (fuzzer runs = {runs})")
+                    Some(format!(" (fuzzer runs = {runs})"))
                 }
             }
         };
     }
+    let fuzzer_report = fuzzer_report.unwrap_or(String::new());
+
     let block_number_message = match any_test_result.latest_block_number() {
         None => String::new(),
         Some(latest_block_number) => {
             format!("\nNumber of the block used for fork testing = {latest_block_number}")
         }
     };
-    let mut gas_usage = String::new();
-    match any_test_result {
+    let gas_usage = match any_test_result {
         AnyTestCaseSummary::Fuzzing(TestCaseSummary::Passed { gas_info, .. }) => {
-            gas_usage = format!("(max: ~{}, min: ~{})", gas_info.max, gas_info.min);
-        }
+            format!("(max: ~{}, min: ~{})", gas_info.max, gas_info.min)
+        },
         AnyTestCaseSummary::Single(TestCaseSummary::Passed { gas_info, .. }) => {
-            gas_usage = format!(", gas: {gas_info}");
-        }
-        _ => {}
-    }
+            format!(", gas: {gas_info}")
+        },
+        _ => String::new()
+    };
     println!(
         "{result_header} {result_name}{fuzzer_report}{gas_usage}{block_number_message}{result_msg}"
     );
