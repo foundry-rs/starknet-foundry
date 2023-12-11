4a1b582f2ce515889343a648a68b5f13dd754dd6
Tomasz Kumor <tomasz.kumor@swmansion.com>
2023-12-11 09:19:36 +0100

    update

--- scripts/install_devnet.sh
+++ scripts/install_devnet.sh
@@ -3,7 +3,7 @@ set -e
 
 DEVNET_INSTALL_DIR="$(git rev-parse --show-toplevel)/crates/cast/tests/utils/devnet"
 DEVNET_REPO="https://github.com/0xSpaceShard/starknet-devnet-rs.git"
-DEVNET_REV="72c11f6"
+DEVNET_REV="78527de"
 
 cargo install --git "$DEVNET_REPO" --rev "$DEVNET_REV" --root "$DEVNET_INSTALL_DIR"
 
