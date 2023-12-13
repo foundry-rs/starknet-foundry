d0f800355d7f8ee7818d6ed3511192acf10e59a6
Oleksii Bulenok <oleksii.bulenok@swmansion.com>
2023-12-13 13:42:01 +0100

    Add test for scripts logging with json flag

--- crates/sncast/tests/e2e/script/logging.rs
+++ crates/sncast/tests/e2e/script/logging.rs
@@ -24,7 +24,7 @@ async fn test_default_verbosity() {
         "--accounts-file",
         accounts_json_path.as_str(),
         "--account",
-        "user3",
+        "user2",
         "--url",
         URL,
         "script",
@@ -69,9 +69,82 @@ async fn test_default_verbosity() {
     "});
 }
 
+
+#[tokio::test]
+async fn test_default_verbosity_with_json() {
+    let current_dir = duplicate_map_script("14");
+    let accounts_json_path = get_accounts_path("tests/data/accounts/accounts.json");
+
+    let script_name = "map_script_all_cheatcodes";
+    let args = vec![
+        "--accounts-file",
+        accounts_json_path.as_str(),
+        "--account",
+        "user3",
+        "--url",
+        URL,
+        "--json",
+        "script",
+        &script_name,
+    ];
+
+    let snapbox = Command::new(cargo_bin!("sncast"))
+        .current_dir(current_dir.path().join("scripts"))
+        .args(args);
+
+    snapbox.assert().success().stdout_matches(indoc! {r#"
+        {"status":"compiling","message":"lib(map_script) map_script v0.1.0 ([..])"}
+        {"status":"compiling","message":"starknet-contract(map_script) map_script v0.1.0 ([..])"}
+        {"status":"finished","message":"release target(s) in [..] seconds"}
+
+
+        {
+          "cheatcode": "get_nonce",
+          "response": [..]
+        }
+
+        {
+          "cheatcode": "declare",
+          "class_hash": "0x[..]",
+          "transaction_hash": "0x[..]"
+        }
+
+        {
+          "cheatcode": "get_nonce",
+          "response": [..]
+        }
+
+        {
+          "cheatcode": "deploy",
+          "contract_address": "0x[..]",
+          "transaction_hash": "0x[..]"
+        }
+
+        {
+          "cheatcode": "get_nonce",
+          "response": [..]
+        }
+
+        {
+          "cheatcode": "invoke",
+          "transaction_hash": "0x[..]"
+        }
+
+        {
+          "cheatcode": "call",
+          "response": "[0x2]"
+        }
+
+        {
+          "command": "script",
+          "status": "success"
+        }
+    "#});
+}
+
 #[tokio::test]
 async fn test_quiet() {
-    let current_dir = duplicate_map_script("14");
+    let current_dir = duplicate_map_script("15");
     let accounts_json_path = get_accounts_path("tests/data/accounts/accounts.json");
 
     let script_name = "map_script_all_cheatcodes";
@@ -99,7 +172,7 @@ async fn test_quiet() {
 
 #[tokio::test]
 async fn test_one_of_the_steps_failing() {
-    let current_dir = duplicate_map_script("15");
+    let current_dir = duplicate_map_script("16");
     let accounts_json_path = get_accounts_path("tests/data/accounts/accounts.json");
 
     let script_name = "map_script_failing_step";
