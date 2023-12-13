1ce058f4fce297afc8cbb225856ad2e18a21bbaf
war-in <marcin.warchol20@gmail.com>
2023-12-13 17:31:22 +0100

    generalize compile function

--- crates/sierra-casm/src/lib.rs
+++ crates/sierra-casm/src/lib.rs
@@ -1,12 +1,9 @@
-use cairo_lang_utils::bigint::BigUintAsHex;
-use num_bigint::BigUint;
 use serde::de::DeserializeOwned;
 use serde::Serialize;
-use serde_json::Error;
-use starknet::core::types::FlattenedSierraClass;
+use serde_json::Value;
 
 use cairo_lang_starknet::casm_contract_class::CasmContractClass;
-use cairo_lang_starknet::contract_class::{ContractClass, ContractEntryPoints};
+use cairo_lang_starknet::contract_class::ContractClass;
 use cairo_lang_starknet_v1_0_0_alpha6::casm_contract_class::CasmContractClass as CasmContractClassV1Alpha;
 use cairo_lang_starknet_v1_0_0_alpha6::contract_class::ContractClass as ContractClassV1Alpha;
 use cairo_lang_starknet_v1_0_0_rc0::casm_contract_class::CasmContractClass as CasmContractClassV1Rc0;
@@ -14,13 +11,18 @@ use cairo_lang_starknet_v1_0_0_rc0::contract_class::ContractClass as ContractCla
 use cairo_lang_starknet_v1_1_1::casm_contract_class::CasmContractClass as CasmContractClassV1_1;
 use cairo_lang_starknet_v1_1_1::contract_class::ContractClass as ContractClassV1_1;
 
-pub fn compile(definition: &FlattenedSierraClass) -> anyhow::Result<CasmContractClass> {
+/// `sierra_json` should be a json containing `sierra_program` and `entry_points_by_type`
+pub fn compile(mut sierra_json: Value) -> Result<CasmContractClass, String> {
+    sierra_json["abi"] = Value::Null;
+    sierra_json["sierra_program_debug_info"] = Value::Null;
+    sierra_json["contract_class_version"] = Value::String(String::new());
+
     macro_rules! compile_contract {
         ($sierra_type:ty, $casm_type:ty) => {{
-            let sierra_class = flattened_sierra_to_contract_class::<$sierra_type>(definition)?;
-            let maybe_casm_class = <$casm_type>::from_contract_class(sierra_class, true);
-            if let Ok(casm_class) = maybe_casm_class {
-                return old_casm_to_newest_casm::<$casm_type>(&casm_class);
+            if let Ok(sierra_class) = serde_json::from_value::<$sierra_type>(sierra_json.clone()) {
+                if let Ok(casm_class) = <$casm_type>::from_contract_class(sierra_class, true) {
+                    return Ok(old_casm_to_newest_casm::<$casm_type>(&casm_class));
+                }
             }
         }};
     }
@@ -30,36 +32,15 @@ pub fn compile(definition: &FlattenedSierraClass) -> anyhow::Result<CasmContract
     compile_contract!(ContractClassV1Rc0, CasmContractClassV1Rc0);
     compile_contract!(ContractClassV1Alpha, CasmContractClassV1Alpha);
 
-    unreachable!()
-}
-
-fn flattened_sierra_to_contract_class<T: DeserializeOwned>(
-    value: &FlattenedSierraClass,
-) -> Result<T, Error> {
-    let converted_sierra_program: Vec<BigUintAsHex> = value
-        .sierra_program
-        .iter()
-        .map(|field_element| BigUintAsHex {
-            value: BigUint::from_bytes_be(&field_element.to_bytes_be()),
-        })
-        .collect();
-    let converted_entry_points: ContractEntryPoints =
-        serde_json::from_str(&serde_json::to_string(&value.entry_points_by_type)?)?;
-
-    let json = serde_json::json!({
-        "sierra_program": converted_sierra_program,
-        "contract_class_version": value.contract_class_version,
-        "entry_points_by_type": converted_entry_points,
-    });
-    serde_json::from_value::<T>(json)
+    Err("Unable to compile Sierra to Casm".to_string())
 }
 
 /// converts `CasmContractClass` from the old `cairo_lang_starknet` library version
 /// to the `CasmContractClass` from the newest version
-fn old_casm_to_newest_casm<T>(value: &T) -> anyhow::Result<CasmContractClass>
+fn old_casm_to_newest_casm<T>(value: &T) -> CasmContractClass
 where
     T: Serialize + DeserializeOwned,
 {
-    let serialized = serde_json::to_value(value)?;
-    Ok(serde_json::from_value::<CasmContractClass>(serialized)?)
+    let serialized = serde_json::to_value(value).unwrap();
+    serde_json::from_value::<CasmContractClass>(serialized).unwrap()
 }
--- crates/sierra-casm/tests/compilation.rs
+++ crates/sierra-casm/tests/compilation.rs
@@ -1,44 +1,50 @@
+use serde_json::Value;
 use sierra_casm::compile;
-use starknet::core::types::FlattenedSierraClass;
 use std::fs::File;
-use std::io::Read;
 
 #[test]
 fn compile_alpha6() {
-    let flattened_sierra = read_flattened_sierra("tests/data/compilable_by_alpha6.json");
+    let sierra_json = read_flattened_sierra("tests/data/compilable_by_alpha6.json");
 
-    let casm_class = compile(&flattened_sierra);
+    let casm_class = compile(sierra_json);
     assert!(casm_class.is_ok());
 }
 
 #[test]
 fn compile_rc0() {
-    let flattened_sierra = read_flattened_sierra("tests/data/compilable_by_rc0.json");
+    let sierra_json = read_flattened_sierra("tests/data/compilable_by_rc0.json");
 
-    let casm_class = compile(&flattened_sierra);
+    let casm_class = compile(sierra_json);
     assert!(casm_class.is_ok());
 }
 
 #[test]
 fn compile_1_1_1() {
-    let flattened_sierra = read_flattened_sierra("tests/data/compilable_by_1_1_1.json");
+    let sierra_json = read_flattened_sierra("tests/data/compilable_by_1_1_1.json");
 
-    let casm_class = compile(&flattened_sierra);
+    let casm_class = compile(sierra_json);
     assert!(casm_class.is_ok());
 }
 
 #[test]
 fn compile_latest() {
-    let flattened_sierra = read_flattened_sierra("tests/data/compilable_by_latest.json");
+    let sierra_json = read_flattened_sierra("tests/data/compilable_by_latest.json");
 
-    let casm_class = compile(&flattened_sierra);
+    let casm_class = compile(sierra_json);
     assert!(casm_class.is_ok());
 }
 
-fn read_flattened_sierra(path: &str) -> FlattenedSierraClass {
-    let mut file = File::open(path).unwrap();
-    let mut serialized_data = String::new();
-    file.read_to_string(&mut serialized_data).unwrap();
+#[test]
+fn wrong_json() {
+    let sierra_json = serde_json::json!({
+        "wrong": "data"
+    });
 
-    serde_json::from_str::<FlattenedSierraClass>(&serialized_data).unwrap()
+    let casm_class = compile(sierra_json);
+    assert!(casm_class.is_err());
+}
+
+fn read_flattened_sierra(path: &str) -> Value {
+    let file = File::open(path).unwrap();
+    serde_json::from_reader(file).unwrap()
 }
