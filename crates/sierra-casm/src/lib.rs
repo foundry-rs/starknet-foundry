9aedd08c5c13375a462f5d6e16b1d90b09dab3e7
war-in <marcin.warchol20@gmail.com>
2023-12-15 16:17:07 +0100

    simplify sierra version parsing

--- crates/sierra-casm/src/lib.rs
+++ crates/sierra-casm/src/lib.rs
@@ -8,8 +8,6 @@ use cairo_lang_starknet_sierra_0_1_0::casm_contract_class::CasmContractClass as 
 use cairo_lang_starknet_sierra_0_1_0::contract_class::ContractClass as ContractClassSierraV0;
 use cairo_lang_starknet_sierra_1_0_0::casm_contract_class::CasmContractClass as CasmContractClassSierraV1;
 use cairo_lang_starknet_sierra_1_0_0::contract_class::ContractClass as ContractClassSierraV1;
-use cairo_lang_starknet_sierra_1_1_0::casm_contract_class::CasmContractClass as CasmContractClassSierraV1_1;
-use cairo_lang_starknet_sierra_1_1_0::contract_class::ContractClass as ContractClassSierraV1_1;
 
 /// `sierra_json` should be a json containing `sierra_program` and `entry_points_by_type`
 pub fn compile(mut sierra_json: Value) -> Result<CasmContractClass, String> {
@@ -25,16 +23,11 @@ pub fn compile(mut sierra_json: Value) -> Result<CasmContractClass, String> {
         }};
     }
 
-    let major = parse_sierra_version(1, &sierra_json)?;
-    if major.as_str() == "0" {
-        compile_contract!(ContractClassSierraV0, CasmContractClassSierraV0)
-    }
-
-    let sierra_version = parse_sierra_version(3, &sierra_json)?;
-    match sierra_version.as_str() {
-        "1.4.0" | "1.3.0" | "1.2.0" => compile_contract!(ContractClass, CasmContractClass),
-        "1.1.0" => compile_contract!(ContractClassSierraV1, CasmContractClassSierraV1),
-        "1.0.0" => compile_contract!(ContractClassSierraV1_1, CasmContractClassSierraV1_1),
+    let sierra_version = parse_sierra_version(&sierra_json)?;
+    match sierra_version.as_slice() {
+        [1, 2..=4, 0] => compile_contract!(ContractClass, CasmContractClass),
+        [1, 0..=1, 0] => compile_contract!(ContractClassSierraV1, CasmContractClassSierraV1),
+        [0, ..] => compile_contract!(ContractClassSierraV0, CasmContractClassSierraV0),
         _ => {}
     };
 
@@ -54,18 +47,14 @@ where
     serde_json::from_value::<CasmContractClass>(serialized).unwrap()
 }
 
-fn parse_sierra_version(slice_length: usize, sierra_json: &Value) -> Result<String, String> {
-    let parsed_values: Vec<String> = sierra_json["sierra_program"]
+fn parse_sierra_version(sierra_json: &Value) -> Result<Vec<u8>, String> {
+    let parsed_values: Vec<u8> = sierra_json["sierra_program"]
         .as_array()
         .ok_or("Unable to read sierra_program. Make sure it is an array of felts")?
         .iter()
-        .take(slice_length)
-        .map(|x| {
-            u8::from_str_radix(&x.as_str().unwrap()[2..], 16)
-                .unwrap_or_default()
-                .to_string()
-        })
+        .take(3)
+        .map(|x| u8::from_str_radix(&x.as_str().unwrap()[2..], 16).unwrap_or_default())
         .collect();
 
-    Ok(parsed_values.join("."))
+    Ok(parsed_values)
 }
