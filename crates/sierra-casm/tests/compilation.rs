3578930a62e1a5de242883216b9589006693e170
war-in <marcin.warchol20@gmail.com>
2023-12-15 14:15:28 +0100

    update compilation tests

--- crates/sierra-casm/tests/compilation.rs
+++ crates/sierra-casm/tests/compilation.rs
@@ -1,50 +1,29 @@
-use serde_json::Value;
-use sierra_casm::compile;
-use std::fs::File;
+#[cfg(test)]
+mod tests {
+    use sierra_casm::compile;
+    use std::fs::File;
+    use test_case::test_case;
 
-#[test]
-fn compile_alpha6() {
-    let sierra_json = read_flattened_sierra("tests/data/compilable_by_alpha6.json");
+    #[test_case("1_4_0"; "sierra 1.4.0")]
+    #[test_case("1_3_0"; "sierra 1.3.0")]
+    #[test_case("1_1_0"; "sierra 1.1.0")]
+    #[test_case("1_0_0"; "sierra 1.0.0")]
+    #[test_case("0_1_0"; "sierra 0.1.0")]
+    fn compile_sierra(sierra_version: &str) {
+        let file = File::open("tests/data/sierra_".to_string() + sierra_version + ".json").unwrap();
+        let sierra_json = serde_json::from_reader(file).unwrap();
 
-    let casm_class = compile(sierra_json);
-    assert!(casm_class.is_ok());
-}
+        let casm_class = compile(sierra_json);
+        assert!(casm_class.is_ok());
+    }
 
-#[test]
-fn compile_rc0() {
-    let sierra_json = read_flattened_sierra("tests/data/compilable_by_rc0.json");
+    #[test]
+    fn wrong_json() {
+        let sierra_json = serde_json::json!({
+            "wrong": "data"
+        });
 
-    let casm_class = compile(sierra_json);
-    assert!(casm_class.is_ok());
-}
-
-#[test]
-fn compile_1_1_1() {
-    let sierra_json = read_flattened_sierra("tests/data/compilable_by_1_1_1.json");
-
-    let casm_class = compile(sierra_json);
-    assert!(casm_class.is_ok());
-}
-
-#[test]
-fn compile_latest() {
-    let sierra_json = read_flattened_sierra("tests/data/compilable_by_latest.json");
-
-    let casm_class = compile(sierra_json);
-    assert!(casm_class.is_ok());
-}
-
-#[test]
-fn wrong_json() {
-    let sierra_json = serde_json::json!({
-        "wrong": "data"
-    });
-
-    let casm_class = compile(sierra_json);
-    assert!(casm_class.is_err());
-}
-
-fn read_flattened_sierra(path: &str) -> Value {
-    let file = File::open(path).unwrap();
-    serde_json::from_reader(file).unwrap()
+        let casm_class = compile(sierra_json);
+        assert!(casm_class.is_err());
+    }
 }
