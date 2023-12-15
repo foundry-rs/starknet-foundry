61907fac42d890e1969f1f41d5d5f59c72947973
war-in <marcin.warchol20@gmail.com>
2023-12-15 16:21:48 +0100

    add sierra 1.2.0 test case

--- crates/sierra-casm/tests/compilation.rs
+++ crates/sierra-casm/tests/compilation.rs
@@ -6,6 +6,7 @@ mod tests {
 
     #[test_case("1_4_0"; "sierra 1.4.0")]
     #[test_case("1_3_0"; "sierra 1.3.0")]
+    #[test_case("1_2_0"; "sierra 1.2.0")]
     #[test_case("1_1_0"; "sierra 1.1.0")]
     #[test_case("1_0_0"; "sierra 1.0.0")]
     #[test_case("0_1_0"; "sierra 0.1.0")]
