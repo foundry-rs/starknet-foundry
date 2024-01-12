dd4e414f8282e589f204274f348871f8342affbc
war-in <marcin.warchol20@gmail.com>
2024-01-12 10:20:04 +0100

    calculate initial gas

--- crates/forge-runner/src/running.rs
+++ crates/forge-runner/src/running.rs
@@ -205,10 +205,13 @@ pub fn run_test_case(
     );
 
     let func = runner.find_function(case.name.as_str()).unwrap();
+    let initial_gas = runner
+        .get_initial_available_gas(func, case.available_gas)
+        .unwrap();
     let runner_args: Vec<Arg> = args.into_iter().map(Arg::Value).collect();
 
     let (entry_code, builtins) = runner
-        .create_entry_code(func, &runner_args, usize::MAX)
+        .create_entry_code(func, &runner_args, initial_gas)
         .unwrap();
     let footer = SierraCasmRunner::create_code_footer();
     let instructions = chain!(
