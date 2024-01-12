070bae0789eec5cabebb979749ed10fbd80be1f8
Tomasz Kumor <tomasz.kumor@swmansion.com>
2024-01-12 08:37:15 +0100

    fmt

--- crates/sncast/tests/e2e/main_tests.rs
+++ crates/sncast/tests/e2e/main_tests.rs
@@ -270,12 +270,7 @@ async fn test_keystore_declare() {
         .current_dir(contract_path.path())
         .args(args);
 
-    assert!(snapbox
-        .assert()
-        .success()
-        .get_output()
-        .stderr
-        .is_empty());
+    assert!(snapbox.assert().success().get_output().stderr.is_empty());
 
     fs::remove_dir_all(contract_path).unwrap();
 }
