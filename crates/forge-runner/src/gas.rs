cb28a880c5664f81b3a3be767af7817c98deb3fa
war-in <marcin.warchol20@gmail.com>
2024-01-26 13:22:06 +0100

    adjust gas

--- crates/forge-runner/src/gas.rs
+++ crates/forge-runner/src/gas.rs
@@ -1,9 +1,8 @@
 use std::collections::HashMap;
 
 use crate::test_case_summary::{Single, TestCaseSummary};
-use blockifier::fee::eth_gas_constants;
 use blockifier::fee::fee_utils::calculate_tx_l1_gas_usage;
-use blockifier::fee::gas_usage::get_message_segment_length;
+use blockifier::fee::gas_usage::calculate_tx_gas_usage;
 use blockifier::fee::os_resources::OS_RESOURCES;
 use blockifier::fee::os_usage::get_additional_os_resources;
 use blockifier::state::cached_state::{CachedState, StateChangesCount};
@@ -33,12 +32,14 @@ pub fn calculate_used_gas(
     // which we don't want to include in gas cost
     state_changes.compiled_class_hash_updates.clear();
 
-    let l1_gas_usage = get_l1_gas_usage(
+    let l1_gas_usage = calculate_tx_gas_usage(
         &resources.l2_to_l1_payloads_length,
         StateChangesCount::from(&state_changes),
+        None,
     );
 
     let resource_mapping = used_resources_to_resource_mapping(&total_vm_usage, l1_gas_usage);
+
     calculate_tx_l1_gas_usage(&resource_mapping, block_context)
         .expect("Calculating gas failed, some resources were not included.")
 }
@@ -75,35 +76,6 @@ fn get_total_vm_usage(resources: &ExecutionResources) -> VmExecutionResources {
         .unwrap()
             - unnecessary_added_resources);
     total_vm_usage.filter_unused_builtins()
-}
-
-fn get_l1_gas_usage(
-    l2_to_l1_payloads_length: &[usize],
-    state_changes_count: StateChangesCount,
-) -> usize {
-    let message_segment_length = get_message_segment_length(l2_to_l1_payloads_length, None);
-    let onchain_data_segment_length = get_onchain_data_segment_length(state_changes_count);
-
-    message_segment_length * eth_gas_constants::SHARP_GAS_PER_MEMORY_WORD
-        + onchain_data_segment_length * eth_gas_constants::SHARP_GAS_PER_MEMORY_WORD
-}
-
-// TODO::copied from blockifier, because it became private
-fn get_onchain_data_segment_length(state_changes_count: StateChangesCount) -> usize {
-    // For each newly modified contract:
-    // contract address (1 word).
-    // + 1 word with the following info: A flag indicating whether the class hash was updated, the
-    // number of entry updates, and the new nonce.
-    let mut onchain_data_segment_length = state_changes_count.n_modified_contracts * 2;
-    // For each class updated (through a deploy or a class replacement).
-    onchain_data_segment_length +=
-        state_changes_count.n_class_hash_updates * constants::CLASS_UPDATE_SIZE;
-    // For each modified storage cell: key, new value.
-    onchain_data_segment_length += state_changes_count.n_storage_updates * 2;
-    // For each compiled class updated (through declare): class_hash, compiled_class_hash
-    onchain_data_segment_length += state_changes_count.n_compiled_class_hash_updates * 2;
-
-    onchain_data_segment_length
 }
 
 pub fn check_available_gas(
