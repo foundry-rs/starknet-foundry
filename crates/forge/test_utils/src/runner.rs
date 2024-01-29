a943178b8a4daa4bf42ef31f46933ccfdc323388
drknzz <kamil.jankowski.x@gmail.com>
2024-01-29 18:11:43 +0100

    Add tests

--- crates/forge/test_utils/src/runner.rs
+++ crates/forge/test_utils/src/runner.rs
@@ -323,7 +323,7 @@ macro_rules! assert_syscalls {
         assert!(result.test_case_summaries.iter().any(|any_case| {
             match any_case {
                 AnyTestCaseSummary::Fuzzing(_) => {
-                    panic!("Cannot use assert_syscall! for fuzzing tests")
+                    panic!("Cannot use assert_syscalls! for fuzzing tests")
                 }
                 AnyTestCaseSummary::Single(case) => match case {
                     TestCaseSummary::Passed { used_resources, .. } => {
@@ -331,6 +331,43 @@ macro_rules! assert_syscalls {
                             .execution_resources
                             .syscall_counter
                             .get(&$syscall)
+                            .unwrap_or(&0)
+                            == &$expected_count
+                            && any_case
+                                .name()
+                                .unwrap()
+                                .ends_with(test_name_suffix.as_str())
+                    }
+                    _ => false,
+                },
+            }
+        }));
+    }};
+}
+
+#[macro_export]
+macro_rules! assert_builtins {
+    ($result:expr, $test_case_name:expr, $builtin:expr, $expected_count:expr) => {{
+        use forge_runner::test_case_summary::{AnyTestCaseSummary, TestCaseSummary};
+        use $crate::runner::TestCase;
+
+        let test_case_name = $test_case_name;
+        let test_name_suffix = format!("::{test_case_name}");
+
+        let result = TestCase::find_test_result(&$result);
+
+        assert!(result.test_case_summaries.iter().any(|any_case| {
+            match any_case {
+                AnyTestCaseSummary::Fuzzing(_) => {
+                    panic!("Cannot use assert_builtins! for fuzzing tests")
+                }
+                AnyTestCaseSummary::Single(case) => match case {
+                    TestCaseSummary::Passed { used_resources, .. } => {
+                        used_resources
+                            .execution_resources
+                            .vm_resources
+                            .builtin_instance_counter
+                            .get($builtin)
                             .unwrap_or(&0)
                             == &$expected_count
                             && any_case
--- crates/forge/tests/integration/resources.rs
+++ crates/forge/tests/integration/resources.rs
@@ -1,12 +1,155 @@
-use blockifier::execution::deprecated_syscalls::DeprecatedSyscallSelector::StorageWrite;
+use blockifier::execution::deprecated_syscalls::DeprecatedSyscallSelector::{
+    Deploy, EmitEvent, GetBlockHash, GetExecutionInfo, Keccak, SendMessageToL1, StorageRead,
+    StorageWrite,
+};
 use indoc::indoc;
 use std::path::Path;
 use test_utils::runner::Contract;
 use test_utils::running_tests::run_test_case;
-use test_utils::{assert_passed, assert_syscalls, test_case};
+use test_utils::{assert_builtins, assert_passed, assert_syscalls, test_case};
 
 #[test]
-fn syscall_count() {
+fn builtins_count() {
+    let test = test_case!(indoc!(
+        r"
+            #[test]
+            fn range_check() {
+                assert(1_u8 >= 1_u8, 'error message');
+            }
+
+            #[test]
+            fn bitwise() {
+                let _bitwise = 1_u8 & 1_u8;
+                assert(1 == 1, 'error message');
+            }
+
+            #[test]
+            fn pedersen() {
+                core::pedersen::pedersen(1, 2);
+                assert(1 == 1, 'error message');
+            }
+
+            #[test]
+            fn poseidon() {
+                core::poseidon::hades_permutation(0, 0, 0);
+                assert(1 == 1, 'error message');
+            }
+
+            #[test]
+            fn ec_op() {
+                let ec_point = core::ec::EcPointTrait::new_from_x(1).unwrap();
+                core::ec::EcPointTrait::mul(ec_point, 2);
+                assert(1 == 1, 'error message');
+            }
+        "
+    ));
+
+    let result = run_test_case(&test);
+
+    assert_passed!(result);
+
+    // No ECDSA and Keccak builtins
+    assert_builtins!(result, "range_check", "range_check_builtin", 1);
+    assert_builtins!(result, "bitwise", "bitwise_builtin", 1);
+    assert_builtins!(result, "pedersen", "pedersen_builtin", 1);
+    assert_builtins!(result, "poseidon", "poseidon_builtin", 1);
+    assert_builtins!(result, "ec_op", "ec_op_builtin", 1);
+}
+
+#[test]
+fn syscalls_count() {
+    let test = test_case!(
+        indoc!(
+            r##"
+            use starknet::syscalls::{
+                call_contract_syscall, keccak_syscall, deploy_syscall, get_block_hash_syscall, emit_event_syscall,
+                send_message_to_l1_syscall, get_execution_info_syscall, get_execution_info_v2_syscall,
+                SyscallResult
+            };
+            use starknet::SyscallResultTrait;
+            use snforge_std::{declare, ContractClass, ContractClassTrait};
+            
+            #[test]
+            fn keccak() {
+                let input = array![1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
+                keccak_syscall(input.span()).unwrap_syscall();
+            }
+            
+            #[test]
+            fn deploy() {
+                let contract = declare('HelloStarknet');
+                deploy_syscall(contract.class_hash, 0, array![].span(), false).unwrap_syscall();
+            }
+            
+            #[test]
+            fn storage_read() {
+                let contract = declare('HelloStarknet');
+                let (address, _) = deploy_syscall(contract.class_hash, 0, array![].span(), false)
+                    .unwrap_syscall();
+            
+                call_contract_syscall(address, selector!("get_balance"), array![].span()).unwrap_syscall();
+            }
+            
+            #[test]
+            fn storage_write() {
+                let contract = declare('HelloStarknet');
+                let (address, _) = deploy_syscall(contract.class_hash, 0, array![].span(), false)
+                    .unwrap_syscall();
+            
+                call_contract_syscall(address, selector!("increase_balance"), array![123].span())
+                    .unwrap_syscall();
+            }
+            
+            #[test]
+            fn get_block_hash() {
+                get_block_hash_syscall(1).unwrap_syscall();
+            }
+            
+            #[test]
+            fn get_execution_info() {
+                get_execution_info_syscall().unwrap_syscall();
+            }
+            
+            #[test]
+            fn get_execution_info_v2() {
+                get_execution_info_v2_syscall().unwrap_syscall();
+            }
+            
+            #[test]
+            fn send_message_to_l1() {
+                send_message_to_l1_syscall(1, array![1].span()).unwrap_syscall();
+            }
+            
+            #[test]
+            fn emit_event() {
+                emit_event_syscall(array![1].span(), array![2].span()).unwrap_syscall();
+            }
+        "##
+        ),
+        Contract::from_code_path(
+            "HelloStarknet".to_string(),
+            Path::new("tests/data/contracts/hello_starknet.cairo"),
+        )
+        .unwrap()
+    );
+
+    let result = run_test_case(&test);
+
+    assert_passed!(result);
+
+    assert_syscalls!(result, "keccak", Keccak, 1);
+    assert_syscalls!(result, "deploy", Deploy, 1);
+    assert_syscalls!(result, "storage_read", StorageRead, 1);
+    assert_syscalls!(result, "storage_write", StorageRead, 1);
+    assert_syscalls!(result, "get_block_hash", GetBlockHash, 1);
+    assert_syscalls!(result, "get_execution_info", GetExecutionInfo, 1);
+    assert_syscalls!(result, "get_execution_info_v2", GetExecutionInfo, 1);
+    assert_syscalls!(result, "send_message_to_l1", SendMessageToL1, 1);
+    assert_syscalls!(result, "emit_event", EmitEvent, 1);
+}
+
+#[test]
+fn accumulate_syscalls() {
     let test = test_case!(
         indoc!(
             r"
