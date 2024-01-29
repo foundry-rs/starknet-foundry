2fc8d17577f2f9a772e93cd249b5ae11f9b7d68a
Piotr Figiela <77412592+Draggu@users.noreply.github.com>
2024-01-29 17:07:08 +0100

    include MetadataCommand and scarb_version_command into ScarbCommand

--- crates/cheatnet/tests/common/mod.rs
+++ crates/cheatnet/tests/common/mod.rs
@@ -12,7 +12,8 @@ use cheatnet::runtime_extensions::common::{create_entry_point_selector, create_e
 use cheatnet::runtime_extensions::forge_runtime_extension::cheatcodes::deploy::deploy;
 use cheatnet::state::{BlockifierState, CheatnetState};
 use conversions::felt252::FromShortString;
-use scarb_api::{get_contracts_map, StarknetContractArtifacts};
+use scarb_api::metadata::MetadataCommandExt;
+use scarb_api::{get_contracts_map, ScarbCommand, StarknetContractArtifacts};
 use starknet::core::utils::get_selector_from_name;
 use starknet_api::core::ContractAddress;
 use starknet_api::core::PatriciaKey;
@@ -36,10 +37,10 @@ pub fn recover_data(output: CallOutput) -> Vec<Felt252> {
 }
 
 pub fn get_contracts() -> HashMap<String, StarknetContractArtifacts> {
-    let scarb_metadata = scarb_metadata::MetadataCommand::new()
+    let scarb_metadata = ScarbCommand::metadata()
         .inherit_stderr()
         .manifest_path(Utf8PathBuf::from("tests/contracts/Scarb.toml"))
-        .exec()
+        .run()
         .unwrap();
 
     let package = scarb_metadata.packages.first().unwrap();
--- crates/scarb-api/src/version.rs
+++ crates/scarb-api/src/version.rs
@@ -9,34 +9,39 @@ pub struct ScarbVersionOutput {
     pub cairo: Version,
 }
 
-pub fn scarb_version_command() -> Result<ScarbVersionOutput> {
-    let scarb_version = ScarbCommand::new()
-        .arg("--version")
-        .command()
-        .output()
-        .context("Failed to execute `scarb --version`")?;
+#[derive(Clone, Copy, Debug, Default)]
+pub struct VersionCommand;
 
-    let version_output = from_utf8(&scarb_version.stdout)
-        .context("Failed to parse `scarb --version` output to UTF-8")?;
+impl VersionCommand {
+    pub fn run(self) -> Result<ScarbVersionOutput> {
+        let scarb_version = ScarbCommand::new()
+            .arg("--version")
+            .command()
+            .output()
+            .context("Failed to execute `scarb --version`")?;
 
-    Ok(ScarbVersionOutput {
-        scarb: extract_version(version_output, "scarb")?,
-        cairo: extract_version(version_output, "cairo:")?,
-    })
-}
+        let version_output = from_utf8(&scarb_version.stdout)
+            .context("Failed to parse `scarb --version` output to UTF-8")?;
 
-fn extract_version(version_output: &str, tool: &str) -> Result<Version> {
-    let version_regex = Regex::new(&format!(r#"(?:{tool}?\s*)([0-9]+.[0-9]+.[0-9]+)"#))
-        .context("Could not create version matching regex")?;
+        Ok(ScarbVersionOutput {
+            scarb: Self::extract_version(version_output, "scarb")?,
+            cairo: Self::extract_version(version_output, "cairo:")?,
+        })
+    }
 
-    let version_capture = version_regex
-        .captures(version_output)
-        .context(format!("Could not find {tool} version"))?;
+    fn extract_version(version_output: &str, tool: &str) -> Result<Version> {
+        let version_regex = Regex::new(&format!(r#"(?:{tool}?\s*)([0-9]+.[0-9]+.[0-9]+)"#))
+            .context("Could not create version matching regex")?;
 
-    let version = version_capture
-        .get(1)
-        .context(format!("Could not find {tool} version"))?
-        .as_str();
+        let version_capture = version_regex
+            .captures(version_output)
+            .context(format!("Could not find {tool} version"))?;
 
-    Version::parse(version).context(format!("Failed to parse {tool} version"))
+        let version = version_capture
+            .get(1)
+            .context(format!("Could not find {tool} version"))?
+            .as_str();
+
+        Version::parse(version).context(format!("Failed to parse {tool} version"))
+    }
 }
