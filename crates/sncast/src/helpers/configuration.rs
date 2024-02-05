8be5851ff68070c27a17c88714faa3e95d6c8b94
Wojciech Szymczyk <wojciech.szymczyk@swmansion.com>
2024-02-05 16:07:33 +0100

    Add a mechanism for snfoundry.toml file discovery (#1643)
    https://github.com/foundry-rs/starknet-foundry/issues/1620
    This PR is a part of stack, that aims to replace Scarb.toml tool
    configuration with snfoundry.toml:
    
    -> Add mechanism for snfoundry.toml discovery
    -- Replace Scarb.toml based configuration with snfoundry.toml based one
    (https://github.com/foundry-rs/starknet-foundry/pull/1644)
    -- Use the same profile for sncast and scarb
    (https://github.com/foundry-rs/starknet-foundry/pull/1646)
    -- Update docs
    (https://github.com/foundry-rs/starknet-foundry/pull/1647)
    
    ## Introduced changes
    
    - Add a mechanism for snfoundry.toml file discovery
    
    ## Checklist
    
    <!-- Make sure all of these are complete -->
    
    - [X] Linked relevant issue
    - [X] Updated relevant documentation
    - [X] Added relevant tests
    - [X] Performed self-review of the code
    - [X] Added changes to `CHANGELOG.md`

---
+++ crates/sncast/src/helpers/configuration.rs
@@ -0,0 +1,102 @@
+use super::constants::CONFIG_FILENAME;
+use anyhow::{anyhow, Result};
+use camino::Utf8PathBuf;
+use std::fs;
+
+pub fn search_config_upwards_relative_to(current_dir: &Utf8PathBuf) -> Result<Utf8PathBuf> {
+    current_dir
+        .ancestors()
+        .find(|path| fs::metadata(path.join(CONFIG_FILENAME)).is_ok())
+        .map(|path| path.join(CONFIG_FILENAME))
+        .ok_or_else(|| {
+            anyhow!(
+                "Failed to find snfoundry.toml - not found in current nor any parent directories"
+            )
+        })
+}
+
+pub fn find_config_file() -> Result<Utf8PathBuf> {
+    search_config_upwards_relative_to(&Utf8PathBuf::try_from(
+        std::env::current_dir().expect("Failed to get current directory"),
+    )?)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use tempfile::TempDir;
+
+    fn copy_config_to_tempdir(src_path: &str, additional_path: Option<&str>) -> TempDir {
+        let temp_dir = TempDir::new().expect("Failed to create a temporary directory");
+        if let Some(dir) = additional_path {
+            let path = temp_dir.path().join(dir);
+            fs::create_dir_all(path).expect("Failed to create directories in temp dir");
+        };
+        let temp_dir_file_path = temp_dir.path().join(CONFIG_FILENAME);
+        fs::copy(src_path, temp_dir_file_path).expect("Failed to copy config file to temp dir");
+        temp_dir
+    }
+
+    #[test]
+    fn find_config_in_current_dir() {
+        let tempdir = copy_config_to_tempdir("tests/data/files/correct_snfoundry.toml", None);
+        let path = search_config_upwards_relative_to(
+            &Utf8PathBuf::try_from(tempdir.path().to_path_buf()).unwrap(),
+        )
+        .unwrap();
+        assert_eq!(path, tempdir.path().join(CONFIG_FILENAME));
+    }
+
+    #[test]
+    fn find_config_in_parent_dir() {
+        let tempdir =
+            copy_config_to_tempdir("tests/data/files/correct_snfoundry.toml", Some("childdir"));
+        let path = search_config_upwards_relative_to(
+            &Utf8PathBuf::try_from(tempdir.path().to_path_buf().join("childdir")).unwrap(),
+        )
+        .unwrap();
+        assert_eq!(path, tempdir.path().join(CONFIG_FILENAME));
+    }
+
+    #[test]
+    fn find_config_in_parent_dir_two_levels() {
+        let tempdir = copy_config_to_tempdir(
+            "tests/data/files/correct_snfoundry.toml",
+            Some("childdir1/childdir2"),
+        );
+        let path = search_config_upwards_relative_to(
+            &Utf8PathBuf::try_from(tempdir.path().to_path_buf().join("childdir1/childdir2"))
+                .unwrap(),
+        )
+        .unwrap();
+        assert_eq!(path, tempdir.path().join(CONFIG_FILENAME));
+    }
+
+    #[test]
+    fn find_config_in_parent_dir_available_in_multiple_parents() {
+        let tempdir =
+            copy_config_to_tempdir("tests/data/files/correct_snfoundry.toml", Some("childdir1"));
+        fs::copy(
+            "tests/data/files/correct_snfoundry.toml",
+            tempdir.path().join("childdir1").join(CONFIG_FILENAME),
+        )
+        .expect("Failed to copy config file to temp dir");
+        let path = search_config_upwards_relative_to(
+            &Utf8PathBuf::try_from(tempdir.path().to_path_buf().join("childdir1")).unwrap(),
+        )
+        .unwrap();
+        assert_eq!(path, tempdir.path().join("childdir1").join(CONFIG_FILENAME));
+    }
+
+    #[test]
+    fn no_config_in_current_nor_parent_dir() {
+        let tempdir = TempDir::new().expect("Failed to create a temporary directory");
+        assert!(
+            search_config_upwards_relative_to(
+                &Utf8PathBuf::try_from(tempdir.path().to_path_buf()).unwrap()
+            )
+            .is_err(),
+            "Failed to find snfoundry.toml - not found in current nor any parent directories"
+        );
+    }
+}
