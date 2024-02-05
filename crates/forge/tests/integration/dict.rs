328c54a72c3996a8afae2d60c03668971b37c315
Kamil Jankowski <kamil.jankowski.x@gmail.com>
2024-02-05 09:45:20 +0100

    Run tests from casm (#1626)
    <!-- Reference any GitHub issues resolved by this PR -->
    
    Closes #1140
    
    ## Introduced changes
    - use upstreamed changes to `SierraCasmRunner`
    
    ## Checklist
    
    <!-- Make sure all of these are complete -->
    
    - [x] Linked relevant issue
    - [x] Updated relevant documentation
    - [x] Added relevant tests
    - [x] Performed self-review of the code
    - [x] Added changes to `CHANGELOG.md`
    
    ---------
    
    Co-authored-by: Tomasz Rejowski <34059895+Arcticae@users.noreply.github.com>

---
+++ crates/forge/tests/integration/dict.rs
@@ -0,0 +1,53 @@
+use indoc::indoc;
+use std::path::Path;
+use test_utils::assert_passed;
+use test_utils::runner::Contract;
+use test_utils::running_tests::run_test_case;
+
+#[test]
+fn using_dict() {
+    let test = test_utils::test_case!(
+        indoc!(
+            r"
+        use result::ResultTrait;
+        use snforge_std::{ declare, ContractClass, ContractClassTrait };
+        use array::ArrayTrait;
+        
+        #[starknet::interface]
+        trait IDictUsingContract<TContractState> {
+            fn get_unique(self: @TContractState) -> u8; 
+            fn write_unique(self: @TContractState, values: Array<felt252>); 
+        }
+        
+        #[test]
+        fn using_dict() {
+            let contract = declare('DictUsingContract');
+            let numbers = array![1, 2, 3, 3, 3, 3 ,3, 4, 4, 4, 4, 4, 5, 5, 5, 5];
+            let mut inputs: Array<felt252> = array![];
+            numbers.serialize(ref inputs);
+            
+            let contract_address = contract.deploy(@inputs).unwrap();
+            let dispatcher = IDictUsingContractDispatcher { contract_address };
+            
+            let unq = dispatcher.get_unique();
+            assert(unq == 5, 'wrong unique count');
+
+            numbers.serialize(ref inputs);
+            dispatcher.write_unique(array![1, 2, 3, 3, 3, 3 ,3, 4, 4, 4, 4, 4]);
+            
+            let unq = dispatcher.get_unique();
+            assert(unq == 4, 'wrong unique count');
+        }
+        "
+        ),
+        Contract::from_code_path(
+            "DictUsingContract".to_string(),
+            Path::new("tests/data/contracts/dict_using_contract.cairo"),
+        )
+        .unwrap()
+    );
+
+    let result = run_test_case(&test);
+
+    assert_passed!(result);
+}
