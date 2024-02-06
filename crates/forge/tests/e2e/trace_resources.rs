4eddda31f0bf188fc619a7bfd6adf80590d9d197
Piotr Magiera <piotrmagiera150@gmail.com>
2024-02-06 16:19:31 +0100

    refactor tests

--- crates/forge/tests/e2e/trace_resources.rs
+++ crates/forge/tests/e2e/trace_resources.rs
@@ -1,4 +1,5 @@
 use assert_fs::TempDir;
+use std::collections::HashMap;
 use std::fs;
 
 use forge_runner::trace_data::ProfilerDeprecatedSyscallSelector::{
@@ -33,10 +34,10 @@ fn trace_resources() {
     }
 
     // tests for Deploy, CallContract and LibraryCall syscalls as they cannot be tested as easily as the rest
-    ensure_test_call_syscalls(&temp);
-    ensure_test_deploy_syscalls(&temp);
-    ensure_test_l1_handler_syscalls(&temp);
-    ensure_test_libcall_syscalls(&temp);
+    ensure_test_call_not_easily_countable_syscalls(&temp);
+    ensure_test_deploy_not_easily_countable_syscalls(&temp);
+    ensure_test_l1_handler_not_easily_countable_syscalls(&temp);
+    ensure_test_libcall_not_easily_countable_syscalls(&temp);
 }
 
 fn deserialize_call_trace(test_name: &str, temp_dir: &TempDir) -> ProfilerCallTrace {
@@ -109,217 +110,109 @@ fn assert_correct_diff_for_easily_countable_syscalls(resource_diff: &ProfilerExe
 // When sth fails in the functions below and you didn't change anything in the cairo code it is a BUG.
 // If you changed the corresponding cairo code count the expected occurrences of syscalls manually first, then assert them.
 // TL;DR: DON't mindlessly change numbers to fix the tests if they ever fail.
-fn ensure_test_deploy_syscalls(temp_dir: &TempDir) {
+fn ensure_test_call_not_easily_countable_syscalls(temp_dir: &TempDir) {
     let test_call_trace = deserialize_call_trace("test_call", temp_dir);
-    let top_call_syscalls = test_call_trace
-        .used_resources
-        .execution_resources
-        .syscall_counter;
-    assert_eq!(*top_call_syscalls.get(&Deploy).unwrap(), 11); // FIXME in #1631 (should be 14)
-    assert_eq!(*top_call_syscalls.get(&CallContract).unwrap(), 4); // FIXME in #1631 (should be 8)
-    assert_eq!(*top_call_syscalls.get(&LibraryCall).unwrap(), 1);
+    assert_not_easily_countable_syscalls(&test_call_trace, 11, 4, 1); // FIXME in #1631 (should be 14, 8, 1)
+
     let regular_call = &test_call_trace.nested_calls[1];
-    let regular_call_syscalls = &regular_call
-        .used_resources
-        .execution_resources
-        .syscall_counter;
-    assert_eq!(*regular_call_syscalls.get(&Deploy).unwrap(), 2);
-    assert_eq!(*regular_call_syscalls.get(&CallContract).unwrap(), 1);
-    assert!(regular_call_syscalls.get(&LibraryCall).is_none());
+    assert_not_easily_countable_syscalls(regular_call, 2, 1, 0);
+
     let from_proxy = &regular_call.nested_calls[0];
-    let from_proxy_syscalls = &from_proxy
-        .used_resources
-        .execution_resources
-        .syscall_counter;
-    assert_eq!(*from_proxy_syscalls.get(&Deploy).unwrap(), 1);
-    assert!(from_proxy_syscalls.get(&CallContract).is_none());
-    assert!(from_proxy_syscalls.get(&LibraryCall).is_none());
+    assert_not_easily_countable_syscalls(from_proxy, 1, 0, 0);
 
     let with_libcall = &test_call_trace.nested_calls[2];
-    let with_libcall_syscalls = &with_libcall
-        .used_resources
-        .execution_resources
-        .syscall_counter;
-    assert_eq!(*with_libcall_syscalls.get(&Deploy).unwrap(), 2);
-    assert!(from_proxy_syscalls.get(&CallContract).is_none());
-    assert_eq!(*with_libcall_syscalls.get(&LibraryCall).unwrap(), 1);
+    assert_not_easily_countable_syscalls(with_libcall, 2, 0, 1);
+
     let from_proxy = &with_libcall.nested_calls[0];
-    let from_proxy_syscalls = &from_proxy
-        .used_resources
-        .execution_resources
-        .syscall_counter;
-    assert_eq!(*from_proxy_syscalls.get(&Deploy).unwrap(), 1);
-    assert!(from_proxy_syscalls.get(&CallContract).is_none());
-    assert!(from_proxy_syscalls.get(&LibraryCall).is_none());
+    assert_not_easily_countable_syscalls(from_proxy, 1, 0, 0);
 
     let call_two = &test_call_trace.nested_calls[3];
-    let call_two_syscalls = &call_two.used_resources.execution_resources.syscall_counter;
-    assert_eq!(*call_two_syscalls.get(&Deploy).unwrap(), 3);
-    assert_eq!(*call_two_syscalls.get(&CallContract).unwrap(), 2);
-    assert!(call_two_syscalls.get(&LibraryCall).is_none());
+    assert_not_easily_countable_syscalls(call_two, 3, 2, 0);
+
     let from_proxy = &call_two.nested_calls[0];
-    let from_proxy_syscalls = &from_proxy
-        .used_resources
-        .execution_resources
-        .syscall_counter;
-    assert_eq!(*from_proxy_syscalls.get(&Deploy).unwrap(), 1);
-    assert!(from_proxy_syscalls.get(&CallContract).is_none());
-    assert!(from_proxy_syscalls.get(&LibraryCall).is_none());
+    assert_not_easily_countable_syscalls(from_proxy, 1, 0, 0);
+
     let from_proxy_dummy = &call_two.nested_calls[1];
-    let from_proxy_dummy_syscalls = &from_proxy_dummy
-        .used_resources
-        .execution_resources
-        .syscall_counter;
-    assert_eq!(*from_proxy_dummy_syscalls.get(&Deploy).unwrap(), 1);
-    assert!(from_proxy_dummy_syscalls.get(&CallContract).is_none());
-    assert!(from_proxy_dummy_syscalls.get(&LibraryCall).is_none());
+    assert_not_easily_countable_syscalls(from_proxy_dummy, 1, 0, 0);
 
     let from_proxy = &test_call_trace.nested_calls[4];
-    let from_proxy_syscalls = &from_proxy
-        .used_resources
-        .execution_resources
-        .syscall_counter;
-    assert_eq!(*from_proxy_syscalls.get(&Deploy).unwrap(), 1);
-    assert!(from_proxy_syscalls.get(&CallContract).is_none());
-    assert!(from_proxy_syscalls.get(&LibraryCall).is_none());
+    assert_not_easily_countable_syscalls(from_proxy, 1, 0, 0);
 }
 
-fn ensure_test_call_syscalls(temp_dir: &TempDir) {
+fn ensure_test_deploy_not_easily_countable_syscalls(temp_dir: &TempDir) {
     let test_call_trace = deserialize_call_trace("test_deploy", temp_dir);
-    let top_call_syscalls = test_call_trace
-        .used_resources
-        .execution_resources
-        .syscall_counter;
-    assert_eq!(*top_call_syscalls.get(&Deploy).unwrap(), 11); // FIXME in #1631 (should be 14)
-    assert_eq!(*top_call_syscalls.get(&CallContract).unwrap(), 4);
-    assert!(top_call_syscalls.get(&LibraryCall).is_none());
+    assert_not_easily_countable_syscalls(&test_call_trace, 11, 4, 0); // FIXME in #1631 (should be 14, 4, 0)
 
     for deploy_proxy in test_call_trace.nested_calls {
-        let deploy_proxy_syscalls = deploy_proxy
-            .used_resources
-            .execution_resources
-            .syscall_counter;
-        assert_eq!(*deploy_proxy_syscalls.get(&Deploy).unwrap(), 2);
-        assert_eq!(*deploy_proxy_syscalls.get(&CallContract).unwrap(), 1);
-        assert!(deploy_proxy_syscalls.get(&LibraryCall).is_none());
+        assert_not_easily_countable_syscalls(&deploy_proxy, 2, 1, 0);
 
         let from_proxy = &deploy_proxy.nested_calls[0];
-        let from_proxy_syscalls = &from_proxy
-            .used_resources
-            .execution_resources
-            .syscall_counter;
-        assert_eq!(*from_proxy_syscalls.get(&Deploy).unwrap(), 1);
-        assert!(from_proxy_syscalls.get(&CallContract).is_none());
-        assert!(from_proxy_syscalls.get(&LibraryCall).is_none());
+        assert_not_easily_countable_syscalls(from_proxy, 1, 0, 0);
     }
 }
 
-fn ensure_test_l1_handler_syscalls(temp_dir: &TempDir) {
+fn ensure_test_l1_handler_not_easily_countable_syscalls(temp_dir: &TempDir) {
     let test_call_trace = deserialize_call_trace("test_l1_handler", temp_dir);
-    let top_call_syscalls = test_call_trace
-        .used_resources
-        .execution_resources
-        .syscall_counter;
-    assert_eq!(*top_call_syscalls.get(&Deploy).unwrap(), 6); // FIXME in #1631 (should be 8)
-    assert_eq!(*top_call_syscalls.get(&CallContract).unwrap(), 3);
-    assert!(top_call_syscalls.get(&LibraryCall).is_none());
+    assert_not_easily_countable_syscalls(&test_call_trace, 6, 3, 0); // FIXME in #1631 (should be 8, 3, 0)
 
     let handle_l1 = &test_call_trace.nested_calls[1];
-    let handle_l1_syscalls = &handle_l1.used_resources.execution_resources.syscall_counter;
-    assert_eq!(*handle_l1_syscalls.get(&Deploy).unwrap(), 3);
-    assert_eq!(*handle_l1_syscalls.get(&CallContract).unwrap(), 2);
-    assert!(handle_l1_syscalls.get(&LibraryCall).is_none());
+    assert_not_easily_countable_syscalls(handle_l1, 3, 2, 0);
 
     let regular_call = &handle_l1.nested_calls[0];
-    let regular_call_syscalls = &regular_call
-        .used_resources
-        .execution_resources
-        .syscall_counter;
-    assert_eq!(*regular_call_syscalls.get(&Deploy).unwrap(), 2);
-    assert_eq!(*regular_call_syscalls.get(&CallContract).unwrap(), 1);
-    assert!(regular_call_syscalls.get(&LibraryCall).is_none());
+    assert_not_easily_countable_syscalls(regular_call, 2, 1, 0);
 
     let from_proxy = &regular_call.nested_calls[0];
-    let from_proxy_syscalls = &from_proxy
-        .used_resources
-        .execution_resources
-        .syscall_counter;
-    assert_eq!(*from_proxy_syscalls.get(&Deploy).unwrap(), 1);
-    assert!(from_proxy_syscalls.get(&CallContract).is_none());
-    assert!(from_proxy_syscalls.get(&LibraryCall).is_none());
+    assert_not_easily_countable_syscalls(from_proxy, 1, 0, 0);
 }
 
-fn ensure_test_libcall_syscalls(temp_dir: &TempDir) {
+fn ensure_test_libcall_not_easily_countable_syscalls(temp_dir: &TempDir) {
     let test_call_trace = deserialize_call_trace("test_lib_call", temp_dir);
-    let top_call_syscalls = test_call_trace
-        .used_resources
-        .execution_resources
-        .syscall_counter;
-    assert_eq!(*top_call_syscalls.get(&Deploy).unwrap(), 9); // FIXME in #1631 (should be 11)
-    assert_eq!(*top_call_syscalls.get(&CallContract).unwrap(), 3);
-    assert_eq!(*top_call_syscalls.get(&LibraryCall).unwrap(), 1); // FIXME in #1631 (should be 5)
+    assert_not_easily_countable_syscalls(&test_call_trace, 9, 3, 1); // FIXME in #1631 (should be 11, 3, 5)
+
     let regular_call = &test_call_trace.nested_calls[0];
-    let regular_call_syscalls = &regular_call
-        .used_resources
-        .execution_resources
-        .syscall_counter;
-    assert_eq!(*regular_call_syscalls.get(&Deploy).unwrap(), 2);
-    assert_eq!(*regular_call_syscalls.get(&CallContract).unwrap(), 1);
-    assert!(regular_call_syscalls.get(&LibraryCall).is_none());
+    assert_not_easily_countable_syscalls(regular_call, 2, 1, 0);
+
     let from_proxy = &regular_call.nested_calls[0];
-    let from_proxy_syscalls = &from_proxy
-        .used_resources
-        .execution_resources
-        .syscall_counter;
-    assert_eq!(*from_proxy_syscalls.get(&Deploy).unwrap(), 1);
-    assert!(from_proxy_syscalls.get(&CallContract).is_none());
-    assert!(from_proxy_syscalls.get(&LibraryCall).is_none());
+    assert_not_easily_countable_syscalls(from_proxy, 1, 0, 0);
 
     let with_libcall = &test_call_trace.nested_calls[1];
-    let with_libcall_syscalls = &with_libcall
-        .used_resources
-        .execution_resources
-        .syscall_counter;
-    assert_eq!(*with_libcall_syscalls.get(&Deploy).unwrap(), 2);
-    assert!(from_proxy_syscalls.get(&CallContract).is_none());
-    assert_eq!(*with_libcall_syscalls.get(&LibraryCall).unwrap(), 1);
-    let from_proxy = &with_libcall.nested_calls[0];
-    let from_proxy_syscalls = &from_proxy
-        .used_resources
-        .execution_resources
-        .syscall_counter;
-    assert_eq!(*from_proxy_syscalls.get(&Deploy).unwrap(), 1);
-    assert!(from_proxy_syscalls.get(&CallContract).is_none());
-    assert!(from_proxy_syscalls.get(&LibraryCall).is_none());
+    assert_not_easily_countable_syscalls(with_libcall, 2, 0, 1);
 
     let call_two = &test_call_trace.nested_calls[2];
-    let call_two_syscalls = &call_two.used_resources.execution_resources.syscall_counter;
-    assert_eq!(*call_two_syscalls.get(&Deploy).unwrap(), 3);
-    assert_eq!(*call_two_syscalls.get(&CallContract).unwrap(), 2);
-    assert!(call_two_syscalls.get(&LibraryCall).is_none());
+    assert_not_easily_countable_syscalls(call_two, 3, 2, 0);
+
     let from_proxy = &call_two.nested_calls[0];
-    let from_proxy_syscalls = &from_proxy
-        .used_resources
-        .execution_resources
-        .syscall_counter;
-    assert_eq!(*from_proxy_syscalls.get(&Deploy).unwrap(), 1);
-    assert!(from_proxy_syscalls.get(&CallContract).is_none());
-    assert!(from_proxy_syscalls.get(&LibraryCall).is_none());
+    assert_not_easily_countable_syscalls(from_proxy, 1, 0, 0);
+
     let from_proxy_dummy = &call_two.nested_calls[1];
-    let from_proxy_dummy_syscalls = &from_proxy_dummy
-        .used_resources
-        .execution_resources
-        .syscall_counter;
-    assert_eq!(*from_proxy_dummy_syscalls.get(&Deploy).unwrap(), 1);
-    assert!(from_proxy_dummy_syscalls.get(&CallContract).is_none());
-    assert!(from_proxy_dummy_syscalls.get(&LibraryCall).is_none());
+    assert_not_easily_countable_syscalls(from_proxy_dummy, 1, 0, 0);
 
     let from_proxy = &test_call_trace.nested_calls[3];
-    let from_proxy_syscalls = &from_proxy
-        .used_resources
-        .execution_resources
-        .syscall_counter;
-    assert_eq!(*from_proxy_syscalls.get(&Deploy).unwrap(), 1);
-    assert!(from_proxy_syscalls.get(&CallContract).is_none());
-    assert!(from_proxy_syscalls.get(&LibraryCall).is_none());
+    assert_not_easily_countable_syscalls(from_proxy, 1, 0, 0);
+}
+
+fn assert_not_easily_countable_syscalls(
+    call: &ProfilerCallTrace,
+    deploy_count: usize,
+    call_contract_count: usize,
+    library_call_count: usize,
+) {
+    let syscall_counter = &call.used_resources.execution_resources.syscall_counter;
+
+    let expected_counts: HashMap<_, _> = [
+        (Deploy, deploy_count),
+        (CallContract, call_contract_count),
+        (LibraryCall, library_call_count),
+    ]
+    .into_iter()
+    .filter(|(_key, val)| *val > 0)
+    .collect();
+
+    for key in [Deploy, CallContract, LibraryCall] {
+        assert_eq!(
+            syscall_counter.get(&key).copied(),
+            expected_counts.get(&key).copied(),
+            "Incorrect count for {key:?}"
+        );
+    }
 }
