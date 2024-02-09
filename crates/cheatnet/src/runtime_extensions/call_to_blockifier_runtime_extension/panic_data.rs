9944f2071df945e3571c90585a3ab34f56104624
Arcticae <tomekgsd@gmail.com>
2024-02-09 15:10:37 +0100

    Change the regexp, add tests

--- crates/cheatnet/src/runtime_extensions/call_to_blockifier_runtime_extension/panic_data.rs
+++ crates/cheatnet/src/runtime_extensions/call_to_blockifier_runtime_extension/panic_data.rs
@@ -4,7 +4,7 @@ use regex::Regex;
 
 #[must_use]
 pub fn try_extract_panic_data(err: &str) -> Option<Vec<Felt252>> {
-    let re_felt_array = Regex::new(r"Got an exception while executing a hint: Hint Error: Execution failed\. Failure reason:\s\w*\s\('(.*)'\)\.")
+    let re_felt_array = Regex::new(r"Got an exception while executing a hint: Hint Error: Execution failed\. Failure reason: \w+ \('(.*)'\)\.")
         .expect("Could not create felt panic_data matching regex");
 
     let re_string = Regex::new(r#"(?s)Got an exception while executing a hint: Hint Error: Execution failed\. Failure reason: "(.*)"\."#)
@@ -40,9 +40,13 @@ pub fn try_extract_panic_data(err: &str) -> Option<Vec<Felt252>> {
 mod test {
     use super::*;
     use cairo_felt::Felt252;
+    use cairo_lang_utils::byte_array::BYTE_ARRAY_MAGIC;
+    use conversions::felt252::FromShortString;
+    use indoc::indoc;
+    use num_traits::Num;
 
     #[test]
-    fn string_extracting_panic_data() {
+    fn extracting_plain_panic_data() {
         let cases: [(&str, Option<Vec<Felt252>>); 4] = [
             (
                 "Beginning of trace\nGot an exception while executing a hint: Hint Error: Execution failed. Failure reason: 0x434d3232 ('PANIK, DAYTA').\n
@@ -58,6 +62,68 @@ mod test {
                 Some(vec![])
             ),
             ("Custom Hint Error: Invalid trace: \"PANIC, DATA\"", None)
+        ];
+
+        for (str, expected) in cases {
+            assert_eq!(try_extract_panic_data(str), expected);
+        }
+    }
+
+    #[test]
+    fn extracting_string_panic_data() {
+        let cases: [(&str, Option<Vec<Felt252>>); 4] = [
+            (
+                indoc!(
+                    r#"
+                    Beginning of trace
+                    Got an exception while executing a hint: Hint Error: Execution failed. Failure reason: "wow message is exactly 31 chars".
+                    End of trace
+                    "#
+                ),
+                Some(vec![
+                    Felt252::from_str_radix(BYTE_ARRAY_MAGIC, 16).unwrap(),
+                    Felt252::from(1),
+                    Felt252::from_short_string("wow message is exactly 31 chars").unwrap(),
+                    Felt252::from(0),
+                    Felt252::from(0),
+                ]),
+            ),
+            (
+                indoc!(
+                    r#"
+                    Beginning of trace
+                    Got an exception while executing a hint: Hint Error: Execution failed. Failure reason: "".
+                    End of trace
+                    "#
+                ),
+                Some(vec![
+                    Felt252::from_str_radix(BYTE_ARRAY_MAGIC, 16).unwrap(),
+                    Felt252::from(0),
+                    Felt252::from(0),
+                    Felt252::from(0),
+                ]),
+            ),
+            (
+                indoc!(
+                    r#"
+                    Beginning of trace
+                    Got an exception while executing a hint: Hint Error: Execution failed. Failure reason: "A very long and multiline
+                    thing is also being parsed, and can
+                    also can be very long as you can see".
+                    End of trace
+                    "#
+                ),
+                Some(vec![
+                    Felt252::from_str_radix(BYTE_ARRAY_MAGIC, 16).unwrap(),
+                    Felt252::from(3),
+                    Felt252::from_short_string("A very long and multiline\nthing").unwrap(),
+                    Felt252::from_short_string(" is also being parsed, and can\n").unwrap(),
+                    Felt252::from_short_string("also can be very long as you ca").unwrap(),
+                    Felt252::from_short_string("n see").unwrap(),
+                    Felt252::from(5),
+                ]),
+            ),
+            ("Custom Hint Error: Invalid trace: \"PANIC DATA\"", None),
         ];
 
         for (str, expected) in cases {
