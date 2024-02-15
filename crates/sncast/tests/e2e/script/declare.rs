0280417085bd04137c90db12a606b4465cbad6c9
Maksim Zdobnikau <maksim.zdobnikau@swmansion.com>
2024-02-15 11:50:33 +0100

    Execute `script declare` tests in tempdir

--- crates/sncast/tests/e2e/script/declare.rs
+++ crates/sncast/tests/e2e/script/declare.rs
@@ -1,13 +1,20 @@
-use crate::helpers::constants::{SCRIPTS_DIR, URL};
+use crate::helpers::constants::{ACCOUNT_FILE_PATH, SCRIPTS_DIR, URL};
+use crate::helpers::fixtures::{duplicate_script_directory, get_accounts_path};
+use crate::helpers::runner::runner;
 use indoc::indoc;
-use snapbox::cmd::{cargo_bin, Command};
 
 #[tokio::test]
 async fn test_missing_field() {
+    let tempdir = duplicate_script_directory(
+        SCRIPTS_DIR.to_owned() + "/declare/missing_field",
+        Vec::<String>::new(),
+    );
+    let accounts_json_path = get_accounts_path(ACCOUNT_FILE_PATH);
+
     let script_name = "missing_field";
     let args = vec![
         "--accounts-file",
-        "../../../accounts/accounts.json",
+        accounts_json_path.as_str(),
         "--account",
         "user4",
         "--url",
@@ -16,9 +23,7 @@ async fn test_missing_field() {
         &script_name,
     ];
 
-    let snapbox = Command::new(cargo_bin!("sncast"))
-        .current_dir(SCRIPTS_DIR.to_owned() + "/declare/missing_field")
-        .args(args);
+    let snapbox = runner(&args).current_dir(tempdir.path());
     snapbox.assert().failure().stdout_matches(indoc! {r"
         ...
         error: Wrong number of arguments. Expected 3, found: 2
@@ -28,10 +33,16 @@ async fn test_missing_field() {
 
 #[tokio::test]
 async fn test_wrong_contract_name() {
+    let tempdir = duplicate_script_directory(
+        SCRIPTS_DIR.to_owned() + "/declare/no_contract",
+        Vec::<String>::new(),
+    );
+    let accounts_json_path = get_accounts_path(ACCOUNT_FILE_PATH);
+
     let script_name = "no_contract";
     let args = vec![
         "--accounts-file",
-        "../../../accounts/accounts.json",
+        accounts_json_path.as_str(),
         "--account",
         "user4",
         "--url",
@@ -40,9 +51,7 @@ async fn test_wrong_contract_name() {
         &script_name,
     ];
 
-    let snapbox = Command::new(cargo_bin!("sncast"))
-        .current_dir(SCRIPTS_DIR.to_owned() + "/declare/no_contract")
-        .args(args);
+    let snapbox = runner(&args).current_dir(tempdir.path());
     snapbox.assert().success().stderr_matches(indoc! {r"
         command: script
         error: Got an exception [..] Failed to find Mapaaaa artifact in starknet_artifacts.json file. Please make sure you have specified correct package using `--package` flag and that you have enabled sierra and casm code generation in Scarb.toml.
