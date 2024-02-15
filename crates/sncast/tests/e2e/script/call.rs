5fb6ad639b9426b07a58384c120d2b52a763a148
Maksim Zdobnikau <maksim.zdobnikau@swmansion.com>
2024-02-15 11:35:42 +0100

    Execute `script call` tests in tempdir

--- crates/sncast/tests/e2e/script/call.rs
+++ crates/sncast/tests/e2e/script/call.rs
@@ -1,15 +1,17 @@
 use crate::helpers::constants::{SCRIPTS_DIR, URL};
+use crate::helpers::fixtures::duplicate_script_directory;
+use crate::helpers::runner::runner;
 use indoc::indoc;
-use snapbox::cmd::{cargo_bin, Command};
 
 #[tokio::test]
 async fn test_happy_case() {
+    let tempdir =
+        duplicate_script_directory(SCRIPTS_DIR.to_owned() + "/misc", Vec::<String>::new());
+
     let script_name = "call_happy";
     let args = vec!["--url", URL, "script", &script_name];
 
-    let snapbox = Command::new(cargo_bin!("sncast"))
-        .current_dir(SCRIPTS_DIR.to_owned() + "/misc")
-        .args(args);
+    let snapbox = runner(&args).current_dir(tempdir.path());
     snapbox.assert().success().stdout_matches(indoc! {r"
         ...
         command: script
@@ -19,12 +21,13 @@ async fn test_happy_case() {
 
 #[tokio::test]
 async fn test_failing() {
+    let tempdir =
+        duplicate_script_directory(SCRIPTS_DIR.to_owned() + "/misc", Vec::<String>::new());
+
     let script_name = "call_fail";
     let args = vec!["--url", URL, "script", &script_name];
 
-    let snapbox = Command::new(cargo_bin!("sncast"))
-        .current_dir(SCRIPTS_DIR.to_owned() + "/misc")
-        .args(args);
+    let snapbox = runner(&args).current_dir(tempdir.path());
     snapbox.assert().success().stderr_matches(indoc! {r"
         command: script
         error: Got an exception while executing a hint: Hint Error: An error [..]Entry point EntryPointSelector(StarkFelt[..]not found in contract[..]
