065f6c68e11cd9c8233821660119c764d015317b
Maksim Zdobnikau <maksim.zdobnikau@swmansion.com>
2024-02-16 14:35:50 +0100

    Replace TempDir::new calls with tempdir()

--- crates/sncast/src/helpers/configuration.rs
+++ crates/sncast/src/helpers/configuration.rs
@@ -3,7 +3,7 @@ use anyhow::{anyhow, Result};
 use camino::Utf8PathBuf;
 use serde::{Deserialize, Serialize};
 use std::fs;
-use tempfile::TempDir;
+use tempfile::{tempdir, TempDir};
 use toml::Value;
 
 #[derive(Deserialize, Serialize, Clone, Debug, PartialEq)]
@@ -170,7 +170,7 @@ pub fn find_config_file() -> Result<Utf8PathBuf> {
 
 #[must_use]
 pub fn copy_config_to_tempdir(src_path: &str, additional_path: Option<&str>) -> TempDir {
-    let temp_dir = TempDir::new().expect("Failed to create a temporary directory");
+    let temp_dir = tempdir().expect("Failed to create a temporary directory");
     if let Some(dir) = additional_path {
         let path = temp_dir.path().join(dir);
         fs::create_dir_all(path).expect("Failed to create directories in temp dir");
@@ -183,7 +183,6 @@ pub fn copy_config_to_tempdir(src_path: &str, additional_path: Option<&str>) -> 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use tempfile::TempDir;
 
     #[test]
     fn find_config_in_current_dir() {
@@ -238,7 +237,7 @@ mod tests {
 
     #[test]
     fn no_config_in_current_nor_parent_dir() {
-        let tempdir = TempDir::new().expect("Failed to create a temporary directory");
+        let tempdir = tempdir().expect("Failed to create a temporary directory");
         assert!(
             search_config_upwards_relative_to(
                 &Utf8PathBuf::try_from(tempdir.path().to_path_buf()).unwrap()
@@ -275,7 +274,7 @@ mod tests {
 
     #[test]
     fn load_config_not_found() {
-        let tempdir = TempDir::new().expect("Failed to create a temporary directory");
+        let tempdir = tempdir().expect("Failed to create a temporary directory");
         let config = load_config(
             &None,
             &Some(Utf8PathBuf::try_from(tempdir.path().to_path_buf()).unwrap()),
