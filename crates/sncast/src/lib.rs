a0539bbb4c46253881e634e587f007ddeceb6c53
Oleksii Bulenok <63914185+abulenok@users.noreply.github.com>
2024-02-21 10:13:40 +0100

    Update error message in the case of wrong account address (#1729)
    <!-- Reference any GitHub issues resolved by this PR -->
    
    Closes #1491
    
    ## Introduced changes
    
    <!-- A brief description of the changes -->
    
    - Added account address verification
    - Created tests with nonexistent account address
    
    ## Checklist
    
    <!-- Make sure all of these are complete -->
    
    - [x] Linked relevant issue
    - [ ] Updated relevant documentation
    - [x] Added relevant tests
    - [ ] Performed self-review of the code
    - [ ] Added changes to `CHANGELOG.md`

--- crates/sncast/src/lib.rs
+++ crates/sncast/src/lib.rs
@@ -31,6 +31,7 @@ use starknet::{
 };
 
 use crate::helpers::constants::{WAIT_RETRY_INTERVAL, WAIT_TIMEOUT};
+use starknet::accounts::ConnectedAccount;
 use std::collections::HashMap;
 use std::thread::sleep;
 use std::time::Duration;
@@ -222,7 +223,23 @@ pub async fn get_account<'a>(
         get_account_from_accounts_file(account, accounts_file, provider, chain_id)?
     };
 
-    Ok(account.set_block_id(get_block_id("pending")?).clone())
+    account.set_block_id(get_block_id("pending")?);
+    verify_account_address(account.clone()).await?;
+
+    Ok(account)
+}
+
+async fn verify_account_address(account: impl ConnectedAccount + std::marker::Sync) -> Result<()> {
+    match account.get_nonce().await {
+        Ok(_) => Ok(()),
+        Err(error) => {
+            if let StarknetError(ContractNotFound) = error {
+                Err(anyhow!("Invalid account address"))
+            } else {
+                handle_rpc_error::<()>(error)
+            }
+        }
+    }
 }
 
 fn get_account_from_keystore<'a>(
