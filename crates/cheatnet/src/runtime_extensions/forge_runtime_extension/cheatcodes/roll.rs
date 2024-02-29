f179fd97c8196784fdb3d2763b0d8fc06fb90b12
Kamil Jankowski <kamil.jankowski.x@gmail.com>
2024-02-29 13:48:21 +0100

    Roll n times (#1779)
    <!-- Reference any GitHub issues resolved by this PR -->
    
    Closes #
    
    ## Introduced changes
    
    <!-- A brief description of the changes -->
    
    - Add `CheatSpan` to `start_roll` signature (backend)
    - Add integration tests for roll
    
    ## Checklist
    
    <!-- Make sure all of these are complete -->
    
    - [x] Linked relevant issue
    - [x] Updated relevant documentation
    - [x] Added relevant tests
    - [x] Performed self-review of the code
    - [x] Added changes to `CHANGELOG.md`

--- crates/cheatnet/src/runtime_extensions/forge_runtime_extension/cheatcodes/roll.rs
+++ crates/cheatnet/src/runtime_extensions/forge_runtime_extension/cheatcodes/roll.rs
@@ -3,14 +3,18 @@ use crate::CheatnetState;
 use cairo_felt::Felt252;
 
 impl CheatnetState {
-    pub fn start_roll(&mut self, target: CheatTarget, block_number: Felt252) {
+    pub fn roll(&mut self, target: CheatTarget, block_number: Felt252, span: CheatSpan) {
         start_cheat(
             &mut self.global_roll,
             &mut self.rolled_contracts,
             target,
             block_number,
-            CheatSpan::Indefinite,
+            span,
         );
+    }
+
+    pub fn start_roll(&mut self, target: CheatTarget, block_number: Felt252) {
+        self.roll(target, block_number, CheatSpan::Indefinite);
     }
 
     pub fn stop_roll(&mut self, target: CheatTarget) {
