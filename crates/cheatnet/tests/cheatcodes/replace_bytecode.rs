06256d59d19e375545d0608e16a71970a8bc4137
Piotr Figiela <77412592+Draggu@users.noreply.github.com>
2024-03-04 14:18:49 +0100

    add fork test

--- crates/cheatnet/tests/cheatcodes/replace_bytecode.rs
+++ crates/cheatnet/tests/cheatcodes/replace_bytecode.rs
@@ -1,6 +1,59 @@
 use crate::{assert_success, cheatcodes::test_environment::TestEnvironment, common::get_contracts};
+use blockifier::state::cached_state::{CachedState, GlobalContractCache};
 use cairo_felt::Felt252;
-use cheatnet::state::CheatnetState;
+use cheatnet::{
+    constants::build_testing_state,
+    forking::state::ForkStateReader,
+    state::{CheatnetState, ExtendedStateReader},
+};
+use num_traits::Zero;
+use starknet_api::{
+    block::BlockNumber,
+    contract_address,
+    core::{ContractAddress, PatriciaKey},
+    hash::StarkHash,
+    patricia_key,
+};
+use tempfile::TempDir;
+
+#[test]
+fn fork() {
+    let cache_dir = TempDir::new().unwrap();
+    let mut cheatnet_state = CheatnetState::default();
+    let mut test_env = TestEnvironment::new(&mut cheatnet_state);
+    test_env.cached_state = CachedState::new(
+        ExtendedStateReader {
+            dict_state_reader: build_testing_state(),
+            fork_state_reader: Some(ForkStateReader::new(
+                "http://188.34.188.184:6060/rpc/v0_6".parse().unwrap(),
+                BlockNumber(957_613),
+                cache_dir.path().to_str().unwrap(),
+            )),
+        },
+        GlobalContractCache::default(),
+    );
+    let contracts = get_contracts();
+
+    let class_hash = test_env.declare("ReplaceInFork", &contracts);
+    let contract =
+        contract_address!("0x066ecea8cc2444d33214b9e379be1acef84aa340469b8cd285201e0517c5cb14");
+
+    let output = test_env.call_contract(&contract, "get_admin", &[]);
+
+    assert!(matches!(
+        output,
+        cheatnet::runtime_extensions::call_to_blockifier_runtime_extension::rpc::CallResult::Success { ret_data, .. }
+        if ret_data != vec![Felt252::zero()],
+    ));
+
+    test_env
+        .cheatnet_state()
+        .replace_class_for_contract(contract, class_hash);
+
+    let output = test_env.call_contract(&contract, "get_admin", &[]);
+
+    assert_success!(output, vec![Felt252::zero()]);
+}
 
 #[test]
 fn override_entrypoint() {
--- crates/cheatnet/tests/contracts/src/replace_bytecode.cairo
+++ crates/cheatnet/tests/contracts/src/replace_bytecode.cairo
@@ -1,2 +1,3 @@
 mod replace_bytecode_a;
 mod replace_bytecode_b;
+mod replace_fork;
