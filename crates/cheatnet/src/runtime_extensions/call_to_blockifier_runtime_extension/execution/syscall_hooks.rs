73c8021852af958f60c73842c0da971587476433
Piotr Figiela <77412592+Draggu@users.noreply.github.com>
2024-03-06 17:50:59 +0100

    fix cairo0 events capture

--- crates/cheatnet/src/runtime_extensions/call_to_blockifier_runtime_extension/execution/syscall_hooks.rs
+++ crates/cheatnet/src/runtime_extensions/call_to_blockifier_runtime_extension/execution/syscall_hooks.rs
@@ -1,9 +1,12 @@
-use blockifier::execution::syscalls::hint_processor::SyscallHintProcessor;
-
 use crate::{
     runtime_extensions::forge_runtime_extension::cheatcodes::spy_events::Event,
     state::CheatnetState,
 };
+use blockifier::execution::{
+    call_info::OrderedEvent, deprecated_syscalls::hint_processor::DeprecatedSyscallHintProcessor,
+    syscalls::hint_processor::SyscallHintProcessor,
+};
+use starknet_api::core::ContractAddress;
 
 pub fn emit_event_hook(
     syscall_handler: &SyscallHintProcessor<'_>,
@@ -14,10 +17,33 @@ pub fn emit_event_hook(
         .code_address
         .unwrap_or(syscall_handler.call.storage_address);
 
+    emit_event_hook_inner(
+        contract_address,
+        syscall_handler.events.last().unwrap(),
+        cheatnet_state,
+    );
+}
+pub fn emit_event_hook_deprecated(
+    syscall_handler: &DeprecatedSyscallHintProcessor<'_>,
+    cheatnet_state: &mut CheatnetState,
+) {
+    let contract_address = syscall_handler.storage_address;
+
+    emit_event_hook_inner(
+        contract_address,
+        syscall_handler.events.last().unwrap(),
+        cheatnet_state,
+    );
+}
+
+pub fn emit_event_hook_inner(
+    contract_address: ContractAddress,
+    ordered_event: &OrderedEvent,
+    cheatnet_state: &mut CheatnetState,
+) {
     for spy_on in &cheatnet_state.spies {
         if spy_on.does_spy(contract_address) {
-            let event =
-                Event::from_ordered_event(syscall_handler.events.last().unwrap(), contract_address);
+            let event = Event::from_ordered_event(ordered_event, contract_address);
             cheatnet_state.detected_events.push(event);
             break;
         }
