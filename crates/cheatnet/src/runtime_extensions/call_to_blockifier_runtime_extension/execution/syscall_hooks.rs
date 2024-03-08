1062779767c71b9c7ab4df878a017288e6723abc
Piotr Figiela <77412592+Draggu@users.noreply.github.com>
2024-03-08 11:06:04 +0100

    apply suggestions

--- crates/cheatnet/src/runtime_extensions/call_to_blockifier_runtime_extension/execution/syscall_hooks.rs
+++ crates/cheatnet/src/runtime_extensions/call_to_blockifier_runtime_extension/execution/syscall_hooks.rs
@@ -10,14 +10,14 @@ use starknet_api::core::ContractAddress;
 
 pub trait SyscallHintProcessorExt {
     fn contract_address(&self) -> ContractAddress;
-    fn ordered_event(&self) -> &OrderedEvent;
+    fn last_event(&self) -> &OrderedEvent;
 }
 
 impl SyscallHintProcessorExt for SyscallHintProcessor<'_> {
     fn contract_address(&self) -> ContractAddress {
         self.call.code_address.unwrap_or(self.call.storage_address)
     }
-    fn ordered_event(&self) -> &OrderedEvent {
+    fn last_event(&self) -> &OrderedEvent {
         self.events.last().unwrap()
     }
 }
@@ -26,7 +26,7 @@ impl SyscallHintProcessorExt for DeprecatedSyscallHintProcessor<'_> {
     fn contract_address(&self) -> ContractAddress {
         self.storage_address
     }
-    fn ordered_event(&self) -> &OrderedEvent {
+    fn last_event(&self) -> &OrderedEvent {
         self.events.last().unwrap()
     }
 }
@@ -36,7 +36,7 @@ pub fn emit_event_hook(
     cheatnet_state: &mut CheatnetState,
 ) {
     let contract_address = syscall_handler.contract_address();
-    let ordered_event = syscall_handler.ordered_event();
+    let last_event = syscall_handler.last_event();
     let is_spied_on = cheatnet_state
         .spies
         .iter()
@@ -45,6 +45,6 @@ pub fn emit_event_hook(
     if is_spied_on {
         cheatnet_state
             .detected_events
-            .push(Event::from_ordered_event(ordered_event, contract_address));
+            .push(Event::from_ordered_event(last_event, contract_address));
     }
 }
