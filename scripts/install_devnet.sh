09b244cc165ddfe9de5e1b88d1505cc7479951bc
Piotr Figiela <77412592+Draggu@users.noreply.github.com>
2024-03-12 12:29:17 +0100

    bump devnet

--- scripts/install_devnet.sh
+++ scripts/install_devnet.sh
@@ -3,11 +3,11 @@ set -e
 
 DEVNET_INSTALL_DIR="$(git rev-parse --show-toplevel)/crates/sncast/tests/utils/devnet"
 DEVNET_REPO="https://github.com/0xSpaceShard/starknet-devnet-rs.git"
-DEVNET_REV="1a76e9d"
+DEVNET_REV="37dc6e6"
 
 # https://github.com/0xSpaceShard/starknet-devnet-rs/blob/main/.cargo/config.toml
 export STARKNET_VERSION="0.13.0"
-export RPC_SPEC_VERSION="0.6.0"
+export RPC_SPEC_VERSION="0.7.0-rc1"
 
 cargo install --locked --git "$DEVNET_REPO" --rev "$DEVNET_REV" --root "$DEVNET_INSTALL_DIR"
 
