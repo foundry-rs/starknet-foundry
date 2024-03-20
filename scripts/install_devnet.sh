9ac52b45232a1609c18a1fb5f6b94c4d8be53249
Maksymilian Demitraszek <maksymilian.demitraszek@protonmail.com>
2024-03-20 11:02:34 +0100

    Fix devnet installation
    commit-id:7c6ea481

--- scripts/install_devnet.sh
+++ scripts/install_devnet.sh
@@ -5,7 +5,7 @@ DEVNET_INSTALL_DIR="$(git rev-parse --show-toplevel)/crates/sncast/tests/utils/d
 DEVNET_REPO="https://github.com/0xSpaceShard/starknet-devnet-rs.git"
 DEVNET_REV="37dc6e6"
 
-cargo install --locked --git "$DEVNET_REPO" --rev "$DEVNET_REV" --root "$DEVNET_INSTALL_DIR"
+cargo install --locked --git "$DEVNET_REPO" --rev "$DEVNET_REV" --root "$DEVNET_INSTALL_DIR" --force
 
 echo "All done!"
 exit 0
