66dd80062e4af423e9177d65d0c4ae47a0001d39
Dariusz Doktorski <dariusz.doktorski@swmansion.com>
2024-04-08 19:50:01 +0200

    Fix init script

--- crates/sncast/tests/e2e/script/init.rs
+++ crates/sncast/tests/e2e/script/init.rs
@@ -63,8 +63,8 @@ fn test_script_init_happy_case() {
             // The example below uses a contract deployed to the Sepolia testnet
             fn main() {
                 let contract_address = 0x07e867f1fa6da2108dd2b3d534f1fbec411c5ec9504eb3baa1e49c7a0bef5ab5;
-                let call_result = call(contract_address.try_into().unwrap(), 'get_greeting', array![]);
-                assert(*call_result.data[0]=='Hello, Starknet!', *call_result.data[0]);
+                let call_result = call(contract_address.try_into().unwrap(), selector!("get_greeting"), array![]).expect('call failed');
+                assert(*call_result.data[1]=='Hello, Starknet!', *call_result.data[1]);
                 println!("{:?}", call_result);
             }
         "#}
