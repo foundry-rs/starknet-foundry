ee4a0b34d2f0c4c067e4dec3a2b48879886ec79b
ddoktorski <45050160+ddoktorski@users.noreply.github.com>
2024-04-09 11:53:51 +0200

    Remove goerli related code (#1992)
    <!-- Reference any GitHub issues resolved by this PR -->
    
    Closes #1811 
    
    ## Introduced changes
    
    <!-- A brief description of the changes -->
    
    - Update devnet to the latest version that includes chain ID
    `SN_SEPOLIA`
    - Remove goerli and use sepolia as a default chain in the tests
    - Update `OZ_CLASS_HASH` to the newer OpenZeppelin contract that
    supports V3 transactions
    - Update init script
    
    ## Checklist
    
    <!-- Make sure all of these are complete -->
    
    - [X] Linked relevant issue
    - [X] Updated relevant documentation
    - [ ] Added relevant tests
    - [X] Performed self-review of the code
    - [ ] Added changes to `CHANGELOG.md`
    
    ---------
    
    Co-authored-by: Wojciech Szymczyk <wojciech.szymczyk@swmansion.com>

--- scripts/install_devnet.sh
+++ scripts/install_devnet.sh
@@ -3,7 +3,7 @@ set -e
 
 DEVNET_INSTALL_DIR="$(git rev-parse --show-toplevel)/crates/sncast/tests/utils/devnet"
 DEVNET_REPO="https://github.com/0xSpaceShard/starknet-devnet-rs.git"
-DEVNET_REV="37dc6e6"
+DEVNET_REV="0a0d6ec"
 
 cargo install --locked --git "$DEVNET_REPO" --rev "$DEVNET_REV" --root "$DEVNET_INSTALL_DIR" --force
 
