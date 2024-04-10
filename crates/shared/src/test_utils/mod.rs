c6602e3ff05b5a7df095e8b731d4fa4536dc9787
Tomasz Miśkowicz <tomasz.miskowicz@swmansion.com>
2024-04-10 18:37:52 +0200

    Add functions to retrieve `RPC_URL`
    commit-id:918e569e
    
    Fix cargo lint errors
    
    Change implementation of rpc_url()

--- crates/shared/src/test_utils/mod.rs
+++ crates/shared/src/test_utils/mod.rs
@@ -1,1 +1,24 @@
+use crate::consts::RPC_URL_VERSION;
+use anyhow::{Context, Result};
+use std::env;
+use url::Url;
+
 pub mod output_assert;
+
+pub fn rpc_url() -> Result<Url> {
+    let rpc_url = env::var("RPC_URL")
+        .with_context(|| "The required environmental variable `RPC_URL` is not set. Please set it manually or in .cargo/config.toml file"
+    )?;
+
+    Url::parse(&rpc_url).with_context(|| {
+        format!("Failed to parse the URL from the `RPC_URL` environmental variable: {rpc_url}")
+    })
+}
+
+pub fn rpc_url_with_version() -> Result<Url> {
+    let mut url = rpc_url().unwrap();
+    url.set_path(format!("rpc/{RPC_URL_VERSION}").as_str());
+    url.set_query(None);
+
+    Ok(url)
+}
