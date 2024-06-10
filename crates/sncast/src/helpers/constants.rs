ffa8e674ca01d60d501a6335edd8fece710cd078
Karol Sewiło <95349104+ksew1@users.noreply.github.com>
2024-06-10 12:05:47 +0200

    Add support for braavos accounts (#2176)
    <!-- Reference any GitHub issues resolved by this PR -->
    
    Related #1854
    
    ## Introduced changes
    
    - Add fixture to deploy a braavos account
    - Run tests using the deployed braavos account
    - Enable creating a braavos account to the accounts file and starkli
    format
    - Support deploying braavos account from the accounts file and starkli
    account
    - Use AccountType enum in AccountData
    
    ## Missing
    - Documentation and option to specify base_account_clash_hash will be
    added in next PR, to reduce time of review
    
    
    ## Checklist
    
    <!-- Make sure all of these are complete -->
    
    - [x] Linked relevant issue
    - [ ] Updated relevant documentation
    - [x] Added relevant tests
    - [x] Performed self-review of the code
    - [x] Added changes to `CHANGELOG.md`
    
    ---------
    
    Co-authored-by: Karol Sewilo <karol.sewilo@swmansion.com>

--- crates/sncast/src/helpers/constants.rs
+++ crates/sncast/src/helpers/constants.rs
@@ -18,6 +18,12 @@ pub const OZ_CLASS_HASH: &str =
 pub const ARGENT_CLASS_HASH: &str =
     "0x029927c8af6bccf3f6fda035981e765a7bdbf18a2dc0d630494f8758aa908e2b";
 
+pub const BRAAVOS_CLASS_HASH: &str =
+    "0x00816dd0297efc55dc1e7559020a3a825e81ef734b558f03c83325d4da7e6253";
+
+pub const BRAAVOS_BASE_ACCOUNT_CLASS_HASH: &str =
+    "0x013bfe114fb1cf405bfc3a7f8dbe2d91db146c17521d40dcf57e16d6b59fa8e6";
+
 // used in wait_for_tx. Txs will be fetched every 5s with timeout of 300s - so 60 attempts
 #[allow(dead_code)]
 pub const WAIT_TIMEOUT: u16 = 300;
