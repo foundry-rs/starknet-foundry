6fc670f4ef0ca76e447e25ce81904aa1fd4cf430
Piotr Figiela <77412592+Draggu@users.noreply.github.com>
2024-06-12 17:33:26 +0200

    extract running helpers to separate file

--- crates/forge-runner/src/running.rs
+++ crates/forge-runner/src/running.rs
@@ -1,10 +1,3 @@
-use std::cell::RefCell;
-use std::collections::HashMap;
-use std::default::Default;
-use std::marker::PhantomData;
-use std::rc::Rc;
-use std::sync::Arc;
-
 use crate::build_trace_data::test_sierra_program_path::VersionedProgramPath;
 use crate::forge_config::{RuntimeConfig, TestRunnerConfig};
 use crate::gas::calculate_used_gas;
@@ -12,29 +5,14 @@ use crate::package_tests::with_config_resolved::{ResolvedForkConfig, TestCaseWit
 use crate::test_case_summary::{Single, TestCaseSummary};
 use anyhow::{bail, ensure, Result};
 use blockifier::execution::entry_point::EntryPointExecutionContext;
-use blockifier::execution::execution_utils::ReadOnlySegments;
-use blockifier::execution::syscalls::hint_processor::SyscallHintProcessor;
 use blockifier::state::cached_state::{
     CachedState, GlobalContractCache, GLOBAL_CONTRACT_CACHE_SIZE_FOR_TEST,
 };
-use blockifier::state::state_api::State;
 use cairo_felt::Felt252;
-use cairo_lang_casm::hints::Hint;
-use cairo_lang_casm::instructions::Instruction;
-use cairo_lang_runner::casm_run::{
-    build_cairo_runner, hint_to_hint_params, run_function_with_runner,
-};
-use cairo_lang_runner::{initialize_vm, Arg, RunResult, RunnerError, SierraCasmRunner};
-use cairo_lang_sierra::extensions::segment_arena::SegmentArenaType;
-use cairo_lang_sierra::extensions::NoGenericArgsGenericType;
-use cairo_lang_sierra::ids::GenericTypeId;
-use cairo_vm::serde::deserialize_program::HintParams;
-use cairo_vm::types::relocatable::{MaybeRelocatable, Relocatable};
+use cairo_lang_runner::{RunResult, RunnerError, SierraCasmRunner};
 use cairo_vm::vm::runners::cairo_runner::ExecutionResources;
-use cairo_vm::vm::vm_core::VirtualMachine;
 use camino::Utf8Path;
 use cheatnet::constants as cheatnet_constants;
-use cheatnet::constants::build_test_entry_point;
 use cheatnet::forking::state::ForkStateReader;
 use cheatnet::runtime_extensions::call_to_blockifier_runtime_extension::rpc::UsedResources;
 use cheatnet::runtime_extensions::call_to_blockifier_runtime_extension::CallToBlockifierExtension;
@@ -45,15 +23,24 @@ use cheatnet::runtime_extensions::forge_runtime_extension::{
     update_top_call_vm_trace, ForgeExtension, ForgeRuntime,
 };
 use cheatnet::state::{BlockInfoReader, CallTrace, CheatnetState, ExtendedStateReader};
+use helpers::{
+    build_syscall_handler, create_entry_code, create_hints_dict, get_assembled_program,
+    get_syscall_segment_index, run_with_runner,
+};
 use runtime::starknet::context::{build_context, set_max_steps};
 use runtime::{ExtendedRuntime, StarknetRuntime};
+use std::cell::RefCell;
+use std::default::Default;
+use std::marker::PhantomData;
+use std::rc::Rc;
+use std::sync::Arc;
 use tokio::sync::mpsc::Sender;
 use tokio::task::JoinHandle;
-use universal_sierra_compiler_api::{
-    AssembledCairoProgramWithSerde, AssembledProgramWithDebugInfo, CasmCodeOffset,
-    CasmInstructionIdx,
-};
+use universal_sierra_compiler_api::AssembledProgramWithDebugInfo;
 
+mod helpers;
+
+#[must_use]
 pub fn run_test(
     case: Arc<TestCaseWithResolvedConfig>,
     casm_program: Arc<AssembledProgramWithDebugInfo>,
@@ -133,41 +120,6 @@ pub(crate) fn run_fuzz_test(
     })
 }
 
-fn get_syscall_segment_index(test_param_types: &[(GenericTypeId, i16)]) -> isize {
-    // Segment arena is allocated conditionally, so segment index is automatically moved (+2 segments)
-    if test_param_types
-        .iter()
-        .any(|(ty, _)| ty == &SegmentArenaType::ID)
-    {
-        12
-    } else {
-        10
-    }
-}
-
-fn build_syscall_handler<'a>(
-    blockifier_state: &'a mut dyn State,
-    string_to_hint: &'a HashMap<String, Hint>,
-    execution_resources: &'a mut ExecutionResources,
-    context: &'a mut EntryPointExecutionContext,
-    syscall_segment_index: isize,
-) -> SyscallHintProcessor<'a> {
-    let entry_point = build_test_entry_point();
-
-    SyscallHintProcessor::new(
-        blockifier_state,
-        execution_resources,
-        context,
-        Relocatable {
-            segment_index: syscall_segment_index,
-            offset: 0,
-        },
-        entry_point,
-        string_to_hint,
-        ReadOnlySegments::default(),
-    )
-}
-
 pub struct RunResultWithInfo {
     pub(crate) run_result: Result<RunResult, RunnerError>,
     pub(crate) call_trace: Rc<RefCell<CallTrace>>,
@@ -187,27 +139,13 @@ pub fn run_test_case(
         "\n\t`available_gas` attribute was incorrectly configured. Make sure you use scarb >= 2.4.4\n"
     );
 
-    let initial_gas = usize::MAX;
-    let runner_args: Vec<Arg> = args.into_iter().map(Arg::Value).collect();
-    let sierra_instruction_idx = case.test_details.sierra_entry_point_statement_idx;
-    let casm_entry_point_offset =
-        get_casm_instruction_offset(&casm_program.debug_info, sierra_instruction_idx);
+    let (entry_code, builtins) = create_entry_code(args, &case.test_details, casm_program);
 
-    let (entry_code, builtins) = SierraCasmRunner::create_entry_code_from_params(
-        &case.test_details.parameter_types,
-        &runner_args,
-        initial_gas,
-        casm_entry_point_offset,
-    )
-    .unwrap();
     let footer = SierraCasmRunner::create_code_footer();
 
-    let assembled_program: &mut AssembledCairoProgramWithSerde =
-        &mut casm_program.assembled_cairo_program.clone();
-    add_header(entry_code, assembled_program);
-    add_footer(footer, assembled_program);
+    let assembled_program = get_assembled_program(casm_program, entry_code, footer);
 
-    let (string_to_hint, hints_dict) = create_hints_dict(assembled_program);
+    let (string_to_hint, hints_dict) = create_hints_dict(&assembled_program);
 
     let mut state_reader = ExtendedStateReader {
         dict_state_reader: cheatnet_constants::build_testing_state(),
@@ -223,11 +161,11 @@ pub fn run_test_case(
     if let Some(max_n_steps) = runtime_config.max_n_steps {
         set_max_steps(&mut context, max_n_steps);
     }
-    let mut execution_resources = ExecutionResources::default();
     let mut cached_state = CachedState::new(
         state_reader,
         GlobalContractCache::new(GLOBAL_CONTRACT_CACHE_SIZE_FOR_TEST),
     );
+    let mut execution_resources = ExecutionResources::default();
     let syscall_handler = build_syscall_handler(
         &mut cached_state,
         &string_to_hint,
@@ -267,64 +205,48 @@ pub fn run_test_case(
         extended_runtime: call_to_blockifier_runtime,
     };
 
-    let mut vm = VirtualMachine::new(true);
+    let run_result =
+        match run_with_runner(&assembled_program, builtins, hints_dict, &mut forge_runtime) {
+            Ok((vm, runner)) => {
+                let vm_resources_without_inner_calls = runner
+                    .get_execution_resources(&vm)
+                    .unwrap()
+                    .filter_unused_builtins();
+                *forge_runtime
+                    .extended_runtime
+                    .extended_runtime
+                    .extended_runtime
+                    .hint_handler
+                    .resources += &vm_resources_without_inner_calls;
 
-    let data: Vec<MaybeRelocatable> = assembled_program
-        .bytecode
-        .iter()
-        .map(Felt252::from)
-        .map(MaybeRelocatable::from)
-        .collect();
-    let data_len = data.len();
-    let mut runner = build_cairo_runner(data, builtins, hints_dict)?;
+                let cells = runner.relocated_memory;
+                let ap = vm.get_relocated_trace().unwrap().last().unwrap().ap;
 
-    let run_result = match run_function_with_runner(
-        &mut vm,
-        data_len,
-        initialize_vm,
-        &mut forge_runtime,
-        &mut runner,
-    ) {
-        Ok(()) => {
-            let vm_resources_without_inner_calls = runner
-                .get_execution_resources(&vm)
-                .unwrap()
-                .filter_unused_builtins();
-            *forge_runtime
-                .extended_runtime
-                .extended_runtime
-                .extended_runtime
-                .hint_handler
-                .resources += &vm_resources_without_inner_calls;
+                let (results_data, gas_counter) =
+                    SierraCasmRunner::get_results_data(&case.test_details.return_types, &cells, ap);
+                assert_eq!(results_data.len(), 1);
 
-            let cells = runner.relocated_memory;
-            let ap = vm.get_relocated_trace().unwrap().last().unwrap().ap;
+                let (_, values) = results_data[0].clone();
+                let value = SierraCasmRunner::handle_main_return_value(
+                    // Here we assume that all test either panic or do not return any value
+                    // This is true for all test right now, but in case it changes
+                    // this logic will need to be updated
+                    Some(0),
+                    values,
+                    &cells,
+                );
 
-            let (results_data, gas_counter) =
-                SierraCasmRunner::get_results_data(&case.test_details.return_types, &cells, ap);
-            assert_eq!(results_data.len(), 1);
+                update_top_call_vm_trace(&mut forge_runtime, &vm);
 
-            let (_, values) = results_data[0].clone();
-            let value = SierraCasmRunner::handle_main_return_value(
-                // Here we assume that all test either panic or do not return any value
-                // This is true for all test right now, but in case it changes
-                // this logic will need to be updated
-                Some(0),
-                values,
-                &cells,
-            );
-
-            update_top_call_vm_trace(&mut forge_runtime, &vm);
-
-            Ok(RunResult {
-                gas_counter,
-                memory: cells,
-                value,
-                profiling_info: None,
-            })
-        }
-        Err(err) => Err(RunnerError::CairoRunError(err)),
-    };
+                Ok(RunResult {
+                    gas_counter,
+                    memory: cells,
+                    value,
+                    profiling_info: None,
+                })
+            }
+            Err(err) => Err(RunnerError::CairoRunError(err)),
+        };
 
     let call_trace_ref = get_call_trace_ref(&mut forge_runtime);
 
@@ -344,13 +266,6 @@ pub fn run_test_case(
         used_resources,
         call_trace: call_trace_ref,
     })
-}
-
-fn get_casm_instruction_offset(
-    debug_info: &[(CasmCodeOffset, CasmInstructionIdx)],
-    sierra_statement_idx: usize,
-) -> CasmCodeOffset {
-    debug_info[sierra_statement_idx].0
 }
 
 fn extract_test_case_summary(
@@ -427,60 +342,4 @@ fn get_call_trace_ref(runtime: &mut ForgeRuntime) -> Rc<RefCell<CallTrace>> {
         .trace_data
         .current_call_stack
         .top()
-}
-
-fn add_header(
-    entry_code: Vec<Instruction>,
-    assembled_program: &mut AssembledCairoProgramWithSerde,
-) {
-    let mut new_bytecode = vec![];
-    let mut new_hints = vec![];
-    for instruction in entry_code {
-        if !instruction.hints.is_empty() {
-            new_hints.push((new_bytecode.len(), instruction.hints.clone()));
-        }
-        new_bytecode.extend(instruction.assemble().encode().into_iter());
-    }
-
-    let new_bytecode_len = new_bytecode.len();
-    assembled_program.hints.iter_mut().for_each(|hint| {
-        hint.0 += new_bytecode_len;
-    });
-
-    assembled_program.hints = [new_hints, assembled_program.hints.clone()].concat();
-    assembled_program.bytecode = [new_bytecode, assembled_program.bytecode.clone()].concat();
-}
-
-fn add_footer(footer: Vec<Instruction>, assembled_program: &mut AssembledCairoProgramWithSerde) {
-    for instruction in footer {
-        assembled_program
-            .bytecode
-            .extend(instruction.assemble().encode().into_iter());
-    }
-}
-
-fn create_hints_dict(
-    assembled_program: &mut AssembledCairoProgramWithSerde,
-) -> (HashMap<String, Hint>, HashMap<usize, Vec<HintParams>>) {
-    let string_to_hint: HashMap<String, Hint> = assembled_program
-        .hints
-        .iter()
-        .flat_map(|(_, hints)| hints.iter().cloned())
-        .map(|hint| (hint.representing_string(), hint))
-        .collect();
-    let hints_dict = assembled_program
-        .hints
-        .iter()
-        .map(|(offset, hints)| {
-            (
-                *offset,
-                hints
-                    .iter()
-                    .map(hint_to_hint_params)
-                    .collect::<Vec<HintParams>>(),
-            )
-        })
-        .collect();
-
-    (string_to_hint, hints_dict)
 }
