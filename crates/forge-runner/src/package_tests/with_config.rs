8068a811474ee5d59a0a5036d868ecc6488c5ffa
Piotr Figiela <77412592+Draggu@users.noreply.github.com>
2024-06-12 17:28:09 +0200

    add conversions

--- crates/forge-runner/src/package_tests/with_config.rs
+++ crates/forge-runner/src/package_tests/with_config.rs
@@ -1,8 +1,9 @@
-use super::{
-    raw::{RawForkConfig, RawFuzzerConfig},
-    TestCase, TestTarget,
+use super::{TestCase, TestTarget};
+use crate::expected_result::{ExpectedPanicValue, ExpectedTestResult};
+use cheatnet::runtime_extensions::forge_config_extension::config::{
+    Expected, RawForgeConfig, RawForkConfig, RawFuzzerConfig, RawShouldPanicConfig,
 };
-use crate::expected_result::ExpectedTestResult;
+use conversions::serde::serialize::SerializeToFeltVec;
 
 pub type TestTargetWithConfig = TestTarget<TestCaseConfig>;
 
@@ -17,4 +18,30 @@ pub struct TestCaseConfig {
     pub expected_result: ExpectedTestResult,
     pub fork_config: Option<RawForkConfig>,
     pub fuzzer_config: Option<RawFuzzerConfig>,
+}
+
+impl From<RawForgeConfig> for TestCaseConfig {
+    fn from(value: RawForgeConfig) -> Self {
+        Self {
+            available_gas: value.available_gas.map(|v| v.gas),
+            ignored: value.ignore.is_some_and(|v| v.is_ignored),
+            expected_result: value.should_panic.into(),
+            fork_config: value.fork,
+            fuzzer_config: value.fuzzer,
+        }
+    }
+}
+
+impl From<Option<RawShouldPanicConfig>> for ExpectedTestResult {
+    fn from(value: Option<RawShouldPanicConfig>) -> Self {
+        match value {
+            None => Self::Success,
+            Some(RawShouldPanicConfig { expected }) => Self::Panics(match expected {
+                Expected::Any => ExpectedPanicValue::Any,
+                Expected::Array(arr) => ExpectedPanicValue::Exact(arr),
+                Expected::ByteArray(arr) => ExpectedPanicValue::Exact(arr.serialize_with_magic()),
+                Expected::ShortString(str) => ExpectedPanicValue::Exact(str.serialize_to_vec()),
+            }),
+        }
+    }
 }
