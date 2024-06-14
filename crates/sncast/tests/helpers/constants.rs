cf265845a0f61ad1f0e6fab2b59c4c367e0485f6
Karol Sewiło <95349104+ksew1@users.noreply.github.com>
2024-06-14 10:58:48 +0200

    Use felt! for constants.rs. Delete parse_number and use .parse() (#2198)
    <!-- Reference any GitHub issues resolved by this PR -->
    
    Related  https://github.com/foundry-rs/starknet-foundry/issues/1854
    Related https://github.com/foundry-rs/starknet-foundry/pull/2188
    
    ## Introduced changes
    
    <!-- A brief description of the changes -->
    
    - Change base_clash_hashes from &str to FieldElement to avoid parsing
    them at runtime
    - Delete `parse_number` and use `.parse()` method of `FieldElement` as
    both do the same.
    
    ## Checklist
    
    <!-- Make sure all of these are complete -->
    
    
    - [x] Linked relevant issue
    - [ ] Updated relevant documentation
    - [ ] Added relevant tests
    - [x] Performed self-review of the code
    - [ ] Added changes to `CHANGELOG.md`
    
    Co-authored-by: Karol Sewilo <karol.sewilo@swmansion.com>

--- crates/sncast/tests/helpers/constants.rs
+++ crates/sncast/tests/helpers/constants.rs
@@ -18,10 +18,6 @@ pub const DEVNET_OZ_CLASS_HASH_CAIRO_0: &str =
 pub const DEVNET_PREDEPLOYED_ACCOUNT_ADDRESS: &str =
     "0x691a61b12a7105b1372cc377f135213c11e8400a546f6b0e7ea0296046690ce";
 
-// https://github.com/0xSpaceShard/starknet-devnet-rs/blob/1a76e9d29541af2667ca815c47bb332cead27c55/crates/starknet/src/constants.rs#L17
-pub const DEVNET_OZ_CLASS_HASH_CAIRO_1: &str =
-    "0x61dac032f228abef9c6626f995015233097ae253a7f72d68552db02f2971b8f";
-
 pub const MAP_CONTRACT_ADDRESS_SEPOLIA: &str =
     "0xcd8f9ab31324bb93251837e4efb4223ee195454f6304fcfcb277e277653008";
 
@@ -33,9 +29,3 @@ pub const MAP_CONTRACT_DECLARE_TX_HASH_SEPOLIA: &str =
 
 pub const CONSTRUCTOR_WITH_PARAMS_CONTRACT_CLASS_HASH_SEPOLIA: &str =
     "0x59426c817fb8103edebdbf1712fa084c6744b2829db9c62d1ea4dce14ee6ded";
-
-pub const ARGENT_ACCOUNT_CLASS_HASH: &str =
-    "0x29927c8af6bccf3f6fda035981e765a7bdbf18a2dc0d630494f8758aa908e2b";
-
-pub const BRAAVOS_CLASS_HASH: &str =
-    "0x816dd0297efc55dc1e7559020a3a825e81ef734b558f03c83325d4da7e6253";
