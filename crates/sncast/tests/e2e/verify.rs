f0568e82ba196f1e127ddf2b9e8542d0d59816a2
Roman <barabanov1233@gmail.com>
2024-07-06 15:29:26 +0400

    Add test case with confirm verification flag

--- crates/sncast/tests/e2e/verify.rs
+++ crates/sncast/tests/e2e/verify.rs
@@ -169,3 +169,53 @@ async fn test_wrong_contract_name_passed() {
         ),
     );
 }
+
+#[tokio::test]
+async fn test_happy_case_with_confirm_verification_flag() {
+    let contract_path = copy_directory_to_tempdir(CONTRACTS_DIR.to_string() + "/map");
+
+    let mock_server = MockServer::start().await;
+
+    let verifier_response = "Contract successfully verified";
+
+    Mock::given(method("POST"))
+        .and(path("/v1/sn_sepolia/verify"))
+        .respond_with(
+            ResponseTemplate::new(200)
+                .append_header("content-type", "text/plain")
+                .set_body_string(verifier_response),
+        )
+        .mount(&mock_server)
+        .await;
+
+    let mut args = default_cli_args();
+    args.append(&mut vec![
+        "verify",
+        "--contract-address",
+        MAP_CONTRACT_ADDRESS_SEPOLIA,
+        "--contract-name",
+        "Map",
+        "--verifier",
+        "walnut",
+        "--network",
+        "sepolia",
+        "--confirm-verification",
+    ]);
+
+    let snapbox = runner(&args)
+        .env("WALNUT_API_URL", mock_server.uri())
+        .current_dir(contract_path.path());
+
+    let output = snapbox.assert().success();
+
+    assert_stdout_contains(
+        output,
+        formatdoc!(
+            r"
+        command: verify
+        message: {}
+        ",
+            verifier_response
+        ),
+    );
+}
