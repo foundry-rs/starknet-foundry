8e34826f67ec2f1d97943d8e8cdb544051151188
Jan Smółka <jsmolka@student.agh.edu.pl>
2024-07-18 10:35:22 +0200

    Redesigned printing

--- crates/sncast/src/starknet_commands/account/list.rs
+++ crates/sncast/src/starknet_commands/account/list.rs
@@ -1,15 +1,18 @@
 use anyhow::Context;
 use camino::Utf8PathBuf;
 use clap::Args;
+use conversions::string::IntoDecStr;
+use conversions::string::IntoHexStr;
 use itertools::Itertools;
-use serde_json::Value;
+use serde::Deserialize;
+use serde::Serialize;
+use sncast::AccountType;
 use sncast::{
     check_account_file_exists, read_and_parse_json_file, response::print::OutputFormat,
     AccountData, NumbersFormat,
 };
-use starknet::core::types::FieldElement;
 use std::collections::HashMap;
-use std::fmt::{Debug, Display, LowerHex};
+use std::fmt::Display;
 
 #[derive(Args, Debug)]
 #[command(
@@ -23,52 +26,122 @@ pub struct List {
     pub display_private_keys: bool,
 }
 
-fn println_numeric_field<T: Debug + Display + LowerHex>(
-    title: &'static str,
-    format: NumbersFormat,
-    item: T,
-) {
-    match format {
-        NumbersFormat::Default | NumbersFormat::Hex => println!("  {title}: {item:#x}"),
-        NumbersFormat::Decimal => println!("  {title}: {item}"),
+#[derive(Deserialize, Serialize, Clone, Debug)]
+pub struct AccountDataRepresentation {
+    pub public_key: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub private_key: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub network: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub address: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub salt: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub deployed: Option<bool>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub class_hash: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub legacy: Option<bool>,
+    #[serde(default, rename(serialize = "type", deserialize = "type"))]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub account_type: Option<AccountType>,
+}
+
+impl AccountDataRepresentation {
+    fn new(
+        account: &AccountData,
+        display_private_key: bool,
+        numbers_format: NumbersFormat,
+    ) -> Self {
+        match numbers_format {
+            NumbersFormat::Default | NumbersFormat::Hex => Self {
+                private_key: display_private_key.then(|| account.private_key.into_hex_string()),
+                public_key: account.public_key.into_hex_string(),
+                network: None,
+                address: account.address.map(IntoHexStr::into_hex_string),
+                salt: account.salt.map(IntoHexStr::into_hex_string),
+                deployed: account.deployed,
+                class_hash: account.class_hash.map(IntoHexStr::into_hex_string),
+                legacy: account.legacy,
+                account_type: account.account_type,
+            },
+            NumbersFormat::Decimal => Self {
+                private_key: display_private_key.then(|| account.private_key.into_dec_string()),
+                public_key: account.public_key.into_dec_string(),
+                network: None,
+                address: account.address.map(IntoDecStr::into_dec_string),
+                salt: account.salt.map(IntoDecStr::into_dec_string),
+                deployed: account.deployed,
+                class_hash: account.class_hash.map(IntoDecStr::into_dec_string),
+                legacy: account.legacy,
+                account_type: account.account_type,
+            },
+        }
+    }
+
+    fn set_network(&mut self, network: &str) {
+        self.network = Some(network.to_owned());
     }
 }
 
-fn print_pretty(
-    account: &AccountData,
-    name: &str,
-    network: &str,
+type NestedMap<T> = HashMap<String, HashMap<String, T>>;
+
+fn represent(
+    data: &NestedMap<AccountData>,
     display_private_keys: bool,
     numbers_format: NumbersFormat,
-) {
-    macro_rules! println_some {
-        ($title:expr, $numbers_format:expr, $item:expr) => {
-            if let Some(it) = $item {
-                println_numeric_field($title, $numbers_format, it);
-            }
-        };
+) -> NestedMap<AccountDataRepresentation> {
+    data.iter()
+        .map(|(network, accounts)| {
+            let accounts = accounts
+                .iter()
+                .map(|(name, account)| {
+                    (
+                        name.to_owned(),
+                        AccountDataRepresentation::new(
+                            account,
+                            display_private_keys,
+                            numbers_format,
+                        ),
+                    )
+                })
+                .collect::<HashMap<_, _>>();
 
-        ($title:expr, $item:expr) => {
-            if let Some(it) = $item {
-                println!("  {}: {}", $title, it);
-            }
-        };
+            (network.to_owned(), accounts)
+        })
+        .collect::<HashMap<_, _>>()
+}
+
+fn print_as_json(networks: &NestedMap<AccountDataRepresentation>) -> anyhow::Result<()> {
+    if networks.values().all(|net| net.values().len() == 0) {
+        println!("{{}}");
+        return Ok(());
     }
 
+    let json = serde_json::to_string_pretty(networks)?;
+    print!("{json}");
+
+    Ok(())
+}
+
+fn print_if_some<T: Display>(title: &str, item: &Option<T>) {
+    if let Some(ref item) = item {
+        println!("  {title}: {item}");
+    }
+}
+
+fn print_pretty(data: &AccountDataRepresentation, name: &str) {
     println!("- {name}:");
-
-    if display_private_keys {
-        println_numeric_field("private key", numbers_format, account.private_key);
-    };
-
-    println_numeric_field("public key", numbers_format, account.public_key);
-    println!("  network: {network}");
-    println_some!("address", numbers_format, account.address);
-    println_some!("salt", numbers_format, account.salt);
-    println_some!("class hash", numbers_format, account.class_hash);
-    println_some!("deployed", account.deployed);
-    println_some!("legacy", account.legacy);
-    println_some!("type", account.account_type);
+    print_if_some("network", &data.network);
+    print_if_some("private key", &data.private_key);
+    println!("  public key: {}", data.public_key);
+    print_if_some("address", &data.address);
+    print_if_some("salt", &data.salt);
+    print_if_some("class hash", &data.class_hash);
+    print_if_some("deployed", &data.deployed);
+    print_if_some("legacy", &data.legacy);
+    print_if_some("type", &data.account_type);
     println!();
 }
 
@@ -87,71 +160,17 @@ fn print_as_human(
 
     for (network, accounts) in networks.iter().sorted_by_key(|(name, _)| *name) {
         for (name, data) in accounts.iter().sorted_by_key(|(name, _)| *name) {
-            print_pretty(data, name, network, display_private_keys, numbers_format);
+            let mut data_repr =
+                AccountDataRepresentation::new(data, display_private_keys, numbers_format);
+
+            data_repr.set_network(network);
+            print_pretty(&data_repr, name);
         }
     }
 
     if !display_private_keys {
         println!("\nTo show private keys too, run with --display-private-keys or -p");
     }
-}
-
-type JsonMap = HashMap<String, HashMap<String, HashMap<String, Value>>>;
-
-fn erase_private_keys(networks: &mut JsonMap) {
-    for (_, accounts) in networks.iter_mut() {
-        for (_, account) in accounts.iter_mut() {
-            account.remove("private_key");
-        }
-    }
-}
-
-fn reformat_numeric_fields(networks: &mut JsonMap) -> anyhow::Result<()> {
-    macro_rules! replace_some {
-        ($account:expr, $field:expr) => {
-            if let Some(field) = $account.get_mut($field) {
-                let value = FieldElement::from_hex_be(field.as_str().unwrap())
-                    .context("Failed to parse account JSON")?;
-                *field = Value::String(format!("{value}"));
-            }
-        };
-    }
-
-    for (_, accounts) in networks.iter_mut() {
-        for (_, account) in accounts.iter_mut() {
-            replace_some!(account, "private_key");
-            replace_some!(account, "public_key");
-            replace_some!(account, "address");
-            replace_some!(account, "salt");
-            replace_some!(account, "class_hash");
-        }
-    }
-
-    Ok(())
-}
-
-fn print_as_json(
-    networks: &mut JsonMap,
-    display_private_keys: bool,
-    numbers_format: NumbersFormat,
-) -> anyhow::Result<()> {
-    if networks.values().all(|net| net.values().len() == 0) {
-        println!("{{}}");
-        return Ok(());
-    }
-
-    if !display_private_keys {
-        erase_private_keys(networks);
-    }
-
-    if numbers_format == NumbersFormat::Decimal {
-        reformat_numeric_fields(networks)?;
-    }
-
-    let json = serde_json::to_string_pretty(networks)?;
-    print!("{json}");
-
-    Ok(())
 }
 
 pub fn print_account_list(
@@ -169,9 +188,10 @@ pub fn print_account_list(
 
     match output_format {
         OutputFormat::Json => {
-            let mut networks: JsonMap = read_and_parse_json_file(accounts_file)?;
+            let networks: NestedMap<AccountData> = read_and_parse_json_file(accounts_file)?;
+            let networks = represent(&networks, display_private_keys, numbers_format);
 
-            print_as_json(&mut networks, display_private_keys, numbers_format)?;
+            print_as_json(&networks)?;
         }
         OutputFormat::Human => {
             let networks: HashMap<String, HashMap<String, AccountData>> =
