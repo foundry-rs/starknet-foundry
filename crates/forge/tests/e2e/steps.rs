ac1433fdfd710904e038e7b59f8be03dc950928c
Jan Smółka <jsmolka@student.agh.edu.pl>
2024-08-20 10:23:59 +0200

    Fixed steps-related tests to match the new blockifier limit

--- crates/forge/tests/e2e/steps.rs
+++ crates/forge/tests/e2e/steps.rs
@@ -1,6 +1,6 @@
 use super::common::runner::{setup_package, test_runner};
 use indoc::indoc;
-use shared::test_utils::output_assert::{assert_stdout_contains, AsOutput};
+use shared::test_utils::output_assert::assert_stdout_contains;
 
 #[test]
 fn should_allow_less_than_default() {
@@ -18,25 +18,24 @@ fn should_allow_less_than_default() {
                 [..]Compiling[..]
                 [..]Finished[..]
 
-
                 Collected 4 test(s) from steps package
                 Running 4 test(s) from src/
-                [FAIL] steps::tests::steps_4000005
-
-                Failure data:
-                    Could not reach the end of the program. RunResources has no remaining steps.
-
-                [FAIL] steps::tests::steps_5699625
-
-                Failure data:
-                    Could not reach the end of the program. RunResources has no remaining steps.
-
-                [FAIL] steps::tests::steps_3999990
-
-                Failure data:
-                    Could not reach the end of the program. RunResources has no remaining steps.
-
                 [FAIL] steps::tests::steps_570030
+
+                Failure data:
+                    Could not reach the end of the program. RunResources has no remaining steps.
+
+                [FAIL] steps::tests::steps_11250075
+
+                Failure data:
+                    Could not reach the end of the program. RunResources has no remaining steps.
+
+                [FAIL] steps::tests::steps_10000005
+
+                Failure data:
+                    Could not reach the end of the program. RunResources has no remaining steps.
+
+                [FAIL] steps::tests::steps_9999990
 
                 Failure data:
                     Could not reach the end of the program. RunResources has no remaining steps.
@@ -44,21 +43,21 @@ fn should_allow_less_than_default() {
                 Tests: 0 passed, 4 failed, 0 skipped, 0 ignored, 0 filtered out
 
                 Failures:
-                    steps::tests::steps_4000005
-                    steps::tests::steps_5699625
-                    steps::tests::steps_3999990
                     steps::tests::steps_570030
+                    steps::tests::steps_11250075
+                    steps::tests::steps_10000005
+                    steps::tests::steps_9999990
             "
         ),
     );
 }
 #[test]
-// 4_000_000 is blockifier limit we want to omit
-fn should_allow_more_than_4m() {
+// 10_000_000 is blockifier limit we want to omit
+fn should_allow_more_than_10m() {
     let temp = setup_package("steps");
 
     let output = test_runner(&temp)
-        .args(["--max-n-steps", "5700031"])
+        .args(["--max-n-steps", "15000100"])
         .assert()
         .code(0);
 
@@ -69,53 +68,50 @@ fn should_allow_more_than_4m() {
                 [..]Compiling[..]
                 [..]Finished[..]
 
-
                 Collected 4 test(s) from steps package
                 Running 4 test(s) from src/
-                [PASS] steps::tests::steps_570030 [..]
-                [PASS] steps::tests::steps_4000005 [..]
-                [PASS] steps::tests::steps_3999990 [..]
-                [PASS] steps::tests::steps_5699625 [..]
+                [PASS] steps::tests::steps_570030 (gas: ~1521)
+                [PASS] steps::tests::steps_10000005 (gas: ~26667)
+                [PASS] steps::tests::steps_9999990 (gas: ~26667)
+                [PASS] steps::tests::steps_11250075 (gas: ~30001)
                 Tests: 4 passed, 0 failed, 0 skipped, 0 ignored, 0 filtered out
             "
         ),
     );
 }
 #[test]
-fn should_default_to_4m() {
+fn should_default_to_10m() {
     let temp = setup_package("steps");
 
-    let output = test_runner(&temp).assert().code(0);
+    let output = test_runner(&temp).assert().code(1);
 
-    panic!("{}", output.as_stdout());
+    assert_stdout_contains(
+        output,
+        indoc!(
+            r"
+            [..]Compiling[..]
+            [..]Finished[..]
 
-    // assert_stdout_contains(
-    //     output,
-    //     indoc!(
-    //         r"
-    //             [..]Compiling[..]
-    //             [..]Finished[..]
+            Collected 4 test(s) from steps package
+            Running 4 test(s) from src/
+            [PASS] steps::tests::steps_570030 (gas: ~1521)
+            [FAIL] steps::tests::steps_10000005
 
-    //             Collected 4 test(s) from steps package
-    //             Running 4 test(s) from src/
-    //             [PASS] steps::tests::steps_570030 [..]
-    //             [FAIL] steps::tests::steps_4000005
+            Failure data:
+                Could not reach the end of the program. RunResources has no remaining steps.
 
-    //             Failure data:
-    //                 Could not reach the end of the program. RunResources has no remaining steps.
+            [FAIL] steps::tests::steps_11250075
 
-    //             [FAIL] steps::tests::steps_5699625
+            Failure data:
+                Could not reach the end of the program. RunResources has no remaining steps.
 
-    //             Failure data:
-    //                 Could not reach the end of the program. RunResources has no remaining steps.
+            [PASS] steps::tests::steps_9999990 (gas: ~26667)
+            Tests: 2 passed, 2 failed, 0 skipped, 0 ignored, 0 filtered out
 
-    //             [PASS] steps::tests::steps_3999990 [..]
-    //             Tests: 2 passed, 2 failed, 0 skipped, 0 ignored, 0 filtered out
-
-    //             Failures:
-    //                 steps::tests::steps_4000005
-    //                 steps::tests::steps_5699625
-    //         "
-    //     ),
-    // );
+            Failures:
+                steps::tests::steps_10000005
+                steps::tests::steps_11250075
+            "
+        ),
+    );
 }
