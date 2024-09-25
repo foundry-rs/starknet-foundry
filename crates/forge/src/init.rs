537143b89e0f4a067bf10176cacb6b6ee1fa9de2
Fiiranek <jellybean@onet.eu>
2024-09-25 17:11:00 +0200

    Update `Scarb.toml` template content

--- crates/forge/src/init.rs
+++ crates/forge/src/init.rs
@@ -44,7 +44,7 @@ fn add_template_to_scarb_manifest(path: &PathBuf) -> Result<()> {
     # Visit https://foundry-rs.github.io/starknet-foundry/appendix/scarb-toml.html for more information
 
     # [tool.snforge]                                             # Define `snforge` tool section
-    # exit_first = true                                          # Stop test execution immediately upon the first failure.
+    # exit_first = true                                          # Stop test execution immediately upon the first failure
     # fuzzer_runs = 1234                                         # Number of runs of the random fuzzer
     # fuzzer_seed = 1111                                         # Seed for the random fuzzer
 
