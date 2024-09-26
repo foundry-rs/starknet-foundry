9a6940ce1e5e6f8512c78f7fac577fbc1efe6863
Fiiranek <jellybean@onet.eu>
2024-09-27 01:35:29 +0200

    Add test for common arguments after subcommand

--- crates/sncast/tests/e2e/invoke.rs
+++ crates/sncast/tests/e2e/invoke.rs
@@ -52,6 +52,45 @@ async fn test_happy_case(account: &str) {
     assert!(matches!(receipt, Invoke(_)));
 }
 
+#[test_case("oz_cairo_0"; "cairo_0_account")]
+#[test_case("oz_cairo_1"; "cairo_1_account")]
+#[test_case("oz"; "oz_account")]
+#[test_case("argent"; "argent_account")]
+#[test_case("braavos"; "braavos_account")]
+#[tokio::test]
+
+async fn test_happy_case_common_arguments_after_subcommand(account: &str) {
+    let args = vec![
+        "invoke",
+        "--accounts-file",
+        ACCOUNT_FILE_PATH,
+        "--account",
+        account,
+        "--int-format",
+        "--json",
+        "--url",
+        URL,
+        "--contract-address",
+        MAP_CONTRACT_ADDRESS_SEPOLIA,
+        "--function",
+        "put",
+        "--calldata",
+        "0x1 0x2",
+        "--max-fee",
+        "99999999999999999",
+        "--fee-token",
+        "eth",
+    ];
+
+    let snapbox = runner(&args);
+    let output = snapbox.assert().success().get_output().stdout.clone();
+
+    let hash = get_transaction_hash(&output);
+    let receipt = get_transaction_receipt(hash).await;
+
+    assert!(matches!(receipt, Invoke(_)));
+}
+
 #[tokio::test]
 async fn test_happy_case_human_readable() {
     let tempdir = create_and_deploy_account(OZ_CLASS_HASH, AccountType::OpenZeppelin).await;
