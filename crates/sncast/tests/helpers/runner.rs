b6e8af3eafbcf5cb794e4f352cdf0fbdc4a20803
Fiiranek <jellybean@onet.eu>
2024-10-01 16:19:12 +0200

    Revert "Add command validation"
    This reverts commit 88a235d2c0cc62cef395c198cb1e4e1c12027e47.

--- crates/sncast/tests/helpers/runner.rs
+++ crates/sncast/tests/helpers/runner.rs
@@ -1,11 +1,7 @@
-use clap::{Arg, Command as ClapCommand};
 use snapbox::cmd::{cargo_bin, Command};
 
 #[must_use]
 pub fn runner(args: &[&str]) -> Command {
-    let clap_command = ClapCommand::new(cargo_bin!("sncast").to_str().unwrap())
-        .args(args.iter().map(|&s| Arg::new(s.to_string())));
-    clap_command.debug_assert();
     let command = Command::new(cargo_bin!("sncast")).args(args);
     command
 }
