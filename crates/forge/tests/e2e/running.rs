29f1e5eabf40192419cfd80f9a6d5b30b4e26892
Fiiranek <jellybean@onet.eu>
2024-10-10 01:26:44 +0200

    Fix `init_new_project`

--- crates/forge/tests/e2e/running.rs
+++ crates/forge/tests/e2e/running.rs
@@ -770,7 +770,7 @@ fn validate_init(temp: &TempDir, validate_snforge_std: bool) {
         Running 2 test(s) from tests/
         [PASS] test_name_integrationtest::test_contract::test_increase_balance [..]
         [PASS] test_name_integrationtest::test_contract::test_cannot_increase_balance_with_zero_value [..]
-        Tests: 2 passed, 0 failed, 0 skipped, 0 ignored, other filtered out
+        Tests: 2 passed, 0 failed, 0 skipped, 0 ignored, 0 filtered out
         "
     );
 
