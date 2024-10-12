7b87ce158265a071e8fbe9e4cf3049139518affb
chen <23054115+cwkang1998@users.noreply.github.com>
2024-10-13 00:23:42 +0800

    Make `verify` & some shared methods default

--- crates/sncast/src/starknet_commands/verification/base.rs
+++ crates/sncast/src/starknet_commands/verification/base.rs
@@ -4,43 +4,31 @@ use camino::Utf8PathBuf;
 use reqwest::StatusCode;
 use serde::Serialize;
 use sncast::response::structs::VerifyResponse;
-use sncast::Network;
 use starknet::core::types::Felt;
 use std::ffi::OsStr;
 use walkdir::WalkDir;
 
 #[async_trait]
 pub trait VerificationInterface {
-    fn new(network: Network, workspace_dir: Utf8PathBuf) -> Self;
-    async fn verify(
-        &self,
-        contract_address: Option<Felt>,
-        class_hash: Option<Felt>,
-        class_name: String,
-    ) -> Result<VerifyResponse>;
+    fn get_workspace_dir(&self) -> Utf8PathBuf;
+
     fn gen_explorer_url(&self) -> String;
-}
 
-pub struct BaseVerificationInterface {
-    pub network: Network,
-    pub workspace_dir: Utf8PathBuf,
-}
-
-impl BaseVerificationInterface {
-    pub fn read_workspace_files(&self) -> Result<serde_json::Map<String, serde_json::Value>> {
+    fn read_workspace_files(&self) -> Result<serde_json::Map<String, serde_json::Value>> {
         // Read all files name along with their contents in a JSON format
         // in the workspace dir recursively
         // key is the file name and value is the file content
         let mut file_data = serde_json::Map::new();
 
         // Recursively read files and their contents in workspace directory
-        for entry in WalkDir::new(self.workspace_dir.clone()).follow_links(true) {
+        let workspace_dir = self.get_workspace_dir();
+        for entry in WalkDir::new(workspace_dir.clone()).follow_links(true) {
             let entry = entry?;
             let path = entry.path();
             if path.is_file() {
                 if let Some(extension) = path.extension() {
                     if extension == OsStr::new("cairo") || extension == OsStr::new("toml") {
-                        let relative_path = path.strip_prefix(self.workspace_dir.clone())?;
+                        let relative_path = path.strip_prefix(workspace_dir.clone())?;
                         let file_content = std::fs::read_to_string(path)?;
                         file_data.insert(
                             relative_path.to_string_lossy().into_owned(),
@@ -53,7 +41,7 @@ impl BaseVerificationInterface {
         Ok(file_data)
     }
 
-    pub async fn send_verification_request(
+    async fn send_verification_request(
         &self,
         url: String,
         payload: VerificationPayload,
@@ -78,6 +66,24 @@ impl BaseVerificationInterface {
             let message = api_res.text().await.context("Failed to verify contract")?;
             Err(anyhow!(message))
         }
+    }
+
+    async fn verify(
+        &self,
+        contract_address: Option<Felt>,
+        class_hash: Option<Felt>,
+        class_name: String,
+    ) -> Result<VerifyResponse> {
+        let file_data = self.read_workspace_files()?;
+        let source_code = serde_json::Value::Object(file_data);
+        let payload = VerificationPayload {
+            class_name,
+            contract_address,
+            class_hash,
+            source_code,
+        };
+        let url = self.gen_explorer_url();
+        self.send_verification_request(url, payload).await
     }
 }
 
--- crates/sncast/src/starknet_commands/verification/voyager.rs
+++ crates/sncast/src/starknet_commands/verification/voyager.rs
@@ -1,43 +1,27 @@
-use super::base::{BaseVerificationInterface, VerificationInterface, VerificationPayload};
-use anyhow::Result;
+use super::base::VerificationInterface;
 use async_trait::async_trait;
 use camino::Utf8PathBuf;
-use sncast::response::structs::VerifyResponse;
 use sncast::Network;
-use starknet::core::types::Felt;
 use std::env;
 
 pub struct VoyagerVerificationInterface {
-    base: BaseVerificationInterface,
+    pub network: Network,
+    pub workspace_dir: Utf8PathBuf,
+}
+
+impl VoyagerVerificationInterface {
+    pub fn new(network: Network, workspace_dir: Utf8PathBuf) -> Self {
+        VoyagerVerificationInterface {
+            network,
+            workspace_dir,
+        }
+    }
 }
 
 #[async_trait]
 impl VerificationInterface for VoyagerVerificationInterface {
-    fn new(network: Network, workspace_dir: Utf8PathBuf) -> Self {
-        VoyagerVerificationInterface {
-            base: BaseVerificationInterface {
-                network,
-                workspace_dir,
-            },
-        }
-    }
-
-    async fn verify(
-        &self,
-        contract_address: Option<Felt>,
-        class_hash: Option<Felt>,
-        class_name: String,
-    ) -> Result<VerifyResponse> {
-        let file_data = self.base.read_workspace_files()?;
-        let source_code = serde_json::Value::Object(file_data);
-        let payload = VerificationPayload {
-            class_name,
-            contract_address,
-            class_hash,
-            source_code,
-        };
-        let url = self.gen_explorer_url();
-        self.base.send_verification_request(url, payload).await
+    fn get_workspace_dir(&self) -> Utf8PathBuf {
+        self.workspace_dir.clone()
     }
 
     fn gen_explorer_url(&self) -> String {
@@ -46,7 +30,7 @@ impl VerificationInterface for VoyagerVerificationInterface {
             return custom_api_url;
         }
 
-        let api_verification_url = match self.base.network {
+        let api_verification_url = match self.network {
             Network::Mainnet => "https://api.voyager.online/beta/class-verify-v2",
             Network::Sepolia => "https://sepolia-api.voyager.online/beta/class-verify-v2",
         };
