426d245dd969e0a15bf4be0f4cc4bbc1d892d24c
Artur Michałek <52135326+cptartur@users.noreply.github.com>
2024-10-23 17:17:37 +0200

    Get features from all targets - refactor (#2600)
    <!-- Reference any GitHub issues resolved by this PR -->
    
    Closes #
    
    ## Introduced changes
    
    <!-- A brief description of the changes -->
    
    Part of stack
    
    Base: https://github.com/foundry-rs/starknet-foundry/pull/2570
    
    ## Checklist
    
    <!-- Make sure all of these are complete -->
    
    - [x] Linked relevant issue
    - [x] Updated relevant documentation
    - [x] Added relevant tests
    - [x] Performed self-review of the code
    - [x] Added changes to `CHANGELOG.md`

---
+++ crates/scarb-api/src/artifacts.rs
@@ -0,0 +1,4 @@
+#[allow(clippy::module_inception)]
+pub mod artifacts;
+pub mod deserialized;
+pub mod representation;
---
+++ crates/scarb-api/src/artifacts/representation.rs
@@ -0,0 +1,78 @@
+use crate::artifacts::deserialized::{artifacts_for_package, StarknetArtifacts};
+use anyhow::anyhow;
+use camino::{Utf8Path, Utf8PathBuf};
+
+pub(super) struct StarknetArtifactsRepresentation {
+    path: Utf8PathBuf,
+    artifacts: StarknetArtifacts,
+}
+
+impl StarknetArtifactsRepresentation {
+    pub(super) fn try_from_path(path: &Utf8Path) -> anyhow::Result<Self> {
+        let artifacts = artifacts_for_package(path)?;
+        let path = path
+            .parent()
+            .ok_or_else(|| anyhow!("Failed to get parent for path = {}", &path))?
+            .to_path_buf();
+
+        Ok(Self { path, artifacts })
+    }
+
+    pub(super) fn artifacts(self) -> Vec<(String, Utf8PathBuf)> {
+        self.artifacts
+            .contracts
+            .into_iter()
+            .map(|contract| {
+                (
+                    contract.contract_name,
+                    self.path.join(contract.artifacts.sierra.as_path()),
+                )
+            })
+            .collect()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::ScarbCommand;
+    use assert_fs::fixture::{FileTouch, FileWriteStr, PathChild, PathCopy};
+    use assert_fs::TempDir;
+    use camino::Utf8PathBuf;
+
+    use super::*;
+
+    #[test]
+    fn parsing_starknet_artifacts() {
+        let temp = crate::tests::setup_package("basic_package");
+
+        ScarbCommand::new_with_stdio()
+            .current_dir(temp.path())
+            .arg("build")
+            .run()
+            .unwrap();
+
+        let artifacts_path = temp
+            .path()
+            .join("target/dev/basic_package.starknet_artifacts.json");
+        let artifacts_path = Utf8PathBuf::from_path_buf(artifacts_path).unwrap();
+
+        let artifacts = artifacts_for_package(&artifacts_path).unwrap();
+
+        assert!(!artifacts.contracts.is_empty());
+    }
+
+    #[test]
+    fn parsing_starknet_artifacts_on_invalid_file() {
+        let temp = TempDir::new().unwrap();
+        temp.copy_from("../../", &[".tool-versions"]).unwrap();
+        let path = temp.child("wrong.json");
+        path.touch().unwrap();
+        path.write_str("\"aa\": {}").unwrap();
+        let artifacts_path = Utf8PathBuf::from_path_buf(path.to_path_buf()).unwrap();
+
+        let result = artifacts_for_package(&artifacts_path);
+        let err = result.unwrap_err();
+
+        assert!(err.to_string().contains(&format!("Failed to parse {artifacts_path:?} contents. Make sure you have enabled sierra code generation in Scarb.toml")));
+    }
+}
