13ad8b75c0a9d0795321143ef64b4210d1a9c8ed
Dariusz Doktorski <dariusz.doktorski@swmansion.com>
2024-11-08 16:47:57 +0100

    Ensure tests can be run with different Scarb versions

--- crates/forge/test_utils/src/lib.rs
+++ crates/forge/test_utils/src/lib.rs
@@ -6,6 +6,11 @@ use std::path::PathBuf;
 use anyhow::Result;
 use assert_fs::fixture::PathCopy;
 use project_root::get_project_root;
+use scarb_api::ScarbCommand;
+use semver::Version;
+
+const DEFAULT_ASSERT_MACROS: Version = Version::new(0, 1, 0);
+const MINIMAL_SCARB_FOR_CORRESPONDING_ASSERT_MACROS: Version = Version::new(2, 8, 0);
 
 pub fn get_local_snforge_std_absolute_path() -> Result<PathBuf> {
     Ok(get_project_root()?.canonicalize()?.join("snforge_std"))
@@ -16,4 +21,15 @@ pub fn tempdir_with_tool_versions() -> Result<assert_fs::TempDir> {
     let temp_dir = assert_fs::TempDir::new()?;
     temp_dir.copy_from(project_root, &[".tool-versions"])?;
     Ok(temp_dir)
+}
+
+pub fn get_assert_macros_version() -> Result<Version> {
+    let scarb_version_output = ScarbCommand::version().run()?;
+    let assert_macros_version =
+        if scarb_version_output.scarb < MINIMAL_SCARB_FOR_CORRESPONDING_ASSERT_MACROS {
+            DEFAULT_ASSERT_MACROS
+        } else {
+            scarb_version_output.cairo
+        };
+    Ok(assert_macros_version)
 }
