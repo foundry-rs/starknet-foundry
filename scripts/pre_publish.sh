d943e439c52d26a56ea73ba4fb6e07f86afb93e6
kkawula <57270771+kkawula@users.noreply.github.com>
2024-11-13 00:31:23 +0100

    Update script

--- scripts/pre_publish.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-SOURCE=$1
-
-MANIFEST_FILE="../snforge_std/Scarb.toml"
-
-sed -i.bak "/snforge_scarb_plugin/ s/\(snforge_scarb_plugin = \).*/\1\"${SOURCE}\"/" $MANIFEST_FILE
-
-rm ${MANIFEST_FILE}.bak 2> /dev/null
---
+++ scripts/set_plugin_version.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PLUGIN_FILE_PATH="../crates/snforge-scarb-plugin/Scarb.toml"
+
+VERSION=$(grep '^version' "$PLUGIN_FILE_PATH" | sed -E 's/version = "([^"]*)"/\1/')
+
+STD_FILE_PATH="../snforge_std/Scarb.toml"
+
+sed -i.bak "/snforge_scarb_plugin/ s/\(snforge_scarb_plugin = \).*/\1\"^${VERSION}\"/" $STD_FILE_PATH
+
+rm ${STD_FILE_PATH}.bak 2> /dev/null
