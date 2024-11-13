7bc764fe37ea840b2e3deb4ea582e479d995c446
pedro rosalba <pedro.dandrea@gmail.com>
2024-11-13 14:46:24 -0300

    added module to lib and fmt code

--- crates/cheatnet/src/runtime_extensions/forge_runtime_extension/cheatcodes/cheat_execution_info.rs
+++ crates/cheatnet/src/runtime_extensions/forge_runtime_extension/cheatcodes/cheat_execution_info.rs
@@ -2,10 +2,10 @@ use crate::{
     state::{CheatSpan, CheatStatus},
     CheatnetState,
 };
+use cairo_vm::Felt252;
 use conversions::serde::{deserialize::CairoDeserialize, serialize::CairoSerialize};
 use starknet_api::core::ContractAddress;
 use starknet_types_core::felt::Felt;
-use cairo_vm::Felt252;
 
 #[derive(CairoDeserialize, Clone, Debug)]
 pub struct CheatArguments<T> {
