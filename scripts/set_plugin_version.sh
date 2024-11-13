9badfea44e5babf7ceb8d51c6705b3d07a50347c
kkawula <57270771+kkawula@users.noreply.github.com>
2024-11-13 14:24:13 +0100

    Revert "Update script"
    This reverts commit 9ce17e18a96c3db34f435f8f32e7d72051201311.

--- scripts/set_plugin_version.sh
+++ scripts/set_plugin_version.sh
@@ -6,4 +6,6 @@ VERSION=$(cat "$PLUGIN_FILE_PATH" | grep version | cut -d '"' -f 2)
 
 STD_FILE_PATH="../snforge_std/Scarb.toml"
 
-sed -i "/snforge_scarb_plugin/ s/\(snforge_scarb_plugin = \).*/\1\"^${VERSION}\"/" $STD_FILE_PATH
+sed -i.bak "/snforge_scarb_plugin/ s/\(snforge_scarb_plugin = \).*/\1\"^${VERSION}\"/" $STD_FILE_PATH
+
+rm ${STD_FILE_PATH}.bak 2> /dev/null
