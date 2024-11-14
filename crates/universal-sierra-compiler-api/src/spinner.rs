5c5e3d9aaf13fb1e1b45dfde53e2dbef0d279cac
Dariusz Doktorski <dariusz.doktorski@swmansion.com>
2024-11-14 12:43:56 +0100

    Replace `Utf8PathBuf` to `Utf8Path` in `spinner.rs`

--- crates/universal-sierra-compiler-api/src/spinner.rs
+++ crates/universal-sierra-compiler-api/src/spinner.rs
@@ -1,10 +1,10 @@
 use anyhow::Result;
-use camino::Utf8PathBuf;
+use camino::Utf8Path;
 use indicatif::{ProgressBar, ProgressStyle};
 use std::env;
 use std::time::Duration;
 
-pub fn spawn_spinner_message(sierra_file_path: &Utf8PathBuf) -> Result<ProgressBar> {
+pub fn spawn_spinner_message(sierra_file_path: &Utf8Path) -> Result<ProgressBar> {
     let spinner = ProgressBar::new_spinner();
     spinner.set_style(ProgressStyle::with_template("\n{spinner} {msg}\n")?);
     spinner.enable_steady_tick(Duration::from_millis(100));
@@ -24,7 +24,7 @@ pub fn spawn_spinner_message(sierra_file_path: &Utf8PathBuf) -> Result<ProgressB
     Ok(spinner)
 }
 
-fn is_temp_file(file_path: &Utf8PathBuf) -> Result<bool> {
+fn is_temp_file(file_path: &Utf8Path) -> Result<bool> {
     let temp_dir = env::temp_dir().canonicalize()?;
     Ok(file_path.canonicalize()?.starts_with(temp_dir))
 }
