46aa9d485157199ffa37511d5afcd5f8914654f2
Fiiranek <jellybean@onet.eu>
2024-11-15 11:05:13 +0100

    Add skipped args in forge

--- crates/forge/tests/e2e/docs_snippets_validation.rs
+++ crates/forge/tests/e2e/docs_snippets_validation.rs
@@ -13,15 +13,24 @@ fn test_docs_snippets() {
     let snippets = extract_matches_from_directory(&docs_dir, &re, extension)
         .expect("Failed to extract snforge command snippets");
 
+    let skipped_args = [
+        // for some reason `try_parse_from` fails on `--version` flag
+        vec!["snforge", "--version"],
+    ];
+
     for snippet in snippets {
         let args = snippet_to_command_args(snippet.as_str());
         let args: Vec<&str> = args.iter().map(String::as_str).collect();
+
+        if skipped_args.contains(&args) {
+            continue;
+        }
 
         let parse_result = Cli::try_parse_from(args);
 
         assert!(
             parse_result.is_ok(),
-            "Found invalid snforge snippet in the docs: {:?}\nError: {}",
+            "Found invalid snforge snippet in the docs: {:?}\n{}",
             snippet,
             parse_result.err().unwrap()
         );
