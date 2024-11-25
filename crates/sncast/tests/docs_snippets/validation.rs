ce93644b3266520cfcacbed9bac5bdb1f7da3b70
Fiiranek <jellybean@onet.eu>
2024-11-25 02:06:31 +0100

    Remove unneeded import

--- crates/sncast/tests/docs_snippets/validation.rs
+++ crates/sncast/tests/docs_snippets/validation.rs
@@ -1,6 +1,5 @@
 use std::fs;
 
-use configuration::CONFIG_FILENAME;
 use docs::snippet::{Snippet, SnippetType};
 use docs::utils::{
     assert_valid_snippet, get_parent_dir, print_skipped_snippet_message, print_success_message,
