8cd610deac576f158d8c8fdd97857c3af2c51206
Fiiranek <jellybean@onet.eu>
2024-11-26 18:15:33 +0100

    Use `scarb check` instead of `scarb test`

--- scripts/verify_cairo_listings.sh
+++ scripts/verify_cairo_listings.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-# TODO: ATM there is a bug in Scarb - .so library isn't getting build, so we need to build it manually
+# TODO(#): ATM there is a bug in Scarb - .so library isn't getting build, so we need to build it manually
 # Should be removed once the bug is fixed
 cargo build --release --manifest-path ./crates/snforge-scarb-plugin/Cargo.toml
 
-for d in ./docs/listings/*; do (cd "$d" && scarb test); done
+for d in ./docs/listings/*; do (cd "$d" && scarb check); done
