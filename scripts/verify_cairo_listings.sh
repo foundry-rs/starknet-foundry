5cf8560ee70799d02aa87980ce1c488521ca0349
Fiiranek <jellybean@onet.eu>
2024-11-26 17:53:55 +0100

    Fix CI

--- scripts/verify_cairo_listings.sh
+++ scripts/verify_cairo_listings.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 set -e
 
-for d in ./docs/listings/*; do (cd "$d" && scarb check); done
+# TODO: ATM there is a bug in Scarb - .so library isn't getting build, so we need to build it manually
+# Should be removed once the bug is fixed
+cargo build --release --manifest-path ./crates/snforge-scarb-plugin/Cargo.toml
+
+for d in ./docs/listings/*; do (cd "$d" && scarb test); done
