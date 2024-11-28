0fa653078a57eae2ab0b708e8f553fab21d2399e
kkawula <57270771+kkawula@users.noreply.github.com>
2024-11-28 15:44:48 +0100

    Update test

--- crates/sncast/tests/e2e/account/create.rs
+++ crates/sncast/tests/e2e/account/create.rs
@@ -146,6 +146,9 @@ pub async fn test_happy_case_generate_salt() {
         max_fee: [..]
         message: Account successfully created[..]
 
+        After prefunding the address run:
+        sncast account deploy --url http://127.0.0.1:5055/rpc --name my_account --fee-token strk
+
         To see account creation details, visit:
         account: [..]
         "});
