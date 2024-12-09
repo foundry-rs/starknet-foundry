1a51b130d6cdd40fb4e71a61edbcc1856f162e99
Artur Michalek <artur.michalek@swmansion.com>
2024-12-09 17:08:10 +0100

    Use revision instead of tag

--- scripts/install_devnet.ps1
+++ scripts/install_devnet.ps1
@@ -9,10 +9,10 @@ $DEVNET_INSTALL_DIR = Join-Path (git rev-parse --show-toplevel) "crates\sncast\t
 
 # Set the DEVNET repository and revision
 $DEVNET_REPO = "https://github.com/0xSpaceShard/starknet-devnet-rs.git"
-$DEVNET_TAG = "v0.2.2"
+$DEVNET_REV = "ef789b700770fa27a2fc057b3d1c610771be27d9"
 
 # Perform cargo install
-cargo install --locked --git $DEVNET_REPO --tag $DEVNET_TAG --root $DEVNET_INSTALL_DIR --force
+cargo install --locked --git $DEVNET_REPO --rev $DEVNET_REV --root $DEVNET_INSTALL_DIR --force
 
 # Output completion message
 Write-Host "All done!"
