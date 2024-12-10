04637a9479821a352e27a165d3c4df1d0354f79c
Artur Michalek <artur.michalek@swmansion.com>
2024-12-10 14:57:57 +0100

    Lint, use reference

--- crates/forge/tests/e2e/io_operations.rs
+++ crates/forge/tests/e2e/io_operations.rs
@@ -1,6 +1,6 @@
 use super::common::runner::{setup_package_with_file_patterns, test_runner, BASE_FILE_PATTERNS};
 use assert_fs::fixture::PathChild;
-use indoc::{formatdoc, indoc};
+use indoc::formatdoc;
 use shared::test_utils::output_assert::assert_stdout_contains;
 
 #[test]
@@ -73,19 +73,19 @@ fn file_reading() {
     // run from different directories to make sure cwd is always set to package directory
     let output = test_runner(&temp).assert().code(1);
 
-    assert_stdout_contains(output, expected);
+    assert_stdout_contains(output, &expected);
 
     let output = test_runner(&temp)
         .current_dir(temp.child("src"))
         .assert()
         .code(1);
 
-    assert_stdout_contains(output, expected);
+    assert_stdout_contains(output, &expected);
 
     let output = test_runner(&temp)
         .current_dir(temp.child("data"))
         .assert()
         .code(1);
 
-    assert_stdout_contains(output, expected);
+    assert_stdout_contains(output, &expected);
 }
