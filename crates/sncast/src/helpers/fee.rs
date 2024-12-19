ee9b354df3270f82a54ed9df5b224dff3a28ebcd
Fiiranek <jellybean@onet.eu>
2024-12-19 17:44:38 +0100

    Add newline ad the end of fee conversion message

--- crates/sncast/src/helpers/fee.rs
+++ crates/sncast/src/helpers/fee.rs
@@ -281,7 +281,7 @@ fn parse_fee_token(s: &str) -> Result<FeeToken, String> {
 
 fn print_max_fee_conversion_info(max_fee: Felt, max_gas: Felt, max_gas_unit_price: Felt) {
     println!(
-        "Specifying '--max-fee' flag while using v3 transactions results in conversion to '--max-gas' and '--max-gas-unit-price' flags\nConverted {max_fee} max fee to {max_gas} max gas and {max_gas_unit_price} max gas unit price",
+        "Specifying '--max-fee' flag while using v3 transactions results in conversion to '--max-gas' and '--max-gas-unit-price' flags\nConverted {max_fee} max fee to {max_gas} max gas and {max_gas_unit_price} max gas unit price\n",
     );
 }
 
