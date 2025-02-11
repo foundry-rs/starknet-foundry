b6fcfac280fe3fdac752c3218f6aaf8d217f5358
Artur Michalek <artur.michalek@swmansion.com>
2025-02-11 14:10:52 +0100

    Fix traces (modify blockifer code)

--- crates/cheatnet/src/runtime_extensions/call_to_blockifier_runtime_extension/execution/cairo1_execution.rs
+++ crates/cheatnet/src/runtime_extensions/call_to_blockifier_runtime_extension/execution/cairo1_execution.rs
@@ -6,11 +6,17 @@ use crate::runtime_extensions::cheatable_starknet_runtime_extension::CheatableSt
 use crate::runtime_extensions::common::get_relocated_vm_trace;
 use blockifier::execution::contract_class::CompiledClassV1;
 use blockifier::execution::entry_point_execution::{
-    finalize_execution, initialize_execution_context, prepare_call_arguments, VmExecutionContext,
+    finalize_execution, prepare_call_arguments, VmExecutionContext,
+};
+use blockifier::execution::errors::PreExecutionError;
+use blockifier::execution::execution_utils::{
+    write_felt, write_maybe_relocatable, ReadOnlySegments,
 };
 use blockifier::execution::stack_trace::{
     extract_trailing_cairo1_revert_trace, Cairo1RevertHeader,
 };
+use blockifier::execution::syscalls::hint_processor::SyscallHintProcessor;
+use blockifier::versioned_constants::GasCosts;
 use blockifier::{
     execution::{
         contract_class::EntryPointV1,
@@ -20,12 +26,105 @@ use blockifier::{
     },
     state::state_api::State,
 };
+use cairo_vm::types::layout_name::LayoutName;
+use cairo_vm::types::relocatable::MaybeRelocatable;
 use cairo_vm::vm::errors::cairo_run_errors::CairoRunError;
 use cairo_vm::{
     hint_processor::hint_processor_definition::HintProcessor,
     vm::runners::cairo_runner::{CairoArg, CairoRunner},
 };
 use runtime::{ExtendedRuntime, StarknetRuntime};
+use starknet_types_core::felt::Felt;
+
+// FIXME copied code
+fn prepare_program_extra_data(
+    runner: &mut CairoRunner,
+    contract_class: &CompiledClassV1,
+    read_only_segments: &mut ReadOnlySegments,
+    gas_costs: &GasCosts,
+) -> Result<usize, PreExecutionError> {
+    // Create the builtin cost segment, the builtin order should be the same as the price builtin
+    // array in the os in compiled_class.cairo in load_compiled_class_facts.
+    let builtin_price_array = [
+        gas_costs.builtins.pedersen,
+        gas_costs.builtins.bitwise,
+        gas_costs.builtins.ecop,
+        gas_costs.builtins.poseidon,
+        gas_costs.builtins.add_mod,
+        gas_costs.builtins.mul_mod,
+    ];
+
+    let data = builtin_price_array
+        .iter()
+        .map(|&x| MaybeRelocatable::from(Felt::from(x)))
+        .collect::<Vec<_>>();
+    let builtin_cost_segment_start = read_only_segments.allocate(&mut runner.vm, &data)?;
+
+    // Put a pointer to the builtin cost segment at the end of the program (after the
+    // additional `ret` statement).
+    let mut ptr = (runner.vm.get_pc() + contract_class.bytecode_length())?;
+    // Push a `ret` opcode.
+
+    write_felt(
+        &mut runner.vm,
+        &mut ptr,
+        Felt::from(0x208b7fff7fff7ffe_u128),
+    )?;
+    // Push a pointer to the builtin cost segment.
+    write_maybe_relocatable(&mut runner.vm, &mut ptr, builtin_cost_segment_start)?;
+
+    let program_extra_data_length = 2;
+    Ok(program_extra_data_length)
+}
+
+// FIXME copied code
+fn initialize_execution_context<'a>(
+    call: CallEntryPoint,
+    compiled_class: &'a CompiledClassV1,
+    state: &'a mut dyn State,
+    context: &'a mut EntryPointExecutionContext,
+) -> Result<VmExecutionContext<'a>, PreExecutionError> {
+    let entry_point = compiled_class.get_entry_point(&call)?;
+
+    // Instantiate Cairo runner.
+    let proof_mode = false;
+    // FIXME modified blockifier code to enable traces
+    let trace_enabled = true;
+    let mut runner = CairoRunner::new(
+        &compiled_class.0.program,
+        LayoutName::starknet,
+        proof_mode,
+        trace_enabled,
+    )?;
+
+    runner.initialize_function_runner_cairo_1(&entry_point.builtins)?;
+    let mut read_only_segments = ReadOnlySegments::default();
+    let program_extra_data_length = prepare_program_extra_data(
+        &mut runner,
+        compiled_class,
+        &mut read_only_segments,
+        &context.versioned_constants().os_constants.gas_costs,
+    )?;
+
+    // Instantiate syscall handler.
+    let initial_syscall_ptr = runner.vm.add_memory_segment();
+    let syscall_handler = SyscallHintProcessor::new(
+        state,
+        context,
+        initial_syscall_ptr,
+        call,
+        &compiled_class.hints,
+        read_only_segments,
+    );
+
+    Ok(VmExecutionContext {
+        runner,
+        syscall_handler,
+        initial_syscall_ptr,
+        entry_point,
+        program_extra_data_length,
+    })
+}
 
 // blockifier/src/execution/cairo1_execution.rs:48 (execute_entry_point_call)
 pub fn execute_entry_point_call_cairo1(
@@ -85,6 +184,7 @@ pub fn execute_entry_point_call_cairo1(
     .on_error_get_last_pc(&mut runner)?;
 
     let trace = get_relocated_vm_trace(&mut runner);
+    dbg!(&trace.is_some());
 
     let syscall_counter = cheatable_runtime
         .extended_runtime
