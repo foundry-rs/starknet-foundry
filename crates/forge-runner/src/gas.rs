05911674336ebfbea1722b305d0562468e095a6f
Fiiranek <jellybean@onet.eu>
2025-02-14 02:16:35 +0100

    Refactor returned gas vector in `calculate_used_gas`

--- crates/forge-runner/src/gas.rs
+++ crates/forge-runner/src/gas.rs
@@ -55,12 +55,10 @@ pub fn calculate_used_gas(
 
     dbg!(&transaction_resources);
 
-    // FIXME this is the tricky part, how to figure the computation mode here
     Ok(transaction_resources.to_gas_vector(
         versioned_constants,
-        // FIXME actually check it
-        true,
-        &GasVectorComputationMode::NoL2Gas,
+        transaction_context.block_context.block_info().use_kzg_da,
+        &transaction_context.get_gas_vector_computation_mode(),
     ))
 }
 
