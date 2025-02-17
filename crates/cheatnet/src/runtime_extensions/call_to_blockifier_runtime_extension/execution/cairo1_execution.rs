2191e39bffd64c57c389bec827d287a5d285ce09
Artur Michalek <artur.michalek@swmansion.com>
2025-02-17 17:59:12 +0100

    Link to copied code

--- crates/cheatnet/src/runtime_extensions/call_to_blockifier_runtime_extension/execution/cairo1_execution.rs
+++ crates/cheatnet/src/runtime_extensions/call_to_blockifier_runtime_extension/execution/cairo1_execution.rs
@@ -36,7 +36,8 @@ use cairo_vm::{
 use runtime::{ExtendedRuntime, StarknetRuntime};
 use starknet_types_core::felt::Felt;
 
-// FIXME copied code
+// TODO remove copied code
+// Copied from https://github.com/starkware-libs/sequencer/blob/545761f29b859d06f125bd6c332b6182845734f0/crates/blockifier/src/execution/entry_point_execution.rs#L180
 fn prepare_program_extra_data(
     runner: &mut CairoRunner,
     contract_class: &CompiledClassV1,
@@ -77,7 +78,8 @@ fn prepare_program_extra_data(
     Ok(program_extra_data_length)
 }
 
-// FIXME copied code
+// TODO remov copied code
+// Copied from https://github.com/starkware-libs/sequencer/blob/0e1e92e0b90790e4bec20721c069c312d6a60a13/crates/blockifier/src/execution/entry_point_execution.rs#L98
 fn initialize_execution_context<'a>(
     call: CallEntryPoint,
     compiled_class: &'a CompiledClassV1,
@@ -88,7 +90,7 @@ fn initialize_execution_context<'a>(
 
     // Instantiate Cairo runner.
     let proof_mode = false;
-    // FIXME modified blockifier code to enable traces
+    // region: Modified blockifier code
     let trace_enabled = true;
     let mut runner = CairoRunner::new(
         &compiled_class.0.program,
@@ -96,6 +98,7 @@ fn initialize_execution_context<'a>(
         proof_mode,
         trace_enabled,
     )?;
+    // endregion
 
     runner.initialize_function_runner_cairo_1(&entry_point.builtins)?;
     let mut read_only_segments = ReadOnlySegments::default();
