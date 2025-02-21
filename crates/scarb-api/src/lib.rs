932a2fdb688dfb0be7bc54df2bd5cc4eef89beee
Fiiranek <jellybean@onet.eu>
2025-02-21 13:40:38 +0100

    Change starknet version in `package_matches_version_requirement_test`

--- crates/scarb-api/src/lib.rs
+++ crates/scarb-api/src/lib.rs
@@ -391,7 +391,7 @@ mod tests {
                 sierra = true
 
                 [dependencies]
-                starknet = "2.5.4"
+                starknet = "2.8.5"
                 "#,
             ))
             .unwrap();
