4e0619b53f5ea815f5c05a05d5b06f4fbae2ae0f
kkawula <kamilkawula@icloud.com>
2025-03-03 11:30:36 +0100

    Rename `generate_completions_to_stdout`

--- crates/shared/src/auto_completions.rs
+++ crates/shared/src/auto_completions.rs
@@ -1,6 +1,6 @@
 use anyhow::Result;
 use clap::{Args, Command};
-use clap_complete::{Generator, Shell, generate};
+use clap_complete::{Generator, Shell};
 use std::io;
 
 #[derive(Args, Debug)]
@@ -8,15 +8,15 @@ pub struct Completion {
     pub shell: Option<Shell>,
 }
 
-pub fn print_completions<G: Generator>(shell: G, cmd: &mut Command) {
-    generate(shell, cmd, cmd.get_name().to_string(), &mut io::stdout());
+pub fn generate_completions_to_stdout<G: Generator>(shell: G, cmd: &mut Command) {
+    clap_complete::generate(shell, cmd, cmd.get_name().to_string(), &mut io::stdout());
 }
 
 pub fn generate_completions(shell: Option<Shell>, cmd: &mut Command) -> Result<()> {
     if let Some(shell) = shell {
-        print_completions(shell, cmd);
+        generate_completions_to_stdout(shell, cmd);
     } else if let Some(shell) = Shell::from_env() {
-        print_completions(shell, cmd);
+        generate_completions_to_stdout(shell, cmd);
     } else {
         anyhow::bail!("Unsupported shell");
     }
