7275aa5c6f9a44fddfada13d68a6a56c95b3b267
Dariusz Doktorski <dariusz.doktorski@swmansion.com>
2025-03-11 15:12:31 +0100

    Bump `cairo-annotations` and fix remaining tests

--- crates/forge/tests/e2e/running.rs
+++ crates/forge/tests/e2e/running.rs
@@ -782,6 +782,7 @@ fn incompatible_snforge_std_version_warning() {
         .parse::<DocumentMut>()
         .unwrap();
     scarb_toml["dev-dependencies"]["snforge_std"] = value("0.34.1");
+    scarb_toml["dev-dependencies"]["snforge_scarb_plugin"] = value("0.34.1");
     manifest_path.write_str(&scarb_toml.to_string()).unwrap();
 
     let output = test_runner(&temp).assert().failure();
