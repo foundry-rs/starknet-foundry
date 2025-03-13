c5a810298502b99d4b0ad2a27f740668c7bbd4b8
Jakub Kopański <jakub@famisoft.pl>
2025-03-13 16:26:24 +0100

    Clippy lint

--- crates/sncast/src/starknet_commands/verify/voyager.rs
+++ crates/sncast/src/starknet_commands/verify/voyager.rs
@@ -65,122 +65,114 @@ where
     anyhow!("Scarb metadata failed for {name}: {path}")
 }
 
-impl Voyager {
-    fn gather_packages(
-        &self,
-        metadata: &Metadata,
-        packages: &mut Vec<PackageMetadata>,
-    ) -> Result<()> {
-        let mut workspace_packages: Vec<PackageMetadata> = metadata
-            .packages
-            .clone()
-            .into_iter()
-            .filter(|package_meta| metadata.workspace.members.contains(&package_meta.id))
-            .filter(|package_meta| !packages.contains(package_meta))
-            .collect();
+fn gather_packages(metadata: &Metadata, packages: &mut Vec<PackageMetadata>) -> Result<()> {
+    let mut workspace_packages: Vec<PackageMetadata> = metadata
+        .packages
+        .clone()
+        .into_iter()
+        .filter(|package_meta| metadata.workspace.members.contains(&package_meta.id))
+        .filter(|package_meta| !packages.contains(package_meta))
+        .collect();
 
-        let workspace_packages_names = workspace_packages
-            .iter()
-            .map(|package| package.name.clone())
-            .collect_vec();
+    let workspace_packages_names = workspace_packages
+        .iter()
+        .map(|package| package.name.clone())
+        .collect_vec();
 
-        // find all dependencies listed by path
-        let mut dependencies: HashMap<String, PathBuf> = HashMap::new();
-        for package in &workspace_packages {
-            for dependency in &package.dependencies {
-                let name = &dependency.name;
-                let url = Url::parse(&dependency.source.repr)
-                    .map_err(|_| dependency_path(name.clone(), dependency.source.repr.clone()))?;
+    // find all dependencies listed by path
+    let mut dependencies: HashMap<String, PathBuf> = HashMap::new();
+    for package in &workspace_packages {
+        for dependency in &package.dependencies {
+            let name = &dependency.name;
+            let url = Url::parse(&dependency.source.repr)
+                .map_err(|_| dependency_path(name.clone(), dependency.source.repr.clone()))?;
 
-                if url.scheme().starts_with("path") {
-                    let path = url.to_file_path().map_err(|_| {
-                        dependency_path(name.clone(), dependency.source.repr.clone())
-                    })?;
-                    dependencies.insert(name.clone(), path);
+            if url.scheme().starts_with("path") {
+                let path = url
+                    .to_file_path()
+                    .map_err(|()| dependency_path(name.clone(), dependency.source.repr.clone()))?;
+                dependencies.insert(name.clone(), path);
+            }
+        }
+    }
+
+    packages.append(&mut workspace_packages);
+
+    // filter out dependencies already covered by workspace
+    let out_of_workspace_dependencies: HashMap<&String, &PathBuf> = dependencies
+        .iter()
+        .filter(|&(k, _)| !workspace_packages_names.contains(k))
+        .collect();
+
+    for (name, manifest) in out_of_workspace_dependencies {
+        let new_meta = MetadataCommand::new()
+            .json()
+            .manifest_path(manifest)
+            .exec()
+            .map_err(|_| metadata_error(name.clone(), manifest.to_string_lossy().to_string()))?;
+        gather_packages(&new_meta, packages)?;
+    }
+
+    Ok(())
+}
+
+fn package_sources(package_metadata: &PackageMetadata) -> Result<Vec<Utf8PathBuf>> {
+    let mut sources: Vec<Utf8PathBuf> = WalkDir::new(package_metadata.root.clone())
+        .into_iter()
+        .filter_map(std::result::Result::ok)
+        .filter(|f| f.file_type().is_file())
+        .filter(|f| {
+            if let Some(ext) = f.path().extension() {
+                if ext == OsStr::new(CAIRO_EXT) {
+                    return true;
                 }
-            }
-        }
+            };
 
-        packages.append(&mut workspace_packages);
+            false
+        })
+        .map(walkdir::DirEntry::into_path)
+        .map(Utf8PathBuf::try_from)
+        .try_collect()?;
 
-        // filter out dependencies already covered by workspace
-        let out_of_workspace_dependencies: HashMap<&String, &PathBuf> = dependencies
-            .iter()
-            .filter(|&(k, _)| !workspace_packages_names.contains(&k))
-            .collect();
+    sources.push(package_metadata.manifest_path.clone());
+    let package_root = &package_metadata.root;
 
-        for (name, manifest) in out_of_workspace_dependencies {
-            let new_meta = MetadataCommand::new()
-                .json()
-                .manifest_path(manifest)
-                .exec()
-                .map_err(|_| {
-                    metadata_error(name.clone(), manifest.to_string_lossy().to_string())
-                })?;
-            self.gather_packages(&new_meta, packages)?;
-        }
-
-        Ok(())
+    if let Some(lic) = package_metadata
+        .manifest_metadata
+        .license_file
+        .as_ref()
+        .map(Utf8Path::new)
+        .map(Utf8Path::to_path_buf)
+    {
+        sources.push(package_root.join(lic));
     }
 
-    fn package_sources(package_metadata: &PackageMetadata) -> Result<Vec<Utf8PathBuf>> {
-        let mut sources: Vec<Utf8PathBuf> = WalkDir::new(package_metadata.root.clone())
-            .into_iter()
-            .filter_map(|f| f.ok())
-            .filter(|f| f.file_type().is_file())
-            .filter(|f| {
-                if let Some(ext) = f.path().extension() {
-                    if ext == OsStr::new(CAIRO_EXT) {
-                        return true;
-                    }
-                };
-
-                return false;
-            })
-            .map(|dir_entry| dir_entry.into_path())
-            .map(Utf8PathBuf::try_from)
-            .try_collect()?;
-
-        sources.push(package_metadata.manifest_path.clone().into());
-        let package_root = &package_metadata.root;
-
-        if let Some(lic) = package_metadata
-            .manifest_metadata
-            .license_file
-            .as_ref()
-            .map(Utf8Path::new)
-            .map(Utf8Path::to_path_buf)
-        {
-            sources.push(package_root.join(lic))
-        }
-
-        if let Some(readme) = package_metadata
-            .manifest_metadata
-            .readme
-            .as_deref()
-            .map(Utf8Path::new)
-            .map(Utf8Path::to_path_buf)
-        {
-            sources.push(package_root.join(readme));
-        }
-
-        Ok(sources)
+    if let Some(readme) = package_metadata
+        .manifest_metadata
+        .readme
+        .as_deref()
+        .map(Utf8Path::new)
+        .map(Utf8Path::to_path_buf)
+    {
+        sources.push(package_root.join(readme));
     }
 
-    fn biggest_common_prefix<P: AsRef<Utf8Path> + Clone>(
-        paths: &Vec<Utf8PathBuf>,
-        first_guess: P,
-    ) -> Utf8PathBuf {
-        let mut ancestors = Utf8Path::ancestors(first_guess.as_ref());
-        let mut biggest_prefix: &Utf8Path = first_guess.as_ref();
-        while let Some(prefix) = ancestors.next() {
-            if paths.iter().all(|src| src.starts_with(prefix)) {
-                biggest_prefix = prefix;
-                break;
-            }
+    Ok(sources)
+}
+
+fn biggest_common_prefix<P: AsRef<Utf8Path> + Clone>(
+    paths: &[Utf8PathBuf],
+    first_guess: P,
+) -> Utf8PathBuf {
+    let ancestors = Utf8Path::ancestors(first_guess.as_ref());
+    let mut biggest_prefix: &Utf8Path = first_guess.as_ref();
+    for prefix in ancestors {
+        if paths.iter().all(|src| src.starts_with(prefix)) {
+            biggest_prefix = prefix;
+            break;
         }
-        biggest_prefix.to_path_buf()
     }
+    biggest_prefix.to_path_buf()
 }
 
 #[async_trait::async_trait]
@@ -219,15 +211,15 @@ impl VerificationInterface for Voyager {
             }
         };
         let mut packages = vec![];
-        self.gather_packages(&self.metadata, &mut packages)?;
+        gather_packages(&self.metadata, &mut packages)?;
 
         let mut sources: Vec<Utf8PathBuf> = vec![];
         for package in &packages {
-            let mut package_sources = Voyager::package_sources(package)?;
+            let mut package_sources = package_sources(package)?;
             sources.append(&mut package_sources);
         }
 
-        let prefix = Voyager::biggest_common_prefix(&sources, &self.metadata.workspace.root);
+        let prefix = biggest_common_prefix(&sources, &self.metadata.workspace.root);
         let manifest_path = &self.metadata.workspace.manifest_path;
         let manifest = manifest_path
             .strip_prefix(&prefix)
@@ -277,12 +269,10 @@ impl VerificationInterface for Voyager {
                     "More than one package found in scarb metadata - specify package using --package flag"
                 )),
             }
+        } else if let Some(p) = workspace_packages.pop() {
+            Ok(p)
         } else {
-            if let Some(p) = workspace_packages.pop() {
-                Ok(p)
-            } else {
-                Err(anyhow!("No packages found in scarb metadata"))
-            }
+            Err(anyhow!("No packages found in scarb metadata"))
         })?;
 
         let project_dir_path = self
@@ -337,13 +327,10 @@ impl VerificationInterface for Voyager {
         match response.status() {
             StatusCode::OK => {
                 let message = format!(
-                    "{} submitted for verification, you can query the status at: {}",
+                    "{} submitted for verification, you can query the status at: {}/class-verify/job/{}",
                     contract_name.clone(),
-                    format!(
-                        "{}/class-verify/job/{}",
-                        self.gen_explorer_url(),
-                        response.json::<VerificationJobDispatch>().await?.job_id,
-                    )
+                    self.gen_explorer_url(),
+                    response.json::<VerificationJobDispatch>().await?.job_id,
                 );
                 Ok(VerifyResponse { message })
             }
