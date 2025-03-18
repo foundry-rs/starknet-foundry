c6f0c52f1c0153d1bfb2c96a8152daf717960007
Fiiranek <jellybean@onet.eu>
2025-03-18 18:19:11 +0100

    Uncomment some tests

--- crates/sncast/tests/e2e/account/deploy.rs
+++ crates/sncast/tests/e2e/account/deploy.rs
@@ -13,9 +13,7 @@ use serde_json::Value;
 use shared::test_utils::output_assert::{AsOutput, assert_stderr_contains};
 use sncast::AccountType;
 use sncast::helpers::constants::{
-    // ARGENT_CLASS_HASH, BRAAVOS_CLASS_HASH,
-    KEYSTORE_PASSWORD_ENV_VAR,
-    OZ_CLASS_HASH,
+    ARGENT_CLASS_HASH, BRAAVOS_CLASS_HASH, KEYSTORE_PASSWORD_ENV_VAR, OZ_CLASS_HASH,
 };
 use starknet::core::types::TransactionReceipt::DeployAccount;
 use std::fs;
@@ -24,8 +22,8 @@ use test_case::test_case;
 
 #[test_case(DEVNET_OZ_CLASS_HASH_CAIRO_0, "oz"; "cairo_0_class_hash")]
 #[test_case(&OZ_CLASS_HASH.into_hex_string(), "oz"; "cairo_1_class_hash")]
-// #[test_case(&ARGENT_CLASS_HASH.into_hex_string(), "argent"; "argent_class_hash")]
-// #[test_case(&BRAAVOS_CLASS_HASH.into_hex_string(), "braavos"; "braavos_class_hash")]
+#[test_case(&ARGENT_CLASS_HASH.into_hex_string(), "argent"; "argent_class_hash")]
+#[test_case(&BRAAVOS_CLASS_HASH.into_hex_string(), "braavos"; "braavos_class_hash")]
 #[tokio::test]
 pub async fn test_happy_case(class_hash: &str, account_type: &str) {
     let tempdir = create_account(false, class_hash, account_type).await;
