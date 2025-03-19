1953628b3e6a2dbd21f3443585520a78dbff44a6
kkawula <57270771+kkawula@users.noreply.github.com>
2025-03-19 16:04:12 +0100

    Remove recommended version warning if not applicable (#3112)
    <!-- Reference any GitHub issues resolved by this PR -->
    
    Closes #
    
    ## Introduced changes
    
    <!-- A brief description of the changes -->
    
    -
    
    ## Checklist
    
    <!-- Make sure all of these are complete -->
    
    - [x] Linked relevant issue
    - [x] Updated relevant documentation
    - [x] Added relevant tests
    - [x] Performed self-review of the code
    - [x] Added changes to `CHANGELOG.md`

--- crates/forge/tests/e2e/running.rs
+++ crates/forge/tests/e2e/running.rs
@@ -791,7 +791,7 @@ fn incompatible_snforge_std_version_warning() {
     assert_stdout_contains(
         output,
         indoc! {r"
-        [WARNING] Package snforge_std version does not meet the recommended version requirement =0.[..], [..]
+        [WARNING] Package snforge_std version does not meet the recommended version requirement ^0.[..], [..]
         [..]Compiling[..]
         [..]Finished[..]
 
