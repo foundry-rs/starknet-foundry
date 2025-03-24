075ef336954c67f23f444034a8d56007ed1b190b
kkawula <kamilkawula@icloud.com>
2025-03-24 21:17:22 +0100

    Update initial template

--- crates/sncast/src/starknet_commands/script/init.rs
+++ crates/sncast/src/starknet_commands/script/init.rs
@@ -122,13 +122,20 @@ fn create_script_main_file(script_name: &str, script_root_dir: &Utf8PathBuf) -> 
     fs::write(
         script_main_file_path,
         indoc! {r#"
-            use sncast_std::{call, CallResult};
+            use sncast_std::call;
 
             // The example below uses a contract deployed to the Sepolia testnet
+            const CONTRACT_ADDRESS: felt252 =
+                0x07e867f1fa6da2108dd2b3d534f1fbec411c5ec9504eb3baa1e49c7a0bef5ab5;
+
             fn main() {
-                let contract_address = 0x07e867f1fa6da2108dd2b3d534f1fbec411c5ec9504eb3baa1e49c7a0bef5ab5;
-                let call_result = call(contract_address.try_into().unwrap(), selector!("get_greeting"), array![]).expect('call failed');
-                assert(*call_result.data[1]=='Hello, Starknet!', *call_result.data[1]);
+                let call_result = call(
+                    CONTRACT_ADDRESS.try_into().unwrap(), selector!("get_greeting"), array![],
+                )
+                    .expect('call failed');
+
+                assert(*call_result.data[1] == 'Hello, Starknet!', *call_result.data[1]);
+
                 println!("{:?}", call_result);
             }
         "#},
