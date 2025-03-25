8ef33585acfc2305cab7745c484dcf99f09f32f6
Fiiranek <jellybean@onet.eu>
2025-03-25 12:08:22 +0100

    Update comment

--- crates/conversions/src/serde/serialize/serialize_impl.rs
+++ crates/conversions/src/serde/serialize/serialize_impl.rs
@@ -37,8 +37,8 @@ impl CairoSerialize for ContractErrorData {
 impl CairoSerialize for ContractExecutionError {
     fn serialize(&self, output: &mut BufferWriter) {
         match &self {
+            // We need to add 0 and 1 because of enum variants serialization
             ContractExecutionError::Nested(inner) => {
-                // Need to add 0 because of enum serialization
                 0.serialize(output);
                 inner.class_hash.serialize(output);
                 inner.contract_address.serialize(output);
@@ -46,7 +46,6 @@ impl CairoSerialize for ContractExecutionError {
                 inner.error.serialize(output);
             }
             ContractExecutionError::Message(msg) => {
-                // Need to add 1 because of enum serialization
                 1.serialize(output);
                 ByteArray::from(msg.as_str()).serialize(output);
             }
