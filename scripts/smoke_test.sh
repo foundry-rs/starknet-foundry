cfdcde857b5017bf6b259b97bf7b5274215fbf44
kkawula <kamilkawula@icloud.com>
2025-03-26 10:13:41 +0100

    Update workflow

--- scripts/smoke_test.sh
+++ scripts/smoke_test.sh
@@ -1,21 +1,46 @@
 #!/bin/bash
 set -e
 
-export DEV_DISABLE_SNFORGE_STD_DEPENDENCY=true
-
 RPC_URL="$1"
 SNFORGE_PATH="$2"
 SNCAST_PATH="$3"
 REPO_URL="$4"
 REVISION="$5"
+VERSION="$6"
 
-# Check forge
-
-$SNFORGE_PATH init my_project
-pushd my_project || exit
+# Check forge github
+echo "Check forge github"
+$SNFORGE_PATH init my_project_0
+pushd my_project_0 || exit
 scarb add --dev snforge_std --git "$REPO_URL" --rev "$REVISION"
 $SNFORGE_PATH test || exit
 popd || exit
+scarb cache clean
+
+# Check forge registry prebuild
+echo "Check forge registry prebuild"
+$SNFORGE_PATH new my_project_1
+pushd my_project_1 || exit
+sed -i.bak "/snforge_std/ s/\(snforge_std = \).*/\1{ version = \"${VERSION}\", registry = \"https:\/\/scarbs.dev\/\" }/" Scarb.toml
+rm Scarb.toml.bak 2> /dev/null
+if $SNFORGE_PATH test | grep -q 'Compiling snforge_scarb_plugin'; then
+  exit 1
+fi 
+popd || exit
+scarb cache clean
+
+# Check forge registry build
+echo "Check forge registry build"
+
+$SNFORGE_PATH new my_project_2
+pushd my_project_2 || exit
+sed -i.bak "/snforge_std/ s/\(snforge_std = \).*/\1{ version = \"${VERSION}\", registry = \"https:\/\/scarbs.dev\/\" }/" Scarb.toml
+sed -i.bak '/^allow-prebuilt-plugins = \["snforge_std"\]$/d' Scarb.toml
+rm Scarb.toml.bak 2> /dev/null
+$SNFORGE_PATH test || exit
+popd || exit
+scarb cache clean
+
 
 # Check cast
 
