2b287ec4cca0e0268ab3ab6c370455e0442011cb
Artur Michalek <artur.michalek@swmansion.com>
2025-03-28 14:41:04 +0100

    Run tests through blockifier logic
    commit-id:2777abe1

--- crates/forge-runner/src/running.rs
+++ crates/forge-runner/src/running.rs
@@ -3,29 +3,27 @@ use crate::forge_config::{RuntimeConfig, TestRunnerConfig};
 use crate::gas::calculate_used_gas;
 use crate::package_tests::with_config_resolved::{ResolvedForkConfig, TestCaseWithResolvedConfig};
 use crate::test_case_summary::{Single, TestCaseSummary};
-use anyhow::{Result, ensure};
-use blockifier::execution::contract_class::TrackedResource;
-use blockifier::execution::entry_point::EntryPointExecutionContext;
+use anyhow::{Result, bail};
+use blockifier::execution::call_info::CallInfo;
+use blockifier::execution::contract_class::{EntryPointV1, TrackedResource};
+use blockifier::execution::entry_point::{EntryPointExecutionContext, ExecutableCallEntryPoint};
+use blockifier::execution::errors::{EntryPointExecutionError, PreExecutionError};
+use blockifier::execution::execution_utils::ReadOnlySegments;
+use blockifier::execution::syscalls::hint_processor::SyscallHintProcessor;
 use blockifier::state::cached_state::CachedState;
-use cairo_lang_runner::{Arg, RunResultValue, SierraCasmRunner};
-use cairo_lang_sierra::extensions::NamedType;
-use cairo_lang_sierra::extensions::bitwise::BitwiseType;
-use cairo_lang_sierra::extensions::circuit::{AddModType, MulModType};
-use cairo_lang_sierra::extensions::ec::EcOpType;
-use cairo_lang_sierra::extensions::gas::GasBuiltinType;
-use cairo_lang_sierra::extensions::pedersen::PedersenType;
-use cairo_lang_sierra::extensions::poseidon::PoseidonType;
-use cairo_lang_sierra::extensions::range_check::{RangeCheck96Type, RangeCheckType};
-use cairo_lang_sierra::extensions::segment_arena::SegmentArenaType;
-use cairo_lang_sierra::extensions::starknet::syscalls::SystemType;
-use cairo_lang_sierra::ids::GenericTypeId;
-use cairo_lang_utils::unordered_hash_set::UnorderedHashSet;
+use blockifier::state::state_api::State;
+use cairo_lang_casm::hints::Hint;
 use cairo_vm::Felt252;
+use cairo_vm::types::builtin_name::BuiltinName;
+use cairo_vm::types::layout_name::LayoutName;
+use cairo_vm::types::program::Program;
+use cairo_vm::types::relocatable::Relocatable;
 use cairo_vm::vm::errors::cairo_run_errors::CairoRunError;
 use cairo_vm::vm::errors::vm_errors::VirtualMachineError;
+use cairo_vm::vm::runners::cairo_runner::CairoRunner;
 use camino::{Utf8Path, Utf8PathBuf};
-use casm::{get_assembled_program, run_assembled_program};
 use cheatnet::constants as cheatnet_constants;
+use cheatnet::constants::build_test_entry_point;
 use cheatnet::forking::state::ForkStateReader;
 use cheatnet::runtime_extensions::call_to_blockifier_runtime_extension::CallToBlockifierExtension;
 use cheatnet::runtime_extensions::call_to_blockifier_runtime_extension::rpc::UsedResources;
@@ -38,14 +36,14 @@ use cheatnet::runtime_extensions::forge_runtime_extension::{
 use cheatnet::state::{
     BlockInfoReader, CallTrace, CheatnetState, EncounteredErrors, ExtendedStateReader,
 };
-use entry_code::create_entry_code;
-use hints::{hints_by_representation, hints_to_params};
+use hints::hints_by_representation;
 use rand::prelude::StdRng;
 use runtime::starknet::context::{build_context, set_max_steps};
 use runtime::{ExtendedRuntime, StarknetRuntime};
+use starknet_api::deprecated_contract_class::EntryPointOffset;
 use starknet_api::execution_resources::GasVector;
-use starknet_types_core::felt::Felt;
 use std::cell::RefCell;
+use std::collections::HashMap;
 use std::default::Default;
 use std::marker::PhantomData;
 use std::rc::Rc;
@@ -56,12 +54,17 @@ use universal_sierra_compiler_api::AssembledProgramWithDebugInfo;
 
 mod casm;
 pub mod config_run;
+mod copied_code;
 mod entry_code;
 mod hints;
 mod syscall_handler;
 pub mod with_config;
 
+use crate::running::copied_code::{
+    finalize_execution, prepare_call_arguments, prepare_program_extra_data, run_entry_point,
+};
 use crate::running::syscall_handler::build_syscall_handler;
+pub use hints::hints_to_params;
 pub use syscall_handler::has_segment_arena;
 pub use syscall_handler::syscall_handler_offset;
 
@@ -148,15 +151,6 @@ pub enum RunStatus {
     Panic(Vec<Felt252>),
 }
 
-impl From<RunResultValue> for RunStatus {
-    fn from(value: RunResultValue) -> Self {
-        match value {
-            RunResultValue::Success(value) => Self::Success(value),
-            RunResultValue::Panic(value) => Self::Panic(value),
-        }
-    }
-}
-
 pub struct RunCompleted {
     pub(crate) status: RunStatus,
     pub(crate) call_trace: Rc<RefCell<CallTrace>>,
@@ -166,6 +160,55 @@ pub struct RunCompleted {
     pub(crate) fuzzer_args: Vec<String>,
 }
 
+pub struct VmExecutionContext<'a> {
+    pub runner: CairoRunner,
+    pub syscall_handler: SyscallHintProcessor<'a>,
+    pub initial_syscall_ptr: Relocatable,
+    // Additional data required for execution is appended after the program bytecode.
+    pub program_extra_data_length: usize,
+}
+
+fn initialize_execution_context<'a>(
+    call: ExecutableCallEntryPoint,
+    hints: &'a HashMap<String, Hint>,
+    program: &Program,
+    state: &'a mut dyn State,
+    context: &'a mut EntryPointExecutionContext,
+) -> std::result::Result<VmExecutionContext<'a>, PreExecutionError> {
+    // Instantiate Cairo runner.
+    let proof_mode = false;
+    let trace_enabled = true;
+    let mut runner = CairoRunner::new(program, LayoutName::all_cairo, proof_mode, trace_enabled)?;
+
+    runner.initialize_function_runner_cairo_1(&builtins_from_program(program))?;
+    let mut read_only_segments = ReadOnlySegments::default();
+    let program_extra_data_length = prepare_program_extra_data(
+        &mut runner,
+        program.data_len(),
+        &mut read_only_segments,
+        &context.versioned_constants().os_constants.gas_costs,
+    )?;
+
+    // Instantiate syscall handler.
+    let initial_syscall_ptr = runner.vm.add_memory_segment();
+    let syscall_handler = SyscallHintProcessor::new(
+        state,
+        context,
+        initial_syscall_ptr,
+        call,
+        hints,
+        read_only_segments,
+    );
+
+    Ok(VmExecutionContext {
+        runner,
+        syscall_handler,
+        initial_syscall_ptr,
+        program_extra_data_length,
+    })
+}
+
+#[allow(clippy::too_many_lines)]
 pub struct RunError {
     pub(crate) error: Box<CairoRunError>,
     pub(crate) call_trace: Rc<RefCell<CallTrace>>,
@@ -178,6 +221,28 @@ pub enum RunResult {
     Error(RunError),
 }
 
+// Builtins is private in program, so we need this workaround
+fn builtins_from_program(program: &Program) -> Vec<BuiltinName> {
+    program.iter_builtins().copied().collect::<Vec<_>>()
+}
+
+fn build_test_call_and_entry_point(
+    case: &TestCaseWithResolvedConfig,
+    casm_program: &AssembledProgramWithDebugInfo,
+    program: &Program,
+) -> (ExecutableCallEntryPoint, EntryPointV1) {
+    let sierra_instruction_idx = case.test_details.sierra_entry_point_statement_idx;
+    let casm_entry_point_offset = casm_program.debug_info[sierra_instruction_idx].0;
+
+    let call = build_test_entry_point();
+    let entry_point = EntryPointV1 {
+        selector: call.entry_point_selector,
+        offset: EntryPointOffset(casm_entry_point_offset),
+        builtins: builtins_from_program(program),
+    };
+    (call, entry_point)
+}
+
 #[expect(clippy::too_many_lines)]
 pub fn run_test_case(
     case: &TestCaseWithResolvedConfig,
@@ -185,20 +250,8 @@ pub fn run_test_case(
     runtime_config: &RuntimeConfig,
     fuzzer_rng: Option<Arc<Mutex<StdRng>>>,
 ) -> Result<RunResult> {
-    ensure!(
-        case.config
-            .available_gas
-            .as_ref()
-            .is_none_or(|gas| !gas.is_zero()),
-        "\n\t`available_gas` attribute was incorrectly configured. Make sure you use scarb >= 2.4.4\n"
-    );
-
-    let (entry_code, builtins) = create_entry_code(&case.test_details, casm_program);
-
-    let assembled_program = get_assembled_program(casm_program, entry_code);
-
-    let string_to_hint = hints_by_representation(&assembled_program);
-    let hints_dict = hints_to_params(&assembled_program);
+    let program = case.try_into_program(casm_program)?;
+    let (call, entry_point) = build_test_call_and_entry_point(case, casm_program, &program);
 
     let mut state_reader = ExtendedStateReader {
         dict_state_reader: cheatnet_constants::build_testing_state(),
@@ -210,20 +263,26 @@ pub fn run_test_case(
     let block_info = state_reader.get_block_info()?;
     let chain_id = state_reader.get_chain_id()?;
     let tracked_resource = TrackedResource::from(runtime_config.tracked_resource);
-
     let mut context = build_context(&block_info, chain_id, &tracked_resource);
 
     if let Some(max_n_steps) = runtime_config.max_n_steps {
         set_max_steps(&mut context, max_n_steps);
     }
     let mut cached_state = CachedState::new(state_reader);
-    let syscall_handler = build_syscall_handler(
+
+    let hints = hints_by_representation(&casm_program.assembled_cairo_program);
+    let VmExecutionContext {
+        mut runner,
+        syscall_handler,
+        initial_syscall_ptr,
+        program_extra_data_length,
+    } = initialize_execution_context(
+        call.clone(),
+        &hints,
+        &program,
         &mut cached_state,
-        &string_to_hint,
         &mut context,
-        &case.test_details.parameter_types,
-        builtins.len(),
-    );
+    )?;
 
     let mut cheatnet_state = CheatnetState {
         block_info,
@@ -237,12 +296,7 @@ pub fn run_test_case(
         },
         extended_runtime: StarknetRuntime {
             hint_handler: syscall_handler,
-            // Max gas is no longer set by `create_entry_code_from_params`
-            // Instead, call to `ExternalHint::WriteRunParam` is added by it, and we need to
-            // store the gas value to be read by logic handling the hint
-            // TODO(#2966) we should subtract initial cost of the function from this value to be more exact.
-            //  But as a workaround it should be good enough.
-            user_args: vec![vec![Arg::Value(Felt::from(i64::MAX as u64))]],
+            user_args: vec![],
             panic_traceback: None,
         },
     };
@@ -264,44 +318,87 @@ pub fn run_test_case(
         extended_runtime: call_to_blockifier_runtime,
     };
 
-    let run_result =
-        match run_assembled_program(&assembled_program, builtins, hints_dict, &mut forge_runtime) {
-            Ok(mut runner) => {
-                let vm_resources_without_inner_calls = runner
-                    .get_execution_resources()
-                    .expect("Execution resources missing")
-                    .filter_unused_builtins();
-                add_resources_to_top_call(
-                    &mut forge_runtime,
-                    &vm_resources_without_inner_calls,
-                    &tracked_resource,
-                );
+    let entry_point_initial_budget = forge_runtime
+        .extended_runtime
+        .extended_runtime
+        .extended_runtime
+        .hint_handler
+        .base
+        .context
+        .gas_costs()
+        .base
+        .entry_point_initial_budget;
+    let args = prepare_call_arguments(
+        &forge_runtime
+            .extended_runtime
+            .extended_runtime
+            .extended_runtime
+            .hint_handler
+            .base
+            .call
+            .clone()
+            .into(),
+        &mut runner,
+        initial_syscall_ptr,
+        &mut forge_runtime
+            .extended_runtime
+            .extended_runtime
+            .extended_runtime
+            .hint_handler
+            .read_only_segments,
+        &entry_point,
+        entry_point_initial_budget,
+    )?;
 
-                let ap = runner.relocated_trace.as_ref().unwrap().last().unwrap().ap;
+    let n_total_args = args.len();
 
-                let results_data = get_results_data(
-                    &case.test_details.return_types,
-                    &runner.relocated_memory,
-                    ap,
-                );
-                assert_eq!(results_data.len(), 1);
+    // Execute.
+    let bytecode_length = program.data_len();
+    let program_segment_size = bytecode_length + program_extra_data_length;
+    let result: Result<CallInfo, CairoRunError> = match run_entry_point(
+        &mut runner,
+        &mut forge_runtime,
+        entry_point,
+        args,
+        program_segment_size,
+    ) {
+        Ok(()) => {
+            let call_info = finalize_execution(
+                &mut runner,
+                &mut forge_runtime
+                    .extended_runtime
+                    .extended_runtime
+                    .extended_runtime
+                    .hint_handler,
+                n_total_args,
+                program_extra_data_length,
+                tracked_resource,
+            )?;
 
-                let (_, values) = results_data[0].clone();
-                let value = SierraCasmRunner::handle_main_return_value(
-                    // Here we assume that all test either panic or do not return any value
-                    // This is true for all test right now, but in case it changes
-                    // this logic will need to be updated
-                    Some(0),
-                    values,
-                    &runner.relocated_memory,
-                );
+            // TODO this can be done better, we can take gas directly from call info
+            let vm_resources_without_inner_calls = runner
+                .get_execution_resources()
+                .expect("Execution resources missing")
+                .filter_unused_builtins();
 
-                update_top_call_vm_trace(&mut forge_runtime, &mut runner);
+            add_resources_to_top_call(
+                &mut forge_runtime,
+                &vm_resources_without_inner_calls,
+                &tracked_resource,
+            );
 
-                Ok(value)
+            update_top_call_vm_trace(&mut forge_runtime, &mut runner);
+
+            Ok(call_info)
+        }
+        Err(error) => Err(match error {
+            EntryPointExecutionError::CairoRunError(CairoRunError::VmException(err)) => {
+                CairoRunError::VirtualMachine(err.inner_exc)
             }
-            Err(err) => Err(err),
-        };
+            EntryPointExecutionError::CairoRunError(err) => err,
+            err => bail!(err),
+        }),
+    };
 
     let encountered_errors = forge_runtime
         .extended_runtime
@@ -311,7 +408,7 @@ pub fn run_test_case(
         .encountered_errors
         .clone();
 
-    let call_trace = get_call_trace_ref(&mut forge_runtime);
+    let call_trace_ref = get_call_trace_ref(&mut forge_runtime);
 
     update_top_call_resources(&mut forge_runtime, &tracked_resource);
     update_top_call_l1_resources(&mut forge_runtime);
@@ -327,77 +424,32 @@ pub fn run_test_case(
     let transaction_context = get_context(&forge_runtime).tx_context.clone();
     let used_resources =
         get_all_used_resources(forge_runtime, &transaction_context, tracked_resource);
-    let gas_used = calculate_used_gas(
+    let gas = calculate_used_gas(
         &transaction_context,
         &mut cached_state,
         used_resources.clone(),
     )?;
 
-    Ok(match run_result {
-        Ok(result) => RunResult::Completed(Box::from(RunCompleted {
-            status: result.into(),
-            call_trace,
-            gas_used,
+    Ok(match result {
+        Ok(result) => RunResult::Completed(Box::new(RunCompleted {
+            status: if result.execution.failed {
+                RunStatus::Panic(result.execution.retdata.0)
+            } else {
+                RunStatus::Success(result.execution.retdata.0)
+            },
+            call_trace: call_trace_ref,
+            gas_used: gas,
             used_resources,
             encountered_errors,
             fuzzer_args,
         })),
         Err(error) => RunResult::Error(RunError {
-            error,
-            call_trace,
+            error: Box::new(error),
+            call_trace: call_trace_ref,
             encountered_errors,
             fuzzer_args,
         }),
     })
-}
-
-// TODO(#2958) Remove copied code
-// Copied and modified from https://github.com/starkware-libs/cairo/blob/a8da296d7d03f19af3bdb0e7ae17637e66192e4b/crates/cairo-lang-runner/src/lib.rs#L543
-#[allow(clippy::cast_sign_loss)]
-#[must_use]
-pub fn get_results_data(
-    return_types: &[(GenericTypeId, i16)],
-    cells: &[Option<Felt252>],
-    mut ap: usize,
-) -> Vec<(GenericTypeId, Vec<Felt252>)> {
-    let mut results_data = vec![];
-    for (ty, ty_size) in return_types.iter().rev() {
-        let size = *ty_size as usize;
-        let values: Vec<Felt252> = ((ap - size)..ap)
-            .map(|index| cells[index].unwrap())
-            .collect();
-        ap -= size;
-        results_data.push((ty.clone(), values));
-    }
-
-    // Handling implicits.
-    results_data.retain_mut(|(ty, values)| {
-        let generic_ty = ty;
-        if *generic_ty == GasBuiltinType::ID {
-            values.remove(0);
-            assert!(values.is_empty());
-            false
-        } else {
-            // region: Modified code
-            let non_user_types: UnorderedHashSet<GenericTypeId> = UnorderedHashSet::from_iter([
-                AddModType::ID,
-                BitwiseType::ID,
-                GasBuiltinType::ID,
-                EcOpType::ID,
-                MulModType::ID,
-                PedersenType::ID,
-                PoseidonType::ID,
-                RangeCheck96Type::ID,
-                RangeCheckType::ID,
-                SegmentArenaType::ID,
-                SystemType::ID,
-            ]);
-            !non_user_types.contains(generic_ty)
-            // endregion
-        }
-    });
-
-    results_data
 }
 
 fn extract_test_case_summary(
@@ -426,8 +478,8 @@ fn extract_test_case_summary(
                     *run_error.error
                 {
                     message.push_str(
-                            "\n    Suggestion: Consider using the flag `--max-n-steps` to increase allowed limit of steps",
-                        );
+                        "\n    Suggestion: Consider using the flag `--max-n-steps` to increase allowed limit of steps",
+                    );
                 }
                 TestCaseSummary::Failed {
                     name: case.name.clone(),
