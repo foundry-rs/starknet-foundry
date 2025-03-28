3761e7bf5872515c0a3acc6e580edc133ca88486
ksew1 <95349104+ksew1@users.noreply.github.com>
2025-03-28 16:47:22 +0100

    Handle specially top level call in trace (#3134)
    <!-- Reference any GitHub issues resolved by this PR -->
    
    Closes #3079
    
    <!-- A brief description of the changes -->
    
    - display top level call as test name
    
    <!-- Make sure all of these are complete -->
    
    - [x] Linked relevant issue
    - [x] Updated relevant documentation
    - [x] Added relevant tests
    - [x] Performed self-review of the code
    - [x] Added changes to `CHANGELOG.md`
    
    commit-id:fa0238c1

--- crates/forge/tests/e2e/debugging.rs
+++ crates/forge/tests/e2e/debugging.rs
@@ -1,15 +1,51 @@
 use super::common::runner::{setup_package, test_runner};
-use indoc::{formatdoc, indoc};
+use indoc::formatdoc;
 use shared::test_utils::output_assert::assert_stdout_contains;
 
-const DEBUGGING_TRACE: &str = indoc! {r"
-        [selector] 0x17340c6779204ea2a91c87d1c2226a3aebda65c64da3672a36893c4330ea27b
-        ├─ [entry point type] External
-        ├─ [calldata] []
-        ├─ [storage address] 0x1724987234973219347210837402
-        ├─ [caller address] 0x0
-        ├─ [call type] Call
-        ├─ [call result] success: []
+#[test]
+fn debugging_trace() {
+    let temp = setup_package("debugging");
+
+    let output = test_runner(&temp).assert().code(1);
+
+    assert_stdout_contains(
+        output,
+        formatdoc! {r"
+        [..]Compiling[..]
+        [..]Finished[..]
+
+        Collected 2 test(s) from trace_info package
+        Running 2 test(s) from tests/
+        [FAIL] trace_info_integrationtest::test_trace::test_debugging_trace_fail
+
+        Failure data:
+            (0x1, 0x2, 0x3, 0x4, 0x5)
+
+        note: run with `SNFORGE_BACKTRACE=1` environment variable to display a backtrace
+        {debugging_trace_fail}
+
+        [PASS] trace_info_integrationtest::test_trace::test_debugging_trace_success (l1_gas: ~0, l1_data_gas: ~288, l2_gas: ~1440000)
+
+        Success data:
+            (0x1, 0x2, 0x3, 0x4, 0x5)
+
+        note: run with `SNFORGE_BACKTRACE=1` environment variable to display a backtrace
+        {debugging_trace_pass}
+
+        Running 0 test(s) from src/
+        Tests: 1 passed, 1 failed, 0 skipped, 0 ignored, 0 filtered out
+
+        Failures:
+            trace_info_integrationtest::test_trace::test_debugging_trace_fail
+        ",
+        debugging_trace_fail = debugging_trace_message("fail"),
+        debugging_trace_pass = debugging_trace_message("success")},
+    );
+}
+
+fn debugging_trace_message(test_name: &str) -> String {
+    formatdoc! {r"
+        [test name] trace_info_integrationtest::test_trace::test_debugging_trace_{test_name}
         ├─ [selector] execute_calls
         │  ├─ [contract name] SimpleContract
         │  ├─ [entry point type] External
@@ -58,43 +94,5 @@ const DEBUGGING_TRACE: &str = indoc! {r"
            ├─ [caller address] 0x1724987234973219347210837402
            ├─ [call type] Call
            └─ [call result] panic: [0x1, 0x2, 0x3, 0x4, 0x5]
-        "};
-
-#[test]
-fn debugging_trace() {
-    let temp = setup_package("debugging");
-
-    let output = test_runner(&temp).assert().code(1);
-
-    assert_stdout_contains(
-        output,
-        formatdoc! {r"
-        [..]Compiling[..]
-        [..]Finished[..]
-
-        Collected 2 test(s) from trace_info package
-        Running 2 test(s) from tests/
-        [FAIL] trace_info_integrationtest::test_trace::test_debugging_trace_fail
-
-        Failure data:
-            (0x1, 0x2, 0x3, 0x4, 0x5)
-
-        note: run with `SNFORGE_BACKTRACE=1` environment variable to display a backtrace
-        {debugging_trace}
-
-        [PASS] trace_info_integrationtest::test_trace::test_debugging_trace_success (l1_gas: ~0, l1_data_gas: ~288, l2_gas: ~1440000)
-
-        Success data:
-            (0x1, 0x2, 0x3, 0x4, 0x5)
-
-        note: run with `SNFORGE_BACKTRACE=1` environment variable to display a backtrace
-        {debugging_trace}
-
-        Running 0 test(s) from src/
-        Tests: 1 passed, 1 failed, 0 skipped, 0 ignored, 0 filtered out
-
-        Failures:
-            trace_info_integrationtest::test_trace::test_debugging_trace_fail
-        ", debugging_trace = DEBUGGING_TRACE},
-    );
+        "}
 }
