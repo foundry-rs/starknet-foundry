75f74c047212a3df6c5ed47ec04d86572cca6c16
kkawula <kamilkawula@icloud.com>
2025-04-15 08:58:07 +0200

    Update `DEVNET_REV`

--- scripts/install_devnet.ps1
+++ scripts/install_devnet.ps1
@@ -5,7 +5,7 @@ $ErrorActionPreference = "Stop"
 $DEVNET_INSTALL_DIR = Join-Path (git rev-parse --show-toplevel) "crates\sncast\tests\utils\devnet"
 
 $DEVNET_REPO = "https://github.com/0xSpaceShard/starknet-devnet-rs.git"
-$DEVNET_REV = "fc5a2753a2eedcc27eed7a4fae3ecac08c2ca1b4"
+$DEVNET_REV = "fc5a2753a2eedcc27eed7a4fae3ecac08c2ca1b4" # v0.3.0
 
 cargo install --locked --git $DEVNET_REPO --rev $DEVNET_REV --root $DEVNET_INSTALL_DIR --force
 
