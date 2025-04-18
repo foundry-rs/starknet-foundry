5f7d808f24bddc77c2b6ea646d332bd8df4259b5
Artur Michalek <artur.michalek@swmansion.com>
2025-04-18 17:19:52 +0200

    Use devnet supporting RPC 0.8.1, bump supported version

--- scripts/install_devnet.ps1
+++ scripts/install_devnet.ps1
@@ -5,7 +5,7 @@ $ErrorActionPreference = "Stop"
 $DEVNET_INSTALL_DIR = Join-Path (git rev-parse --show-toplevel) "crates\sncast\tests\utils\devnet"
 
 $DEVNET_REPO = "https://github.com/0xSpaceShard/starknet-devnet-rs.git"
-$DEVNET_REV = "fc5a2753a2eedcc27eed7a4fae3ecac08c2ca1b4" # v0.3.0
+$DEVNET_REV = "4ee3267d46099f11e12bc9751d08ef31a4d48512" # v0.4.0
 
 cargo install --locked --git $DEVNET_REPO --rev $DEVNET_REV --root $DEVNET_INSTALL_DIR --force
 
