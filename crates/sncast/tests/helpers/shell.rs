7d061af7543a911816e416394cd369ef6cd80f4b
Artur Michalek <artur.michalek@swmansion.com>
2025-04-27 21:02:41 +0200

    Pass absolute path of the test script to the command

--- crates/sncast/tests/helpers/shell.rs
+++ crates/sncast/tests/helpers/shell.rs
@@ -5,14 +5,15 @@ use snapbox::cmd::Command;
 pub fn os_specific_shell(script_path: &Utf8PathBuf) -> Command {
     let script_extension = if cfg!(windows) { "ps1" } else { "sh" };
     let test_path = script_path.with_extension(script_extension);
+    let absolute_test_path = test_path.canonicalize_utf8().unwrap();
 
     if cfg!(windows) {
         Command::new("powershell")
             .arg("-ExecutionPolicy")
             .arg("Bypass")
             .arg("-File")
-            .arg(test_path)
+            .arg(absolute_test_path)
     } else {
-        Command::new(test_path)
+        Command::new(absolute_test_path)
     }
 }
