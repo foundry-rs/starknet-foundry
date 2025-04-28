2cb8160b7308f602a5f7be34a714b0c1e32fbc91
kkawula <kamilkawula@icloud.com>
2025-04-28 13:44:36 +0200

    Add utils script

---
+++ scripts/handle-version.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+get_version() {
+  local overridden_version=$1
+  local default_version=$(grep "^version =" crates/snforge-scarb-plugin/Scarb.toml | cut -d '"' -f 2)
+
+  if [ -z "$overridden_version" ]; then
+    echo "$default_version"
+  else
+    echo "$overridden_version"
+  fi
+}
+
+update_version_in_file() {
+  local file=$1
+  local version=$2
+
+  if [ -n "$version" ]; then
+    sed -i.bak "/\[package\]/,/version =/ s/version = \".*/version = \"$version\"/" "$file"
+    rm "$file.bak" 2> /dev/null
+  fi
+}
+
+export -f get_version
+export -f update_version_in_file
