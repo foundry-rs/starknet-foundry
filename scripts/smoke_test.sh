c99950b7f7a418e5c8a6509def7ce8fa88e5d216
kkawula <57270771+kkawula@users.noreply.github.com>
2025-04-30 15:50:47 +0200

    Add tests using `snforge_std` from registry (#3143)
    <!-- Reference any GitHub issues resolved by this PR -->
    
    Closes #3060 
    Closes #3069 
    Closes #3124
    
    ## Introduced changes
    
    <!-- A brief description of the changes -->
    
    Current State:
    - `smoke_test.sh` - run binary with
        - `snforge_std` dependency from github link and the appropriate rev
    - `release workflow` - build foundry binaries, run smoke tests, and
    apart from it publish plugin and stds to the official registry, it
    doesn't matter if tests pass or not
    - `scheduled tests` - run cargo test
    
    New State:
    - `smoke_test.sh` - run binary with
    - `snforge_std` dependency from github link and the appropriate rev,
         - dev registry dependency, also check if prebuild works
         - dev registry dependency, without prebuild
    - `release workflow`- build foundry binaries, build plugin binaries,
    publish plugin and stds to dev registry, run smoke tests, and only if
    passed, publish it to the official registry
    - `scheduled tests` - run cargo test, build foundry binaries, build
    plugin binaries, publish plugin and stds to dev registry, run smoke
    tests,
    
    ## Checklist
    
    <!-- Make sure all of these are complete -->
    
    - [x] Linked relevant issue
    - [x] Updated relevant documentation
    - [x] Added relevant tests
    - [x] Performed self-review of the code
    - [x] Added changes to `CHANGELOG.md`

--- scripts/smoke_test.sh
+++ scripts/smoke_test.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -e
 
 export DEV_DISABLE_SNFORGE_STD_DEPENDENCY=true
 
@@ -8,14 +7,49 @@ SNFORGE_PATH="$2"
 SNCAST_PATH="$3"
 REPO_URL="$4"
 REVISION="$5"
+VERSION="$6"
 
-# Check forge
+# Check snforge_std from github repository
 
-$SNFORGE_PATH init my_project
-pushd my_project || exit
+$SNFORGE_PATH new my_project_0
+pushd my_project_0 || exit
 scarb add --dev snforge_std --git "$REPO_URL" --rev "$REVISION"
 $SNFORGE_PATH test || exit
 popd || exit
+scarb cache clean
+
+# Check snforge_std from registry with prebuilt plugin
+
+$SNFORGE_PATH new my_project_1
+pushd my_project_1 || exit
+sed -i.bak "/^\[dev-dependencies\]/a\\
+snforge_std = { version = \"=${VERSION}\", registry = \"https://scarbs.dev/\" }\\
+" Scarb.toml
+rm Scarb.toml.bak 2>/dev/null
+
+test_output=$($SNFORGE_PATH test)
+test_exit=$?
+
+echo $test_output
+
+if [[ $test_exit -ne 0 ]] || echo "$test_output" | grep -q 'Compiling snforge_scarb_plugin'; then
+    exit 1
+fi
+popd || exit
+scarb cache clean
+
+# Check snforge_std from registry without prebuilt plugin
+
+$SNFORGE_PATH new my_project_2
+pushd my_project_2 || exit
+sed -i.bak "/^\[dev-dependencies\]/a\\
+snforge_std = { version = \"=${VERSION}\", registry = \"https://scarbs.dev/\" }\\
+" Scarb.toml
+sed -i.bak '/^allow-prebuilt-plugins = \["snforge_std"\]$/d' Scarb.toml
+rm Scarb.toml.bak 2>/dev/null
+$SNFORGE_PATH test || exit
+popd || exit
+scarb cache clean
 
 # Check cast
 
