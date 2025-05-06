0145a3f6f6123d4955ed089c831e53831c578081
Fiiranek <jellybean@onet.eu>
2025-05-06 17:31:17 +0200

    Fix linting

--- crates/forge/src/run_tests/helpers.rs
+++ crates/forge/src/run_tests/helpers.rs
@@ -1,11 +1,13 @@
 use forge_runner::package_tests::with_config::TestCaseConfig;
 
+#[must_use]
 pub fn skip_fork_tests() -> bool {
     std::env::var("SNFORGE_SKIP_FORK_TESTS")
         .map(|v| v == "1")
         .unwrap_or(false)
 }
 
+#[must_use]
 pub fn is_test_case_ignored(case_config: &TestCaseConfig, skip_fork_tests_from_env: bool) -> bool {
     case_config.ignored || (skip_fork_tests_from_env && case_config.fork_config.is_some())
 }
