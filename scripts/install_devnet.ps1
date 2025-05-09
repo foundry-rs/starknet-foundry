62e7c18cec3d917d68aa18e26f7625ad402bccd7
Artur Michalek <artur.michalek@swmansion.com>
2025-05-09 10:44:37 +0200

    Bump devnet

--- scripts/install_devnet.ps1
+++ scripts/install_devnet.ps1
@@ -5,7 +5,7 @@ $ErrorActionPreference = "Stop"
 $DEVNET_INSTALL_DIR = Join-Path (git rev-parse --show-toplevel) "crates\sncast\tests\utils\devnet"
 
 $DEVNET_REPO = "https://github.com/0xSpaceShard/starknet-devnet-rs.git"
-$DEVNET_REV = "4ee3267d46099f11e12bc9751d08ef31a4d48512" # v0.4.0
+$DEVNET_REV = "26292d1f92807090776b470f43b321f150f55ffd" # v0.4.0
 
 cargo install --locked --git $DEVNET_REPO --rev $DEVNET_REV --root $DEVNET_INSTALL_DIR --force
 
