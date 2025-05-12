4636ae1bea91ca7211a957ca9afcaf21c43219fe
ksew1 <95349104+ksew1@users.noreply.github.com>
2025-05-12 13:16:38 +0200

    Fix debugging tests (#3326)
    <!-- Reference any GitHub issues resolved by this PR -->
    
    
    ## Introduced changes
    
    <!-- A brief description of the changes -->
    
    - Fix docs snippets related to debugging
    - Fix debugging tests
    
    ## Checklist
    
    <!-- Make sure all of these are complete -->
    
    - [x] Linked relevant issue
    - [x] Updated relevant documentation
    - [x] Added relevant tests
    - [x] Performed self-review of the code
    - [x] Added changes to `CHANGELOG.md`

--- crates/forge/tests/e2e/debugging.rs
+++ crates/forge/tests/e2e/debugging.rs
@@ -55,7 +55,7 @@ fn test_output(trace_message_fn: fn(&str) -> String) -> String {
         note: run with `SNFORGE_BACKTRACE=1` environment variable to display a backtrace
         {debugging_trace_fail}
 
-        [PASS] trace_info_integrationtest::test_trace::test_debugging_trace_success (l1_gas: ~0, l1_data_gas: ~288, l2_gas: ~1600000)
+        [PASS] trace_info_integrationtest::test_trace::test_debugging_trace_success (l1_gas: ~[..], l1_data_gas: ~[..], l2_gas: ~[..])
         {debugging_trace_pass}
 
         Running 0 test(s) from src/
@@ -75,49 +75,49 @@ fn detailed_debugging_trace_message(test_name: &str) -> String {
         ├─ [selector] execute_calls
         │  ├─ [contract name] SimpleContract
         │  ├─ [entry point type] External
-        │  ├─ [calldata] array![RecursiveCall {{ contract_address: ContractAddress(0x10a2fac439604ce4129fe7c205b711e8141e12e2e52e08f7f898fe7ac13f0a), payload: array![RecursiveCall {{ contract_address: ContractAddress(0x28f58bf524dc0adcf7468c67d7ffdac1e5d885d347c6a498978f538984dbda), payload: array![] }}, RecursiveCall {{ contract_address: ContractAddress(0x28f58bf524dc0adcf7468c67d7ffdac1e5d885d347c6a498978f538984dbda), payload: array![] }}] }}, RecursiveCall {{ contract_address: ContractAddress(0x28f58bf524dc0adcf7468c67d7ffdac1e5d885d347c6a498978f538984dbda), payload: array![] }}]
-        │  ├─ [storage address] 0x7b29abec6baad44d169ee10b37c9a1eae834d71887607f60d2f90836f6eb973
-        │  ├─ [caller address] 0x1724987234973219347210837402
+        │  ├─ [calldata] array![RecursiveCall {{ contract_address: ContractAddress([..]), payload: array![RecursiveCall {{ contract_address: ContractAddress([..]), payload: array![] }}, RecursiveCall {{ contract_address: ContractAddress([..]), payload: array![] }}] }}, RecursiveCall {{ contract_address: ContractAddress([..]), payload: array![] }}]
+        │  ├─ [storage address] [..]
+        │  ├─ [caller address] [..]
         │  ├─ [call type] Call
-        │  ├─ [call result] success: array![RecursiveCall {{ contract_address: ContractAddress(0x10a2fac439604ce4129fe7c205b711e8141e12e2e52e08f7f898fe7ac13f0a), payload: array![RecursiveCall {{ contract_address: ContractAddress(0x28f58bf524dc0adcf7468c67d7ffdac1e5d885d347c6a498978f538984dbda), payload: array![] }}, RecursiveCall {{ contract_address: ContractAddress(0x28f58bf524dc0adcf7468c67d7ffdac1e5d885d347c6a498978f538984dbda), payload: array![] }}] }}, RecursiveCall {{ contract_address: ContractAddress(0x28f58bf524dc0adcf7468c67d7ffdac1e5d885d347c6a498978f538984dbda), payload: array![] }}]
+        │  ├─ [call result] success: array![RecursiveCall {{ contract_address: ContractAddress([..]), payload: array![RecursiveCall {{ contract_address: ContractAddress([..]), payload: array![] }}, RecursiveCall {{ contract_address: ContractAddress([..]), payload: array![] }}] }}, RecursiveCall {{ contract_address: ContractAddress([..]), payload: array![] }}]
         │  ├─ [selector] execute_calls
         │  │  ├─ [contract name] SimpleContract
         │  │  ├─ [entry point type] External
-        │  │  ├─ [calldata] array![RecursiveCall {{ contract_address: ContractAddress(0x28f58bf524dc0adcf7468c67d7ffdac1e5d885d347c6a498978f538984dbda), payload: array![] }}, RecursiveCall {{ contract_address: ContractAddress(0x28f58bf524dc0adcf7468c67d7ffdac1e5d885d347c6a498978f538984dbda), payload: array![] }}]
-        │  │  ├─ [storage address] 0x10a2fac439604ce4129fe7c205b711e8141e12e2e52e08f7f898fe7ac13f0a
-        │  │  ├─ [caller address] 0x7b29abec6baad44d169ee10b37c9a1eae834d71887607f60d2f90836f6eb973
+        │  │  ├─ [calldata] array![RecursiveCall {{ contract_address: ContractAddress([..]), payload: array![] }}, RecursiveCall {{ contract_address: ContractAddress([..]), payload: array![] }}]
+        │  │  ├─ [storage address] [..]
+        │  │  ├─ [caller address] [..]
         │  │  ├─ [call type] Call
-        │  │  ├─ [call result] success: array![RecursiveCall {{ contract_address: ContractAddress(0x28f58bf524dc0adcf7468c67d7ffdac1e5d885d347c6a498978f538984dbda), payload: array![] }}, RecursiveCall {{ contract_address: ContractAddress(0x28f58bf524dc0adcf7468c67d7ffdac1e5d885d347c6a498978f538984dbda), payload: array![] }}]
+        │  │  ├─ [call result] success: array![RecursiveCall {{ contract_address: ContractAddress([..]), payload: array![] }}, RecursiveCall {{ contract_address: ContractAddress([..]), payload: array![] }}]
         │  │  ├─ [selector] execute_calls
         │  │  │  ├─ [contract name] SimpleContract
         │  │  │  ├─ [entry point type] External
         │  │  │  ├─ [calldata] array![]
-        │  │  │  ├─ [storage address] 0x28f58bf524dc0adcf7468c67d7ffdac1e5d885d347c6a498978f538984dbda
-        │  │  │  ├─ [caller address] 0x10a2fac439604ce4129fe7c205b711e8141e12e2e52e08f7f898fe7ac13f0a
+        │  │  │  ├─ [storage address] [..]
+        │  │  │  ├─ [caller address] [..]
         │  │  │  ├─ [call type] Call
         │  │  │  └─ [call result] success: array![]
         │  │  └─ [selector] execute_calls
         │  │     ├─ [contract name] SimpleContract
         │  │     ├─ [entry point type] External
         │  │     ├─ [calldata] array![]
-        │  │     ├─ [storage address] 0x28f58bf524dc0adcf7468c67d7ffdac1e5d885d347c6a498978f538984dbda
-        │  │     ├─ [caller address] 0x10a2fac439604ce4129fe7c205b711e8141e12e2e52e08f7f898fe7ac13f0a
+        │  │     ├─ [storage address] [..]
+        │  │     ├─ [caller address] [..]
         │  │     ├─ [call type] Call
         │  │     └─ [call result] success: array![]
         │  └─ [selector] execute_calls
         │     ├─ [contract name] SimpleContract
         │     ├─ [entry point type] External
         │     ├─ [calldata] array![]
-        │     ├─ [storage address] 0x28f58bf524dc0adcf7468c67d7ffdac1e5d885d347c6a498978f538984dbda
-        │     ├─ [caller address] 0x7b29abec6baad44d169ee10b37c9a1eae834d71887607f60d2f90836f6eb973
+        │     ├─ [storage address] [..]
+        │     ├─ [caller address] [..]
         │     ├─ [call type] Call
         │     └─ [call result] success: array![]
         └─ [selector] fail
            ├─ [contract name] SimpleContract
            ├─ [entry point type] External
            ├─ [calldata] array![0x1, 0x2, 0x3, 0x4, 0x5]
-           ├─ [storage address] 0x7b29abec6baad44d169ee10b37c9a1eae834d71887607f60d2f90836f6eb973
-           ├─ [caller address] 0x1724987234973219347210837402
+           ├─ [storage address] [..]
+           ├─ [caller address] [..]
            ├─ [call type] Call
            └─ [call result] panic: (0x1, 0x2, 0x3, 0x4, 0x5)
         "}
@@ -128,12 +128,12 @@ fn standard_debugging_trace_message(test_name: &str) -> String {
         [test name] trace_info_integrationtest::test_trace::test_debugging_trace_{test_name}
         ├─ [selector] execute_calls
         │  ├─ [contract name] SimpleContract
-        │  ├─ [calldata] array![RecursiveCall {{ contract_address: ContractAddress(0x10a2fac439604ce4129fe7c205b711e8141e12e2e52e08f7f898fe7ac13f0a), payload: array![RecursiveCall {{ contract_address: ContractAddress(0x28f58bf524dc0adcf7468c67d7ffdac1e5d885d347c6a498978f538984dbda), payload: array![] }}, RecursiveCall {{ contract_address: ContractAddress(0x28f58bf524dc0adcf7468c67d7ffdac1e5d885d347c6a498978f538984dbda), payload: array![] }}] }}, RecursiveCall {{ contract_address: ContractAddress(0x28f58bf524dc0adcf7468c67d7ffdac1e5d885d347c6a498978f538984dbda), payload: array![] }}]
-        │  ├─ [call result] success: array![RecursiveCall {{ contract_address: ContractAddress(0x10a2fac439604ce4129fe7c205b711e8141e12e2e52e08f7f898fe7ac13f0a), payload: array![RecursiveCall {{ contract_address: ContractAddress(0x28f58bf524dc0adcf7468c67d7ffdac1e5d885d347c6a498978f538984dbda), payload: array![] }}, RecursiveCall {{ contract_address: ContractAddress(0x28f58bf524dc0adcf7468c67d7ffdac1e5d885d347c6a498978f538984dbda), payload: array![] }}] }}, RecursiveCall {{ contract_address: ContractAddress(0x28f58bf524dc0adcf7468c67d7ffdac1e5d885d347c6a498978f538984dbda), payload: array![] }}]
+        │  ├─ [calldata] array![RecursiveCall {{ contract_address: ContractAddress([..]), payload: array![RecursiveCall {{ contract_address: ContractAddress([..]), payload: array![] }}, RecursiveCall {{ contract_address: ContractAddress([..]), payload: array![] }}] }}, RecursiveCall {{ contract_address: ContractAddress([..]), payload: array![] }}]
+        │  ├─ [call result] success: array![RecursiveCall {{ contract_address: ContractAddress([..]), payload: array![RecursiveCall {{ contract_address: ContractAddress([..]), payload: array![] }}, RecursiveCall {{ contract_address: ContractAddress([..]), payload: array![] }}] }}, RecursiveCall {{ contract_address: ContractAddress([..]), payload: array![] }}]
         │  ├─ [selector] execute_calls
         │  │  ├─ [contract name] SimpleContract
-        │  │  ├─ [calldata] array![RecursiveCall {{ contract_address: ContractAddress(0x28f58bf524dc0adcf7468c67d7ffdac1e5d885d347c6a498978f538984dbda), payload: array![] }}, RecursiveCall {{ contract_address: ContractAddress(0x28f58bf524dc0adcf7468c67d7ffdac1e5d885d347c6a498978f538984dbda), payload: array![] }}]
-        │  │  ├─ [call result] success: array![RecursiveCall {{ contract_address: ContractAddress(0x28f58bf524dc0adcf7468c67d7ffdac1e5d885d347c6a498978f538984dbda), payload: array![] }}, RecursiveCall {{ contract_address: ContractAddress(0x28f58bf524dc0adcf7468c67d7ffdac1e5d885d347c6a498978f538984dbda), payload: array![] }}]
+        │  │  ├─ [calldata] array![RecursiveCall {{ contract_address: ContractAddress([..]), payload: array![] }}, RecursiveCall {{ contract_address: ContractAddress([..]), payload: array![] }}]
+        │  │  ├─ [call result] success: array![RecursiveCall {{ contract_address: ContractAddress([..]), payload: array![] }}, RecursiveCall {{ contract_address: ContractAddress([..]), payload: array![] }}]
         │  │  ├─ [selector] execute_calls
         │  │  │  ├─ [contract name] SimpleContract
         │  │  │  ├─ [calldata] array![]
