e3dea3575094e98b964d4a9f8da923359b0e17e8
Fiiranek <jellybean@onet.eu>
2025-05-15 16:12:27 +0200

    Apply code review suggestion

--- crates/sncast/tests/code_quality.rs
+++ crates/sncast/tests/code_quality.rs
@@ -3,16 +3,10 @@ use packages_validation::check_and_lint;
 
 #[test]
 fn validate_sncast_std() {
-    let package_path: Utf8PathBuf = std::env::current_dir()
-        .expect("Failed to get current directory")
-        .ancestors()
-        .nth(2)
-        .expect("Failed to get grandparent directory")
-        .join("snforge_std")
+    let package_path = Utf8PathBuf::from("../../sncast_std")
         .canonicalize()
-        .expect("Failed to canonicalize path")
+        .unwrap()
         .try_into()
-        .expect("Failed to convert to Utf8PathBuf");
-
+        .unwrap();
     check_and_lint(&package_path);
 }
