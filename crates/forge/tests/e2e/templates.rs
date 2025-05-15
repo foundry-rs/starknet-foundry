40a6fb05a351915e027e86d61a950264b8b1c4f0
Fiiranek <jellybean@onet.eu>
2025-05-15 12:21:50 +0200

    Refactor `validate_templates` test

--- crates/forge/tests/e2e/templates.rs
+++ crates/forge/tests/e2e/templates.rs
@@ -1,42 +1,38 @@
 use std::fs;
 
+use super::common::runner::runner;
 use assert_fs::prelude::PathChild;
 use camino::Utf8PathBuf;
-use clap::ValueEnum;
 use forge::Template;
 use packages_validation::check_and_lint;
+use test_case::test_case;
 
-use super::common::runner::runner;
-
-#[test]
-fn validate_templates() {
+#[test_case(&Template::CairoProgram; "cairo-program")]
+#[test_case(&Template::BalanceContract; "balance-contract")]
+#[test_case(&Template::Erc20Contract; "erc20-contract")]
+fn validate_templates(template: &Template) {
     let temp_dir = assert_fs::TempDir::new().expect("Unable to create a temporary directory");
+    let package_name = format!("{}_test", template.to_string().replace('-', "_"));
 
-    let templates = Template::value_variants();
+    runner(&temp_dir)
+        .args([
+            "new",
+            "--template",
+            template.to_string().as_str(),
+            &package_name,
+        ])
+        .assert()
+        .success();
 
-    for template in templates {
-        let package_name = format!("{}_test", template.to_string().replace('-', "_"));
+    let package_path = temp_dir.child(package_name);
+    let package_path = Utf8PathBuf::from_path_buf(package_path.to_path_buf())
+        .expect("Failed to convert to Utf8PathBuf");
 
-        runner(&temp_dir)
-            .args([
-                "new",
-                "--template",
-                template.to_string().as_str(),
-                &package_name,
-            ])
-            .assert()
-            .success();
+    // Overwrite Scarb.toml with `allow-warnings = false`
+    let scarb_toml_path = package_path.join("Scarb.toml");
+    let mut scarb_toml = fs::read_to_string(&scarb_toml_path).unwrap();
+    scarb_toml.push_str("\n[cairo]\nallow-warnings = false\n");
+    fs::write(&scarb_toml_path, &scarb_toml).expect("Failed to write to Scarb.toml");
 
-        let package_path = temp_dir.child(package_name);
-        let package_path = Utf8PathBuf::from_path_buf(package_path.to_path_buf())
-            .expect("Failed to convert to Utf8PathBuf");
-
-        // Overwrite Scarb.toml with `allow-warnings = false`
-        let scarb_toml_path = package_path.join("Scarb.toml");
-        let mut scarb_toml = fs::read_to_string(&scarb_toml_path).unwrap();
-        scarb_toml.push_str("\n[cairo]\nallow-warnings = false\n");
-        fs::write(&scarb_toml_path, &scarb_toml).expect("Failed to write to Scarb.toml");
-
-        check_and_lint(&package_path);
-    }
+    check_and_lint(&package_path);
 }
