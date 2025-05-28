499eb7e89f68f0dfabc758c83127bc7744cf9d34
Artur Michalek <artur.michalek@swmansion.com>
2025-05-28 11:48:02 +0200

    Disable scarb 2.10.0 specific tests
    commit-id:3ae67c8b

--- crates/forge/tests/e2e/running.rs
+++ crates/forge/tests/e2e/running.rs
@@ -861,7 +861,6 @@ fn detailed_resources_flag() {
 }
 
 #[test]
-#[cfg_attr(not(feature = "scarb_since_2_10"), ignore)]
 fn detailed_resources_flag_sierra_gas() {
     let temp = setup_package("erc20_package");
     let output = test_runner(&temp)
