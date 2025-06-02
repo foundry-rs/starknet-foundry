04527af6c9c666a573a7cda7b453471876723a2f
Fiiranek <jellybean@onet.eu>
2025-06-02 14:55:47 +0200

    Apply code review suggestions

--- crates/foundry-ui/src/message.rs
+++ crates/foundry-ui/src/message.rs
@@ -3,14 +3,10 @@ use serde::Serialize;
 /// A typed object that can be either printed as a human-readable message or serialized as JSON.
 pub trait Message: Serialize {
     /// Return textual (human) representation of this message.
-    ///
-    /// Default implementation returns empty string, making [`Ui`] skip printing this message.
     fn text(&self) -> String;
 
     /// Return JSON representation of this message.
-    fn json(&self) -> String {
-        serde_json::to_string(self).expect("Failed to serialize message to JSON")
-    }
+    fn json(&self) -> String;
 }
 
 impl<T: ToString> Message for T
@@ -19,5 +15,10 @@ where
 {
     fn text(&self) -> String {
         self.to_string()
+    }
+
+    fn json(&self) -> String {
+        serde_json::to_string(&serde_json::json!({ "message": self.to_string() }))
+            .expect("Failed to serialize message to JSON")
     }
 }
