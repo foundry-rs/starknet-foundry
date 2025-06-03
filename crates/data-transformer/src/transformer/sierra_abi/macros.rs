8af190cebae16b23e1809e46f548ff112cfff046
Fiiranek <jellybean@onet.eu>
2025-06-03 17:48:02 +0200

    Fix formatting

--- crates/data-transformer/src/transformer/sierra_abi/macros.rs
+++ crates/data-transformer/src/transformer/sierra_abi/macros.rs
@@ -63,7 +63,6 @@ impl SupportedCalldataKind for ExprInlineMacro {
                         .collect::<Result<Vec<_>>>(),
                 ),
                 PathSegment::Missing(_segment) => Some(Err(anyhow::anyhow!("Path segment missing")))
-                
             })
             .transpose()?
             .with_context(|| format!(r#"Invalid argument type, expected "{expected_type}", got array"#))?;
