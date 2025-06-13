b2d1f296034842fbd109ae0941047210d8ec0a96
Artur Michalek <artur.michalek@swmansion.com>
2025-06-13 15:02:32 +0200

    Use `snforge_std` or `snforge_std_compatibility` in `snforge` new
    Closes #3461
    
    commit-id:182fb50c

--- crates/forge/src/new.rs
+++ crates/forge/src/new.rs
@@ -48,11 +48,20 @@ struct TemplateManifestConfig {
 }
 
 impl TemplateManifestConfig {
-    fn add_dependencies(&self, scarb_manifest_path: &PathBuf) -> Result<()> {
+    fn add_dependencies(
+        &self,
+        scarb_version: &Version,
+        scarb_manifest_path: &PathBuf,
+    ) -> Result<()> {
         if env::var("DEV_DISABLE_SNFORGE_STD_DEPENDENCY").is_err() {
+            let snforge_package = if scarb_version >= &Version::new(2, 12, 0) {
+                "snforge_std"
+            } else {
+                "snforge_std_compatibility"
+            };
             let snforge_version = env!("CARGO_PKG_VERSION");
             Dependency {
-                name: "snforge_std".to_string(),
+                name: snforge_package.to_string(),
                 version: snforge_version.to_string(),
                 dev: true,
             }
@@ -323,6 +332,8 @@ pub fn new(
         );
     }
     let name = infer_name(name, &path)?;
+    let scarb_version = ScarbCommand::version().run()?.scarb;
+    println!("Scarb version {scarb_version}");
 
     fs::create_dir_all(&path)?;
     let project_path = path.canonicalize()?;
@@ -361,7 +372,7 @@ pub fn new(
     }
 
     let template_config = TemplateManifestConfig::try_from(&template)?;
-    template_config.add_dependencies(&scarb_manifest_path)?;
+    template_config.add_dependencies(&scarb_version, &scarb_manifest_path)?;
     template_config.update_config(&scarb_manifest_path)?;
 
     let template_dir = get_template_dir(&template)?;
