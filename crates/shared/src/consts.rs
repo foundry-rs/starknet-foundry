8a7077c29f93e5775d383ba3d124044e90e99cf6
kkawula <kamilkawula@icloud.com>
2025-06-16 17:38:40 +0200

    Remove unused const

--- crates/shared/src/consts.rs
+++ crates/shared/src/consts.rs
@@ -1,5 +1,4 @@
 pub const EXPECTED_RPC_VERSION: &str = "0.8.1";
 pub const RPC_URL_VERSION: &str = "v0_8";
 pub const SNFORGE_TEST_FILTER: &str = "SNFORGE_TEST_FILTER";
-pub const SNFORGE_TEST_SKIP: &str = "SNFORGE_TEST_SKIP";
 pub const FREE_RPC_PROVIDER_URL: &str = "https://starknet-sepolia.public.blastapi.io/rpc/v0_8";
