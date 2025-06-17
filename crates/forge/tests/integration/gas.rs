24e5d989999a01d1cec4cb97a9fa118639f91081
Fiiranek <jellybean@onet.eu>
2025-06-17 16:14:35 +0200

    Update asserted gas value in `l1_message_cost_for_proxy_sierra_gas`

--- crates/forge/tests/integration/gas.rs
+++ crates/forge/tests/integration/gas.rs
@@ -1620,15 +1620,15 @@ fn l1_message_cost_for_proxy_sierra_gas() {
     //      -> 1 pedersen costs 4050, 1 range check costs 70
     // 175300 = cost of 2 call contract syscalls (see `multiple_storage_writes_cost_sierra_gas` test)
     // 14170 = cost of 1 SendMessageToL1 syscall (see `l1_message_cost_sierra_gas` test)
-    // 111220 = reported consumed sierra gas
-    // 29524 l1_gas + (128 + 64) l1_data_gas + (285620 + 175300 + 14170 + 111220) l2 gas
+    // 97050 = reported consumed sierra gas
+    // 29524 l1_gas + (128 + 64) l1_data_gas + (285620 + 175300 + 14170 + 97050) l2 gas
     assert_gas(
         &result,
         "l1_message_cost_for_proxy",
         GasVector {
             l1_gas: GasAmount(29524),
             l1_data_gas: GasAmount(192),
-            l2_gas: GasAmount(586_310),
+            l2_gas: GasAmount(572_140),
         },
     );
 }
