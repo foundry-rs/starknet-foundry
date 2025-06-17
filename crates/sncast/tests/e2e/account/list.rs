0a4826997f95d0880de21aa03d3de867f25519cf
kkawula <kamilkawula@icloud.com>
2025-06-17 15:28:33 +0200

    Remove `--int-format` and `--hex-format`
    commit-id:ff341643

--- crates/sncast/tests/e2e/account/list.rs
+++ crates/sncast/tests/e2e/account/list.rs
@@ -108,61 +108,6 @@ fn test_happy_case_with_private_keys() {
 }
 
 #[test]
-fn test_happy_case_hex_format() {
-    let accounts_file_name = "temp_accounts.json";
-    let temp_dir = create_tempdir_with_accounts_file(accounts_file_name, true);
-
-    let accounts_file_path = temp_dir
-        .path()
-        .canonicalize()
-        .expect("Unable to resolve a temporary directory path")
-        .join(accounts_file_name);
-
-    let args = vec![
-        "--hex-format",
-        "--accounts-file",
-        &accounts_file_name,
-        "account",
-        "list",
-        "--display-private-keys",
-    ];
-
-    let snapbox = runner(&args).current_dir(temp_dir.path());
-    let output = snapbox.assert().success();
-
-    assert!(output.as_stderr().is_empty());
-
-    let expected = formatdoc!(
-        "
-        Available accounts (at {}):
-        - user0:
-          network: alpha-sepolia
-          private key: 0x1e9038bdc68ce1d27d54205256988e85
-          public key: 0x2f91ed13f8f0f7d39b942c80bfcd3d0967809d99e0cc083606cbe59033d2b39
-          address: 0x4f5f24ceaae64434fa2bc2befd08976b51cf8f6a5d8257f7ec3616f61de263a
-          type: OpenZeppelin
-
-        - user3:
-          network: custom-network
-          private key: 0xe3e70682c2094cac629f6fbed82c07cd
-          public key: 0x7e52885445756b313ea16849145363ccb73fb4ab0440dbac333cf9d13de82b9
-          address: 0x7e00d496e324876bbc8531f2d9a82bf154d1a04a50218ee74cdd372f75a551a
-
-        - user4:
-          network: custom-network
-          private key: 0x73fbb3c1eff11167598455d0408f3932e42c678bd8f7fbc6028c716867cc01f
-          public key: 0x43a74f86b7e204f1ba081636c9d4015e1f54f5bb03a4ae8741602a15ffbb182
-          address: 0x7ccdf182d27c7aaa2e733b94db4a3f7b28ff56336b34abf43c15e3a9edfbe91
-          salt: 0x54aa715a5cff30ccf7845ad4659eb1dac5b730c2541263c358c7e3a4c4a8064
-          deployed: true
-        ",
-        accounts_file_path.to_str().unwrap()
-    );
-
-    assert_stdout_contains(output, expected);
-}
-
-#[test]
 fn test_happy_case_int_format() {
     let accounts_file_name = "temp_accounts.json";
     let temp_dir = create_tempdir_with_accounts_file(accounts_file_name, true);
@@ -174,7 +119,6 @@ fn test_happy_case_int_format() {
         .join(accounts_file_name);
 
     let args = vec![
-        "--int-format",
         "--accounts-file",
         &accounts_file_name,
         "account",
@@ -326,7 +270,6 @@ fn test_happy_case_with_private_keys_json_int_format() {
 
     let args = vec![
         "--json",
-        "--int-format",
         "--accounts-file",
         &accounts_file_name,
         "account",
