7c240e24d42bd7487a4d660bc4ade1bbe2685ef4
Fiiranek <jellybean@onet.eu>
2025-07-18 15:51:43 +0200

    Update todos

--- scripts/verify_cairo_listings_scarb_2_12.sh
+++ scripts/verify_cairo_listings_scarb_2_12.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -xe
 
+# TODO(#3469): Remove this whole script once Cairo 2.12 is the minimal version.
 scarb_2_12_packages=("deployment_with_constructor_args")
 
-# iterate overy the packages from scarb_2_12_packages
 for package in "${scarb_2_12_packages[@]}"; do (cd "$d" && scarb build); done
