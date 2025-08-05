c3b69ed99392254f651e18b4075304d536e5a4b7
Fiiranek <jellybean@onet.eu>
2025-08-05 15:26:08 +0200

    Remove todos

--- scripts/verify_cairo_listings_scarb_2_12.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -xe
-
-# TODO(#3469): Remove this whole script once Cairo 2.12 is the minimal version.
-scarb_2_12_packages=("deployment_with_constructor_args")
-
-for package in "${scarb_2_12_packages[@]}"; do
-    (cd "./docs/listings/$package" && scarb build);
-done
