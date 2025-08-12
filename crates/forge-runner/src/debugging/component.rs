4c0173f586a9907ea5595ed1fafd449e67d2d2a0
ksew1 <karol.sewilo@swmansion.com>
2025-08-12 11:57:32 +0200

    Add test for function level trace
    commit-id:14569926

--- crates/forge-runner/src/debugging/component.rs
+++ crates/forge-runner/src/debugging/component.rs
@@ -19,6 +19,8 @@ pub enum Component {
     CallType,
     /// The result of the call, transformed for display.
     CallResult,
+    /// The function trace, will be only shown if the contract is not a fork.
+    FunctionTrace,
 }
 impl Component {
     /// Returns minimal [`TraceVerbosity`] for the component.
@@ -30,7 +32,8 @@ impl Component {
             Component::ContractAddress
             | Component::CallerAddress
             | Component::EntryPointType
-            | Component::CallType => TraceVerbosity::Detailed,
+            | Component::CallType
+            | Component::FunctionTrace => TraceVerbosity::Detailed,
         }
     }
 }
@@ -45,6 +48,7 @@ impl From<&Component> for debugging::Component {
             Component::CallerAddress => debugging::Component::CallerAddress,
             Component::CallType => debugging::Component::CallType,
             Component::CallResult => debugging::Component::CallResult,
+            Component::FunctionTrace => debugging::Component::FunctionTrace,
         }
     }
 }
