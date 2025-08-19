887573f5362bbf717da8bf02117ef605df6bb7ab
Nnamdi Aninye <nnamdi@Nnamdis-MacBook-Pro.local>
2025-08-19 15:52:36 +0100

    add e2e test test_happy_case_get_class_hash

--- crates/sncast/tests/e2e/class_hash.rs
+++ crates/sncast/tests/e2e/class_hash.rs
@@ -1,2 +1,29 @@
+use crate::helpers::{
+    constants::CONTRACTS_DIR, fixtures::duplicate_contract_directory_with_salt, runner::runner,
+};
+use indoc::indoc;
+use shared::test_utils::output_assert::assert_stdout_contains;
+
 #[test]
-fn test_happy_case_class_hash() {}
+fn test_happy_case_get_class_hash() {
+    let contract_path = duplicate_contract_directory_with_salt(
+        CONTRACTS_DIR.to_string() + "/map",
+        "put",
+        "human_readable",
+    );
+
+    let args = vec!["class-hash", "--contract-name", "Map"];
+
+    let snapbox = runner(&args).current_dir(contract_path.path());
+
+    let output = snapbox.assert().success();
+
+    assert_stdout_contains(
+        output,
+        indoc! {r"
+        Success: Class hash generated
+
+        Class Hash: 0x6ee50dd7f45bd73b5e0e6f3c32013529f31477af0103c4c593915b6c1d6eefe
+    "},
+    );
+}
