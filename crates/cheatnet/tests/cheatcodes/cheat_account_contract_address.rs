fe4bdf2ba343d5beee17e88abf87770c32ed6508
Nnamdi Aninye <unix1gl@gmail.com>
2025-09-04 21:15:55 +0100

    setup cheat_account_contract_address cheatcode test

--- crates/cheatnet/tests/cheatcodes/cheat_account_contract_address.rs
+++ crates/cheatnet/tests/cheatcodes/cheat_account_contract_address.rs
@@ -1,1 +1,42 @@
-trait CheatAccountContractAddress {}
+use cheatnet::state::CheatSpan;
+use starknet_api::core::ContractAddress;
+
+use crate::cheatcodes::test_environment::TestEnvironment;
+
+trait CheatAccountContractAddressTrait {
+    fn cheat_account_contract_address(
+        &mut self,
+        target: ContractAddress,
+        account_contract_address: ContractAddress,
+        span: CheatSpan,
+    );
+    fn start_cheat_account_contract_address(
+        &mut self,
+        target: ContractAddress,
+        account_contract_address: ContractAddress,
+    );
+    fn stop_cheat_account_contract_address(&mut self, target: ContractAddress);
+}
+
+impl CheatAccountContractAddressTrait for TestEnvironment {
+    fn cheat_account_contract_address(
+        &mut self,
+        target: ContractAddress,
+        account_contract_address: ContractAddress,
+        span: CheatSpan,
+    ) {
+        todo!()
+    }
+
+    fn start_cheat_account_contract_address(
+        &mut self,
+        target: ContractAddress,
+        account_contract_address: ContractAddress,
+    ) {
+        todo!()
+    }
+
+    fn stop_cheat_account_contract_address(&mut self, target: ContractAddress) {
+        todo!()
+    }
+}
