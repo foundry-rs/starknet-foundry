2a28a27fdb881073dfe468bf6d98369f66bc00ac
Nnamdi Aninye <unix1gl@gmail.com>
2025-09-05 08:14:15 +0100

    complete cheatnet impl for cheat_account_contract_address

--- crates/cheatnet/src/runtime_extensions/forge_runtime_extension/cheatcodes/cheat_account_contract_address.rs
+++ crates/cheatnet/src/runtime_extensions/forge_runtime_extension/cheatcodes/cheat_account_contract_address.rs
@@ -2,30 +2,75 @@ use crate::{
     runtime_extensions::forge_runtime_extension::cheatcodes::cheat_execution_info::{
         CheatArguments, ExecutionInfoMockOperations, Operation, TxInfoMockOperations,
     },
-    state::CheatnetState,
+    state::{CheatSpan, CheatnetState},
 };
-use starknet_types_core::felt::Felt;
+use starknet_api::core::ContractAddress;
 
 impl CheatnetState {
-    pub fn cheat_account_contract_address(&mut self, operation: Operation<Felt>) {
-        match operation {
-            Operation::Start(args) => {
-                self.cheat_execution_info(ExecutionInfoMockOperations {
-                    tx_info: TxInfoMockOperations {
-                        account_contract_address: Operation::Start(CheatArguments {
-                            value: args.value,
-                            span: args.span,
-                            target: args.target,
-                        }),
-                        ..Default::default()
-                    },
-                    ..Default::default()
-                });
+    pub fn cheat_account_contract_address(
+        &mut self,
+        target: ContractAddress,
+        account_contract_address: ContractAddress,
+        span: CheatSpan,
+    ) {
+        self.cheat_execution_info(ExecutionInfoMockOperations {
+            tx_info: TxInfoMockOperations {
+                account_contract_address: Operation::Start(CheatArguments {
+                    value: account_contract_address.into(),
+                    span,
+                    target,
+                }),
+                ..Default::default()
             },
-            Operation::Stop(contract_address) => todo!(),
-            Operation::StopGlobal => todo!(),
-            Operation::StartGlobal(_) => todo!(),
-            Operation::Retain => todo!(),
-        }
+            ..Default::default()
+        });
+    }
+
+    pub fn start_cheat_account_contract_address(
+        &mut self,
+        target: ContractAddress,
+        account_contract_address: ContractAddress,
+    ) {
+        self.cheat_account_contract_address(
+            target,
+            account_contract_address,
+            CheatSpan::Indefinite,
+        );
+    }
+
+    pub fn stop_cheat_account_contract_address(&mut self, target: ContractAddress) {
+        self.cheat_execution_info(ExecutionInfoMockOperations {
+            tx_info: TxInfoMockOperations {
+                account_contract_address: Operation::Stop(target),
+                ..Default::default()
+            },
+            ..Default::default()
+        });
+    }
+
+    pub fn start_cheat_account_contract_address_global(
+        &mut self,
+        account_contract_address: ContractAddress,
+    ) {
+        self.cheat_execution_info(ExecutionInfoMockOperations {
+            tx_info: TxInfoMockOperations {
+                account_contract_address: Operation::StartGlobal(account_contract_address.into()),
+                ..Default::default()
+            },
+            ..Default::default()
+        });
+    }
+
+    pub fn stop_cheat_account_contract_address_global(
+        &mut self,
+        account_contract_address: ContractAddress,
+    ) {
+        self.cheat_execution_info(ExecutionInfoMockOperations {
+            tx_info: TxInfoMockOperations {
+                account_contract_address: Operation::StartGlobal(account_contract_address.into()),
+                ..Default::default()
+            },
+            ..Default::default()
+        });
     }
 }
