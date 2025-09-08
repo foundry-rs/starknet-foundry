d542f6ce185978d68fe365ed968613c4477df6d0
ksew1 <95349104+ksew1@users.noreply.github.com>
2025-09-08 20:53:09 +0200

    Add sierra validation for native (#3714)
    **Stack**:
    - #3717
    - #3714 ⬅
    
    
    ⚠️ *Part of a stack created by [spr](https://github.com/ejoffe/spr). Do
    not merge manually using the UI - doing so may have unexpected results.*

--- crates/native-api/src/lib.rs
+++ crates/native-api/src/lib.rs
@@ -7,23 +7,31 @@ use cairo_lang_starknet_classes::contract_class::ContractClass;
 use cairo_native::executor::AotContractExecutor;
 use starknet_api::contract_class::SierraVersion;
 
+#[derive(Debug, thiserror::Error)]
+pub enum NativeCompilationError {
+    #[error("Unsupported Sierra version {0}. cairo-native requires version 1.7.0 or later.")]
+    UnsupportedSierraVersion(String),
+}
+
 /// Compiles a given Sierra [`ContractClass`] into an [`AotContractExecutor`] for `cairo-native` execution.
-#[must_use]
-pub fn compile_contract_class(contract_class: &ContractClass) -> AotContractExecutor {
+pub fn compile_contract_class(
+    contract_class: &ContractClass,
+) -> Result<AotContractExecutor, NativeCompilationError> {
+    let sierra_version = extract_sierra_version(contract_class);
+    check_sierra_version(&sierra_version)?;
+
     let sierra_program = contract_class
         .extract_sierra_program()
         .expect("extraction should succeed");
 
-    let sierra_version = extract_sierra_version(contract_class);
-
-    AotContractExecutor::new(
+    Ok(AotContractExecutor::new(
         &sierra_program,
         &contract_class.entry_points_by_type,
         sierra_version.clone().into(),
         cairo_native::OptLevel::Default,
         None,
     )
-    .expect("compilation should succeed")
+    .expect("compilation should succeed"))
 }
 
 /// Extracts the Sierra version from the given [`ContractClass`].
@@ -37,4 +45,16 @@ fn extract_sierra_version(contract_class: &ContractClass) -> SierraVersion {
 
     SierraVersion::extract_from_program(&sierra_version_values)
         .expect("version extraction should succeed")
+}
+
+/// Checks if the given Sierra version is supported by `cairo-native`.
+fn check_sierra_version(sierra_version: &SierraVersion) -> Result<(), NativeCompilationError> {
+    let minimal_supported_version = SierraVersion::new(1, 7, 0);
+    if sierra_version < &minimal_supported_version {
+        Err(NativeCompilationError::UnsupportedSierraVersion(
+            sierra_version.to_string(),
+        ))
+    } else {
+        Ok(())
+    }
 }
