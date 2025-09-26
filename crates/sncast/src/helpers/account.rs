22132859270f9bfbf64d7b25e4aa52913252b4ce
Fiiranek <jellybean@onet.eu>
2025-09-26 17:04:06 +0200

    Remove functions

--- crates/sncast/src/helpers/account.rs
+++ crates/sncast/src/helpers/account.rs
@@ -1,11 +1,6 @@
-use crate::{NestedMap, build_account, helpers::devnet_provider::DevnetProvider};
-use anyhow::{Result, bail};
+use crate::NestedMap;
+use anyhow::Result;
 use camino::Utf8PathBuf;
-use starknet::{
-    accounts::SingleOwnerAccount,
-    providers::{JsonRpcClient, Provider, jsonrpc::HttpTransport},
-    signers::LocalWallet,
-};
 use std::collections::HashSet;
 use std::fs;
 
@@ -52,42 +47,4 @@ pub fn load_accounts(accounts_file: &Utf8PathBuf) -> Result<Value> {
         .with_context(|| format!("Failed to parse accounts file at = {accounts_file}"))?;
 
     Ok(accounts)
-}
-
-#[must_use]
-pub fn is_devnet_account(account: &str) -> bool {
-    account.starts_with("devnet-")
-}
-
-pub async fn get_account_from_devnet<'a>(
-    account: &str,
-    provider: &'a JsonRpcClient<HttpTransport>,
-    url: &str,
-) -> Result<SingleOwnerAccount<&'a JsonRpcClient<HttpTransport>, LocalWallet>> {
-    let account_number: u8 = account
-        .strip_prefix("devnet-")
-        .map(|s| s.parse::<u8>().expect("Invalid devnet account number"))
-        .context("Failed to parse devnet account number")?;
-
-    let devnet_provider = DevnetProvider::new(url);
-    let devnet_config = devnet_provider.get_config().await?;
-
-    if account_number >= devnet_config.total_accounts {
-        bail!(
-            "Devnet account number must be between 1 and {}",
-            devnet_config.total_accounts
-        );
-    }
-
-    let devnet_accounts = devnet_provider.get_predeployed_accounts().await?;
-    let predeployed_account = devnet_accounts
-        .get((account_number - 1) as usize)
-        .expect("Failed to get devnet account")
-        .to_owned();
-
-    let account_data = AccountData::from(predeployed_account);
-
-    let chain_id = provider.chain_id().await?;
-
-    build_account(account_data, chain_id, provider).await
 }
