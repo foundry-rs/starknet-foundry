041636004988905d319bece4cb5eed0346a5f660
Artur Michalek <artur.michalek@swmansion.com>
2025-10-07 12:34:25 +0200

    Fix test

--- crates/forge/tests/integration/too_many_events.rs
+++ crates/forge/tests/integration/too_many_events.rs
@@ -140,21 +140,21 @@ fn too_many_events() {
         &result,
         "emit_too_many_events",
         &format!(
-            "Got an exception while executing a hint: Exceeded the maximum number of events, number events: {emit_too_many_events}, max number events: {max_n_emitted_events}."
+            "Exceeded the maximum number of events, number events: {emit_too_many_events}, max number events: {max_n_emitted_events}."
         ),
     );
     assert_case_output_contains(
         &result,
         "emit_too_many_data",
         &format!(
-            "Got an exception while executing a hint: Exceeded the maximum data length, data length: {emit_too_many_data}, max data length: {max_data_length}."
+            "Exceeded the maximum data length, data length: {emit_too_many_data}, max data length: {max_data_length}."
         ),
     );
     assert_case_output_contains(
         &result,
         "emit_too_many_keys",
         &format!(
-            "Got an exception while executing a hint: Exceeded the maximum keys length, keys length: {emit_too_many_keys}, max keys length: {max_keys_length}."
+            "Exceeded the maximum keys length, keys length: {emit_too_many_keys}, max keys length: {max_keys_length}."
         ),
     );
 }
