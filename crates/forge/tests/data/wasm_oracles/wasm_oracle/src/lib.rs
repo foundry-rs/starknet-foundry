945b1fc90beb6ea07347aa4c3336516e52faa50f
Marek Kaput <marek.kaput@swmansion.com>
2025-10-21 14:03:10 +0200

    Add oracles test
    Closes #3825
    
    commit-id:adef9534
    
    # Conflicts:
    #	Cargo.lock
    
    # Conflicts:
    #	.tool-versions

---
+++ crates/forge/tests/data/wasm_oracles/wasm_oracle/src/lib.rs
@@ -0,0 +1,29 @@
+wit_bindgen::generate!({
+    inline: r#"
+        package testing:oracle;
+
+        world oracle {
+            export add: func(left: u64, right: u64) -> u64;
+            export err: func() -> result<string, string>;
+            export panic: func() -> result<string, string>;
+        }
+    "#
+});
+
+struct MyOracle;
+
+impl Guest for MyOracle {
+    fn add(left: u64, right: u64) -> u64 {
+        left + right
+    }
+
+    fn err() -> Result<String, String> {
+        Err("failed hard".into())
+    }
+
+    fn panic() -> Result<String, String> {
+        panic!("panicked")
+    }
+}
+
+export!(MyOracle);
