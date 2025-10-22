25862e43536112e90934f50c22b5c0cac6db8775
Dariusz Doktorski <dariusz.doktorski@swmansion.com>
2025-10-22 20:54:27 +0200

    Create `ContractsDataStore` only once

--- crates/debugging/src/trace/context.rs
+++ crates/debugging/src/trace/context.rs
@@ -1,7 +1,5 @@
 use crate::Components;
 use crate::contracts_data_store::ContractsDataStore;
-use cheatnet::forking::data::ForkData;
-use cheatnet::runtime_extensions::forge_runtime_extension::contracts_data::ContractsData;
 
 /// Context is a structure that holds the necessary data for creating a [`Trace`](crate::Trace).
 pub struct Context {
@@ -12,12 +10,7 @@ pub struct Context {
 impl Context {
     /// Creates a new instance of [`Context`] from a given `cheatnet` [`ContractsData`], [`ForkData`] and [`Components`].
     #[must_use]
-    pub fn new(
-        contracts_data: &ContractsData,
-        fork_data: &ForkData,
-        components: Components,
-    ) -> Self {
-        let contracts_data_store = ContractsDataStore::new(contracts_data, fork_data);
+    pub fn new(contracts_data_store: ContractsDataStore, components: Components) -> Self {
         Self {
             contracts_data_store,
             components,
