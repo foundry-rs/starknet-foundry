836a1e8c6e17849c25da14d94ab265556b329b20
ksew1 <karol.sewilo@swmansion.com>
2025-10-23 12:48:31 +0200

    Refactor version module
    commit-id:f673da00

--- crates/forge/tests/integration/meta_tx_v0.rs
+++ crates/forge/tests/integration/meta_tx_v0.rs
@@ -1,6 +1,6 @@
 use forge_runner::forge_config::ForgeTrackedResource;
 use indoc::indoc;
-use scarb_api::ScarbCommand;
+use scarb_api::version::scarb_version;
 use semver::Version;
 use std::path::Path;
 use test_utils::runner::{Contract, assert_passed};
@@ -9,9 +9,7 @@ use test_utils::test_case;
 
 // TODO(#3704) Remove scarb version check
 fn skip_scarb_lt_2_11_0() -> bool {
-    let version_info = ScarbCommand::version()
-        .run()
-        .expect("Failed to get Scarb version");
+    let version_info = scarb_version().expect("Failed to get Scarb version");
 
     if version_info.scarb < Version::new(2, 11, 0) {
         eprintln!("[IGNORED] `meta_tx_v0` syscall is not supported in Scarb < 2.11.0");
