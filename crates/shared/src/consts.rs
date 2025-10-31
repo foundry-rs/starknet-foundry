5439928e91ddd8a79d81e8f8cb86aa5e2c842f51
Fiiranek <jellybean@onet.eu>
2025-10-31 13:49:28 +0100

    Remove blast usage

--- crates/shared/src/consts.rs
+++ crates/shared/src/consts.rs
@@ -1,4 +1,10 @@
 pub const EXPECTED_RPC_VERSION: &str = "0.9.0";
 pub const RPC_URL_VERSION: &str = "v0_9";
 pub const SNFORGE_TEST_FILTER: &str = "SNFORGE_TEST_FILTER";
-pub const FREE_RPC_PROVIDER_URL: &str = "https://starknet-sepolia.public.blastapi.io/rpc/v0_9";
+const ALCHEMY_API_KEY: &str = env!("ALCHEMY_API_KEY");
+
+pub fn free_rpc_provider_url() -> String {
+    format!(
+        "https://starknet-sepolia.g.alchemy.com/starknet/version/rpc/{RPC_URL_VERSION}/{ALCHEMY_API_KEY}"
+    )
+}
