7c6b66c7111a613bca72c99ae95c37233e1e674c
Fiiranek <jellybean@onet.eu>
2025-10-31 13:51:24 +0100

    Fix clippy

--- crates/shared/src/consts.rs
+++ crates/shared/src/consts.rs
@@ -3,6 +3,7 @@ pub const RPC_URL_VERSION: &str = "v0_9";
 pub const SNFORGE_TEST_FILTER: &str = "SNFORGE_TEST_FILTER";
 const ALCHEMY_API_KEY: &str = env!("ALCHEMY_API_KEY");
 
+#[must_use]
 pub fn free_rpc_provider_url() -> String {
     format!(
         "https://starknet-sepolia.g.alchemy.com/starknet/version/rpc/{RPC_URL_VERSION}/{ALCHEMY_API_KEY}"
