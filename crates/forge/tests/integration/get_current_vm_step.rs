d17f252a80a06517cf49c4473e62a1ba0e2f13b4
Fiiranek <jellybean@onet.eu>
2025-11-10 19:16:46 +0100

    Improve test for `get_current_vm_step`

--- crates/forge/tests/integration/get_current_vm_step.rs
+++ crates/forge/tests/integration/get_current_vm_step.rs
@@ -13,7 +13,8 @@ fn test_get_current_vm_step() {
             use snforge_std::testing::get_current_vm_step;
             use snforge_std::{ContractClassTrait, DeclareResultTrait, declare};
 
-            const STEPS_MARGIN: u32 = 250;
+
+            const STEPS_MARGIN: u32 = 100;
 
             // 1173 = cost of 1 deploy syscall without calldata
             const DEPLOY_SYSCALL_STEPS: u32 = 1173;
@@ -24,50 +25,48 @@ fn test_get_current_vm_step() {
             // 90 = steps of 1 call contract syscall
             const STORAGE_READ_SYSCALL_STEPS: u32 = 90;
 
-            const TOTAL_GET_BALANCE_CALL_STEPS: u32 = CALL_CONTRACT_SYSCALL_STEPS
-                + STORAGE_READ_SYSCALL_STEPS
-                + 29; // ~29 comes from VM
-
             #[test]
             fn check_current_vm_step() {
                 let contract = declare("HelloStarknet").unwrap().contract_class();
                 let step_a = get_current_vm_step();
 
-                let (contract_address, _) = contract.deploy(@ArrayTrait::new()).unwrap();
-
-                // Step after calling deploy syscall from (as top call)
+                let (contract_address_a, _) = contract.deploy(@ArrayTrait::new()).unwrap();
+                let (contract_address_b, _) = contract.deploy(@ArrayTrait::new()).unwrap();
+                // Sycalls between step_a and step_b:
+                // top call: 2 x deploy syscall
+                // inner call: -/-
                 let step_b = get_current_vm_step();
 
-                let expected_lower = DEPLOY_SYSCALL_STEPS + step_a - STEPS_MARGIN;
-                let expected_upper = DEPLOY_SYSCALL_STEPS + step_a + STEPS_MARGIN;
+                let expected_steps_taken = 2 * DEPLOY_SYSCALL_STEPS + 130; // 130 are steps from VM
+                let expected_lower = expected_steps_taken + step_a - STEPS_MARGIN;
+                let expected_upper = expected_steps_taken + step_a + STEPS_MARGIN;
                 assert!(
                     expected_lower <= step_b && step_b <= expected_upper,
                     "step_b ({step_b}) not in [{expected_lower}, {expected_upper}]",
                 );
 
-                let dispatcher = IHelloStarknetDispatcher { contract_address };
+                let dispatcher_a = IHelloStarknetDispatcher { contract_address: contract_address_a };
 
-                // call get_balance 5 times to accumulate some steps
-                let _balance = dispatcher.get_balance();
-                let _balance = dispatcher.get_balance();
-                let _balance = dispatcher.get_balance();
-                let _balance = dispatcher.get_balance();
-                let _balance = dispatcher.get_balance();
+                // contract A calls `get_balance` from contract B
+                let _balance = dispatcher_a
+                    .call_other_contract(
+                        contract_address_b.try_into().unwrap(), selector!("get_balance"), None,
+                    );
 
-                // Step after calling call_contract syscall (as top call) and
-                // storage read syscall (as inner call)
+                // Sycalls between step_b and step_c:
+                // top call: 1 x call contract syscall
+                // inner calls: 1 x storage read syscall, 1 x call contract syscall
                 let step_c = get_current_vm_step();
-                let expected_lower = step_b + (TOTAL_GET_BALANCE_CALL_STEPS) * 5 - STEPS_MARGIN;
-                let expected_upper = step_b + (TOTAL_GET_BALANCE_CALL_STEPS) * 5 + STEPS_MARGIN;
+
+                let expected_steps_taken = 2 * CALL_CONTRACT_SYSCALL_STEPS
+                    + 1 * STORAGE_READ_SYSCALL_STEPS
+                    + 277; // 277 are steps from VM
+                let expected_lower = expected_steps_taken + step_b - STEPS_MARGIN;
+                let expected_upper = expected_steps_taken + step_b + STEPS_MARGIN;
                 assert!(
                     expected_lower <= step_c && step_c <= expected_upper,
                     "step_c ({step_c}) not in [{expected_lower}, {expected_upper}]",
                 );
-            }
-
-            #[starknet::interface]
-            trait IHelloStarknet<TContractState> {
-                fn get_balance(self: @TContractState) -> felt252;
             }
         "#
         ),
