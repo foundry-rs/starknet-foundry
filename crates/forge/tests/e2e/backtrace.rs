3316d2b3cd54ad9a6d1d2935537cb9964457d9dd
Maksymilian Kowalski <makow.088@gmail.com>
2025-12-10 17:56:33 +0100

    Make `panic-backtrace` required in validation

--- crates/forge/tests/e2e/backtrace.rs
+++ crates/forge/tests/e2e/backtrace.rs
@@ -55,14 +55,6 @@ fn test_backtrace() {
         output,
         indoc! {
            "
-            [WARNING] Scarb version should be 2.12 or higher and `Scarb.toml` should have the following Cairo compiler configuration to get accurate backtrace results:
-
-            [profile.dev.cairo]
-            unstable-add-statements-functions-debug-info = true
-            unstable-add-statements-code-locations-debug-info = true
-            panic-backtrace = true # only for scarb 2.12 or higher
-            ... other entries ...
-
             [FAIL] backtrace_vm_error::Test::test_unwrapped_call_contract_syscall
             
             Failure data:
