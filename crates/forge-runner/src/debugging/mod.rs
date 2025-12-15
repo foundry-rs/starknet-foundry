9a3f2b55778e17c9a30b9fa33188ef67ae1a686f
Fiiranek <jellybean@onet.eu>
2025-12-15 18:34:49 +0100

    Revert "Create `ContractsDataStore` only once (#3856)"
    This reverts commit b01acb90d8ed50e26c471e5660fbfadb26ff0180.

--- crates/forge-runner/src/debugging/mod.rs
+++ crates/forge-runner/src/debugging/mod.rs
@@ -2,20 +2,22 @@ mod args;
 mod component;
 mod trace_verbosity;
 
+use cheatnet::forking::data::ForkData;
+use cheatnet::runtime_extensions::forge_runtime_extension::contracts_data::ContractsData;
 use cheatnet::trace_data::CallTrace;
 
 pub use args::TraceArgs;
-use debugging::ContractsDataStore;
 pub use trace_verbosity::TraceVerbosity;
 
 #[must_use]
 pub fn build_debugging_trace(
     call_trace: &CallTrace,
-    contracts_data_store: &ContractsDataStore,
+    contracts_data: &ContractsData,
     trace_args: &TraceArgs,
     test_name: String,
+    fork_data: &ForkData,
 ) -> Option<debugging::Trace> {
     let components = trace_args.to_components()?;
-    let context = debugging::Context::new(contracts_data_store, components);
+    let context = debugging::Context::new(contracts_data, fork_data, components);
     Some(debugging::Trace::new(call_trace, &context, test_name))
 }
