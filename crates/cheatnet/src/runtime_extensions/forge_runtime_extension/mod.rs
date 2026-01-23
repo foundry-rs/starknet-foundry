182a9bbdaa134730efd5bded4c0e77dea7168a13
Maksim Zdobnikau <maksim.zdobnikau@swmansion.com>
2026-01-23 14:54:12 +0300

    format

--- crates/cheatnet/src/runtime_extensions/forge_runtime_extension/mod.rs
+++ crates/cheatnet/src/runtime_extensions/forge_runtime_extension/mod.rs
@@ -199,7 +199,7 @@ impl<'a> ExtensionLogic for ForgeExtension<'a> {
                 Ok(CheatcodeHandlingResult::from_serializable(()))
             }
             // Internal cheatcode to mark next syscall as coming from cheatcode.
-                "set_next_syscall_from_cheatcode" => {
+            "set_next_syscall_from_cheatcode" => {
                 let state = &mut *extended_runtime.extended_runtime.extension.cheatnet_state;
                 state.set_next_syscall_from_cheatcode();
 
