c46a0799d9e51422efd7224e6143694ce2dd8926
Maksim Zdobnikau <maksim.zdobnikau@swmansion.com>
2026-02-23 13:43:36 +0300

    initial refactor

--- crates/sncast/src/helpers/configuration.rs
+++ crates/sncast/src/helpers/configuration.rs
@@ -1,8 +1,10 @@
 use super::block_explorer;
-use crate::{Network, ValidatedWaitParams};
+use crate::helpers::config::get_global_config_path;
+use crate::response::ui::UI;
+use crate::{Network, PartialWaitParams, ValidatedWaitParams};
 use anyhow::Result;
 use camino::Utf8PathBuf;
-use configuration::Config;
+use configuration::{Config, load_config};
 use serde::{Deserialize, Serialize};
 use url::Url;
 
@@ -132,6 +134,139 @@ impl Config for CastConfig {
         Ok(config)
     }
 }
+
+    
+#[derive(Deserialize, Serialize, Clone, Debug, PartialEq, Default)]
+pub struct PartialCastConfig {
+    pub url: Option<Url>,
+    pub network: Option<Network>,
+    pub account: Option<String>,
+    #[serde(
+        default,
+        rename(serialize = "accounts-file", deserialize = "accounts-file")
+    )]
+    pub accounts_file: Option<Utf8PathBuf>,
+    pub keystore: Option<Utf8PathBuf>,
+    #[serde(
+        default,
+        rename(serialize = "wait-params", deserialize = "wait-params")
+    )]
+    pub wait_params: Option<PartialWaitParams>,
+    #[serde(
+        default,
+        rename(serialize = "block-explorer", deserialize = "block-explorer")
+    )]
+    /// A block explorer service, used to display links to transaction details
+    pub block_explorer: Option<block_explorer::Service>,
+
+    #[serde(
+        default,
+        rename(serialize = "show-explorer-links", deserialize = "show-explorer-links")
+    )]
+    /// Print links pointing to pages with transaction details in the chosen block explorer
+    pub show_explorer_links: Option<bool>,
+
+    #[serde(default)]
+    /// Configurable urls of predefined networks - mainnet, sepolia, and devnet are supported
+    pub networks: Option<NetworksConfig>,
+}
+
+impl Config for PartialCastConfig {
+    fn tool_name() -> &'static str {
+        "sncast"
+    }
+
+    fn from_raw(config: serde_json::Value) -> Result<Self> {
+        let config = serde_json::from_value::<Self>(config)?;
+        // TODO: figure out whether that should be there at all
+        config.validate()?;
+        Ok(config)
+    }
+}
+
+// TODO(#4027)
+impl PartialCastConfig {
+    pub fn validate(&self) -> anyhow::Result<()> {
+        if self.block_explorer.unwrap_or_default() == block_explorer::Service::StarkScan {
+            anyhow::bail!(
+                "starkscan.co was terminated and `'StarkScan'` is no longer available. Please set `block-explorer` to `'Voyager'` or other explorer of your choice."
+            )
+        }
+
+        match (&self.url, &self.network) {
+            (Some(_), Some(_)) => {
+                anyhow::bail!("Only one of `url` or `network` may be specified")
+            }
+            _ => Ok(()),
+        }
+    }
+}
+
+impl PartialCastConfig {
+    pub fn override_with(&self, other: PartialCastConfig) -> PartialCastConfig {
+        PartialCastConfig {
+            url: other.url.clone().or_else(|| self.url.clone()),
+            network: other.network.or(self.network),
+            account: other.account.clone().or_else(|| self.account.clone()),
+            accounts_file: other
+                .accounts_file
+                .clone()
+                .or_else(|| self.accounts_file.clone()),
+            keystore: other.keystore.clone().or_else(|| self.keystore.clone()),
+            wait_params: other.wait_params.or(self.wait_params),
+            block_explorer: other.block_explorer.or(self.block_explorer),
+            show_explorer_links: other.show_explorer_links.or(self.show_explorer_links),
+            networks: other.networks.clone().or_else(|| self.networks.clone()),
+        }
+    }
+
+
+    pub fn local(opts: &CliConfigOpts) -> Result<Self> {
+        let local_config = load_config::<PartialCastConfig>(None, opts.profile.as_deref())
+            .map_err(|err| anyhow::anyhow!(format!("Failed to load config: {err}")))?;
+
+        Ok(local_config)
+    }
+
+    pub fn global(opts: &CliConfigOpts, ui: &UI) -> Result<Self> {
+        let global_config_path = get_global_config_path().unwrap_or_else(|err| {
+            ui.print_error(&opts.command_name, format!("Error getting global config path: {err}"));
+            Utf8PathBuf::new()
+        });
+
+        let global_config =
+            load_config::<PartialCastConfig>(Some(&global_config_path.clone()), opts.profile.as_deref())
+                .or_else(|_| load_config::<PartialCastConfig>(Some(&global_config_path), None))
+                .map_err(|err| anyhow::anyhow!(format!("Failed to load config: {err}")))?;
+        
+        Ok(global_config)
+    }
+}
+
+pub struct CliConfigOpts {
+    pub command_name: String,
+    pub profile: Option<String>,
+}
+
+impl From<PartialCastConfig> for CastConfig {
+    fn from(partial: PartialCastConfig) -> Self {
+        let default = CastConfig::default(); 
+        
+        CastConfig {
+            url: partial.url.or(default.url),
+            network: partial.network.or(default.network),
+            account: partial.account.unwrap_or(default.account),
+            accounts_file: partial.accounts_file.unwrap_or(default.accounts_file),
+            keystore: partial.keystore.or(default.keystore),
+            wait_params: partial.wait_params.map(ValidatedWaitParams::from).unwrap_or(default.wait_params),
+            block_explorer: partial.block_explorer.or(default.block_explorer),
+            show_explorer_links: partial.show_explorer_links.unwrap_or(default.show_explorer_links),
+            networks: partial.networks.unwrap_or(default.networks),
+        }
+    }
+}
+
+
 
 #[cfg(test)]
 mod tests {
