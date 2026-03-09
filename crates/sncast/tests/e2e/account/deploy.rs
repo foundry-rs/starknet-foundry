2fa43457e694030d83415f3e5c1a8416deb8d32a
Maksim Zdobnikau <maksim.zdobnikau@swmansion.com>
2026-03-09 20:15:29 +0300

    updating tests...

--- crates/sncast/tests/e2e/account/deploy.rs
+++ crates/sncast/tests/e2e/account/deploy.rs
@@ -218,7 +218,7 @@ pub async fn test_valid_no_max_fee() {
 }
 
 pub async fn create_account(add_profile: bool, class_hash: &str, account_type: &str) -> TempDir {
-    let tempdir = copy_config_to_tempdir("tests/data/files/correct_snfoundry.toml", None);
+    let tempdir = copy_config_to_tempdir("tests/data/files/snfoundry_correct.toml", None);
     let accounts_file = "accounts.json";
 
     let mut args = vec![
