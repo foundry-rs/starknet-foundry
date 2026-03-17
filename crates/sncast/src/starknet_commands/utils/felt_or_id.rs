683b347c02dc19146ee14bfdcab186a06db6b509
Fiiranek <jellybean@onet.eu>
2026-03-17 12:10:10 +0100

    Apply cr suggestions

--- crates/sncast/src/starknet_commands/utils/felt_or_id.rs
+++ crates/sncast/src/starknet_commands/utils/felt_or_id.rs
@@ -3,25 +3,22 @@ use serde::Deserialize;
 use starknet_types_core::felt::Felt;
 use std::str::FromStr;
 
-use crate::starknet_commands::multicall::contract_registry::ContractRegistry;
-
 const ID_PREFIX: char = '@';
 
 #[derive(Deserialize, Debug, Clone)]
 pub struct FeltOrId(String);
 
 impl FeltOrId {
-    pub fn as_felt(&self) -> Result<Felt> {
+    pub fn try_into_felt(&self) -> Result<Felt> {
         Felt::from_str(&self.0)
             .context("Failed to parse contract address: expected a hex or decimal string")
     }
 
-    pub fn as_id(&self, contracts: &ContractRegistry) -> Result<Felt> {
-        match self.0.strip_prefix(ID_PREFIX) {
-            Some(id) => contracts
-                .get_address_by_id(id)
-                .context(format!("Failed to find contract address for id: {id}")),
-            None => self.as_felt(),
+    pub fn as_id(&self) -> Option<&str> {
+        if let Some(id) = self.0.strip_prefix(ID_PREFIX) {
+            Some(id)
+        } else {
+            None
         }
     }
 }
