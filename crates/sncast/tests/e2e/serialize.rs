e9fe542e0c2931a6579fc3e3f8cfc28148b3b6c0
Maksim Zdobnikau <maksim.zdobnikau@swmansion.com>
2026-03-19 18:19:26 +0300

    Revert "fix test (assert current sota)" - change to current sota
    This reverts commit a1974495bec97eaed3f34dfc4d3349d0463e15fa.

--- crates/sncast/tests/e2e/serialize.rs
+++ crates/sncast/tests/e2e/serialize.rs
@@ -224,7 +224,7 @@ async fn test_wrong_function_name() {
     ];
 
     let snapbox = runner(&args);
-    let output = snapbox.assert().success();
+    let output = snapbox.assert().failure();
 
     assert_stderr_contains(
         output,
