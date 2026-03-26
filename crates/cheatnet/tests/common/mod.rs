07885ee0dd68477c0894649241c0691f6bad7e17
Dariusz Doktorski <dariusz.doktorski@swmansion.com>
2026-03-26 15:00:00 +0100

    Add comment to  function

--- crates/cheatnet/tests/common/mod.rs
+++ crates/cheatnet/tests/common/mod.rs
@@ -238,6 +238,9 @@ pub fn call_contract(
     .call_result
 }
 
+// This executes a library call as from a test contract.
+// `entry_point` below should match `library_call_syscall` entry point, except for `selector` and `calldata`:
+// https://github.com/foundry-rs/starknet-foundry/blob/421a339168a9e0b6502eac4fdc4fdeb0598c72b7/crates/cheatnet/src/runtime_extensions/call_to_blockifier_runtime_extension/mod.rs#L151
 pub fn library_call_contract(
     state: &mut dyn State,
     cheatnet_state: &mut CheatnetState,
