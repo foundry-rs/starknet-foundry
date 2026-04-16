cf157329afda4b855c9fc35fd1f81474a2fba0df
Fiiranek <jellybean@onet.eu>
2026-04-16 21:42:24 +0200

    Revert "Simplify `load_predeployed_contracts`"
    This reverts commit 897d1d22d3188c9b5883067b7ab3075c7e25489e.

--- crates/cheatnet/src/predeployment/contracts_data.rs
+++ crates/cheatnet/src/predeployment/contracts_data.rs
@@ -1,12 +1,38 @@
 use crate::{
     predeployment::erc20::contracts_data::load_erc20_predeployed_contracts,
+    predeployment::erc20::eth::{ETH_CONTRACT_NAME, ETH_SIERRA_CLASS_HASH},
+    predeployment::erc20::strk::{STRK_CONTRACT_NAME, STRK_SIERRA_CLASS_HASH},
     runtime_extensions::forge_runtime_extension::contracts_data::ContractsData,
 };
-use anyhow::Result;
+use anyhow::{Result, anyhow};
+use conversions::string::TryFromHexStr;
 
 pub fn load_predeployed_contracts() -> Result<ContractsData> {
     // In the future, if we have more contracts to predeploy, we can split them into separate functions and merge the results here.
     let erc20_contracts = load_erc20_predeployed_contracts()?;
 
-    ContractsData::try_from(erc20_contracts)
+    let mut contracts_data = ContractsData::try_from(erc20_contracts)?;
+
+    // Class hashes for STRK and ETH contract are different on network than the ones
+    // calculated from their sierras, so we need to update them.
+    let class_hashes_to_update = vec![
+        (STRK_CONTRACT_NAME.to_string(), STRK_SIERRA_CLASS_HASH),
+        (ETH_CONTRACT_NAME.to_string(), ETH_SIERRA_CLASS_HASH),
+    ];
+
+    for (contract_name, class_hash) in class_hashes_to_update {
+        let class_hash = TryFromHexStr::try_from_hex_str(class_hash)?;
+
+        contracts_data
+            .class_hashes
+            .insert(contract_name.clone(), class_hash);
+
+        let contract_data = contracts_data
+            .contracts
+            .get_mut(&contract_name)
+            .ok_or_else(|| anyhow!("contract data for {contract_name} should exist"))?;
+        contract_data.class_hash = class_hash;
+    }
+
+    Ok(contracts_data)
 }
