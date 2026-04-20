3823ec1622f3ec5c46b897fb8d7797c7d92de95d
Fiiranek <jellybean@onet.eu>
2026-04-20 11:17:35 +0200

    Fix lint

--- crates/snforge-scarb-plugin/src/args.rs
+++ crates/snforge-scarb-plugin/src/args.rs
@@ -3,8 +3,8 @@ use crate::attributes::{AttributeInfo, ErrorExt};
 use cairo_lang_macro::Diagnostic;
 use cairo_lang_parser::utils::SimpleParserDatabase;
 use cairo_lang_syntax::node::{
-    ast::{ArgClause, Expr, OptionArgListParenthesized},
     Terminal,
+    ast::{ArgClause, Expr, OptionArgListParenthesized},
 };
 use smol_str::SmolStr;
 use std::collections::HashMap;
@@ -62,7 +62,6 @@ impl Arguments {
         this
     }
 
-    #[must_use]
     pub fn is_empty(&self) -> bool {
         self.named.is_empty() && self.unnamed.is_empty() && self.shorthand.is_empty()
     }
