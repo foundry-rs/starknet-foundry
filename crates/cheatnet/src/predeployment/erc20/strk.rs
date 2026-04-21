65d2dba5da7787957984a9f8b5bf3c79504a9a78
Fiiranek <jellybean@onet.eu>
2026-04-21 17:19:03 +0200

    Change `TryFromHexStr` to `ContractAddress`

--- crates/cheatnet/src/predeployment/erc20/strk.rs
+++ crates/cheatnet/src/predeployment/erc20/strk.rs
@@ -16,7 +16,7 @@ pub fn strk_predeployed_contract() -> PredeployedContract {
     // Link to Cairo contract: https://github.com/starknet-io/starkgate-contracts/blob/07e11c39119a10d5742735be5b1d51894ebf5311/packages/strk/src/erc20_lockable.cairo
     let raw_casm = include_str!("../../data/predeployed_contracts/ERC20Lockable/casm.json");
 
-    let contract_address = TryFromHexStr::try_from_hex_str(STRK_CONTRACT_ADDRESS).unwrap();
+    let contract_address = ContractAddress::try_from_hex_str(STRK_CONTRACT_ADDRESS).unwrap();
     let class_hash = TryFromHexStr::try_from_hex_str(ERC20LOCKABLE_SIERRA_CLASS_HASH).unwrap();
 
     // All storage values are taken from https://voyager.online/contract/0x04718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d#readStorage
