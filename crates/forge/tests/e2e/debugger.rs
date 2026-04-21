2a6ece013613bb7204f79f0a8cfbca26c6880afc
Piotr Magiera <56825108+piotmag769@users.noreply.github.com>
2026-04-21 14:31:09 +0200

    Debugger Scarb.toml validation (#4253)
    Closes https://github.com/software-mansion/cairo-debugger/issues/109
    
    ## Introduced changes
    
    - added validation of Scarb.toml fields when forge is used with
    `--launch-debugger`, similar to coverage checks
    
    ## Checklist
    
    - [x] Linked relevant issue
    - [ ] Updated relevant documentation - not needed, updated in #4160
    - [x] Added relevant tests
    - [x] Performed self-review of the code
    - [ ] Added changes to `CHANGELOG.md` - imo not needed, is obvious part
    of #4160

--- crates/forge/tests/e2e/debugger.rs
+++ crates/forge/tests/e2e/debugger.rs
@@ -1,10 +1,38 @@
-use super::common::runner::{setup_package, snforge_test_bin_path, test_runner};
+use crate::e2e::common::runner::{setup_package, snforge_test_bin_path, test_runner};
 use assert_fs::fixture::{FileWriteStr, PathChild};
-use indoc::formatdoc;
+use indoc::{formatdoc, indoc};
 use shared::test_utils::output_assert::assert_stdout_contains;
 use std::fs;
 use std::io::{BufRead, BufReader};
 use std::process::{Command, Stdio};
+
+#[test]
+fn test_fail_wrong_scarb_toml_configuration_for_launch_debugger() {
+    let temp = setup_package("debugging");
+
+    let output = test_runner(&temp)
+        .arg("test_debugging_trace_success")
+        .arg("--exact")
+        .arg("--launch-debugger")
+        .assert()
+        .failure();
+
+    assert_stdout_contains(
+        output,
+        indoc! {
+            "[ERROR] Scarb.toml must have the following Cairo compiler configuration to launch the debugger:
+
+            [profile.dev.cairo]
+            unstable-add-statements-code-locations-debug-info = true
+            unstable-add-statements-functions-debug-info = true
+            add-functions-debug-info = true
+            skip-optimizations = true
+            ... other entries ...
+
+            "
+        },
+    );
+}
 
 #[test]
 fn test_launch_debugger_waits_for_connection() {
@@ -61,6 +89,19 @@ fn test_launch_debugger_waits_for_connection() {
 #[test]
 fn test_launch_debugger_fails_for_fuzzer_test() {
     let temp = setup_package("debugging");
+
+    let manifest_path = temp.child("Scarb.toml");
+    let existing = fs::read_to_string(&manifest_path).unwrap();
+    manifest_path
+        .write_str(&formatdoc!(
+            "{existing}
+            [profile.dev.cairo]
+            unstable-add-statements-code-locations-debug-info = true
+            unstable-add-statements-functions-debug-info = true
+            add-functions-debug-info = true
+            skip-optimizations = true",
+        ))
+        .unwrap();
 
     let output = test_runner(&temp)
         .args([
