5e30fdc43a4d46f7855b4e3d2d70e2a226cfd1bf
Maksim Zdobnikau <maksim.zdobnikau@swmansion.com>
2026-04-24 01:09:27 +0400

    post-rebase fix

--- crates/sncast/tests/e2e/show_config.rs
+++ crates/sncast/tests/e2e/show_config.rs
@@ -159,7 +159,7 @@ async fn test_show_config_with_network() {
 
 #[tokio::test]
 async fn test_show_config_cli_url_overrides_config_network() {
-    let tempdir = copy_config_to_tempdir("tests/data/files/correct_snfoundry.toml", None);
+    let tempdir = copy_config_to_tempdir("tests/data/files/snfoundry_correct.toml", None);
     let args = vec![
         "--profile",
         "profile7",
