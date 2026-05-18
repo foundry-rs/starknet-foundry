22e062413b15aa67d9992e0b75b3d72bb05e7cf3
Maksim Zdobnikau <maksim.zdobnikau@swmansion.com>
2026-05-18 14:50:49 +0400

    actually add `alias list`

---
+++ crates/sncast/src/starknet_commands/alias/list.rs
@@ -0,0 +1,11 @@
+use clap::Args;
+use sncast::helpers::configuration::CastConfig;
+use sncast::response::alias::list::AliasesListMessage;
+
+#[derive(Args, Debug)]
+#[command(name = "list", about = "List aliases from the current effective config")]
+pub struct List;
+
+pub fn list(config: &CastConfig) -> AliasesListMessage {
+    AliasesListMessage::new(&config.aliases)
+}
