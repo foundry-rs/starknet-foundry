f0d1e3ae2a15027737daec9dbb9d888016832ae5
Fiiranek <jellybean@onet.eu>
2026-06-11 13:40:01 +0200

    Revert "Rename variants of `ContractResolutionError`"
    This reverts commit 4897dc981031d88ee535ff4d9c424787326ce68b.

--- crates/cheatnet/src/runtime_extensions/forge_runtime_extension/cheatcodes/declare.rs
+++ crates/cheatnet/src/runtime_extensions/forge_runtime_extension/cheatcodes/declare.rs
@@ -32,7 +32,7 @@ pub fn declare(
                 anyhow!("Failed to get contract artifact for name = {contract_name}."),
             )));
         }
-        Err(ContractResolutionError::AmbiguousName(module_paths)) => {
+        Err(ContractResolutionError::Ambiguous(module_paths)) => {
             let paths = module_paths
                 .iter()
                 .map(|path| format!("    {path}"))
