3b97a250ff2e391fd4ac39f21087b5fb3ad311a1
Dariusz Doktorski <dariusz.doktorski@swmansion.com>
2026-06-15 11:44:45 +0200

    Destruct returned struct

--- crates/sncast/src/response/block.rs
+++ crates/sncast/src/response/block.rs
@@ -36,45 +36,72 @@ impl SncastCommandMessage for BlockResponse {
 }
 
 fn append_block(builder: OutputBuilder, block: &BlockWithTxHashes) -> OutputBuilder {
+    let BlockWithTxHashes {
+        status,
+        block_hash,
+        parent_hash,
+        block_number,
+        new_root,
+        timestamp,
+        sequencer_address,
+        l1_gas_price,
+        l2_gas_price,
+        l1_data_gas_price,
+        l1_da_mode,
+        starknet_version,
+        event_commitment: _,
+        transaction_commitment: _,
+        receipt_commitment: _,
+        state_diff_commitment: _,
+        event_count: _,
+        transaction_count,
+        state_diff_length: _,
+        transactions,
+    } = block;
     builder
-        .field("Status", fmt_status(block.status))
-        .padded_felt_field("Block Hash", &block.block_hash)
-        .field("Block Number", &block.block_number.to_string())
-        .padded_felt_field("Parent Hash", &block.parent_hash)
-        .padded_felt_field("New Root", &block.new_root)
-        .field("Timestamp", &block.timestamp.to_string())
-        .padded_felt_field("Sequencer Address", &block.sequencer_address)
-        .field("L1 Gas Price", &fmt_resource_price(&block.l1_gas_price))
-        .field("L2 Gas Price", &fmt_resource_price(&block.l2_gas_price))
-        .field(
-            "L1 Data Gas Price",
-            &fmt_resource_price(&block.l1_data_gas_price),
-        )
-        .field("L1 DA Mode", fmt_da_mode(block.l1_da_mode))
-        .field("Starknet Version", &block.starknet_version)
-        .field("Transaction Count", &block.transactions.len().to_string())
-        .felt_list_field("Transactions", &block.transactions)
+        .field("Status", fmt_status(*status))
+        .padded_felt_field("Block Hash", block_hash)
+        .field("Block Number", &block_number.to_string())
+        .padded_felt_field("Parent Hash", parent_hash)
+        .padded_felt_field("New Root", new_root)
+        .field("Timestamp", &timestamp.to_string())
+        .padded_felt_field("Sequencer Address", sequencer_address)
+        .field("L1 Gas Price", &fmt_resource_price(l1_gas_price))
+        .field("L2 Gas Price", &fmt_resource_price(l2_gas_price))
+        .field("L1 Data Gas Price", &fmt_resource_price(l1_data_gas_price))
+        .field("L1 DA Mode", fmt_da_mode(*l1_da_mode))
+        .field("Starknet Version", starknet_version)
+        .field("Transaction Count", &transaction_count.to_string())
+        .felt_list_field("Transactions", transactions)
 }
 
 fn append_pre_confirmed_block(
     builder: OutputBuilder,
     block: &PreConfirmedBlockWithTxHashes,
 ) -> OutputBuilder {
+    let PreConfirmedBlockWithTxHashes {
+        transactions,
+        block_number,
+        timestamp,
+        sequencer_address,
+        l1_gas_price,
+        l2_gas_price,
+        l1_data_gas_price,
+        l1_da_mode,
+        starknet_version,
+    } = block;
     builder
         .field("Status", "Pre confirmed")
-        .field("Block Number", &block.block_number.to_string())
-        .field("Timestamp", &block.timestamp.to_string())
-        .padded_felt_field("Sequencer Address", &block.sequencer_address)
-        .field("L1 Gas Price", &fmt_resource_price(&block.l1_gas_price))
-        .field("L2 Gas Price", &fmt_resource_price(&block.l2_gas_price))
-        .field(
-            "L1 Data Gas Price",
-            &fmt_resource_price(&block.l1_data_gas_price),
-        )
-        .field("L1 DA Mode", fmt_da_mode(block.l1_da_mode))
-        .field("Starknet Version", &block.starknet_version)
-        .field("Transaction Count", &block.transactions.len().to_string())
-        .felt_list_field("Transactions", &block.transactions)
+        .field("Block Number", &block_number.to_string())
+        .field("Timestamp", &timestamp.to_string())
+        .padded_felt_field("Sequencer Address", sequencer_address)
+        .field("L1 Gas Price", &fmt_resource_price(l1_gas_price))
+        .field("L2 Gas Price", &fmt_resource_price(l2_gas_price))
+        .field("L1 Data Gas Price", &fmt_resource_price(l1_data_gas_price))
+        .field("L1 DA Mode", fmt_da_mode(*l1_da_mode))
+        .field("Starknet Version", starknet_version)
+        .field("Transaction Count", &transactions.len().to_string())
+        .felt_list_field("Transactions", transactions)
 }
 
 fn fmt_status(status: BlockStatus) -> &'static str {
