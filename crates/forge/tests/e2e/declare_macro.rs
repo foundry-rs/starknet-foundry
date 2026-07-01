39e0d247bffc678dd7b25c53d201b66460e18109
Fiiranek <jellybean@onet.eu>
2026-07-01 18:23:36 +0200

    Split tests into separate ones

--- crates/forge/tests/e2e/declare_macro.rs
+++ crates/forge/tests/e2e/declare_macro.rs
@@ -3,10 +3,13 @@ use indoc::indoc;
 use shared::test_utils::output_assert::assert_stdout_contains;
 
 #[test]
-fn declare_macro() {
+fn with_full_path() {
     let temp = setup_package("declare_macro");
 
-    let output = test_runner(&temp).assert().success();
+    let output = test_runner(&temp)
+        .arg("declare_with_full_path")
+        .assert()
+        .success();
 
     assert_stdout_contains(
         output,
@@ -14,16 +17,107 @@ fn declare_macro() {
         [..]Compiling[..]
         [..]Finished[..]
 
-
-        Collected 5 test(s) from declare_macro package
+        Collected 1 test(s) from declare_macro package
         Running 0 test(s) from src/
-        Running 5 test(s) from tests/
+        Running 1 test(s) from tests/
         [PASS] declare_macro_integrationtest::tests::declare_with_full_path [..]
+        Tests: 1 passed, 0 failed, 0 ignored, 4 filtered out
+        "},
+    );
+}
+
+#[test]
+fn with_partial_path() {
+    let temp = setup_package("declare_macro");
+
+    let output = test_runner(&temp)
+        .arg("declare_with_partial_path")
+        .assert()
+        .success();
+
+    assert_stdout_contains(
+        output,
+        indoc! {r"
+        [..]Compiling[..]
+        [..]Finished[..]
+
+        Collected 1 test(s) from declare_macro package
+        Running 0 test(s) from src/
+        Running 1 test(s) from tests/
         [PASS] declare_macro_integrationtest::tests::declare_with_partial_path [..]
-        [PASS] declare_macro_integrationtest::tests::declare_with_module_alias_is_not_resolved_as_canonical_path [..]
+        Tests: 1 passed, 0 failed, 0 ignored, 4 filtered out
+        "},
+    );
+}
+
+#[test]
+fn with_contract_name() {
+    let temp = setup_package("declare_macro");
+
+    let output = test_runner(&temp)
+        .arg("declare_with_contract_name")
+        .assert()
+        .success();
+
+    assert_stdout_contains(
+        output,
+        indoc! {r"
+        [..]Compiling[..]
+        [..]Finished[..]
+
+        Collected 1 test(s) from declare_macro package
+        Running 0 test(s) from src/
+        Running 1 test(s) from tests/
         [PASS] declare_macro_integrationtest::tests::declare_with_contract_name [..]
+        Tests: 1 passed, 0 failed, 0 ignored, 4 filtered out
+        "},
+    );
+}
+
+#[test]
+fn with_module_alias_is_not_resolved_as_canonical_path() {
+    let temp = setup_package("declare_macro");
+
+    let output = test_runner(&temp)
+        .arg("declare_with_module_alias_is_not_resolved_as_canonical_path")
+        .assert()
+        .success();
+
+    assert_stdout_contains(
+        output,
+        indoc! {r"
+        [..]Compiling[..]
+        [..]Finished[..]
+
+        Collected 1 test(s) from declare_macro package
+        Running 0 test(s) from src/
+        Running 1 test(s) from tests/
+        [PASS] declare_macro_integrationtest::tests::declare_with_module_alias_is_not_resolved_as_canonical_path [..]
+        Tests: 1 passed, 0 failed, 0 ignored, 4 filtered out
+        "},
+    );
+}
+
+#[test]
+fn with_contract_alias_is_not_resolved_as_canonical_path() {
+    let temp = setup_package("declare_macro");
+
+    let output = test_runner(&temp)
+        .arg("declare_with_contract_alias_is_not_resolved_as_canonical_path")
+        .assert()
+        .success();
+
+    assert_stdout_contains(
+        output,
+        indoc! {r"
+        [..]Compiling[..]
+        [..]Finished[..]
+
+        Collected 1 test(s) from declare_macro package
+        Running 0 test(s) from src/
+        Running 1 test(s) from tests/
         [PASS] declare_macro_integrationtest::tests::declare_with_contract_alias_is_not_resolved_as_canonical_path [..]
-        Tests: 5 passed, 0 failed, 0 ignored, 0 filtered out
+        Tests: 1 passed, 0 failed, 0 ignored, 4 filtered out
         "},
     );
 }
