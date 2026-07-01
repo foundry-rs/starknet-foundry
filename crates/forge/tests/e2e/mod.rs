0750b7c3c4414cc1086d49a19181a0e4eeb6d4e3
Fiiranek <jellybean@onet.eu>
2026-07-01 15:56:26 +0200

    Fix import

--- crates/forge/tests/e2e/mod.rs
+++ crates/forge/tests/e2e/mod.rs
@@ -19,6 +19,7 @@ mod debugger;
 #[cfg(not(feature = "cairo-native"))]
 mod debugging;
 mod declare_from_file;
+mod declare_from_file_macro;
 mod declare_macro;
 mod docs_snippets_validation;
 mod enable_gas_validation;
