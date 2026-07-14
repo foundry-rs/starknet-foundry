16a109adaca9412ec1927f91e03d10837f735b3b
Maksim Zdobnikau <43750648+DelevoXDG@users.noreply.github.com>
2026-07-14 17:57:20 +0200

    Display `networks` in `sncast show-config` (#4486)
    <!-- Reference any GitHub issues resolved by this PR -->
    
    Closes #4364
    
    ## Introduced changes
    
    <!-- A brief description of the changes -->
    
    -
    
    ## Checklist
    
    <!-- Make sure all of these are complete -->
    
    - [x] Linked relevant issue
    - [ ] Updated relevant documentation
    - [x] Added relevant tests
    - [x] Performed self-review of the code
    - [x] Added changes to `CHANGELOG.md`

--- crates/sncast/src/response/show_config.rs
+++ crates/sncast/src/response/show_config.rs
@@ -1,5 +1,6 @@
 use crate::Network;
 use crate::helpers::block_explorer;
+use crate::helpers::configuration::NetworksConfig;
 use crate::response::cast_message::SncastCommandMessage;
 use camino::Utf8PathBuf;
 use foundry_ui::styling;
@@ -21,6 +22,7 @@ pub struct ShowConfigResponse {
     pub block_explorer: Option<block_explorer::Service>,
     pub scarb_profile: String,
     pub alias_count: usize,
+    pub networks: NetworksConfig,
 }
 
 impl SncastCommandMessage for ShowConfigResponse {
@@ -63,6 +65,16 @@ impl SncastCommandMessage for ShowConfigResponse {
         let mut builder = builder.field("Alias Count", &alias_count);
         if self.alias_count != 0 {
             builder = builder.extra("(use `sncast alias list` to display)");
+        }
+
+        if let Some(url) = &self.networks.mainnet {
+            builder = builder.field("Mainnet URL", url.as_ref());
+        }
+        if let Some(url) = &self.networks.sepolia {
+            builder = builder.field("Sepolia URL", url.as_ref());
+        }
+        if let Some(url) = &self.networks.devnet {
+            builder = builder.field("Devnet URL", url.as_ref());
         }
 
         builder.build()
