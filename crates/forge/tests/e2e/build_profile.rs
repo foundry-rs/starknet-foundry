40a4f8871c8c83885d9691e32baed4d0e919f0f4
Maksim Zdobnikau <43750648+DelevoXDG@users.noreply.github.com>
2026-07-16 17:25:35 +0200

    Re-enable profiler tests (#4491)
    with latest profiler, we can re-enable the tests

--- crates/forge/tests/e2e/build_profile.rs
+++ crates/forge/tests/e2e/build_profile.rs
@@ -2,7 +2,6 @@ use super::common::runner::{setup_package, test_runner};
 use forge_runner::profiler_api::PROFILE_DIR;
 
 #[test]
-#[ignore = "TODO(#4477): Re-enable after cairo-profiler Scarb 2.19 support"]
 fn simple_package_build_profile() {
     let temp = setup_package("simple_package");
 
@@ -36,7 +35,6 @@ fn simple_package_build_profile() {
 }
 
 #[test]
-#[ignore = "TODO(#4477): Re-enable after cairo-profiler Scarb 2.19 support"]
 fn simple_package_build_profile_and_pass_args() {
     let temp = setup_package("simple_package");
 
