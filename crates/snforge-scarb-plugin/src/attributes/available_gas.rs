a7fa6fb6856b8efa0b06e1fc1663c3a337aec9f4
Fiiranek <jellybean@onet.eu>
2026-08-07 11:21:39 +0200

    Fix formatting

--- crates/snforge-scarb-plugin/src/attributes/available_gas.rs
+++ crates/snforge-scarb-plugin/src/attributes/available_gas.rs
@@ -5,7 +5,7 @@ use crate::{
     config_statement::extend_with_config_cheatcodes,
     types::{Number, ParseFromExpr},
 };
-use cairo_lang_macro::{quote, Diagnostic, Diagnostics, ProcMacroResult, TokenStream};
+use cairo_lang_macro::{Diagnostic, Diagnostics, ProcMacroResult, TokenStream, quote};
 use cairo_lang_parser::utils::SimpleParserDatabase;
 
 pub struct AvailableGasCollector;
