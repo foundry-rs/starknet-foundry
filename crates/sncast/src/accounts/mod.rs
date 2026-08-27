bea86239677d4ae0e0bc749fe2292f909e27031a
Jan Smółka <jp.smolka@gmail.com>
2026-08-27 12:09:01 +0200

    Introduce versioned schemas of accounts file

--- crates/sncast/src/accounts/mod.rs
+++ crates/sncast/src/accounts/mod.rs
@@ -2,8 +2,9 @@
 
 pub mod error;
 pub mod model;
+pub mod schema;
 
-pub use error::AccountsError;
+pub use error::{AccountsError, AccountsFileError};
 pub use model::{
     AccountName, AccountRecord, AccountRegistry, AccountType, DeployableAccountRecord, NetworkName,
 };
